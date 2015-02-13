c2868e1c96e51d4d8b0570685b54e4c555905a34
ex3ndr <me@ex3ndr.com>
2015-02-13 18:52:59 +0300

    Sent messages state processing

--- actor-common/src/main/java/im/actor/model/modules/updates/MessagesProcessor.java
+++ actor-common/src/main/java/im/actor/model/modules/updates/MessagesProcessor.java
@@ -9,6 +9,7 @@ import im.actor.model.entity.MessageState;
 import im.actor.model.entity.Peer;
 import im.actor.model.entity.content.AbsContent;
 import im.actor.model.entity.content.TextContent;
+import im.actor.model.modules.messages.ConversationActor;
 import im.actor.model.modules.messages.DialogsActor;
 import im.actor.model.modules.messages.DialogsHistoryActor;
 
@@ -72,39 +73,59 @@ public class MessagesProcessor {
 
     public void onMessageRead(im.actor.model.api.Peer _peer, long startDate, long readDate) {
         Peer peer = EntityConverter.convert(_peer);
-
+        messenger.getMessagesModule().getConversationActor(peer)
+                .send(new ConversationActor.MessageRead(startDate));
     }
 
-    public void onMessageReadByMe(im.actor.model.api.Peer _peer, long startDate) {
+    public void onMessageEncryptedRead(im.actor.model.api.Peer _peer, long rid, long readDate) {
         Peer peer = EntityConverter.convert(_peer);
-
+        // TODO: Implement
     }
 
     public void onMessageReceived(im.actor.model.api.Peer _peer, long startDate, long receivedDate) {
         Peer peer = EntityConverter.convert(_peer);
+        messenger.getMessagesModule().getConversationActor(peer)
+                .send(new ConversationActor.MessageReceived(startDate));
+    }
 
+    public void onMessageEncryptedReceived(im.actor.model.api.Peer _peer, long rid, long readDate) {
+        Peer peer = EntityConverter.convert(_peer);
+        // TODO: Implement
+    }
+
+    public void onMessageReadByMe(im.actor.model.api.Peer _peer, long startDate) {
+        Peer peer = EntityConverter.convert(_peer);
+        // TODO: Implement
+    }
+
+    public void onMessageEncryptedReadByMe(im.actor.model.api.Peer _peer, long rid) {
+        Peer peer = EntityConverter.convert(_peer);
+        // TODO: Implement
     }
 
     public void onMessageDelete(im.actor.model.api.Peer _peer, List<Long> rids) {
         Peer peer = EntityConverter.convert(_peer);
-
+        messenger.getMessagesModule().getConversationActor(peer)
+                .send(new ConversationActor.MessageDeleted(rids));
     }
 
     public void onMessageSent(im.actor.model.api.Peer _peer, long rid, long date) {
         Peer peer = EntityConverter.convert(_peer);
-
+        messenger.getMessagesModule().getConversationActor(peer)
+                .send(new ConversationActor.MessageSent(rid, date));
     }
 
     public void onChatClear(im.actor.model.api.Peer _peer) {
         Peer peer = EntityConverter.convert(_peer);
+        // TODO: Move to conversation
         messenger.getMessagesModule().getDialogsActor().send(new DialogsActor.ChatClear(peer));
     }
 
     public void onChatDelete(im.actor.model.api.Peer _peer) {
         Peer peer = EntityConverter.convert(_peer);
+        // TODO: Move to conversation
         messenger.getMessagesModule().getDialogsActor().send(new DialogsActor.ChatDelete(peer));
     }
-
 
     public void onUserRegistered(int uid) {
 
