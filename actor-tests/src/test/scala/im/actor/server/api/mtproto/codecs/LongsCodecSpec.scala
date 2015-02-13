c3a9097e5d99a2985be0dec4856f571c812f65d8
Andrey Kuznetsov <fear@loathing.in>
2015-02-13 09:31:34 +0300

    codecs

--- actor-tests/src/test/scala/im/actor/server/api/mtproto/codecs/LongsCodecSpec.scala
+++ actor-tests/src/test/scala/im/actor/server/api/mtproto/codecs/LongsCodecSpec.scala
@@ -21,3 +21,17 @@ object LongsCodecProp extends Properties("LongsCodec") {
     longs.decode(buf) == (tail, v).right
   }
 }
+
+class LongsCodecSpec extends Specification {
+  "LongsCodec" should {
+    "encode array of longs" in {
+      longs.encode(Vector(200L, Long.MaxValue)) should_== hex"0200000000000000c87fffffffffffffff".bits.right
+      longs.encode(Vector[Long]()) should_== hex"0".bits.right
+    }
+
+    "decode bytes to array of longs" in {
+      val res = longs.decode(hex"0200000000000000c87ffffffffffffffffeed".bits)
+      res should_== (BitVector(hex"feed"), Vector(200L, Long.MaxValue)).right
+    }
+  }
+}
