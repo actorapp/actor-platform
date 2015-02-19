81a44ccb8bde58a6658d3c41d1ab3bb833c13c18
ex3ndr <me@ex3ndr.com>
2015-02-19 22:08:22 +0300

    (fix): Fixed initial user object saving

--- actor-common/src/main/java/im/actor/model/modules/updates/internal/LoggedIn.java
+++ actor-common/src/main/java/im/actor/model/modules/updates/internal/LoggedIn.java
@@ -1,18 +1,24 @@
 package im.actor.model.modules.updates.internal;
 
-import im.actor.model.api.User;
+import im.actor.model.api.rpc.ResponseAuth;
 
 /**
  * Created by ex3ndr on 19.02.15.
  */
 public class LoggedIn extends InternalUpdate {
-    private User user;
+    private ResponseAuth auth;
+    private Runnable runnable;
 
-    public LoggedIn(User user) {
-        this.user = user;
+    public LoggedIn(ResponseAuth auth, Runnable runnable) {
+        this.auth = auth;
+        this.runnable = runnable;
     }
 
-    public User getUser() {
-        return user;
+    public ResponseAuth getAuth() {
+        return auth;
+    }
+
+    public Runnable getRunnable() {
+        return runnable;
     }
 }
