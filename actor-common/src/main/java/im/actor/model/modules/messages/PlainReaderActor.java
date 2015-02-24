675a77b2f05055d82124086c173846863be140a9
Steve Kite <steve@actor.im>
2015-02-24 21:40:42 +0300

    fix(common): Fixed read/receive in groups

--- actor-common/src/main/java/im/actor/model/modules/messages/PlainReaderActor.java
+++ actor-common/src/main/java/im/actor/model/modules/messages/PlainReaderActor.java
@@ -1,18 +1,10 @@
 package im.actor.model.modules.messages;
 
-import java.io.IOException;
-import java.util.HashSet;
-
 import im.actor.model.api.OutPeer;
 import im.actor.model.api.rpc.RequestMessageRead;
 import im.actor.model.api.rpc.ResponseVoid;
 import im.actor.model.entity.Peer;
-import im.actor.model.entity.PeerType;
-import im.actor.model.entity.User;
 import im.actor.model.modules.Modules;
-import im.actor.model.modules.messages.entity.PlainCursor;
-import im.actor.model.modules.messages.entity.PlainCursorsStorage;
-import im.actor.model.modules.utils.ModuleActor;
 import im.actor.model.network.RpcCallback;
 import im.actor.model.network.RpcException;
 
@@ -29,14 +21,8 @@ public class PlainReaderActor extends PlainCursorActor {
 
     @Override
     protected void perform(final Peer peer, final long date) {
-        OutPeer outPeer;
-        if (peer.getPeerType() == PeerType.PRIVATE) {
-            User user = getUser(peer.getPeerId());
-            if (user == null) {
-                return;
-            }
-            outPeer = new OutPeer(im.actor.model.api.PeerType.PRIVATE, user.getUid(), user.getAccessHash());
-        } else {
+        OutPeer outPeer = buidOutPeer(peer);
+        if (outPeer == null) {
             return;
         }
 
