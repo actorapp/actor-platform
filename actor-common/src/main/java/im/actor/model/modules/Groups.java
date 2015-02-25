f8d4ae57c50ff5e74af009ba93b784c17899fcea
ex3ndr <me@ex3ndr.com>
2015-02-25 04:28:15 +0300

    feat(android+common): Added edit group titles

--- actor-common/src/main/java/im/actor/model/modules/Groups.java
+++ actor-common/src/main/java/im/actor/model/modules/Groups.java
@@ -2,8 +2,20 @@ package im.actor.model.modules;
 
 import java.io.IOException;
 
+import im.actor.model.api.GroupOutPeer;
+import im.actor.model.api.base.SeqUpdate;
+import im.actor.model.api.rpc.RequestEditGroupTitle;
+import im.actor.model.api.rpc.ResponseSeqDate;
+import im.actor.model.api.updates.UpdateGroupTitleChanged;
+import im.actor.model.api.updates.UpdateUserLocalNameChanged;
+import im.actor.model.concurrency.Command;
+import im.actor.model.concurrency.CommandCallback;
 import im.actor.model.entity.Group;
+import im.actor.model.modules.utils.RandomUtils;
 import im.actor.model.mvvm.MVVMCollection;
+import im.actor.model.network.RpcCallback;
+import im.actor.model.network.RpcException;
+import im.actor.model.network.RpcInternalException;
 import im.actor.model.storage.KeyValueEngine;
 import im.actor.model.viewmodel.GroupVM;
 
@@ -47,5 +59,51 @@ public class Groups extends BaseModule {
 
     public MVVMCollection<Group, GroupVM> getGroupsCollection() {
         return collection;
+    }
+
+    public Command<Boolean> editTitle(final int gid, final String name) {
+        return new Command<Boolean>() {
+            @Override
+            public void start(final CommandCallback<Boolean> callback) {
+                Group group = getGroups().getValue(gid);
+                if (group == null) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            callback.onError(new RpcInternalException());
+                        }
+                    });
+                    return;
+                }
+                final long rid = RandomUtils.nextRid();
+                request(new RequestEditGroupTitle(new GroupOutPeer(group.getGroupId(), group.getAccessHash()),
+                        rid, name), new RpcCallback<ResponseSeqDate>() {
+                    @Override
+                    public void onResult(ResponseSeqDate response) {
+                        SeqUpdate update = new SeqUpdate(response.getSeq(), response.getState(),
+                                UpdateUserLocalNameChanged.HEADER,
+                                new UpdateGroupTitleChanged(gid, rid, myUid(),
+                                        name, response.getDate()).toByteArray());
+                        updates().onUpdateReceived(update);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                callback.onResult(true);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onError(RpcException e) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                callback.onError(new RpcInternalException());
+                            }
+                        });
+                    }
+                });
+            }
+        };
     }
 }
