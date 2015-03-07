b208edb84f4ef7391ef686b0cf6230a54fa09380
prettynatty <fear@loathing.in>
2015-03-07 05:47:08 +0300

    feat(encryption): GetPublicKeys handler

--- actor-persist/src/main/scala/im/actor/server/persist/UserPublicKey.scala
+++ actor-persist/src/main/scala/im/actor/server/persist/UserPublicKey.scala
@@ -4,6 +4,7 @@ import com.github.tototoshi.slick.PostgresJodaSupport._
 import im.actor.server.models
 import org.joda.time.DateTime
 import slick.driver.PostgresDriver.api._
+import shapeless._, syntax.sized._
 
 class UserPublicKeyTable(tag: Tag) extends Table[models.UserPublicKey](tag, "public_keys") {
   def userId = column[Int]("user_id", O.PrimaryKey)
@@ -18,21 +19,32 @@ class UserPublicKeyTable(tag: Tag) extends Table[models.UserPublicKey](tag, "pub
 object UserPublicKey {
   val pkeys = TableQuery[UserPublicKeyTable]
 
+  private def active =
+    pkeys.filter(_.deletedAt.isEmpty)
+
+  private def activeByUserId(userId: Int) =
+    active.filter(p => p.userId === userId && p.deletedAt.isEmpty)
+
   def create(pk: models.UserPublicKey) =
     pkeys += pk
 
   def delete(userId: Int, hash: Long) =
     pkeys.filter(p => p.userId === userId && p.hash === hash).map(_.deletedAt).update(Some(new DateTime))
 
-  def active =
-    pkeys.filter(_.deletedAt.isEmpty)
-
-  def activeByUserId(userId: Int) =
-    active.filter(p => p.userId === userId && p.deletedAt.isEmpty)
-
   def findKeyHashes(userId: Int)  =
     activeByUserId(userId).map(_.hash).result
 
   def find(userId: Int, authId: Long) =
     active.filter(p => p.userId === userId && p.authId === authId).result
+
+  def findByUserHashes(pairs: Set[(Int, Long)]) = {
+    // TODO: type-based size checking
+    require(pairs.size > 0)
+
+    active.filter { pk =>
+      pairs.view.map {
+        case (userId, hash) => pk.userId === userId && pk.hash === hash
+      }.reduceLeft(_ || _)
+    }.result
+  }
 }
--- actor-tests/src/test/scala/im/actor/server/api/rpc/service/ServiceSpecHelpers.scala
+++ actor-tests/src/test/scala/im/actor/server/api/rpc/service/ServiceSpecHelpers.scala
@@ -1,11 +1,19 @@
 package im.actor.server.api.rpc.service
 
+import akka.actor.ActorSystem
+
 import im.actor.api.{ rpc => api }
 import im.actor.server.persist
+
+import eu.codearte.jfairy.Fairy
+
 import scala.concurrent._, duration._
+
 import slick.driver.PostgresDriver.api._
 
 trait ServiceSpecHelpers {
+  val fairy = Fairy.create()
+
   def buildPhone(): Long = {
     75550000000L + scala.util.Random.nextInt(999999)
   }
@@ -17,28 +25,51 @@ trait ServiceSpecHelpers {
     authId
   }
 
-  def getSmsHash(authId: Long, phoneNumber: Long)(implicit service: api.auth.AuthService): String = {
+  def getSmsHash(authId: Long, phoneNumber: Long)(implicit service: api.auth.AuthService, system: ActorSystem): String = withoutLogs {
     val api.auth.ResponseSendAuthCode(smsHash, _) =
       Await.result(service.handleSendAuthCode(authId, None, phoneNumber, 1, "apiKey"), 1.second).toOption.get._1
     smsHash
   }
 
-  def createUser(authId: Long, phoneNumber: Long)(implicit service: api.auth.AuthService) = {
-    val smsHash = getSmsHash(authId, phoneNumber)(service)
+  def createUser(authId: Long, phoneNumber: Long)(implicit service: api.auth.AuthService, system: ActorSystem) = withoutLogs {
+    val smsHash = getSmsHash(authId, phoneNumber)(service, system)
 
-    Await.result(service.handleSignUp(
+    val (rsp, _) = Await.result(service.handleSignUp(
       authId = authId,
       optUserId = None,
       phoneNumber = phoneNumber,
       smsHash = smsHash,
       smsCode = "0000",
-      name = "Wayne Brain",
+      name = fairy.person().fullName(),
       publicKey = scala.util.Random.nextLong.toBinaryString.getBytes(),
       deviceHash = scala.util.Random.nextLong.toBinaryString.getBytes(),
       deviceTitle = "Specs virtual device",
       appId = 42,
       appKey = "appKey",
       isSilent = false
-    ), 1.second)
+    ), 1.second).toOption.get
+
+    rsp.user
+  }
+
+  def buildAuthService(system: ActorSystem, database: Database) = new auth.AuthServiceImpl {
+    override implicit val ec = system.dispatcher
+    override implicit val actorSystem = system
+
+    val db = database
+  }
+
+  protected def withoutLogs[A](f: => A)(implicit system: ActorSystem): A = {
+    val logger =  org.slf4j.LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME).asInstanceOf[ch.qos.logback.classic.Logger]
+    val logLevel = logger.getLevel()
+    logger.setLevel(ch.qos.logback.classic.Level.OFF)
+
+    system.eventStream.setLogLevel(akka.event.Logging.ErrorLevel)
+
+    val res = f
+
+    logger.setLevel(logLevel)
+
+    res
   }
 }
