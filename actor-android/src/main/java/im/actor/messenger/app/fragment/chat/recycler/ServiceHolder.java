54e92db4283eca824b9282e381e9037e9f5b855c
Steve Kite <steve@actor.im>
2015-03-19 20:46:01 +0300

    feat(i18n): Moved service and dialog messages formatting to i18n module

--- actor-android/src/main/java/im/actor/messenger/app/fragment/chat/recycler/ServiceHolder.java
+++ actor-android/src/main/java/im/actor/messenger/app/fragment/chat/recycler/ServiceHolder.java
@@ -5,16 +5,10 @@ import android.widget.TextView;
 
 import im.actor.messenger.R;
 import im.actor.messenger.app.fragment.chat.MessagesFragment;
-import im.actor.messenger.app.view.MessageTextFormatter;
 import im.actor.model.entity.Message;
 import im.actor.model.entity.content.ServiceContent;
-import im.actor.model.entity.content.ServiceGroupAvatarChanged;
-import im.actor.model.entity.content.ServiceGroupCreated;
-import im.actor.model.entity.content.ServiceGroupTitleChanged;
-import im.actor.model.entity.content.ServiceGroupUserAdded;
-import im.actor.model.entity.content.ServiceGroupUserKicked;
-import im.actor.model.entity.content.ServiceGroupUserLeave;
-import im.actor.model.entity.content.ServiceUserRegistered;
+
+import static im.actor.messenger.app.Core.messenger;
 
 /**
  * Created by ex3ndr on 27.02.15.
@@ -31,31 +25,7 @@ public class ServiceHolder extends MessageHolder {
 
     @Override
     protected void bindData(Message message, boolean isUpdated) {
-        if (message.getContent() instanceof ServiceUserRegistered) {
-            messageText.setText(MessageTextFormatter.joinedActorFull(getPeer().getPeerId()));
-        } else if (message.getContent() instanceof ServiceGroupCreated) {
-            messageText.setText(MessageTextFormatter.groupCreatedFull(message.getSenderId(),
-                    ((ServiceGroupCreated) message.getContent()).getGroupTitle()));
-        } else if (message.getContent() instanceof ServiceGroupUserLeave) {
-            messageText.setText(MessageTextFormatter.groupLeave(message.getSenderId()));
-        } else if (message.getContent() instanceof ServiceGroupUserAdded) {
-            messageText.setText(MessageTextFormatter.groupAdd(message.getSenderId(),
-                    ((ServiceGroupUserAdded) message.getContent()).getAddedUid()));
-        } else if (message.getContent() instanceof ServiceGroupUserKicked) {
-            messageText.setText(MessageTextFormatter.groupKicked(message.getSenderId(),
-                    ((ServiceGroupUserKicked) message.getContent()).getKickedUid()));
-        } else if (message.getContent() instanceof ServiceGroupTitleChanged) {
-            messageText.setText(MessageTextFormatter.groupChangeTitleFull(message.getSenderId(),
-                    ((ServiceGroupTitleChanged) message.getContent()).getNewTitle()));
-        } else if (message.getContent() instanceof ServiceGroupAvatarChanged) {
-            ServiceGroupAvatarChanged avatar = (ServiceGroupAvatarChanged) message.getContent();
-            if (avatar.getNewAvatar() != null) {
-                messageText.setText(MessageTextFormatter.groupChangeAvatar(message.getSenderId()));
-            } else {
-                messageText.setText(MessageTextFormatter.groupRemoveAvatar(message.getSenderId()));
-            }
-        } else {
-            messageText.setText(((ServiceContent) message.getContent()).getCompatText());
-        }
+        messageText.setText(messenger().getFormatter().formatFullServiceMessage(message.getSenderId(),
+                (ServiceContent) message.getContent()));
     }
 }
