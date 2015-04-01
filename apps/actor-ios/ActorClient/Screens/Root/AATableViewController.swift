2d17bb14d3e4a6a6aa202372a289983ca8e52d78
Danil Gontovnik <gontovnik.danil@gmail.com>
2015-04-01 14:59:17 +0100

    Rewrote some auth screens on swift

---
+++ apps/actor-ios/ActorClient/Screens/Root/AATableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AATableViewController.swift
+//  ActorClient
+//
+//  Created by Danil Gontovnik on 3/31/15.
+//  Copyright (c) 2015 Actor LLC. All rights reserved.
+//
+
+import UIKit
+
+class AATableViewController: UITableViewController {
+    
+    // MARK: - 
+    // MARK: Constructors
+    
+    init() {
+        super.init(style: UITableViewStyle.Plain)
+    }
+    
+    override init(style: UITableViewStyle) {
+        super.init(style: style)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
