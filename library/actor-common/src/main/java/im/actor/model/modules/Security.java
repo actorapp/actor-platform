81d16d7bf7a26054a1a0babd24de280de51c9229
Steve Kite <steve@actor.im>
2015-04-02 19:06:50 +0300

    fix(android): Fixing android chat initial scroll

---
+++ library/actor-common/src/main/java/im/actor/model/modules/Security.java
@@ -0,0 +1,10 @@
+package im.actor.model.modules;
+
+/**
+ * Created by ex3ndr on 02.04.15.
+ */
+public class Security extends BaseModule {
+    public Security(Modules modules) {
+        super(modules);
+    }
+}
