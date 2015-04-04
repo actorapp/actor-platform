c3bccf36614655d92c52c30e80fb8e852d0b986b
Danil Gontovnik <gontovnik.danil@gmail.com>
2015-04-04 14:48:38 +0100

    feat(ios): group info screen: - add participants; - place holder screen if left group.

--- apps/actor-ios/ActorClient/CocoaExecution.swift
+++ apps/actor-ios/ActorClient/CocoaExecution.swift
@@ -11,11 +11,17 @@ import Foundation
 extension UIViewController {
     
     func execute(command: AMCommand) {
+        execute(command, completion: nil)
+    }
+    
+    func execute(command: AMCommand, completion: (() -> Void)?) {
         MBProgressHUD.showHUDAddedTo(UIApplication.sharedApplication().keyWindow, animated: true)
         command.startWithAMCommandCallback(CocoaCallback(result: { (val:Any?) -> () in
             MBProgressHUD.hideAllHUDsForView(UIApplication.sharedApplication().keyWindow, animated: true)
+            completion?()
             }, error: { (val) -> () in
                 MBProgressHUD.hideAllHUDsForView(UIApplication.sharedApplication().keyWindow, animated: true)
+                completion?()
         }))
     }
     
