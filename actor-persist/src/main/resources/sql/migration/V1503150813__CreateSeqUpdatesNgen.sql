6ffcb214fd913bf482f3336f35e38805194f3725
John Doe <speed@actor.im>
2015-04-07 03:21:02 +0300

    feat(sequence): the whole sequence persistence is being managed by SeqUpdatesManager

--- actor-persist/src/main/resources/sql/migration/V1503150813__CreateSeqUpdatesNgen.sql
+++ actor-persist/src/main/resources/sql/migration/V1503150813__CreateSeqUpdatesNgen.sql
@@ -1,11 +1,8 @@
 CREATE TABLE seq_updates_ngen (
   auth_id bigint NOT NULL,
-  ref bytea NOT NULL,
+  timestamp bigint NOT NULL,
   seq int NOT NULL,
-  date timestamp NOT NULL,
   header int NOT NULL,
   serialized_data bytea NOT NULL,
-  PRIMARY KEY (auth_id, ref)
+  PRIMARY KEY (auth_id, timestamp)
 );
-
-CREATE INDEX seq_updates_auth_id_date_idx ON seq_updates_ngen(auth_id, date);
--- actor-tests/src/test/scala/im/actor/server/push/SeqUpdatesManagerSpec.scala
+++ actor-tests/src/test/scala/im/actor/server/push/SeqUpdatesManagerSpec.scala
@@ -1,23 +1,29 @@
 package im.actor.server.push
 
+import scala.concurrent.duration._
+
 import akka.testkit._
 import com.typesafe.config._
+import org.specs2.specification.core.Fragments
+
 import im.actor.api.{ rpc => api }
+import im.actor.server.SqlSpecHelpers
 import im.actor.util.testing._
-import scala.concurrent.duration._
 
 class SeqUpdatesManagerSpec extends ActorSpecification(
   ActorSpecification.createSystem(
-    ConfigFactory.parseString("""
+    ConfigFactory.parseString( """
       push.seq-updates-manager.receive-timeout = 1 second
-    """))
-) {
+                               """))
+) with SqlSpecHelpers {
   def is = s2"""
   SeqUpdatesManager should
     increment seq on update push $e1
   """
 
   import SeqUpdatesManager._
+
+  implicit val (ds, db) = migrateAndInitDb()
 
   val region = startRegion()
   val probe = TestProbe()
@@ -26,23 +32,41 @@ class SeqUpdatesManagerSpec extends ActorSpecification(
     val authId = util.Random.nextLong()
     val update = api.contacts.UpdateContactsAdded(Vector(1, 2, 3))
 
-    probe.send(region, Envelope(authId, PushUpdateGetSeq(update)))
-    probe.expectMsg(1001)
+    {
+      probe.send(region, Envelope(authId, PushUpdateGetSequenceState(update.header, update.toByteArray)))
+      val msg = probe.receiveOne(5.seconds).asInstanceOf[SequenceState]
+      msg._1 must be_==(1001)
+    }
 
-    probe.send(region, Envelope(authId, PushUpdateGetSeq(update)))
-    probe.expectMsg(1002)
+    {
+      probe.send(region, Envelope(authId, PushUpdateGetSequenceState(update.header, update.toByteArray)))
+      val msg = probe.receiveOne(1.second).asInstanceOf[SequenceState]
+      msg._1 must be_==(1002)
+    }
 
     probe.expectNoMsg(1.5.seconds)
 
-    probe.send(region, Envelope(authId, PushUpdateGetSeq(update)))
-    probe.expectMsg(2001)
+    {
+      probe.send(region, Envelope(authId, PushUpdateGetSequenceState(update.header, update.toByteArray)))
+      val msg = probe.receiveOne(1.second).asInstanceOf[SequenceState]
+      msg._1 must be_==(2001)
+    }
 
     for (a <- 1 to 600)
-      probe.send(region, Envelope(authId, PushUpdate(update)))
+      probe.send(region, Envelope(authId, PushUpdate(update.header, update.toByteArray)))
 
-    probe.expectNoMsg(1.5.seconds)
+    probe.expectNoMsg(2.seconds)
 
-    probe.send(region, Envelope(authId, PushUpdateGetSeq(update)))
-    probe.expectMsg(3501)
+    {
+      probe.send(region, Envelope(authId, PushUpdateGetSequenceState(update.header, update.toByteArray)))
+      val msg = probe.receiveOne(1.second).asInstanceOf[SequenceState]
+      msg._1 must be_==(3500)
+    }
   }
+
+  override def map(fragments: => Fragments) =
+    super.map(fragments) ^ step(closeDb())
+
+  private def closeDb(): Unit =
+    ds.close()
 }
