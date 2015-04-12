6ae1c04c13a565e19da78636b78eeaefb848a360
kioltk <kioltk@gmail.com>
2015-04-12 09:20:21 +0300

    feat(android): Emoji and stickers keyboard implemented

---
+++ apps/actor-android/src/main/java/im/actor/messenger/app/emoji/stickers/Stickers.java
@@ -0,0 +1,25 @@
+package im.actor.messenger.app.emoji.stickers;
+
+import im.actor.messenger.R;
+
+/**
+ * Created by Jesus Christ. Amen.
+ */
+public class Stickers {
+    public static int[] animalPack = {
+            R.drawable.panda1,
+            R.drawable.doge,
+            R.drawable.bear,
+            R.drawable.koala,
+            R.drawable.grumpy,
+            R.drawable.dove,
+            R.drawable.chicken,
+            R.drawable.monkey,
+            R.drawable.elephant,
+            R.drawable.rat,
+            R.drawable.frog,
+            R.drawable.pig,
+            R.drawable.goat,
+
+    };
+}
