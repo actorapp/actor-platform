5af8d2ae5e67a7c465304ca1e9a7ab1c3e62f8fa
John Doe <speed@actor.im>
2015-04-17 14:49:14 +0300

    feat(messaging): MessageReceived handler

--- actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/messaging/HistoryHandlers.scala
+++ actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/messaging/HistoryHandlers.scala
@@ -1,17 +1,43 @@
 package im.actor.server.api.rpc.service.messaging
 
+import scala.concurrent.Future
+
+import org.joda.time.DateTime
+import slick.driver.PostgresDriver.api._
+
 import im.actor.api.rpc._
-import im.actor.server.models
-import im.actor.server.persist
+import im.actor.api.rpc.messaging.{ UpdateMessageReadByMe, UpdateMessageReceived }
+import im.actor.api.rpc.misc.ResponseVoid
+import im.actor.api.rpc.peers.{ OutPeer, Peer, PeerType }
+import im.actor.server.api.util.HistoryUtils
+import im.actor.server.{ models, persist }
 
 trait HistoryHandlers {
   self: MessagingServiceImpl =>
-/*
-  override def jhandleMessageReceived(peer: im.actor.api.rpc.peers.OutPeer, date: Long, clientData: im.actor.api.rpc.ClientData): scala.concurrent.Future[scalaz.\/[im.actor.api.rpc.RpcError,im.actor.api.rpc.misc.ResponseVoid]] = {
-    val action = requireAuth(clientData).flatMap { client =>
 
+  import HistoryUtils._
+  import im.actor.server.push.SeqUpdatesManager._
+
+  override def jhandleMessageReceived(peer: OutPeer, date: Long, clientData: im.actor.api.rpc.ClientData): Future[HandlerResult[ResponseVoid]] = {
+    val action = requireAuth(clientData).map { client =>
+      val receivedDate = System.currentTimeMillis()
+
+      peer.`type` match {
+        case PeerType.Private =>
+          val update = UpdateMessageReceived(Peer(PeerType.Private, client.userId), date, receivedDate)
+
+          for {
+            _ <- markMessagesReceived(models.Peer.privat(client.userId), models.Peer.privat(peer.id), new DateTime(date))
+            _ <- broadcastUserUpdate(seqUpdManagerRegion, peer.id, update)
+          } yield {
+            Ok(ResponseVoid)
+          }
+        case PeerType.Group =>
+          throw new NotImplementedError()
+        case _ => throw new NotImplementedError()
+      }
     }
 
-    db.run(toDBIOAction(action.transactionally))
-  }*/
+    db.run(toDBIOAction(action map (_.transactionally)))
+  }
 }
--- actor-rpc-api/src/main/scala/im/actor/server/api/util/HistoryUtils.scala
+++ actor-rpc-api/src/main/scala/im/actor/server/api/util/HistoryUtils.scala
@@ -14,52 +14,75 @@ object HistoryUtils {
                           randomId: Long,
                           messageContentHeader: Int,
                           messageContentData: Array[Byte])(implicit ec: ExecutionContext) = {
-    if (fromPeer.typ == models.PeerType.Group) {
-      throw new Exception("fromPeer should be Private")
-    } else {
-      if (toPeer.typ == models.PeerType.Private) {
-        val outMessage = models.HistoryMessage(
-          userId = fromPeer.id,
-          peer = toPeer,
-          date = date,
-          senderUserId = fromPeer.id,
-          randomId = randomId,
-          messageContentHeader = messageContentHeader,
-          messageContentData = messageContentData,
-          deletedAt = None
-        )
+    requirePrivatePeer(fromPeer)
+    requireDifferentPeers(fromPeer, toPeer)
 
-        val inMessage = models.HistoryMessage(
-          userId = toPeer.id,
-          peer = fromPeer,
-          date = date,
-          senderUserId = fromPeer.id,
-          randomId = randomId,
-          messageContentHeader = messageContentHeader,
-          messageContentData = messageContentData,
-          deletedAt = None
-        )
+    if (toPeer.typ == models.PeerType.Private) {
+      val outMessage = models.HistoryMessage(
+        userId = fromPeer.id,
+        peer = toPeer,
+        date = date,
+        senderUserId = fromPeer.id,
+        randomId = randomId,
+        messageContentHeader = messageContentHeader,
+        messageContentData = messageContentData,
+        deletedAt = None
+      )
 
-        for {
-          _ <- persist.HistoryMessage.create(Seq(outMessage, inMessage))
-          _ <- persist.Dialog.updateLastMessageDate(fromPeer.id, toPeer, date)
-          res <- persist.Dialog.updateLastMessageDate(toPeer.id, fromPeer, date)
-        } yield res
-      } else {
-        persist.GroupUser.findUserIds(toPeer.id) flatMap { groupUserIds =>
+      val inMessage = models.HistoryMessage(
+        userId = toPeer.id,
+        peer = fromPeer,
+        date = date,
+        senderUserId = fromPeer.id,
+        randomId = randomId,
+        messageContentHeader = messageContentHeader,
+        messageContentData = messageContentData,
+        deletedAt = None
+      )
 
-          // TODO: #perf eliminate double loop
+      for {
+        _ <- persist.HistoryMessage.create(Seq(outMessage, inMessage))
+        _ <- persist.Dialog.updateLastMessageDate(fromPeer.id, toPeer, date)
+        res <- persist.Dialog.updateLastMessageDate(toPeer.id, fromPeer, date)
+      } yield res
+    } else if (toPeer.typ == models.PeerType.Group) {
+      persist.GroupUser.findUserIds(toPeer.id) flatMap { groupUserIds =>
 
-          val historyMessages = groupUserIds.map { groupUserId =>
-            models.HistoryMessage(groupUserId, toPeer, date, fromPeer.id, randomId, messageContentHeader, messageContentData, None)
-          }
+        // TODO: #perf eliminate double loop
 
-          // TODO: #perf update dialogs in one query
-          val dialogActions = groupUserIds.map(persist.Dialog.updateLastMessageDate(_, toPeer, date))
-
-          DBIO.sequence(dialogActions :+ persist.HistoryMessage.create(historyMessages))
+        val historyMessages = groupUserIds.map { groupUserId =>
+          models.HistoryMessage(groupUserId, toPeer, date, fromPeer.id, randomId, messageContentHeader, messageContentData, None)
         }
+
+        // TODO: #perf update dialogs in one query
+        val dialogActions = groupUserIds.map(persist.Dialog.updateLastMessageDate(_, toPeer, date))
+
+        DBIO.sequence(dialogActions :+ persist.HistoryMessage.create(historyMessages))
       }
+    } else {
+      throw new NotImplementedError()
     }
+  }
+
+  def markMessagesReceived(byPeer: models.Peer, peer: models.Peer, date: DateTime)(implicit ec: ExecutionContext) = {
+    requirePrivatePeer(byPeer)
+    requireDifferentPeers(byPeer, peer)
+
+    peer.typ match {
+      case models.PeerType.Private =>
+        persist.Dialog.updateLastReceivedAt(peer.id, models.Peer.privat(byPeer.id), date)
+      case models.PeerType.Group =>
+        throw new NotImplementedError()
+    }
+  }
+
+  private def requireDifferentPeers(peer1: models.Peer, peer2: models.Peer) = {
+    if (peer1 == peer2)
+      throw new Exception("peers should not be same")
+  }
+
+  private def requirePrivatePeer(peer: models.Peer) = {
+    if (peer.typ != models.PeerType.Private)
+      throw new Exception("peer should be Private")
   }
 }
