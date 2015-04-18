0de13bb5c84acdd9fc32fb6bc178c572e1136f31
John Doe <speed@actor.im>
2015-04-19 01:37:48 +0300

    test(e2e): test user authorization

--- actor-tests/src/e2e/scala/im/actor/server/SimpleServerE2eSpec.scala
+++ actor-tests/src/e2e/scala/im/actor/server/SimpleServerE2eSpec.scala
@@ -2,26 +2,26 @@ package im.actor.server
 
 import java.net.InetSocketAddress
 
-import scala.concurrent.duration.FiniteDuration
-import scala.concurrent.{Promise, Future, Await}
-
 import akka.stream.ActorFlowMaterializer
-import akka.testkit._
-import org.specs2.matcher.ThrownExpectations
-import org.specs2.specification.core.Fragments
 
-import im.actor.api.rpc.{ RpcOk, RpcResult, Request }
-import im.actor.api.rpc.auth.RequestSendAuthCode
+import im.actor.api.rpc.auth.{ ResponseSendAuthCode, RequestSignUp, RequestSendAuthCode }
 import im.actor.api.rpc.codecs.RequestCodec
+import im.actor.api.rpc.sequence.RequestGetDifference
+import im.actor.api.rpc.{ Request, RpcOk, RpcResult }
 import im.actor.server.api.frontend.{ Tcp => TcpFrontend }
-import im.actor.server.api.rpc.{ RpcResultCodec, RpcApiService }
 import im.actor.server.api.rpc.RpcApiService.AttachService
 import im.actor.server.api.rpc.service.auth.AuthServiceImpl
+import im.actor.server.api.rpc.service.contacts.ContactsServiceImpl
+import im.actor.server.api.rpc.service.encryption.EncryptionServiceImpl
+import im.actor.server.api.rpc.service.groups.GroupsServiceImpl
+import im.actor.server.api.rpc.service.messaging.MessagingServiceImpl
+import im.actor.server.api.rpc.service.sequence.SequenceServiceImpl
+import im.actor.server.api.rpc.{ RpcApiService, RpcResultCodec }
 import im.actor.server.db.DbInit
 import im.actor.server.mtproto.codecs.protocol._
 import im.actor.server.mtproto.protocol._
 import im.actor.server.mtproto.transport.{ MTPackage, ProtoPackage, TransportPackage }
-import im.actor.server.push.{ WeakUpdatesManager, SeqUpdatesManager }
+import im.actor.server.push.{ SeqUpdatesManager, WeakUpdatesManager }
 import im.actor.server.session.Session
 import im.actor.util.testing._
 
@@ -30,7 +30,7 @@ class SimpleServerE2eSpec extends ActorFlatSuite with DbInit {
 
   it should "connect and Handshake" in e1
 
-  it should "respond to RPC request" in e2
+  it should "respond to RPC requests" in e2
 
   val serverConfig = system.settings.config
   val sqlConfig = serverConfig.getConfig("persist.sql")
@@ -44,8 +44,18 @@ class SimpleServerE2eSpec extends ActorFlatSuite with DbInit {
   val rpcApiService = system.actorOf(RpcApiService.props())
   val sessionRegion = Session.startRegion(Some(Session.props(rpcApiService, seqUpdManagerRegion, weakUpdManagerRegion)))
 
-  val authService = new AuthServiceImpl(sessionRegion)
-  rpcApiService ! AttachService(authService)
+  val services = Seq(
+    new AuthServiceImpl(sessionRegion),
+    new ContactsServiceImpl(seqUpdManagerRegion),
+    new EncryptionServiceImpl,
+    new MessagingServiceImpl(seqUpdManagerRegion),
+    new GroupsServiceImpl(seqUpdManagerRegion),
+    new SequenceServiceImpl(seqUpdManagerRegion)
+  )
+
+  services foreach { service =>
+    rpcApiService ! AttachService(service)
+  }
 
   TcpFrontend.start(serverConfig, sessionRegion)
 
@@ -66,38 +76,83 @@ class SimpleServerE2eSpec extends ActorFlatSuite with DbInit {
     val sessionId = 2L
     val phoneNumber = 75550000000L
 
-    val messageId = 3L
+    val smsHash = {
+      val messageId = 3L
 
-    val requestBytes = RequestCodec.encode(Request(RequestSendAuthCode(phoneNumber, 1, "apiKey"))).require
-    val mbBytes = MessageBoxCodec.encode(MessageBox(messageId, RpcRequestBox(requestBytes))).require
-    val mtPackage = MTPackage(authId, sessionId, mbBytes)
+      val requestBytes = RequestCodec.encode(Request(RequestSendAuthCode(phoneNumber, 1, "apiKey"))).require
+      val mbBytes = MessageBoxCodec.encode(MessageBox(messageId, RpcRequestBox(requestBytes))).require
+      val mtPackage = MTPackage(authId, sessionId, mbBytes)
 
-    client.send(ProtoPackage(mtPackage))
+      client.send(ProtoPackage(mtPackage))
 
-    expectNewSession(sessionId, messageId)
-    expectMessageAck(messageId)
+      expectNewSession(sessionId, messageId)
+      expectMessageAck(messageId)
 
-    val result = receiveRpcResult(messageId)
-    result shouldBe an [RpcOk]
+      val result = receiveRpcResult(messageId)
+      result shouldBe an[RpcOk]
+
+      result.asInstanceOf[RpcOk].response.asInstanceOf[ResponseSendAuthCode].smsHash
+    }
+
+    {
+      val messageId = 4L
+
+      val requestBytes = RequestCodec.encode(Request(RequestSignUp(
+        phoneNumber = phoneNumber,
+        smsHash = smsHash,
+        smsCode = "0000",
+        name = "Wayne Brain",
+        publicKey = Array(1, 2, 3),
+        deviceHash = Array(4, 5, 6),
+        deviceTitle = "Specs virtual device",
+        appId = 1,
+        appKey = "appKey",
+        isSilent = false
+      ))).require
+      val mbBytes = MessageBoxCodec.encode(MessageBox(messageId, RpcRequestBox(requestBytes))).require
+      val mtPackage = MTPackage(authId, sessionId, mbBytes)
+
+      client.send(ProtoPackage(mtPackage))
+
+      expectMessageAck(messageId)
+
+      val result = receiveRpcResult(messageId)
+      result shouldBe an[RpcOk]
+    }
+
+    {
+      val messageId = 5L
+
+      val requestBytes = RequestCodec.encode(Request(RequestGetDifference(999, Array()))).require
+      val mbBytes = MessageBoxCodec.encode(MessageBox(messageId, RpcRequestBox(requestBytes))).require
+      val mtPackage = MTPackage(authId, sessionId, mbBytes)
+
+      client.send(ProtoPackage(mtPackage))
+
+      expectMessageAck(messageId)
+
+      val result = receiveRpcResult(messageId)
+      result shouldBe an[RpcOk]
+    }
 
     client.close()
   }
 
   private def expectMessageAck(messageId: Long)(implicit client: MTProtoClient): MessageAck = {
     val mb = receiveMessageBox()
-    mb.body shouldBe a [MessageAck]
+    mb.body shouldBe a[MessageAck]
 
     val expectedAck = MessageAck(Vector(messageId))
 
     val ack = mb.body.asInstanceOf[MessageAck]
-    ack should === (expectedAck)
+    ack should ===(expectedAck)
 
     ack
   }
 
   private def receiveRpcResult(messageId: Long)(implicit client: MTProtoClient): RpcResult = {
     val mb = receiveMessageBox()
-    mb.body shouldBe an [RpcResponseBox]
+    mb.body shouldBe an[RpcResponseBox]
 
     val rspBox = mb.body.asInstanceOf[RpcResponseBox]
     rspBox.messageId should ===(messageId)
@@ -116,7 +171,7 @@ class SimpleServerE2eSpec extends ActorFlatSuite with DbInit {
       case None => throw new Exception("Transport package not received")
     }
 
-    body shouldBe a [MTPackage]
+    body shouldBe a[MTPackage]
     body.asInstanceOf[MTPackage]
   }
 
@@ -126,8 +181,8 @@ class SimpleServerE2eSpec extends ActorFlatSuite with DbInit {
     val expectedNewSession = NewSession(sessionId, messageId)
 
     val mb = MessageBoxCodec.decode(mtp.messageBytes).require.value
-    mb.body shouldBe a [NewSession]
-    mb.body should === (expectedNewSession)
+    mb.body shouldBe a[NewSession]
+    mb.body should ===(expectedNewSession)
   }
 
   override def afterAll = {
