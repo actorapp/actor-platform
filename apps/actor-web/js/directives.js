04a257308bcbb64126ade8bf850fcaceb09546ce
Naltox <Altox@MacBook-Pro-Narek.local>
2015-04-20 03:25:59 +0300

    fix(web): fixed messaging

--- apps/actor-web/js/directives.js
+++ apps/actor-web/js/directives.js
@@ -42,3 +42,22 @@ App.factory('focus', function($timeout) {
       });
     };
   }) 
+  
+App.directive('enterSubmit', function () {
+    return {
+      restrict: 'A',
+      link: function (scope, elem, attrs) {
+       
+        elem.bind('keydown', function(event) {
+          var code = event.keyCode || event.which;
+                  
+          if (code === 13) {
+            if (!event.shiftKey) {
+              event.preventDefault();
+              scope.$apply(attrs.enterSubmit);
+            }
+          }
+        });
+      }
+    }
+  });
