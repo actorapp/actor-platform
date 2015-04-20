01b9c0fe3fcde387f8b2133d83addf7a2341413e
John Doe <speed@actor.im>
2015-04-20 21:08:42 +0300

    feat(messaging): ClearChat handler

--- actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/messaging/MessagingServiceImpl.scala
+++ actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/messaging/MessagingServiceImpl.scala
@@ -15,18 +15,6 @@ class MessagingServiceImpl(implicit
                            val db: Database,
                            val actorSystem: ActorSystem)
     extends MessagingService with MessagingHandlers with HistoryHandlers {
-  override def jhandleClearChat(peer: im.actor.api.rpc.peers.OutPeer, clientData: im.actor.api.rpc.ClientData): scala.concurrent.Future[scalaz.\/[im.actor.api.rpc.RpcError,im.actor.api.rpc.misc.ResponseSeq]] = Future {
-    throw new Exception("Not implemented")
-  }
-
-  override def jhandleDeleteChat(peer: im.actor.api.rpc.peers.OutPeer, clientData: im.actor.api.rpc.ClientData): scala.concurrent.Future[scalaz.\/[im.actor.api.rpc.RpcError,im.actor.api.rpc.misc.ResponseSeq]] = Future {
-    throw new Exception("Not implemented")
-  }
-
-  override def jhandleDeleteMessage(peer: im.actor.api.rpc.peers.OutPeer,rids: scala.collection.immutable.Vector[Long], clientData: im.actor.api.rpc.ClientData): scala.concurrent.Future[scalaz.\/[im.actor.api.rpc.RpcError,im.actor.api.rpc.misc.ResponseVoid]] = Future {
-    throw new Exception("Not implemented")
-  }
-
   override def jhandleEncryptedRead(peer: im.actor.api.rpc.peers.OutPeer,randomId: Long, clientData: im.actor.api.rpc.ClientData): scala.concurrent.Future[scalaz.\/[im.actor.api.rpc.RpcError,im.actor.api.rpc.misc.ResponseVoid]] = Future {
     throw new Exception("Not implemented")
   }
