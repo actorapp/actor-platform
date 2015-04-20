148b2209420774d0fce11159adf51ce147923154
prettynatty <fear@loathing.in>
2015-04-20 16:39:39 +0300

    fix(weak): SetOnline handler

--- actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/weak/WeakServiceImpl.scala
+++ actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/weak/WeakServiceImpl.scala
@@ -11,9 +11,11 @@ import im.actor.api.rpc.Implicits._
 import im.actor.api.rpc.misc.ResponseVoid
 import im.actor.api.rpc.peers.{ PeerType, Peer, OutPeer }
 import im.actor.api.rpc.weak.{ UpdateTyping, WeakService }
+import im.actor.server.presences.{ PresenceManagerRegion, PresenceManager }
 import im.actor.server.push.{ WeakUpdatesManagerRegion, WeakUpdatesManager }
 
-class WeakServiceImpl(weakUpdManagerRegion: WeakUpdatesManagerRegion)(implicit db: Database, actorSystem: ActorSystem) extends WeakService {
+class WeakServiceImpl(weakUpdManagerRegion: WeakUpdatesManagerRegion, presenceManagerRegion: PresenceManagerRegion)
+                     (implicit db: Database, actorSystem: ActorSystem) extends WeakService {
   override implicit val ec: ExecutionContext = actorSystem.dispatcher
 
   override def jhandleTyping(peer: OutPeer, typingType: Int, clientData: ClientData): Future[HandlerResult[ResponseVoid]] = {
@@ -33,6 +35,18 @@ class WeakServiceImpl(weakUpdManagerRegion: WeakUpdatesManagerRegion)(implicit d
     db.run(toDBIOAction(authorizedAction))
   }
 
-  override def jhandleSetOnline(isOnline: Boolean, timeout: Long, clientData: ClientData): Future[HandlerResult[ResponseVoid]] =
-    throw new NotImplementedError()
+  override def jhandleSetOnline(isOnline: Boolean, timeout: Long, clientData: ClientData): Future[HandlerResult[ResponseVoid]] = {
+    val authorizedAction = requireAuth(clientData).map { client =>
+
+      if (isOnline) {
+        PresenceManager.presenceSetOnline(presenceManagerRegion, client.userId, timeout)
+      } else {
+        PresenceManager.presenceSetOffline(presenceManagerRegion, client.userId, timeout)
+      }
+
+      DBIO.successful(Ok(ResponseVoid))
+    }
+
+    db.run(toDBIOAction(authorizedAction))
+  }
 }
