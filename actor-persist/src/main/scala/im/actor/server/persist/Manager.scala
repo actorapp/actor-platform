6111aff80314ca272e8c3bf12a572849103fac1c
rock_jam <5min4eq.unity@gmail.com>
2015-04-22 02:16:03 +0300

    feat(dashboard)
    Manager registration; login logout with auth_token in session

---
+++ actor-persist/src/main/scala/im/actor/server/persist/Manager.scala
@@ -0,0 +1,27 @@
+package im.actor.server.persist
+
+import im.actor.server.models
+import slick.driver.PostgresDriver.api._
+
+class ManagerTable(tag: Tag) extends Table[models.Manager](tag, "managers") {
+  def id = column[Int]("id", O.PrimaryKey)
+  def name = column[String]("name")
+  def lastName = column[String]("last_name")
+  def domain = column[String]("domain")
+  def authToken = column[String]("auth_token")
+  def email = column[String]("email")
+  def emailUnique = index("email_idx", email, unique = true)//way to keep email unique
+
+  def * = (id, name, lastName, domain, authToken, email) <>(models.Manager.tupled, models.Manager.unapply)
+}
+
+object Manager {
+  val managers = TableQuery[ManagerTable]
+
+  def create(manager: models.Manager) =
+    managers += manager
+
+  def findByEmail(email: String) =
+    managers.filter(_.email === email).result
+
+}
