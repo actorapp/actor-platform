c816c2e585ab8ca672611c888c000e016cbdb7c8
prettynatty <fear@loathing.in>
2015-04-22 15:38:05 +0300

    feat(files): s3 files api

---
+++ actor-tests/src/test/scala/im/actor/server/api/rpc/service/FilesServiceSpec.scala
@@ -0,0 +1,138 @@
+package im.actor.server.api.rpc.service
+
+import java.io.OutputStreamWriter
+import java.net.{ HttpURLConnection, URL }
+
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider
+import com.amazonaws.services.s3.transfer.TransferManager
+import com.amazonaws.util.IOUtils
+import com.github.dwhjames.awswrap.s3.AmazonS3ScalaClient
+
+import im.actor.api.rpc._
+import im.actor.api.rpc.files._
+import im.actor.server.api.rpc.RpcApiService
+import im.actor.server.api.rpc.service.files.FilesServiceImpl
+import im.actor.server.presences.PresenceManager
+import im.actor.server.push.{ SeqUpdatesManager, WeakUpdatesManager }
+import im.actor.server.session.Session
+
+class FilesServiceSpec extends BaseServiceSuite {
+  behavior of "FilesService"
+
+  it should "Generate upload url" in e1
+
+  it should "Generate valid upload part urls" in e2
+
+  it should "Complete upload" in e3
+
+  it should "Generate valid download urls" in e4
+
+  val awsCredentials = new EnvironmentVariableCredentialsProvider()
+
+  val bucketName = "actor-uploads"
+  implicit val client = new AmazonS3ScalaClient(awsCredentials)
+  implicit val transferManager = new TransferManager(awsCredentials)
+
+  val service = new FilesServiceImpl(bucketName)
+
+  val rpcApiService = system.actorOf(RpcApiService.props())
+  implicit val seqUpdManagerRegion = SeqUpdatesManager.startRegion()
+  implicit val weakUpdManagerRegion = WeakUpdatesManager.startRegion()
+  implicit val presenceManagerRegion = PresenceManager.startRegion()
+  implicit val sessionRegion = Session.startRegion(Some(Session.props(rpcApiService)))
+  implicit val authService = buildAuthService()
+
+  val (user, _, _) = createUser()
+  val authId = createAuthId()
+  val sessionId = createSessionId()
+
+  implicit val clientData = ClientData(authId, sessionId, Some(user.id))
+
+  var uploadKey: Array[Byte] = Array.empty
+
+  var fileLocation: Option[FileLocation] = None
+
+  var expectedContents: Option[String] = None
+
+  def e1() = {
+    val size = 20
+
+    whenReady(service.handleGetFileUploadUrl(size)) { resp =>
+      resp should matchPattern {
+        case Ok(ResponseGetFileUploadUrl(url, key)) =>
+
+      }
+
+      this.uploadKey = resp.toOption.get.uploadKey
+    }
+  }
+
+  def e2() = {
+    val part1Size = 1024 * 32 // big part
+    val part2Size = 5 // small part
+
+    val resp1 = whenReady(service.handleGetFileUploadPartUrl(1, part1Size, uploadKey)) { resp =>
+      resp should matchPattern {
+        case Ok(ResponseGetFileUploadPartUrl(_)) =>
+      }
+
+      (part1Size, resp)
+    }
+
+    val resp2 = whenReady(service.handleGetFileUploadPartUrl(2, part2Size, uploadKey)) { resp =>
+      resp should matchPattern {
+        case Ok(ResponseGetFileUploadPartUrl(_)) =>
+      }
+
+      (part2Size, resp)
+    }
+
+    val parts = List(resp1, resp2) map {
+      case (size, Ok(ResponseGetFileUploadPartUrl(urlStr))) =>
+        println(urlStr)
+        val url = new URL(urlStr)
+        val connection = url.openConnection().asInstanceOf[HttpURLConnection]
+        connection.setDoOutput(true)
+        connection.setRequestMethod("PUT")
+        connection.addRequestProperty("Content-Type", "application/octet-stream")
+        val out = new OutputStreamWriter(connection.getOutputStream)
+        val partContents = ("." * size)
+        out.write(partContents)
+        out.close()
+        val responseCode = connection.getResponseCode()
+        println(connection.getResponseMessage)
+        responseCode should ===(200)
+        partContents
+    }
+
+    this.expectedContents = Some(parts.foldLeft("") { (acc, p) => acc + p })
+  }
+
+  def e3() = {
+    whenReady(service.handleCommitFileUpload(uploadKey)) { resp =>
+      resp should matchPattern {
+        case Ok(ResponseCommitFileUpload(_)) =>
+      }
+
+      this.fileLocation = Some(resp.toOption.get.uploadedFileLocation)
+    }
+  }
+
+  def e4() = {
+    val urlStr = whenReady(service.handleGetFileUrl(fileLocation.get)) { resp =>
+      resp should matchPattern {
+        case Ok(ResponseGetFileUrl(_, _)) =>
+      }
+
+      resp.toOption.get.url
+    }
+
+    val url = new URL(urlStr)
+    val connection = url.openConnection().asInstanceOf[HttpURLConnection]
+    connection.setDoOutput(true)
+    connection.setRequestMethod("GET")
+    connection.getResponseMessage should ===("OK")
+
+    IOUtils.toString(connection.getInputStream) should ===(expectedContents.get)
+  }
+}
