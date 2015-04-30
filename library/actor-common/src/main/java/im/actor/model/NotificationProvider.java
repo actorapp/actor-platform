951daf613c73008fc0169d2664885bb99882c4b4
ex3ndr <me@ex3ndr.com>
2015-04-30 14:17:36 +0300

    feat(android+common+electron): updated notifications provider interface, fixed build config

--- library/actor-common/src/main/java/im/actor/model/NotificationProvider.java
+++ library/actor-common/src/main/java/im/actor/model/NotificationProvider.java
@@ -11,28 +11,34 @@ import im.actor.model.entity.Peer;
 public interface NotificationProvider {
     /**
      * Called when message arrive when user is in app (used for play in app sound).
+     *
+     * @param messenger Messenger object
      */
-    public void onMessageArriveInApp();
+    void onMessageArriveInApp(Messenger messenger);
 
     /**
      * On background notification
      *
+     * @param messenger          Messenger object
      * @param topNotifications   top 10 notifications
      * @param messagesCount      total messages count
      * @param conversationsCount total conversations count
      */
-    public void onNotification(List<Notification> topNotifications, int messagesCount,
-                               int conversationsCount);
+    void onNotification(Messenger messenger, List<Notification> topNotifications, int messagesCount,
+                        int conversationsCount);
 
     /**
      * Called on dialogs open (need for hiding all notifications)
+     *
+     * @param messenger Messenger object
      */
-    public void onDialogsOpen();
+    void onDialogsOpen(Messenger messenger);
 
     /**
      * Called on conversation open (need for hiding conversation notifications)
      *
+     * @param messenger Messenger object
      * @param peer peer of chat
      */
-    public void onChatOpen(Peer peer);
+    void onChatOpen(Messenger messenger, Peer peer);
 }
