ebe9d126a82c10c66a3a46ebcfe8e4dbba990c6d
ex3ndr <me@ex3ndr.com>
2015-05-01 19:41:32 +0300

    feat(jvm): Updated TCP Implementation

--- library/actor-common/src/main/java/im/actor/model/network/connection/AsyncConnectionFactory.java
+++ library/actor-common/src/main/java/im/actor/model/network/connection/AsyncConnectionFactory.java
@@ -6,5 +6,5 @@ import im.actor.model.network.ConnectionEndpoint;
  * Created by ex3ndr on 29.04.15.
  */
 public interface AsyncConnectionFactory {
-    AsyncConnection createConnection(ConnectionEndpoint endpoint, AsyncConnectionInterface connectionInterface);
+    AsyncConnection createConnection(int connectionId, ConnectionEndpoint endpoint, AsyncConnectionInterface connectionInterface);
 }
