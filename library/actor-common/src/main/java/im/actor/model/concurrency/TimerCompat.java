55b42de973b3c4f19caf2b8dc77c249efb27edd0
ex3ndr <me@ex3ndr.com>
2015-05-01 15:47:23 +0300

    feat(js): Implemented new transport

---
+++ library/actor-common/src/main/java/im/actor/model/concurrency/TimerCompat.java
@@ -0,0 +1,38 @@
+package im.actor.model.concurrency;
+
+import im.actor.model.droidkit.actors.ActorCreator;
+import im.actor.model.droidkit.actors.ActorRef;
+import im.actor.model.droidkit.actors.Props;
+
+import static im.actor.model.droidkit.actors.ActorSystem.system;
+
+/**
+ * Created by ex3ndr on 01.05.15.
+ */
+public class TimerCompat {
+
+    private static final ActorRef TIMER_ACTOR = system().actorOf(Props.create(TimerActor.class, new ActorCreator<TimerActor>() {
+        @Override
+        public TimerActor create() {
+            return new TimerActor();
+        }
+    }), "actor/global_timer");
+
+    private Runnable runnable;
+
+    public TimerCompat(Runnable runnable) {
+        this.runnable = runnable;
+    }
+
+    public synchronized void cancel() {
+        TIMER_ACTOR.send(new TimerActor.Cancel(this));
+    }
+
+    public synchronized void schedule(long delay) {
+        TIMER_ACTOR.send(new TimerActor.Schedule(this, delay));
+    }
+
+    void invokeRun() {
+        runnable.run();
+    }
+}
