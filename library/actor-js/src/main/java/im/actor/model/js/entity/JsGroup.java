92acec4fc070b411a1f6f3475b5cf68ef2d8422a
ex3ndr <me@ex3ndr.com>
2015-05-01 17:34:45 +0300

    feat(js): Added group binding, started implementing of file binding

---
+++ library/actor-js/src/main/java/im/actor/model/js/entity/JsGroup.java
@@ -0,0 +1,22 @@
+package im.actor.model.js.entity;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+import im.actor.model.viewmodel.GroupVM;
+
+/**
+ * Created by ex3ndr on 01.05.15.
+ */
+public class JsGroup extends JavaScriptObject {
+    public static JsGroup fromGroupVM(GroupVM groupVM) {
+        return create(groupVM.getId(), groupVM.getName().get(), groupVM.getCreatorId());
+    }
+
+    public static native JsGroup create(int uid, String title, int adminId)/*-{
+        return {uid: uid, title: title, adminId: adminId};
+    }-*/;
+
+    protected JsGroup() {
+        
+    }
+}
