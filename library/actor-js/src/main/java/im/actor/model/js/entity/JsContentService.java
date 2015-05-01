4be911c7199ceb783032afc367ae998b1ad548b9
Steve Kite <steve@actor.im>
2015-05-01 17:34:45 +0300

    feat(js): Added group binding, started implementing of file binding

---
+++ library/actor-js/src/main/java/im/actor/model/js/entity/JsContentService.java
@@ -0,0 +1,14 @@
+package im.actor.model.js.entity;
+
+/**
+ * Created by ex3ndr on 01.05.15.
+ */
+public class JsContentService extends JsContent {
+    public native static JsContentService create(String text)/*-{
+        return {content: "service", text: text};
+    }-*/;
+
+    protected JsContentService() {
+
+    }
+}
