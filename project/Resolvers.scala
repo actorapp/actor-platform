61b6da7306dc4d58b0b556876c264e68b5d31d9d
rock_jam <5min4eq.unity@gmail.com>
2015-05-02 18:43:05 +0300

    fix(dashboard): replace typesafe resolver with sbt predefined

--- project/Resolvers.scala
+++ project/Resolvers.scala
@@ -11,10 +11,10 @@ object Resolvers {
     Resolver.bintrayRepo("dnvriend", "maven"),
     Resolver.bintrayRepo("dwhjames", "maven"),
     Resolver.bintrayRepo("krasserm", "maven"),
+    Resolver.typesafeRepo("releases"),
     "eaio" at "http://eaio.com/maven2",
     "actor snapshots" at "http://repos.81port.com/nexus/content/repositories/snapshots",
     "actor releases" at "http://repos.81port.com/nexus/content/repositories/releases",
-    "gcm-server-repository" at "https://raw.githubusercontent.com/slorber/gcm-server-repository/master/releases/",
-    "Typesafe repository" at "https://repo.typesafe.com/typesafe/releases/"
+    "gcm-server-repository" at "https://raw.githubusercontent.com/slorber/gcm-server-repository/master/releases/"
   )
 }
