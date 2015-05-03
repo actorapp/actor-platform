f207c4eeb7361739278bb65526a67fb8db099fd3
ex3ndr <me@ex3ndr.com>
2015-05-03 20:14:39 +0300

    feat(js): Completed photo and document messages

--- library/actor-js/src/main/java/im/actor/model/js/entity/JsAuthSuccessClosure.java
+++ library/actor-js/src/main/java/im/actor/model/js/entity/JsAuthSuccessClosure.java
@@ -8,11 +8,8 @@ import org.timepedia.exporter.client.Export;
 import org.timepedia.exporter.client.ExportClosure;
 import org.timepedia.exporter.client.Exportable;
 
-/**
- * Created by ex3ndr on 22.02.15.
- */
 @Export
 @ExportClosure
 public interface JsAuthSuccessClosure extends Exportable {
-    public void onResult(String state);
+    void onResult(String state);
 }
