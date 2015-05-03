b9d50b9a05695c0534f396577e1863b3feedf867
ex3ndr <me@ex3ndr.com>
2015-05-03 15:22:59 +0300

    feat(common+js+android): Async file operations, implemented file upload

--- library/actor-js/src/main/java/im/actor/model/js/providers/JsFileSystemProvider.java
+++ library/actor-js/src/main/java/im/actor/model/js/providers/JsFileSystemProvider.java
@@ -4,14 +4,26 @@
 
 package im.actor.model.js.providers;
 
+import java.util.HashMap;
+
 import im.actor.model.FileSystemProvider;
+import im.actor.model.crypto.CryptoUtils;
 import im.actor.model.entity.FileReference;
 import im.actor.model.files.FileSystemReference;
+import im.actor.model.js.providers.fs.JsFile;
 
 /**
  * Created by ex3ndr on 03.05.15.
  */
-public class JsFileSystem implements FileSystemProvider {
+public class JsFileSystemProvider implements FileSystemProvider {
+
+    private HashMap<String, JsFile> files = new HashMap<String, JsFile>();
+
+    public String registerUploadFile(JsFile file) {
+        String res = "upload://" + CryptoUtils.hex(CryptoUtils.randomBytes(16));
+        files.put(res, file);
+        return res;
+    }
 
     @Override
     public FileSystemReference createTempFile() {
@@ -30,6 +42,9 @@ public class JsFileSystem implements FileSystemProvider {
 
     @Override
     public FileSystemReference fileFromDescriptor(String descriptor) {
+        if (files.containsKey(descriptor)) {
+            return new JsFileSystemReference(files.get(descriptor), descriptor);
+        }
         return null;
     }
 }
