803cfc9e4b2255d125b9ffa273c435aef25105be
Nikolay Tatarinov <rockjam@actor.im>
2015-05-04 21:50:07 +0300

    fix(dashboard): models validaition via play json, custom phone validation, tests

--- actor-dashboard/app/im/actor/server/dashboard/controllers/utils/json/UsersJsonImplicits.scala
+++ actor-dashboard/app/im/actor/server/dashboard/controllers/utils/json/UsersJsonImplicits.scala
@@ -2,10 +2,11 @@ package im.actor.server.dashboard.controllers.utils.json
 
 import scala.concurrent.forkjoin.ThreadLocalRandom
 
+import play.api.data.validation.ValidationError
+import play.api.i18n.Lang
 import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
-import im.actor.server.dashboard.controllers._
 import im.actor.server.dashboard.controllers.utils.json.Common._
 import im.actor.server.models
 import im.actor.server.util.IdUtils._
@@ -22,23 +23,31 @@ object UsersJsonImplicits {
     )
   }
 
-  implicit val userReads: Reads[Lang2CompleteUser] = (
-    (JsPath \ "name").read[String](length) and
-    (JsPath \ "dept").read[String](length) and
-    (JsPath \ "phone").read[String](length)
-  )(makeUserAndPhone _)
+  def userReads(lang: Option[Lang]): Reads[CompleteUser] = {
+    val language = lang.getOrElse(Lang("RU")).language.toUpperCase
+    ((JsPath \ "name").read[String](length) and
+      (JsPath \ "dept").read[String](length) and
+      (JsPath \ "phone").read[String](validPhone(language)))(makeUserAndPhone(language) _)
+  }
 
-  implicit val userUpdateReads: Reads[Option[String]] = (JsPath \ "name").readNullable[String](length)
+  def validPhone(language: String)(implicit reads: Reads[String]): Reads[String] =
+    Reads[String](js ⇒ reads.reads(js).filter(ValidationError("error.invalidPhone", js)) { phone ⇒
+      PhoneNumber.isValid(phone, language)
+    })
 
-  private def makeUserAndPhone(name: String, struct: String, phone: String): Lang2CompleteUser = { lang ⇒
+  case class UserUpdate(name: String)
+
+  implicit val userUpdateReads: Reads[UserUpdate] = (JsPath \ "name").read[String](length).map { UserUpdate }
+
+  case class CompleteUser(user: models.User, struct: String, phone: models.UserPhone)
+
+  private def makeUserAndPhone(language: String)(name: String, struct: String, phone: String): CompleteUser = {
     val rnd = ThreadLocalRandom.current()
     val (userId, phoneId) = (nextIntId(rnd), nextIntId(rnd))
-    for {
-      code ← lang.map { _.language.toUpperCase }
-      normalizedPhone ← PhoneNumber.normalizeStr(phone, code)
-      user = models.User(userId, ACLUtils.nextAccessSalt(rnd), name, code, models.NoSex, models.UserState.Registered)
-      userPhone = models.UserPhone(phoneId, userId, ACLUtils.nextAccessSalt(rnd), normalizedPhone, "Mobile phone")
-    } yield (user, struct, userPhone)
+    val normalizedPhone = PhoneNumber.tryNormalize(phone.toLong, language)
+    val user = models.User(userId, ACLUtils.nextAccessSalt(rnd), name, language, models.NoSex, models.UserState.Registered)
+    val userPhone = models.UserPhone(phoneId, userId, ACLUtils.nextAccessSalt(rnd), normalizedPhone, "Mobile phone")
+    CompleteUser(user, struct, userPhone)
   }
 
 }
