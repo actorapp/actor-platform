a91a401b9fc2501933e8d141a182235ef898f5c8
prettynatty <fear@loathing.in>
2015-05-05 22:37:28 +0300

    fix(contacts): properly import contacts with local numbers

--- actor-rpc-api/src/main/scala/im/actor/server/api/util/PhoneNumber.scala
+++ actor-rpc-api/src/main/scala/im/actor/server/api/util/PhoneNumber.scala
@@ -1,32 +1,33 @@
 package im.actor.server.api.util
 
 import scala.annotation.tailrec
-import scala.util.{ Try, Success }
+import scala.util.Try
+
 import com.google.i18n.phonenumbers.PhoneNumberUtil
 
 object PhoneNumber {
   def normalizeStr(number: String, defaultCountry: String = ""): Option[Long] = {
     val phoneUtil = PhoneNumberUtil.getInstance()
-    Try(phoneUtil.parse(number, defaultCountry)) match {
-      case Success(p) ⇒
+    Try(phoneUtil.parse(s"+$number", defaultCountry))
+      .orElse(Try(phoneUtil.parse(number, defaultCountry)))
+      .toOption map { p ⇒
         val phoneNumber = p.getCountryCode * Math.pow(10L, (sizeOf(p.getNationalNumber) + 1).toDouble).longValue + p.getNationalNumber
-        Some(phoneNumber)
-      case _ ⇒ None
-    }
+        phoneNumber
+      }
   }
 
   def isValid(number: String, defaultCountry: String = ""): Boolean = normalizeStr(number, defaultCountry).isDefined
 
-  def normalizeLong(number: Long, defaultCountry: String = ""): Option[Long] = normalizeStr(s"+$number", defaultCountry)
+  def normalizeLong(number: Long, defaultCountry: String = ""): Option[Long] = normalizeStr(s"$number", defaultCountry)
 
   def normalizeWithCountry(number: Long, defaultCountry: String = ""): Option[(Long, String)] = {
     val phoneUtil = PhoneNumberUtil.getInstance()
-    Try(phoneUtil.parse(s"+$number", defaultCountry)) match {
-      case Success(p) ⇒
+    Try(phoneUtil.parse(s"+$number", defaultCountry))
+      .orElse(Try(phoneUtil.parse(s"$number", defaultCountry)))
+      .toOption map { p ⇒
         val phoneNumber = p.getCountryCode * Math.pow(10L, (sizeOf(p.getNationalNumber) + 1).toDouble).longValue + p.getNationalNumber
-        Some((phoneNumber, phoneUtil.getRegionCodeForCountryCode(p.getCountryCode)))
-      case _ ⇒ None
-    }
+        (phoneNumber, phoneUtil.getRegionCodeForCountryCode(p.getCountryCode))
+      }
   }
 
   def tryNormalize(number: Long, defaultCountry: String = "") = normalizeLong(number, defaultCountry).getOrElse(number)
