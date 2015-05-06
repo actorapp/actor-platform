d94f0d411dbb53e04b1daebba6f126e0c90bb177
rock_jam <5min4eq.unity@gmail.com>
2015-05-06 17:10:33 +0300

    fix(rpc-api): move custom matcher to separate trait; add patienceConfig to dashboard spec

---
+++ actor-tests/src/test/scala/im/actor/server/ServiceSpecMatchers.scala
@@ -0,0 +1,14 @@
+package im.actor.server
+
+import scalaz.{ -\/, \/ }
+
+import org.scalatest.Matchers
+import org.scalatest.matchers.Matcher
+
+import im.actor.api.rpc.RpcError
+
+trait ServiceSpecMatchers extends Matchers {
+  def matchNotAuthorized[T]: Matcher[\/[RpcError, T]] = matchPattern {
+    case -\/(RpcError(403, "USER_NOT_AUTHORIZED", _, _, _)) ⇒
+  }
+}
