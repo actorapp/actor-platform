7192c80bcd75bce9075e51ba968c2138bb88448c
ex3ndr <me@ex3ndr.com>
2015-05-07 17:26:35 +0300

    chore(iOS): Updated build messages

--- apps/actor-ios/build.sh
+++ apps/actor-ios/build.sh
@@ -5,16 +5,16 @@ CERTIFICATE=$3
 PROVISION=$4
 
 # Build without any codesign
-echo "##teamcity[progressStart 'Building']"
+echo "##teamcity[progressStart 'Building $1']"
 xcodebuild -workspace ActorApp.xcworkspace -scheme $1 DEPLOYMENT_LOCATION=yes DSTROOT=build DWARF_DSYM_FOLDER_PATH=build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO clean build
-echo "##teamcity[progressFinish 'Building']"
+echo "##teamcity[progressFinish 'Building $1']"
 
 # Uncomment for building with codesign and stripping signature
 # xcodebuild -workspace ActorApp.xcworkspace -scheme ActorApp DEPLOYMENT_LOCATION=yes DSTROOT=build DWARF_DSYM_FOLDER_PATH=build build
 # Remove old mobileprovision
 # rm build/Applications/ActorClient.app/embedded.mobileprovision
 
-echo "##teamcity[progressStart 'Provision and Signing']"
+echo "##teamcity[progressStart 'Provision and Signing $1']"
 # Adding Provision
 cp BuildConfig/$PROVISION.mobileprovision build/Applications/ActorApp.app/embedded.mobileprovision
 
@@ -23,9 +23,9 @@ cp BuildConfig/$PROVISION.mobileprovision build/Applications/ActorApp.app/embedd
 codesign --force --sign "$3" --entitlements "BuildConfig/$PROVISION.entitlements.plist" build/Applications/ActorApp.app/Frameworks/*
 codesign --force --sign "$3" --entitlements "BuildConfig/$PROVISION.entitlements.plist" build/Applications/ActorApp.app
 # cd ../../
-echo "##teamcity[progressFinish 'Provision and Signing']"
+echo "##teamcity[progressFinish 'Provision and Signing $1']"
 
-echo "##teamcity[progressStart 'Packaging App']"
+echo "##teamcity[progressStart 'Packaging App $1']"
 # Package app
 xcrun -sdk iphoneos PackageApplication -v "$PROJECT_PATH/build/Applications/ActorApp.app" -o "$PROJECT_PATH/build/$2.ipa"
 
@@ -33,4 +33,4 @@ xcrun -sdk iphoneos PackageApplication -v "$PROJECT_PATH/build/Applications/Acto
 cd build
 zip -r $2.dSYM.zip ActorApp.app.dSYM
 cd ..
-echo "##teamcity[progressFinish 'Packaging App']"
+echo "##teamcity[progressFinish 'Packaging App $1']"
