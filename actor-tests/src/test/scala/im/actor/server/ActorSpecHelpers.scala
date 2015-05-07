18add07dbcfba85a6e717ec4002a5ee5f87ddd99
prettynatty <fear@loathing.in>
2015-05-07 21:12:57 +0300

    feat(push): support multiple apns certs

--- actor-tests/src/test/scala/im/actor/server/ActorSpecHelpers.scala
+++ actor-tests/src/test/scala/im/actor/server/ActorSpecHelpers.scala
@@ -2,28 +2,18 @@ package im.actor.server.api
 
 import akka.actor.ActorSystem
 import com.google.android.gcm.server.Sender
-import com.relayrides.pushy.apns.util.{ SSLContextUtil, SimpleApnsPushNotification }
-import com.relayrides.pushy.apns.{ ApnsEnvironment, PushManager, PushManagerConfiguration }
 import slick.driver.PostgresDriver.api._
 
-import im.actor.server.push.{ SeqUpdatesManager, SeqUpdatesManagerRegion }
+import im.actor.server.push.{ ApplePushManager, ApplePushManagerConfig, SeqUpdatesManager, SeqUpdatesManagerRegion }
 
 trait ActorSpecHelpers {
   def buildSeqUpdManagerRegion()(implicit system: ActorSystem, db: Database): SeqUpdatesManagerRegion = {
-    val gcmConfig = system.settings.config.getConfig("push.gcm")
-    val apnsConfig = system.settings.config.getConfig("push.apns")
+    val gcmConfig = system.settings.config.getConfig("push.google")
+    val appleConfig = ApplePushManagerConfig.fromConfig(system.settings.config.getConfig("push.apple"))
 
     implicit val gcmSender = new Sender(gcmConfig.getString("key"))
 
-    implicit val apnsManager = new PushManager[SimpleApnsPushNotification](
-      ApnsEnvironment.getProductionEnvironment,
-      SSLContextUtil.createDefaultSSLContext(apnsConfig.getString("cert.path"), apnsConfig.getString("cert.password")),
-      null,
-      null,
-      null,
-      new PushManagerConfiguration(),
-      "ActorPushManager"
-    )
+    implicit val applePushManager = new ApplePushManager(appleConfig)
 
     SeqUpdatesManager.startRegion()
   }
