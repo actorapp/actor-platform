38b7a32476b9efc52b3c9ecdade1a7b662de9b93
ex3ndr <me@ex3ndr.com>
2015-05-07 23:17:20 +0300

    fix(iOS): Fixed dates for first cell and fixed UISearchBar disappear

--- apps/actor-ios/ActorClient/ContactsViewController.swift
+++ apps/actor-ios/ActorClient/ContactsViewController.swift
@@ -75,15 +75,15 @@ class ContactsViewController: ContactsBaseController, UISearchBarDelegate, UISea
         searchDisplay?.searchResultsTableView.backgroundColor = Resources.BackyardColor
         searchDisplay?.searchResultsTableView.frame = tableView.frame
         
-        var header = AATableViewHeader(frame: CGRectMake(0, 0, 320, 44))
-        header.addSubview(searchView!)
+//        var header = AATableViewHeader(frame: CGRectMake(0, 0, 320, 44))
+//        header.addSubview(searchView!)
+//        
+//        var headerShadow = UIImageView(frame: CGRectMake(0, -4, 320, 4));
+//        headerShadow.image = UIImage(named: "CardTop2");
+//        headerShadow.contentMode = UIViewContentMode.ScaleToFill;
+//        header.addSubview(headerShadow);
         
-        var headerShadow = UIImageView(frame: CGRectMake(0, -4, 320, 4));
-        headerShadow.image = UIImage(named: "CardTop2");
-        headerShadow.contentMode = UIViewContentMode.ScaleToFill;
-        header.addSubview(headerShadow);
-        
-        tableView.tableHeaderView = header
+        tableView.tableHeaderView = searchView
         
         searchSource = ContactsSource(searchDisplay: searchDisplay!)
         
