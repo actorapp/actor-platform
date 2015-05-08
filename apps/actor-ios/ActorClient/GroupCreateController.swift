b93ef1e88d4873ed329d553d30e5986e9d122133
Steve Kite <steve@actor.im>
2015-05-08 22:54:10 +0300

    feat(iOS): Starting to create group creation page

--- apps/actor-ios/ActorClient/GroupCreateController.swift
+++ apps/actor-ios/ActorClient/GroupCreateController.swift
@@ -5,9 +5,10 @@
 import Foundation
 
 class GroupCreateController: UIViewController {
-    private var addPhotoButton: UIButton = UIButton()
-    private var avatarImageView: UIImageView = UIImageView()
-    private var groupName: UITextField = UITextField()
+    private var addPhotoButton = UIButton()
+    private var avatarImageView = UIImageView()
+    private var groupName = UITextField()
+    private var titleLabel = UILabel()
     
     init(){
         super.init(nibName: nil, bundle: nil)
@@ -22,15 +23,40 @@ class GroupCreateController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = MainAppTheme.list.bgColor
-        view.addSubview(avatarImageView)
         view.addSubview(addPhotoButton)
+        view.addSubview(avatarImageView)
         view.addSubview(groupName)
+        view.addSubview(titleLabel)
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 110, height: 110), false, 0.0);
+        var context = UIGraphicsGetCurrentContext();
+        CGContextSetFillColorWithColor(context, UIColor.whiteColor().CGColor);
+        CGContextFillEllipseInRect(context, CGRectMake(0.0, 0.0, 110.0, 110.0));
+        CGContextSetStrokeColorWithColor(context, UIColor.RGB(0xd9d9d9).CGColor);
+        CGContextSetLineWidth(context, 1.0);
+        CGContextStrokeEllipseInRect(context, CGRectMake(0.5, 0.5, 109.0, 109.0));
+        let buttonImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        addPhotoButton.exclusiveTouch = true
+        addPhotoButton.setBackgroundImage(buttonImage, forState: UIControlState.Normal)
+        
+        titleLabel.backgroundColor = UIColor.clearColor()
+        titleLabel.textColor = UIColor.blackColor()
+        titleLabel.textAlignment = NSTextAlignment.Center
+        titleLabel.font = isIPad
+            ? UIFont(name: "HelveticaNeue-Thin", size: 50.0)
+            : UIFont(name: "HelveticaNeue-Light", size: 30.0)
+        titleLabel.text = NSLocalizedString("AuthProfileTitle", comment: "Title")
+
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-        avatarImageView.frame = CGRectMake(10, 10, 64, 64)
+        avatarImageView.frame = CGRectMake(24, 24, 64, 64)
         addPhotoButton.frame = avatarImageView.frame
+        
+        titleLabel.frame = CGRectMake(80, 24, 320, 64)
     }
 }
