fdf17a34f7b3af6fb975e3de487b980774a13c7b
ex3ndr <me@ex3ndr.com>
2015-05-10 23:48:28 +0300

    feat(iOS): Implemented notification settings screen

--- apps/actor-ios/ActorClient/Screens/Settings/AASettingsNotificationsController.swift
+++ apps/actor-ios/ActorClient/Screens/Settings/AASettingsNotificationsController.swift
@@ -9,6 +9,8 @@ class AASettingsNotificationsController: AATableViewController {
     // MARK: -
     // MARK: Constructors
     
+    private let CellIdentifier = "CellIdentifier"
+    
     init() {
         super.init(style:
             UITableViewStyle.Grouped)
@@ -24,11 +26,95 @@ class AASettingsNotificationsController: AATableViewController {
         super.viewDidLoad()
         
         navigationItem.title = "Notifications and Sounds" // TODO: Localize
+        
+        tableView.registerClass(AATableViewCell.self, forCellReuseIdentifier: CellIdentifier)
+        tableView.backgroundColor = MainAppTheme.list.backyardColor
+        tableView.separatorStyle = UITableViewCellSeparatorStyle.None
     }
     
     // MARK: - 
     // MARK: UITableView Data Source
     
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (section == 0) {
+            return 1
+        } else if (section == 1) {
+            return 2
+        }
+        
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if (section == 1) {
+            return "Mobile Notifications"
+        }
+        
+        return nil
+    }
+    
+    private func notificationsAlertCell(indexPath: NSIndexPath) -> AATableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier(CellIdentifier, forIndexPath: indexPath) as! AATableViewCell
+        
+        cell.setContent("Sound")
+        cell.style = AATableViewCellStyle.Switch
+        cell.showBottomSeparator()
+        cell.showTopSeparator()
+        cell.setSwitcherOn(MSG.isNotificationSoundEnabled())
+        cell.switchBlock = { (nValue: Bool) in
+            MSG.changeNotificationSoundEnabledWithBoolean(nValue)
+        }
+        
+        return cell
+    }
+    
+    private func notificationsPreviewCell(indexPath: NSIndexPath) -> AATableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier(CellIdentifier, forIndexPath: indexPath) as! AATableViewCell
+        
+        cell.setContent("Preview")
+        cell.style = AATableViewCellStyle.Switch
+        cell.showBottomSeparator()
+        cell.hideTopSeparator()
+        cell.setSwitcherOn(MSG.isShowNotificationsText())
+        cell.switchBlock = { (nValue: Bool) in
+            MSG.changeShowNotificationTextEnabledWithBoolean(nValue)
+        }
+        
+        return cell
+    }
+    
+    private func notificationsTonesCell(indexPath: NSIndexPath) -> AATableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier(CellIdentifier, forIndexPath: indexPath) as! AATableViewCell
+        
+        cell.setContent("Sound Effects")
+        cell.style = AATableViewCellStyle.Switch
+        cell.showBottomSeparator()
+        cell.showTopSeparator()
+        cell.setSwitcherOn(MSG.isConversationTonesEnabled())
+        cell.switchBlock = { (nValue: Bool) in
+            MSG.changeConversationTonesEnabledWithBoolean(nValue)
+        }
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            return notificationsTonesCell(indexPath)
+        } else if (indexPath.section == 1) {
+            if (indexPath.row == 0) {
+                return notificationsAlertCell(indexPath)
+            } else if (indexPath.row == 1) {
+                return notificationsPreviewCell(indexPath)
+            }
+        }
+        return UITableViewCell()
+    }
+
     // MARK: -
     // MARK: UITableView Delegate
 
