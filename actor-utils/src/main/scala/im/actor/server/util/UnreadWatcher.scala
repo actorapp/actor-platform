910113d4be7c8134b14df7927e78f0d37f96eccd
rock_jam <5min4eq.unity@gmail.com>
2015-05-14 15:40:17 +0300

    test(sms-notifications): write spec for UnreadWatcher

--- actor-utils/src/main/scala/im/actor/server/util/UnreadWatcher.scala
+++ actor-utils/src/main/scala/im/actor/server/util/UnreadWatcher.scala
@@ -2,6 +2,7 @@ package im.actor.server.util
 
 import java.util.concurrent.TimeUnit
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
 import com.typesafe.config.Config
@@ -12,7 +13,6 @@ import slick.driver.PostgresDriver.api._
 import im.actor.server.models.PeerType
 import im.actor.server.sms.SmsEngine
 import im.actor.server.{ models, persist }
-import scala.concurrent.ExecutionContext.Implicits.global
 
 case class UnreadWatcherConfig(unreadTimeout: Duration)
 
@@ -22,13 +22,13 @@ object UnreadWatcherConfig {
 }
 
 trait Notifier {
-  def processTask(task: Notication): Unit
+  def processTask(task: Notification): Unit
 }
 
 class PhoneNotifier(engine: SmsEngine) extends Notifier {
-  def processTask(task: Notication) = {
+  def processTask(task: Notification) = {
     val total = task.data.values.sum
-    val senders = task.data.keySet.filter(_.isDefined).map(_.get) mkString " ,"
+    val senders = task.data.keySet.flatten mkString " ,"
     val message = s"You got $total messages from $senders."
     for {
       optPhone ← persist.UserPhone.findByUserId(task.userId).headOption
@@ -37,28 +37,32 @@ class PhoneNotifier(engine: SmsEngine) extends Notifier {
   }
 }
 
-case class Notication(userId: Int, data: Map[Option[String], Int])
+case class Notification(userId: Int, data: Map[Option[String], Int])
 
 class UnreadWatcher(implicit db: Database, config: UnreadWatcherConfig, notifier: Notifier) {
 
   val unreadTimeout = config.unreadTimeout.toMillis
 
   def notifyUsers() = {
+    getNotifications.map { tasks ⇒
+      tasks.foreach(notifier.processTask)
+    }
+  }
+
+  def getNotifications = {
+    val now = DateTime.now
     db.run {
       for {
         users ← persist.User.activeUsersIds
         tasks ← DBIO.sequence(users.map { userId ⇒
-          for (data ← findUnread(userId)) yield Notication(userId, data.toMap)
+          for (data ← findUnread(userId, now)) yield if (data.isEmpty) None else Some(Notification(userId, data.toMap))
         })
-      } yield tasks
-    }.map { tasks ⇒
-      tasks.foreach(notifier.processTask)
+      } yield tasks.flatten
     }
-    ()
   }
 
-  private def findUnread(userId: Int) = {
-    val dateToReadBefore = DateTime.now.minus(unreadTimeout)
+  private def findUnread(userId: Int, now: DateTime) = {
+    val dateToReadBefore = now.minus(unreadTimeout)
     for {
       dialogs ← persist.Dialog.findLastReadBefore(dateToReadBefore, userId)
       senderAndCount ← DBIO.sequence(dialogs.map { dialog ⇒
@@ -66,10 +70,9 @@ class UnreadWatcher(implicit db: Database, config: UnreadWatcherConfig, notifier
           exists ← persist.HistoryMessage.haveMessagesBetween(userId, dialog.peer, dialog.lastReadAt, dateToReadBefore)
           unreadCount ← persist.HistoryMessage.getUnreadCount(userId, dialog.peer, dialog.lastReadAt)
           senderName ← getNameByPeer(dialog.peer)
-          if exists
-        } yield senderName → unreadCount
+        } yield if (exists) Some(senderName → unreadCount) else None
       })
-    } yield senderAndCount
+    } yield senderAndCount.flatten
   }
 
   private def getNameByPeer(peer: models.Peer) =
