3c929d72635d58a78731c66bd2e612cab8b111fe
John Doe <speed@actor.im>
2015-05-15 18:52:53 +0300

    feat(web): typing in react component

--- apps/actor-web/app/shared/messages/messages.jsx
+++ apps/actor-web/app/shared/messages/messages.jsx
@@ -30,26 +30,41 @@ var ChatMessage = React.createClass({
   }
 });
 
+var div = React.createFactory('div');
+
 angular
   .module('actorWeb')
   .factory('Messages', ['$filter', function($filter) {
   return React.createClass({
     propTypes: {
-      peer: React.PropTypes.object.isRequired,
-      messages: React.PropTypes.array.isRequired
+      peer: React.PropTypes.object,
+      messages: React.PropTypes.array.isRequired,
+      typing: React.PropTypes.object.isRequired
     },
     render: function() {
       var peer = this.props.peer;
 
+      var typing = null;
+
+      if (this.props.typing !== null) {
+        typing = <div class="messages-list__typing">
+          <p>
+            <img src="assets/img/icons/ic_keyboard_control_24px.svg"></img>
+            {this.props.typing}
+          </p>
+        </div>
+      }
+
       return (
         <div>
           {
-            this.props.messages.map(function(message) {
+            this.props.messages.map(function (message) {
               return (
                 <ChatMessage key={message.sortKey} peer={peer} message={message}/>
               );
             })
           }
+          {typing}
         </div>
       );
     }
