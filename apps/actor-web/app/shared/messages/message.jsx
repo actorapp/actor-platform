872e445175bef50498354e3a6ac93ae0347a2f91
John Doe <speed@actor.im>
2015-05-18 22:47:41 +0300

    fix(web): re-render message state on state change

--- apps/actor-web/app/shared/messages/message.jsx
+++ apps/actor-web/app/shared/messages/message.jsx
@@ -4,6 +4,7 @@ var PureRenderMixin = require('react/addons').addons.PureRenderMixin;
 var hljs = require('highlight.js');
 var marked = require('marked');
 var memoize = require('memoizee');
+var Isvg = require('react-inlinesvg');
 var memoizedMarked = memoize(marked, {length: 1, maxAge: 60 * 60 * 1000, max: 1000}); // 1h expire, max 1000 elements
 
 var Message = React.createClass({
@@ -117,29 +118,39 @@ Message.State = React.createClass({
 
       switch(message.state) {
         case 'pending':
-          icon = <img src="assets/img/icons/ic_access_time_24px.svg"
-                      className="status status--penging"/>;
+          //icon = <img src="assets/img/icons/ic_access_time_24px.svg"
+          //            className="status status--penging"/>;
+          icon = <Isvg className="status status--penging"
+                       src="assets/img/icons/ic_access_time_24px.svg"></Isvg>
           break;
         case 'sent':
-          icon = <img src="assets/img/icons/ic_done_24px.svg"
-                      className="status status--sent"/>;
+          //icon = <img src="assets/img/icons/ic_done_24px.svg"
+          //            className="status status--sent"/>;
+          icon = <Isvg className="status status--sent"
+                       src="assets/img/icons/ic_done_24px.svg"></Isvg>
           break;
         case 'received':
-          icon = <img src="assets/img/icons/ic_done_all_24px.svg"
-                      className="status status--received"/>;
+          //icon = <img src="assets/img/icons/ic_done_all_24px.svg"
+          //            className="status status--received"/>;
+          icon = <Isvg className="status status--received"
+                       src="assets/img/icons/ic_done_all_24px.svg"></Isvg>
           break;
         case 'read':
-          icon = <img src="assets/img/icons/ic_done_all_24px.svg"
-                      className="status status--read"/>;
+          //icon = <img src="assets/img/icons/ic_done_all_24px.svg"
+          //            className="status status--read"/>;
+          icon = <Isvg className="status status--read"
+                       src="assets/img/icons/ic_done_all_24px.svg"></Isvg>
           break;
         case 'error':
-          icon = <img src="assets/img/icons/ic_report_problem_24px.svg"
-                      className="status status--error"/>;
+          //icon = <img src="assets/img/icons/ic_report_problem_24px.svg"
+          //            className="status status--error"/>;
+          icon = <Isvg className="status status--error"
+                       src="assets/img/icons/ic_report_problem_24px.svg"></Isvg>
           break;
       }
 
       return (
-        <h3 layout="row" className="message__body__title row">
+        <h3 key={message.state} layout="row" className="message__body__title row">
           <span className="col-xs">{message.sender.title}</span>
 
           <div className="message__status">{icon}</div>
