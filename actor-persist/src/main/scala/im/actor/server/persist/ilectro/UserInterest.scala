9dba3393495c165c630bca3f9c4cdd47f2c43197
rock_jam <5min4eq.unity@gmail.com>
2015-05-21 15:10:23 +0300

    feat(ilectro): move persist to ilectro package, add ilectro user, migration

--- actor-persist/src/main/scala/im/actor/server/persist/ilectro/UserInterest.scala
+++ actor-persist/src/main/scala/im/actor/server/persist/ilectro/UserInterest.scala
@@ -1,14 +1,14 @@
-package im.actor.server.persist
+package im.actor.server.persist.ilectro
 
 import slick.driver.PostgresDriver.api._
 
 import im.actor.server.models
 
-class UserInterestTable(tag: Tag) extends Table[models.UserInterest](tag, "users_interests") {
+class UserInterestTable(tag: Tag) extends Table[models.ilectro.UserInterest](tag, "users_interests") {
   def userId = column[Int]("user_id", O.PrimaryKey)
   def interestId = column[Int]("interest_id", O.PrimaryKey)
 
-  def * = (userId, interestId) <> (models.UserInterest.tupled, models.UserInterest.unapply)
+  def * = (userId, interestId) <> (models.ilectro.UserInterest.tupled, models.ilectro.UserInterest.unapply)
 }
 
 object UserInterest {
