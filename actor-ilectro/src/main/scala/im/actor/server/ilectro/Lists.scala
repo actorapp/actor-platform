f87ae62cd19205f1497e2a940886a596b5511b32
John Doe <speed@actor.im>
2015-05-21 15:58:27 +0300

    fix(ilectro): use play-json instead of upickle, some fixes

--- actor-ilectro/src/main/scala/im/actor/server/ilectro/Lists.scala
+++ actor-ilectro/src/main/scala/im/actor/server/ilectro/Lists.scala
@@ -1,6 +1,5 @@
 package im.actor.server.ilectro
 
-import scala.collection.immutable.SortedMap
 import scala.concurrent._
 import scala.concurrent.duration._
 
@@ -9,10 +8,11 @@ import akka.http.scaladsl.HttpExt
 import akka.http.scaladsl.model.HttpMethods.GET
 import akka.http.scaladsl.model._
 import akka.stream.ActorFlowMaterializer
-import upickle._
+import play.api.libs.json._
 
 import im.actor.server.ilectro.Common._
 import im.actor.server.ilectro.results._
+import im.actor.server.models.ilectro.Interest
 
 private[ilectro] class Lists(implicit
   system: ActorSystem,
@@ -20,6 +20,8 @@ private[ilectro] class Lists(implicit
                              materializer:     ActorFlowMaterializer,
                              http:             HttpExt,
                              config:           ILectroConfig) {
+
+  import JsonFormatters._
 
   private implicit val authToken = config.authToken
   private val baseUrl = config.baseUrl
@@ -31,47 +33,13 @@ private[ilectro] class Lists(implicit
       method = GET,
       uri = s"$baseUrl/$resourceName"
     ),
-    success = (entity) ⇒
+    onSuccess = (entity) ⇒
       entity.toStrict(5.seconds).map { e ⇒
-        val body = e.data.decodeString("utf-8")
-        Right(read[List[Interest]](body))
-      },
-    failure = defaultFailure
-  )
-
-  def nestInterestsViaMap(list: List[Interest]): List[ReadyInterest] = {
-    val map = list.groupBy(_.level).map { case (level, sublist) ⇒ level → sublist.groupBy(_.parentId) }
-    val sorted = SortedMap(map.toArray: _*)
-    sorted.foldLeft(List.empty[ReadyInterest]) {
-      case (acc, (level, parentIdToInterests)) ⇒
-        acc match {
-          //only one level of nesting
-          case h :: t ⇒ acc.map(ri ⇒ parentIdToInterests.get(ri.interest.id).map(interests ⇒ ri.copy(children = interests.map(ReadyInterest(_)))).getOrElse(ri))
-          case Nil    ⇒ parentIdToInterests.toList.flatMap { case (_, interests) ⇒ interests.map(ReadyInterest(_)) }
+        Json.parse(e.data.decodeString("utf-8")).validate[List[Interest]] match {
+          case JsSuccess(i, _) ⇒ Right(i)
+          case JsError(e)      ⇒ throw new Exception(s"Failed to parse interest ${e}")
         }
-    }
-  }
-
-  def nestInterests(list: Seq[Interest]): List[ReadyInterest] = {
-    def run(from: List[Interest], acc: List[ReadyInterest]): List[ReadyInterest] = {
-      from match {
-        case h :: t ⇒
-          println(s"parent id ${h.parentId}, id: ${h.id}, level: ${h.level}")
-          if (acc.forall(_.interest.level == h.level)) {
-            run(t, acc :+ ReadyInterest(h))
-          } else {
-            acc.zipWithIndex.find(_._1.interest.id == h.parentId) match {
-              case Some((parent, index)) ⇒
-                run(t, acc.updated(index, parent.copy(children = parent.children :+ ReadyInterest(h))))
-              case None ⇒
-                //problem is here - rewrites existing accumulator, only last level lefts
-                run(t, acc.flatMap(e ⇒ run(List(h), e.children)))
-            }
-          }
-        case _ ⇒ acc
-      }
-    }
-    run(list.toList.sortBy(_.level), List())
-  }
-
+      },
+    onFailure = defaultFailure
+  )
 }
--- actor-ilectro/src/main/scala/im/actor/server/ilectro/results/Results.scala
+++ actor-ilectro/src/main/scala/im/actor/server/ilectro/results/Results.scala
@@ -1,21 +1,5 @@
 package im.actor.server.ilectro.results
 
-import java.util.UUID
-
-import upickle.key
-
-case class User(uuid: UUID, name: String)
-
-case class Interest(
-  name:                       String,
-  id:                         Int,
-  @key("parent_id") parentId: Int,
-  @key("full_path") fullPath: String,
-  level:                      Int
-)
-
 case class Banner(advertUrl: String, imageUrl: String)
 
 case class Errors(errors: String, status: Option[Int] = None)
-
-case class ReadyInterest(interest: Interest, children: List[ReadyInterest] = List())
