ca9a0ddad0bab620c678fd3c48134ffeb0e73621
prettynatty <fear@loathing.in>
2015-05-21 19:08:58 +0300

    refactor(ilectro): change users uuid from string to UUID

--- actor-persist/src/main/scala/im/actor/server/persist/ilectro/IlectroUser.scala
+++ actor-persist/src/main/scala/im/actor/server/persist/ilectro/IlectroUser.scala
@@ -1,12 +1,14 @@
 package im.actor.server.persist.ilectro
 
+import java.util.UUID
+
 import slick.driver.PostgresDriver.api._
 
 import im.actor.server.models
 
 class ILectroUserTable(tag: Tag) extends Table[models.ilectro.ILectroUser](tag, "ilectro_users") {
   def userId = column[Int]("user_id", O.PrimaryKey)
-  def uuid = column[String]("uuid")
+  def uuid = column[UUID]("uuid")
   def name = column[String]("name")
 
   def * = (userId, uuid, name) <> (models.ilectro.ILectroUser.tupled, models.ilectro.ILectroUser.unapply)
