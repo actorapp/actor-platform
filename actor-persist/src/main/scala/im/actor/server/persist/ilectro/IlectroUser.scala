68d04bff116aed91f3312b98d2b107baf49c4cbe
rock_jam <5min4eq.unity@gmail.com>
2015-05-21 15:10:23 +0300

    feat(ilectro): move persist to ilectro package, add ilectro user, migration

---
+++ actor-persist/src/main/scala/im/actor/server/persist/ilectro/IlectroUser.scala
@@ -0,0 +1,16 @@
+package im.actor.server.persist.ilectro
+
+import slick.driver.PostgresDriver.api._
+
+import im.actor.server.models
+
+class IlectroUserTable(tag: Tag) extends Table[models.ilectro.IlectroUser](tag, "ilectro_users") {
+  def userId = column[Int]("user_id", O.PrimaryKey)
+  def uuid = column[Int]("uuid")
+
+  def * = (userId, uuid) <>(models.ilectro.IlectroUser.tupled, models.ilectro.IlectroUser.unapply)
+}
+
+object IlectroUser {
+  val illectroUsers = TableQuery[IlectroUserTable]
+}
