97aabeaf79bdd09905293410dfaf42c0aae5011c
John Doe <speed@actor.im>
2015-05-22 19:41:26 +0300

    chore(*): removed 81port resolver

--- project/Resolvers.scala
+++ project/Resolvers.scala
@@ -12,8 +12,6 @@ object Resolvers {
     Resolver.bintrayRepo("dwhjames", "maven"),
     Resolver.bintrayRepo("krasserm", "maven"),
     "eaio" at "http://eaio.com/maven2",
-    "actor snapshots" at "http://repos.81port.com/nexus/content/repositories/snapshots",
-    "actor releases" at "http://repos.81port.com/nexus/content/repositories/releases",
     "gcm-server-repository" at "https://raw.githubusercontent.com/slorber/gcm-server-repository/master/releases/"
   )
 }
