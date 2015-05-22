e0e1d117ee82786557dc942efaa1119950430c2e
John Doe <speed@actor.im>
2015-05-22 22:05:06 +0300

    feat(web): update AvatarItem component;

--- apps/actor-web/js/components/sidebar/HeaderSection.react.js
+++ apps/actor-web/js/components/sidebar/HeaderSection.react.js
@@ -28,9 +28,7 @@ var HeaderSection = React.createClass({
     return (
       <header className={headerClass}>
         <div className="sidebar__header__user row" onClick={this._toggleHeaderMenu}>
-          <div className="avatar avatar--small">
-            <AvatarItem title={user.name} image={user.avatar} placeholder={user.placeholder}/>
-          </div>
+          <AvatarItem title={user.name} image={user.avatar} placeholder={user.placeholder} size="small"/>
           <span className="sidebar__header__user__name">{user.name}</span>
           <span className="col-xs"></span>
           <img className="sidebar__header__user__expand" src="assets/img/icons/png/ic_expand_more_2x_white.png" alt=""/>
