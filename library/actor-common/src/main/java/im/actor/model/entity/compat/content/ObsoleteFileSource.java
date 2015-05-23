e371d366a44b76508f4fd0adf326ba7bb1921aaf
ex3ndr <me@ex3ndr.com>
2015-05-24 02:10:40 +0300

    wip(common): Compat serialization

---
+++ library/actor-common/src/main/java/im/actor/model/entity/compat/content/ObsoleteFileSource.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2015 Actor LLC. <https://actor.im>
+ */
+
+package im.actor.model.entity.compat.content;
+
+/**
+ * Created by ex3ndr on 24.05.15.
+ */
+public class ObsoleteFileSource {
+}
---
+++ library/actor-common/src/main/java/im/actor/model/entity/compat/content/ObsoleteRemoteFileSource.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2015 Actor LLC. <https://actor.im>
+ */
+
+package im.actor.model.entity.compat.content;
+
+public class ObsoleteRemoteFileSource {
+}
