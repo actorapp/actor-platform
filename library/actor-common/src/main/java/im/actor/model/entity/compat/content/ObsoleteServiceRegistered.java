d5231e29776e6dcd6db2aeaa138e31f0095803b8
ex3ndr <me@ex3ndr.com>
2015-05-24 02:10:40 +0300

    wip(common): Compat serialization

--- library/actor-common/src/main/java/im/actor/model/entity/compat/content/ObsoleteServiceRegistered.java
+++ library/actor-common/src/main/java/im/actor/model/entity/compat/content/ObsoleteServiceRegistered.java
@@ -6,31 +6,28 @@ package im.actor.model.entity.compat.content;
 
 import java.io.IOException;
 
+import im.actor.model.api.Message;
+import im.actor.model.api.ServiceExContactRegistered;
+import im.actor.model.api.ServiceMessage;
 import im.actor.model.droidkit.bser.BserValues;
 import im.actor.model.droidkit.bser.BserWriter;
 
 /**
  * Created by ex3ndr on 24.05.15.
  */
-public class ObsoleteTextAbsContent extends ObsoleteAbsContent {
-
-    private String text;
-
-    public ObsoleteTextAbsContent(BserValues values) throws IOException {
-        parse(values);
-    }
-
-    public String getText() {
-        return text;
+public class ObsoleteServiceRegistered extends ObsoleteAbsContent {
+    @Override
+    public Message toApiMessage() {
+        return new ServiceMessage("User registered", new ServiceExContactRegistered(/*???*/));
     }
 
     @Override
     public void parse(BserValues values) throws IOException {
-        text = values.getString(2);
+
     }
 
     @Override
     public void serialize(BserWriter writer) throws IOException {
-        throw new UnsupportedOperationException();
+
     }
 }
