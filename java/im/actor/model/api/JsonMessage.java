20df740a2c5af983c4a4f4a9bb304f17a36093b4
ex3ndr <me@ex3ndr.com>
2015-05-25 02:42:42 +0300

    fix(codegen): Fixed unsupported

--- java/im/actor/model/api/JsonMessage.java
+++ java/im/actor/model/api/JsonMessage.java
@@ -67,7 +67,6 @@ public class JsonMessage extends Message {
     @Override
     public String toString() {
         String res = "struct JsonMessage{";
-        res += "rawJson=" + this.rawJson;
         res += "}";
         return res;
     }
