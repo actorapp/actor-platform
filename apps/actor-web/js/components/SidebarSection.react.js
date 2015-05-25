5c27e8961397d89f824ec5f76dda1a787b9544a6
Oleg Shilov <me@olegshilov.com>
2015-05-25 21:50:39 +0300

    feat(web): show current peer in toolbar;

--- apps/actor-web/js/components/SidebarSection.react.js
+++ apps/actor-web/js/components/SidebarSection.react.js
@@ -13,35 +13,6 @@ var SidebarSection = React.createClass({
     return (
       <aside className="sidebar">
         <HeaderSection messenger={messenger}/>
-        <ul className="sidebar__list">
-          <li className="sidebar__list__title">
-            Starred
-          </li>
-          <li className="sidebar__list__item">
-            <div className="avatar avatar--tiny">
-              <span className="avatar__placeholder avatar__placeholder--yellow">D</span>
-            </div>
-        <span>
-          Starred
-        </span>
-          </li>
-          <li className="sidebar__list__item sidebar__list__item--active">
-            <div className="avatar avatar--tiny">
-              <span className="avatar__placeholder avatar__placeholder--purple">H</span>
-            </div>
-        <span>
-          Starred conversation 2
-        </span>
-          </li>
-          <li className="sidebar__list__item">
-            <div className="avatar avatar--tiny">
-              <span className="avatar__placeholder avatar__placeholder--green">G</span>
-            </div>
-        <span>
-          Starred conversation 3
-        </span>
-          </li>
-        </ul>
         <RecentSection messenger={messenger}/>
       </aside>
     )
