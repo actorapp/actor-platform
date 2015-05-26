c9c67821a9f02617f111aaab7c5520680794fff5
Oleg Shilov <me@olegshilov.com>
2015-05-26 21:10:57 +0300

    feat(web): messages with styles;

--- apps/actor-web/js/components/DialogSection.react.js
+++ apps/actor-web/js/components/DialogSection.react.js
@@ -5,24 +5,9 @@ var MessagesSection = require('./dialog/MessagesSection.react');
 var DialogSection = React.createClass({
   render: function() {
     return(
-      <div>
-      <MessagesSection></MessagesSection>
-      <section className="compose">
-        <textarea className="compose__message"></textarea>
-        <footer className="compose__footer row">
-          <button className="button">
-            <img src="assets/img/icons/ic_attachment_24px.svg" alt=""/> Send file
-          </button>
-            <button className="button">
-              <img src="assets/img/icons/ic_photo_camera_24px.svg" alt=""/> Send photo
-            </button>
-          <span className="col-xs"></span>
-          <button className="button button--primary">
-            Send
-          </button>
-        </footer>
+      <section className="messages">
+        <MessagesSection></MessagesSection>
       </section>
-      </div>
     )
   }
 });
