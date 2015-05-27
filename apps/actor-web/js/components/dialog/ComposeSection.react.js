084d3c2fa0a717fd6494704eac6a842ac63d6a66
prettynatty <fear@loathing.in>
2015-05-27 22:09:19 +0300

    fix(web): fix scroll

--- apps/actor-web/js/components/dialog/ComposeSection.react.js
+++ apps/actor-web/js/components/dialog/ComposeSection.react.js
@@ -1,10 +1,13 @@
 var React = require('react');
+var PureRenderMixin = require('react/addons').addons.PureRenderMixin;
 
 var MessageActionCreators = require('../../actions/MessageActionCreators');
 
 var ENTER_KEY_CODE = 13;
 
 var ComposeSection = React.createClass({
+  mixins: [PureRenderMixin],
+
   propTypes: {
     dialog: React.PropTypes.object.isRequired
   },
--- apps/actor-web/js/components/dialog/MessagesSection.react.js
+++ apps/actor-web/js/components/dialog/MessagesSection.react.js
@@ -1,28 +1,33 @@
 var React = require('react');
+var PureRenderMixin = require('react/addons').addons.PureRenderMixin;
+
 var _ = require('lodash');
 
 var MessageItem = require('../common/MessageItem.react');
 
-var getMessagesListItem = function (message) {
-  return (
-    <MessageItem message={message}/>
-  );
-};
-
 var MessagesSection = React.createClass({
+  mixins: [PureRenderMixin],
+
   propTypes: {
     messages: React.PropTypes.array.isRequired
   },
 
   render: function() {
-    var messages = _.map(this.props.messages, getMessagesListItem);
+    var messages = _.map(this.props.messages, this._getMessagesListItem);
 
     return(
       <ul className="messages">
         {messages}
       </ul>
     )
+  },
+
+  _getMessagesListItem: function (message, index) {
+    return (
+      <MessageItem key={index} message={message}/>
+    );
   }
+
 });
 
 module.exports = MessagesSection;
