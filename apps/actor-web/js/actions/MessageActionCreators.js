ee5eef57a7e3efd3afe9921110f9ec4418103b0f
John Doe <speed@actor.im>
2015-05-28 19:26:39 +0300

    feat(web): send file and photo messages;

--- apps/actor-web/js/actions/MessageActionCreators.js
+++ apps/actor-web/js/actions/MessageActionCreators.js
@@ -6,13 +6,34 @@ var ActorAppConstants = require('../constants/ActorAppConstants');
 var ActionTypes = ActorAppConstants.ActionTypes;
 
 module.exports = {
-  sendMessageText: function(dialog, text) {
-    ActorClient.sendMessageText(dialog.peer.peer, text);
+  sendTextMessage: function(dialog, text) {
+    ActorClient.sendTextMessage(dialog.peer.peer, text);
 
     ActorAppDispatcher.dispatch({
       type: ActionTypes.SEND_MESSAGE_TEXT,
       dialog: dialog,
       text: text
     });
+  },
+
+  sendFileMessage: function(dialog, file) {
+    ActorClient.sendFileMessage(dialog.peer.peer, file);
+
+    ActorAppDispatcher.dispatch({
+      type: ActionTypes.SEND_MESSAGE_FILE,
+      dialog: dialog,
+      file: file
+    });
+  },
+
+  sendPhotoMessage: function(dialog, photo) {
+    ActorClient.sendPhotoMessage(dialog.peer.peer, photo);
+
+    ActorAppDispatcher.dispatch({
+      type: ActionTypes.SEND_MESSAGE_PHOTO,
+      dialog: dialog,
+      file: photo
+    });
   }
+
 };
