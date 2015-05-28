c1ebce041509a3028ce70966ed19880f8b82af11
John Doe <speed@actor.im>
2015-05-29 02:40:47 +0300

    feat(web): disable fields in LoginSection when needed

--- apps/actor-web/js/components/LoginSection.react.js
+++ apps/actor-web/js/components/LoginSection.react.js
@@ -1,15 +1,37 @@
+var assign = require('object-assign');
+
 var React = require('react');
 
 var LoginActionCreators = require('../actions/LoginActionCreators');
 var LoginStore = require('../stores/LoginStore');
 
+var getStateFromStores = function() {
+  return({
+    smsRequested: LoginStore.isSmsRequested(),
+    phone: '',
+
+    codeSent: false,
+    code: '',
+
+    name: ''
+  })
+};
+
 var LoginSection = React.createClass({
   getInitialState: function() {
-    return({
+    return(assign({
       phone: '',
       code: '',
       name: ''
-    });
+    }, getStateFromStores()))
+  },
+
+  componentWillMount: function() {
+    LoginStore.addChangeListener(this._onChange);
+  },
+
+  componentWillUnmount: function() {
+    LoginStore.removeChangeListener(this._onChange);
   },
 
   render: function() {
@@ -18,11 +40,15 @@ var LoginSection = React.createClass({
         <h2>Sign in to Actor messenger</h2>
         <p>Please enter your full <strong>phone</strong> number to receive <strong>authorization code</strong>.</p>
         <form className="login__form login__form--request" onSubmit={this._onRequestSms}>
-          <input type="phone" name="phone" placeholder="Phone number" onChange={this._onPhoneChange}/>
+          <input type="phone" name="phone" placeholder="Phone number"
+                 onChange={this._onPhoneChange}
+                 disabled={this.state.smsRequested}/>
           <button className="button button--primary button--wide">Request code</button>
         </form>
         <form className="login__form login__form--check" onSubmit={this._onSendCode}>
-          <input type="number" name="code" placeholder="Auth code" onChange={this._onCodeChange}/>
+          <input type="number" name="code" placeholder="Auth code"
+                 onChange={this._onCodeChange}
+                 disabled={!this.state.smsRequested || this.state.codeSent}/>
           <button className="button button--primary button--wide">Validate code</button>
           <a href="#">Wrong number?</a>
         </form>
@@ -32,6 +58,10 @@ var LoginSection = React.createClass({
         </form>
       </div>
     );
+  },
+
+  _onChange: function() {
+    this.setState(getStateFromStores());
   },
 
   _onPhoneChange: function(event) {
@@ -44,16 +74,12 @@ var LoginSection = React.createClass({
 
   _onRequestSms: function(event) {
     event.preventDefault();
-    LoginActionCreators.requestSms(this.state.phone, function() {
-      console.log("Sms requested");
-    });
+    LoginActionCreators.requestSms(this.state.phone);
   },
 
   _onSendCode: function(event) {
     event.preventDefault();
-    LoginActionCreators.sendCode(this.state.code, function() {
-      LoginActionCreators.setLoggedIn();
-    });
+    LoginActionCreators.sendCode(this.state.code);
   }
 });
 
