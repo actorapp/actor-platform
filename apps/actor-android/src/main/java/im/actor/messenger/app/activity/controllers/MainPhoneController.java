44a25ad072f3d56d26c4ef5c0777dde2cde22aa0
Gleb Putintsev <kor_ka@actor.im>
2015-05-28 15:09:19 +0300

    fix(android) clear search crash workaround

--- apps/actor-android/src/main/java/im/actor/messenger/app/activity/controllers/MainPhoneController.java
+++ apps/actor-android/src/main/java/im/actor/messenger/app/activity/controllers/MainPhoneController.java
@@ -338,10 +338,11 @@ public class MainPhoneController extends MainBaseController {
             public boolean onQueryTextChange(String s) {
                 if (isSearchVisible) {
                     if (s.trim().length() > 0) {
-                        searchDisplay.initSearch(s, false);
+                        searchDisplay.initSearch(s.trim().toLowerCase(), false);
                         searchAdapter.setQuery(s.trim().toLowerCase());
                     } else {
-                        searchDisplay.initEmpty();
+                        searchDisplay.initSearch("search clear crash workaround", false);
+                        //searchDisplay.initEmpty();
                     }
                 }
                 return false;
