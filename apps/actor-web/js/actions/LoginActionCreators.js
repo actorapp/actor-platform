28e0e8cb12db3922a1db59152acc04f3d9e92d35
John Doe <speed@actor.im>
2015-05-29 01:36:29 +0300

    refactor(web): login fluxy way

--- apps/actor-web/js/actions/LoginActionCreators.js
+++ apps/actor-web/js/actions/LoginActionCreators.js
@@ -1,21 +1,17 @@
+var ActorClient = require('../utils/ActorClient');
+
 var ActorAppDispatcher = require('../dispatcher/ActorAppDispatcher');
 var ActorAppConstants = require('../constants/ActorAppConstants');
 
 var ActionTypes = ActorAppConstants.ActionTypes;
 
 module.exports = {
-  requestSms: function(phone) {
-    ActorAppDispatcher.dispatch({
-      type: ActionTypes.REQUEST_SMS,
-      phone: phone
-    });
+  requestSms: function(phone, callback) {
+    ActorClient.requestSms(phone, callback);
   },
 
-  sendCode: function(code) {
-    ActorAppDispatcher.dispatch({
-      type: ActionTypes.SEND_CODE,
-      code: code
-    });
+  sendCode: function(code, callback) {
+    ActorClient.sendCode(code, callback);
   },
 
   setLoggedIn: function() {
