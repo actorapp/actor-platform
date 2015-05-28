6a776fa8a7873d816f7927a63ac5d974583acf9d
Oleg Shilov <me@olegshilov.com>
2015-05-28 19:26:39 +0300

    feat(web): send file and photo messages;

--- apps/actor-web/js/components/dialog/ComposeSection.react.js
+++ apps/actor-web/js/components/dialog/ComposeSection.react.js
@@ -23,17 +23,24 @@ var ComposeSection = React.createClass({
       <section className="compose">
         <textarea className="compose__message" value={this.state.text} onChange={this._onChange} onKeyDown={this._onKeyDown}></textarea>
         <footer className="compose__footer row">
-          <button className="button">
+          <button className="button" onClick={this._onSendFileClick}>
             <img src="assets/img/icons/ic_attachment_24px.svg" alt=""/> Send file
           </button>
-          <button className="button">
+          <button className="button" onClick={this._onSendPhotoClick}>
             <img src="assets/img/icons/ic_photo_camera_24px.svg" alt=""/> Send photo
           </button>
           <span className="col-xs"></span>
-          <button className="button button--primary">
-            Send
-          </button>
+          <button className="button button--primary">Send</button>
         </footer>
+
+        <div className="compose__hidden">
+          <input type="file"
+                 id="composeFileInput"
+                 onChange={this._onFileInputChange}/>
+          <input type="file"
+                 id="composePhotoInput"
+                 onChange={this._onPhotoInputChange}/>
+        </div>
       </section>
     )
   },
@@ -47,10 +54,31 @@ var ComposeSection = React.createClass({
       event.preventDefault();
       var text = this.state.text;
       if (text) {
-        MessageActionCreators.sendMessageText(this.props.dialog, text);
+        MessageActionCreators.sendTextMessage(this.props.dialog, text);
       }
       this.setState({text: ''});
     }
+  },
+
+  _onSendFileClick: function() {
+    var fileInput = document.getElementById('composeFileInput');
+    fileInput.click();
+  },
+
+  _onSendPhotoClick: function() {
+    var photoInput = document.getElementById('composePhotoInput');
+    photoInput.accept = "image/*";
+    photoInput.click();
+  },
+
+  _onFileInputChange: function() {
+    var files = document.getElementById('composeFileInput').files;
+    MessageActionCreators.sendFileMessage(this.props.dialog, files[0]);
+  },
+
+  _onPhotoInputChange: function() {
+    var photos = document.getElementById('composePhotoInput').files;
+    MessageActionCreators.sendPhotoMessage(this.props.dialog, photos[0]);
   }
 });
 
