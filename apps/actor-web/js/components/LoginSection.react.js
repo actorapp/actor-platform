5012a47514fa21d33c830784ad679f1507d1325a
Oleg Shilov <me@olegshilov.com>
2015-05-29 19:17:28 +0300

    feat(web): update login forms

--- apps/actor-web/js/components/LoginSection.react.js
+++ apps/actor-web/js/components/LoginSection.react.js
@@ -1,7 +1,6 @@
 var assign = require('object-assign');
-
+var classNames = require('classnames');
 var React = require('react');
-
 var LoginActionCreators = require('../actions/LoginActionCreators');
 var LoginStore = require('../stores/LoginStore');
 
@@ -35,22 +34,41 @@ var LoginSection = React.createClass({
   },
 
   render: function() {
+    var requestFormClassName = classNames('login__form', 'login__form--request', {
+      'login__form--done': this.state.smsRequested,
+      'login__form--active': !this.state.smsRequested
+    });
+    var checkFormClassName = classNames('login__form', 'login__form--check', {
+      'login__form--done': this.state.codeSent,
+      'login__form--active': this.state.smsRequested
+    });
+
+
+    var stepMesssageText = <p>Please enter your full <strong>phone</strong> number to receive <strong>authorization code</strong>.</p>;
+    var smsRequested = this.state.smsRequested;
+
+    if (smsRequested){
+      stepMesssageText =
+        <p>We sent <strong>authorization code</strong> to your <strong>phone</strong>. Please enter it below.</p>
+    }
+
+
     return (
       <div className="login__window">
         <h2>Sign in to Actor messenger</h2>
-        <p>Please enter your full <strong>phone</strong> number to receive <strong>authorization code</strong>.</p>
-        <form className="login__form login__form--request" onSubmit={this._onRequestSms}>
+        {stepMesssageText}
+        <form className={requestFormClassName} onSubmit={this._onRequestSms}>
+          <a href="#">Wrong?</a>
           <input type="phone" name="phone" placeholder="Phone number"
                  onChange={this._onPhoneChange}
                  disabled={this.state.smsRequested}/>
           <button className="button button--primary button--wide">Request code</button>
         </form>
-        <form className="login__form login__form--check" onSubmit={this._onSendCode}>
+        <form className={checkFormClassName} onSubmit={this._onSendCode}>
           <input type="number" name="code" placeholder="Auth code"
                  onChange={this._onCodeChange}
                  disabled={!this.state.smsRequested || this.state.codeSent}/>
           <button className="button button--primary button--wide">Validate code</button>
-          <a href="#">Wrong number?</a>
         </form>
         <form className="login__form login__form--signup">
           <input type="text" name="name" placeholder="Name"/>
