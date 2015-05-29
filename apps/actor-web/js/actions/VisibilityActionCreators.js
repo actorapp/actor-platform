5a9e3bd6c981a7940ba5aa35de03d4cd421e506d
prettynatty <fear@loathing.in>
2015-05-29 19:12:26 +0300

    refactor(web): removed unnessesary logging

--- apps/actor-web/js/actions/VisibilityActionCreators.js
+++ apps/actor-web/js/actions/VisibilityActionCreators.js
@@ -2,12 +2,10 @@ var ActorClient = require('../utils/ActorClient');
 
 var VisibilityActionCreators = {
   createAppHidden: function() {
-    console.warn('onAppHidden');
     ActorClient.onAppHidden();
   },
 
   createAppVisible: function() {
-    console.warn('onAppVisible');
     ActorClient.onAppVisible();
   }
 };
