64d8fc02b6f74ffb777f6d59170176790ee58d0a
rock_jam <5min4eq.unity@gmail.com>
2015-05-30 21:20:54 +0300

    feat(ilectro): add config for number of messagas between ads, direct passing mediator from main

--- actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/llectro/ILectroInterceptionConfig.scala
+++ actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/llectro/ILectroInterceptionConfig.scala
@@ -1,8 +1,10 @@
 package im.actor.server.api.rpc.service.llectro
 
-/**
- * Created by rock_jam on 30.05.15.
- */
-class ILectroInterceptionConfig {
+import com.typesafe.config.Config
 
+object ILectroInterceptionConfig {
+  def fromConfig(config: Config): ILectroInterceptionConfig =
+    ILectroInterceptionConfig(config.getInt("messages-between-ads"))
 }
+
+case class ILectroInterceptionConfig(messagesBetweenAds: Int)
