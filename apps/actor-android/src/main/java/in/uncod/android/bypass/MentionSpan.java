cc09206dda4cfbe94d2402f48ec52c7de865f9ca
Gleb Putintsev <kor_ka@actor.im>
2015-06-01 23:41:35 +0300

    fix(android) joinGroupViaLink add group and users

--- apps/actor-android/src/main/java/in/uncod/android/bypass/MentionSpan.java
+++ apps/actor-android/src/main/java/in/uncod/android/bypass/MentionSpan.java
@@ -1,7 +1,10 @@
 package in.uncod.android.bypass;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.View;
+
+import im.actor.messenger.app.Intents;
 
 public class MentionSpan extends BaseUrlSpan {
 
@@ -16,6 +19,9 @@ public class MentionSpan extends BaseUrlSpan {
             //Do nothing
         }else{
             super.onClick(widget);
+//            int id = Integer.parseInt(getURL().toString().split("://")[1]);
+//            Intents.openProfile(id, widget.getContext());
+//            widget.getContext().startActivity(new Intent());
         }
     }
 }
