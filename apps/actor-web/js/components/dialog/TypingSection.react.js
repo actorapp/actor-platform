2959ce26c59046b4ffe96f4d2a3f37d5e4da36ab
John Doe <speed@actor.im>
2015-06-02 21:42:34 +0300

    feat(web): working typing;

--- apps/actor-web/js/components/dialog/TypingSection.react.js
+++ apps/actor-web/js/components/dialog/TypingSection.react.js
@@ -1,19 +1,45 @@
 'use strict';
 
 var React = require('react');
+var PureRenderMixin = require('react/addons').addons.PureRenderMixin;
+
+var classNames = require('classnames');
+
+var DialogStore = require('../../stores/DialogStore');
 
 var TypingSection = React.createClass({
-  propTypes: {
-    typing: React.PropTypes.object.isRequired
+  mixins: [PureRenderMixin],
+
+  getInitialState: function() {
+    return {typing: null};
+  },
+  componentDidMount: function() {
+    DialogStore.addTypingListener(this._onTypingChange);
+  },
+
+  componentWillUnmount: function() {
+    DialogStore.removeTypingListener(this._onTypingChange);
   },
 
   render: function() {
+    var typing = this.state.typing;
+    var typingClassName = classNames('typing', {
+      'typing--hidden': typing == null
+    });
+
     return (
-      <div className="typing">
-        <i className="material-icons">keyboard</i>Someone typing
+      <div className={typingClassName}>
+        <i className="material-icons">keyboard</i>
+        <span>{typing}</span>
       </div>
     );
+  },
+
+  _onTypingChange: function() {
+    var typing = DialogStore.getSelectedDialogTyping();
+    this.setState({typing: typing});
   }
+
 });
 
 module.exports = TypingSection;
