d2d79718255180c1938663066bd1193b52df12c6
prettynatty <fear@loathing.in>
2015-06-02 16:42:01 +0300

    refactor(web): UserProfile component

--- apps/actor-web/js/stores/ActivityStore.js
+++ apps/actor-web/js/stores/ActivityStore.js
@@ -34,24 +34,22 @@ var ActivityStore = assign({}, EventEmitter.prototype, {
 });
 
 ActivityStore.dispatchToken = ActorAppDispatcher.register(function(action) {
-  var data = null;
-
   switch(action.type) {
     case ActionTypes.CLICK_USER:
-      data = ActorClient.getUser(action.userId);
+      var user = ActorClient.getUser(action.userId);
       _activity = {
         type: ActivityTypes.USER_PROFILE,
         userId: action.userId,
-        data: data
+        user: user
       };
       ActivityStore.emitChange();
       break;
     case ActionTypes.CLICK_GROUP:
-      data = ActorClient.getGroup(action.groupId);
+      var group = ActorClient.getGroup(action.groupId);
       _activity = {
         type: ActivityTypes.GROUP_PROFILE,
         groupId: action.groupId,
-        data: data
+        group: group
       };
       ActivityStore.emitChange();
       break;
