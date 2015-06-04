393b4de47e7df7ea0e1d65ab76a40618e2d87b5f
ex3ndr <me@ex3ndr.com>
2015-06-04 22:02:24 +0300

    feat(js): Adding promise-based actions

--- library/actor-js/src/main/java/im/actor/model/js/entity/JsPromiseExecutor.java
+++ library/actor-js/src/main/java/im/actor/model/js/entity/JsPromiseExecutor.java
@@ -4,6 +4,47 @@
 
 package im.actor.model.js.entity;
 
-public interface JsPromiseExecutor {
-    void execute(JsPromiseResolve resolve, JsPromiseReject reject);
+import com.google.gwt.core.client.JavaScriptObject;
+
+public abstract class JsPromiseExecutor {
+
+    private JavaScriptObject resolveFunc;
+    private JavaScriptObject rejectFunc;
+
+    public JsPromiseExecutor() {
+
+    }
+
+    public final void performExecute(JavaScriptObject resolve, JavaScriptObject reject) {
+        this.resolveFunc = resolve;
+        this.rejectFunc = reject;
+
+        execute();
+    }
+
+    public abstract void execute();
+
+    protected void resolve() {
+        call(resolveFunc);
+    }
+
+    protected void resolve(Object arg) {
+        call(resolveFunc, arg);
+    }
+
+    protected void reject() {
+        call(rejectFunc);
+    }
+
+    protected void reject(Object arg) {
+        call(rejectFunc, arg);
+    }
+
+    private native void call(JavaScriptObject func)/*-{
+        func();
+    }-*/;
+
+    private native void call(JavaScriptObject func, Object arg)/*-{
+        func(arg);
+    }-*/;
 }
