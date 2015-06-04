44dd766dffdcc925f6e49feb77571a424723fb85
prettynatty <fear@loathing.in>
2015-06-04 16:52:26 +0300

    perf(peermanagers): kryo serialization of messages

--- actor-peer-managers/src/main/scala/im/actor/server/peermanagers/PeerManager.scala
+++ actor-peer-managers/src/main/scala/im/actor/server/peermanagers/PeerManager.scala
@@ -1,26 +1,47 @@
 package im.actor.server.peermanagers
 
+import scala.annotation.meta.field
 import scala.concurrent.ExecutionContext
 
 import akka.actor.{ Actor, ActorLogging }
+import com.esotericsoftware.kryo.serializers.TaggedFieldSerializer.Tag
 import org.joda.time.DateTime
 import slick.dbio.DBIO
 
 import im.actor.api.rpc.messaging.{ Message ⇒ ApiMessage, _ }
 import im.actor.api.rpc.peers.{ Peer, PeerType }
+import im.actor.server.commons.serialization.KryoSerializable
 import im.actor.server.models
 import im.actor.server.util.ContactsUtils
 
 object PeerManager {
-  private[peermanagers] sealed trait Message
+  sealed trait Message extends KryoSerializable
 
-  private[peermanagers] case class Envelope(peerId: Int, payload: Message)
+  case class Envelope(
+    @(Tag @field)(0) peerId: Int,
+    @(Tag @field)(1) payload:Message
+  ) extends KryoSerializable
 
-  private[peermanagers] case class SendMessage(senderUserId: Int, senderAuthId: Long, randomId: Long, date: DateTime, message: ApiMessage, isFat: Boolean = false) extends Message
+  case class SendMessage(
+    @(Tag @field)(0) senderUserId:Int,
+    @(Tag @field)(1) senderAuthId:Long,
+    @(Tag @field)(2) randomId:    Long,
+    @(Tag @field)(3) date:        DateTime,
+    @(Tag @field)(4) message:     ApiMessage,
+    @(Tag @field)(5) isFat:       Boolean    = false
+  ) extends Message
 
-  private[peermanagers] case class MessageReceived(receiverUserId: Int, date: Long, receivedDate: Long) extends Message
+  case class MessageReceived(
+    @(Tag @field)(1) receiverUserId:Int,
+    @(Tag @field)(2) date:          Long,
+    @(Tag @field)(3) receivedDate:  Long
+  ) extends Message
 
-  private[peermanagers] case class MessageRead(readerUserId: Int, date: Long, readDate: Long) extends Message
+  case class MessageRead(
+    @(Tag @field)(0) readerUserId:Int,
+    @(Tag @field)(1) date:        Long,
+    @(Tag @field)(2) readDate:    Long
+  ) extends Message
 }
 
 private[peermanagers] trait PeerManager extends Actor with ActorLogging {
