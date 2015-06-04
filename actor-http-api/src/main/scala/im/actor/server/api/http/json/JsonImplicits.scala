fc6c21aec3e09f99a512ee561b3f41d30a76b2e5
Nikolay Tatarinov <rockjam@actor.im>
2015-06-04 22:48:04 +0300

    fix(http-api): specs, code organizing

--- actor-http-api/src/main/scala/im/actor/server/api/http/json/JsonImplicits.scala
+++ actor-http-api/src/main/scala/im/actor/server/api/http/json/JsonImplicits.scala
@@ -1,20 +1,7 @@
-package im.actor.server.api.http
+package im.actor.server.api.http.json
 
+import play.api.libs.json.{ Json, Format, JsPath, Reads }
 import play.api.libs.functional.syntax._
-import play.api.libs.json._
-
-sealed trait Content
-case class Text(text: String) extends Content
-case class Image(imageUrl: String) extends Content
-case class Document(documentUrl: String) extends Content
-
-case class GroupInviteInfo(
-  groupTitle:       String,
-  groupAvatarUrl:   Option[String],
-  inviterName:      String,
-  inviterAvatarUrl: Option[String]
-)
-case class Errors(message: String)
 
 object JsonImplicits {
 
@@ -23,6 +10,7 @@ object JsonImplicits {
       (JsPath \ "document_url").read[String].map[Content] { Document } |
       (JsPath \ "image_url").read[String].map[Content] { Image }
 
+  implicit val avatarUrlsFormat: Format[AvatarUrls] = Json.format[AvatarUrls]
   implicit val groupInviteInfoFormat: Format[GroupInviteInfo] = Json.format[GroupInviteInfo]
   implicit val errorsFormat: Format[Errors] = Json.format[Errors]
 
