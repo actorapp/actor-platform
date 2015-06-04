4950506fa4bea8c4d83b74c9d2e862d5654c1d82
rock_jam <5min4eq.unity@gmail.com>
2015-06-04 18:56:36 +0300

    feat(http-api): methods for group invite info

--- actor-http-api/src/main/scala/im/actor/server/api/http/Models.scala
+++ actor-http-api/src/main/scala/im/actor/server/api/http/Models.scala
@@ -8,11 +8,22 @@ case class Text(text: String) extends Content
 case class Image(imageUrl: String) extends Content
 case class Document(documentUrl: String) extends Content
 
+case class GroupInviteInfo(
+  groupTitle:       String,
+  groupAvatarUrl:   Option[String],
+  inviterName:      String,
+  inviterAvatarUrl: Option[String]
+)
+case class Errors(message: String)
+
 object JsonImplicits {
 
   implicit val textReads: Reads[Content] =
     (JsPath \ "text").read[String].map[Content] { Text } |
       (JsPath \ "document_url").read[String].map[Content] { Document } |
       (JsPath \ "image_url").read[String].map[Content] { Image }
+
+  implicit val groupInviteInfoFormat: Format[GroupInviteInfo] = Json.format[GroupInviteInfo]
+  implicit val errorsFormat: Format[Errors] = Json.format[Errors]
 
 }
