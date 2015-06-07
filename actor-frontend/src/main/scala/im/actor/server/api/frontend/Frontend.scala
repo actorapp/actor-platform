0048c8086b297b95b84f5437839a4cff042f9162
prettynatty <fear@loathing.in>
2015-06-07 08:54:51 +0300

    fix(frontend): unique id prefix for tcp and ws frontends

--- actor-frontend/src/main/scala/im/actor/server/api/frontend/Frontend.scala
+++ actor-frontend/src/main/scala/im/actor/server/api/frontend/Frontend.scala
@@ -5,5 +5,7 @@ import java.util.concurrent.atomic.AtomicLong
 trait Frontend {
   private val connCounter = new AtomicLong(0L)
 
-  protected def nextConnId(): String = s"conn-${connCounter.incrementAndGet()}"
+  protected val connIdPrefix: String
+
+  protected def nextConnId(): String = s"conn-${connIdPrefix}-${connCounter.incrementAndGet()}"
 }
