5392959e43355bdfb981abb4d7a66179b211ff5a
prettynatty <fear@loathing.in>
2015-06-08 05:41:08 +0300

    feat(web): sign up

--- apps/actor-web/src/app/stores/LoginStore.js
+++ apps/actor-web/src/app/stores/LoginStore.js
@@ -12,42 +12,51 @@ var assign = require('object-assign');
 var CHANGE_EVENT = 'change';
 
 var _isSmsRequested = false;
+var _isSignupStarted = false;
 var _myUid = null;
 
 var LoginStore = assign({}, EventEmitter.prototype, {
-  isLoggedIn: function() {
-    return(ActorClient.isLoggedIn());
+  isLoggedIn: function () {
+    return (ActorClient.isLoggedIn());
   },
 
-  isSmsRequested: function() {
-    return(_isSmsRequested);
+  isSmsRequested: function () {
+    return (_isSmsRequested);
   },
 
-  emitChange: function() {
+  isSignupStarted: function () {
+    return (_isSignupStarted);
+  },
+
+  emitChange: function () {
     this.emit(CHANGE_EVENT);
   },
 
-  addChangeListener: function(callback) {
+  addChangeListener: function (callback) {
     this.on(CHANGE_EVENT, callback);
   },
 
-  removeChangeListener: function(callback) {
+  removeChangeListener: function (callback) {
     this.removeListener(CHANGE_EVENT, callback);
   },
 
-  getMyId: function() {
-    return(_myUid);
+  getMyId: function () {
+    return (_myUid);
   }
 });
 
-LoginStore.dispatchToken = ActorAppDispatcher.register(function(action) {
-  switch(action.type) {
+LoginStore.dispatchToken = ActorAppDispatcher.register(function (action) {
+  switch (action.type) {
     case ActionTypes.AUTH_SMS_REQUESTED:
       _isSmsRequested = true;
       LoginStore.emitChange();
       break;
     case ActionTypes.SET_LOGGED_IN:
       _myUid = ActorClient.getUid();
+      LoginStore.emitChange();
+      break;
+    case ActionTypes.START_SIGNUP:
+      _isSignupStarted = true;
       LoginStore.emitChange();
       break;
     default:
