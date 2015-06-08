1fc7303f2caf885d09039c37f47bccb6c6ce743d
prettynatty <fear@loathing.in>
2015-06-08 20:30:19 +0300

    feat(web): react-router, /auth path for authentication

---
+++ apps/actor-web/src/app/components/Main.react.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var _ = require('lodash');
+var React = require('react');
+
+import requireAuth from '../utils/require-auth';
+
+var VisibilityActionCreators = require('../actions/VisibilityActionCreators');
+
+var ActivitySection = require('./ActivitySection.react');
+var SidebarSection = require('./SidebarSection.react');
+var ToolbarSection = require('./ToolbarSection.react');
+var DialogSection = require('./DialogSection.react');
+var ContactSection = require('./ContactSection.react');
+
+var visibilitychange = 'visibilitychange';
+var onVisibilityChange = function () {
+  if (!document.hidden) {
+    VisibilityActionCreators.createAppVisible()
+  } else {
+    VisibilityActionCreators.createAppHidden()
+  }
+};
+
+var Main = requireAuth(React.createClass({
+  componentWillMount: function () {
+    document.addEventListener(visibilitychange, onVisibilityChange);
+
+    if (!document.hidden) {
+      VisibilityActionCreators.createAppVisible()
+    }
+  },
+
+  render: function () {
+    return (
+      <div className="app row">
+
+        <SidebarSection/>
+
+        <section className="main col-xs">
+          <ToolbarSection/>
+          <DialogSection/>
+        </section>
+
+        <ActivitySection/>
+        <ContactSection/>
+      </div>
+    )
+  }
+}));
+
+module.exports = Main;
