dabb481a8f4fa4c3089aded7478e69c87f369854
John Doe <speed@actor.im>
2015-06-09 03:39:59 +0300

    feat(web): invite users

---
+++ apps/actor-web/src/app/actions/InviteUserActions.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import Reflux from 'reflux';
+
+module.exports = {
+  modalOpen: Reflux.createAction(),
+  modalClose: Reflux.createAction()
+};
---
+++ apps/actor-web/src/app/components/modals/InviteUser.react.js
@@ -0,0 +1,92 @@
+'use strict';
+
+import React from 'react';
+
+import ActorClient from '../../utils/ActorClient';
+
+import InviteUserActions from '../../actions/InviteUserActions';
+
+import ContactStore from '../../stores/ContactStore';
+import InviteUserStore from '../../stores/InviteUserStore';
+
+import Modal from 'react-modal';
+
+import ContactItem from './invite-user/ContactItem.react';
+
+const getStateFromStores = function() {
+  return({
+    contacts: ContactStore.getContacts(),
+    group: InviteUserStore.getGroup(),
+    isOpen: InviteUserStore.isModalOpen()
+  })
+};
+
+const hasMember = (group, userId) =>
+  undefined !== _.find(group.members, (c) => c.peerInfo.peer.id === userId);
+
+var InviteUser = React.createClass({
+  getInitialState () {
+    return getStateFromStores()
+  },
+
+  componentWillMount() {
+    this.unsubscribe = InviteUserStore.listen(this._onChange);
+    ContactStore.addChangeListener(this._onChange);
+  },
+
+  componentWillUnmount() {
+    this.unsubscribe();
+    ContactStore.removeChangeListener(this._onChange);
+  },
+
+  render () {
+    var contacts = this.state.contacts;
+    var isOpen = this.state.isOpen;
+
+    if (isOpen) {
+      let contactList = [];
+
+      _.forEach(contacts, function(contact, i) {
+        if (!hasMember(this.state.group, contact.uid)) {
+          contactList.push(
+            <ContactItem key={i} contact={contact} onSelect={this._onContactSelect}/>
+          );
+        }
+      }, this);
+
+      return(
+        <Modal closeTimeoutMS={150}
+               isOpen={isOpen} className="modal contacts">
+
+          <header className="modal__header">
+            <a className="modal__header__close material-icons" onClick={this._onClose}>clear</a>
+            <h3>Contact list</h3>
+          </header>
+
+          <div className="modal__body">
+            <ul className="contacts__list">
+              {contactList}
+            </ul>
+          </div>
+        </Modal>
+      );
+    } else {
+      return (null);
+    }
+  },
+
+  _onChange () {
+    this.setState(getStateFromStores());
+  },
+
+  _onClose () {
+    InviteUserActions.modalClose();
+  },
+
+  _onContactSelect (contact) {
+    ActorClient.inviteMember(this.state.group.id, contact.uid)
+      .then(() => InviteUserActions.modalClose());
+  }
+});
+
+module.exports = InviteUser;
