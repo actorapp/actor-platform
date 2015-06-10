581f5a9973a521bfdd715c63dcff05b11746b493
prettynatty <fear@loathing.in>
2015-06-10 18:36:06 +0300

    test(util): completed spec for UserUtils

--- actor-tests/src/test/scala/im/actor/server/util/UserUtilsSpec.scala
+++ actor-tests/src/test/scala/im/actor/server/util/UserUtilsSpec.scala
@@ -1,10 +1,12 @@
 package im.actor.server.util
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
 import scala.language.postfixOps
 
 import im.actor.api.rpc.auth.AuthService
-import im.actor.api.rpc.users.User
-import im.actor.server.{ ImplicitRegions, BaseAppSuite }
+import im.actor.api.rpc.users.{ ContactRecord, ContactType, User }
+import im.actor.server.{ persist, ImplicitRegions, BaseAppSuite }
 
 class UserUtilsSpec extends BaseAppSuite with ImplicitRegions {
 
@@ -14,17 +16,33 @@ class UserUtilsSpec extends BaseAppSuite with ImplicitRegions {
 
   implicit val authService: AuthService = buildAuthService()
 
-  val (users, authIds, phones) =
-    Seq(createUser(), createUser()).foldLeft(Seq.empty[User], Seq.empty[Long], Seq.empty[Long]) {
-      case ((uacc, aacc, pacc), (u, a, p)) ⇒
-        (uacc :+ u, aacc :+ a, pacc :+ p)
-    }
+  val userTups = Seq(createUser(), createUser()) map {
+    case (struct, authId, phone) ⇒
+      (Await.result(db.run(persist.User.find(struct.id) map (_.head)), 5.seconds), authId, phone)
+  }
 
-  val userIds = users map (_.id) toSet
+  val userIds = userTups map (_._1.id) toSet
 
   def e1() = {
     val (requestingUser, requestingAuthId, _) = createUser()
 
-    val structs = userStructs(userIds, requestingUser.id, requestingAuthId)
+    whenReady(db.run(userStructs(userIds, requestingUser.id, requestingAuthId))) { structs ⇒
+      val expectedStructs = userTups map {
+        case (user, authId, phone) ⇒
+          User(
+            user.id,
+            ACLUtils.userAccessHash(requestingAuthId, user),
+            user.name,
+            None,
+            None,
+            Some(phone),
+            None,
+            Some(false),
+            Vector(ContactRecord(ContactType.Phone, None, Some(phone), Some("Mobile phone"), None))
+          )
+      }
+
+      structs shouldEqual expectedStructs
+    }
   }
 }
