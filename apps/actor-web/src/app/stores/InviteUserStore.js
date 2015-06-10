a6e247a005d0811bf011b03a4b7a8b96893e566e
prettynatty <fear@loathing.in>
2015-06-10 05:35:35 +0300

    feat(web): invite url in Add member popup

--- apps/actor-web/src/app/stores/InviteUserStore.js
+++ apps/actor-web/src/app/stores/InviteUserStore.js
@@ -1,10 +1,13 @@
 import Reflux from 'reflux';
 
+import ActorClient from '../utils/ActorClient';
+
 import InviteUserActions from '../actions/InviteUserActions';
 
 let
   _isModalOpen = false,
-  _group = null;
+  _group = null,
+  _inviteUrl = null;
 
 export default Reflux.createStore({
   init () {
@@ -20,9 +23,19 @@ export default Reflux.createStore({
     return _group;
   },
 
+  getInviteUrl () {
+    return _inviteUrl;
+  },
+
   onModalOpen (group) {
     _isModalOpen = true;
     _group = group;
+
+    ActorClient.getInviteUrl(group.id)
+      .then((url) => {
+        _inviteUrl = url;
+        this.trigger();
+      });
 
     this.trigger();
   },
