17f6d89c1ca8ab084dc09a5bb829ac56bf0fd51c
John Doe <speed@actor.im>
2015-06-11 23:04:56 +0300

    feat(web): move emoji from data-uri to images

--- apps/actor-web/webpack.config.js
+++ apps/actor-web/webpack.config.js
@@ -31,11 +31,16 @@ module.exports = {
         (path.resolve(__dirname, "./node_modules"))
       },
 
+      {
+        test: /\.png$/,
+        loader: "file-loader"
+      },
+
       // required for bootstrap icons
-      { test: /\.woff$/,   loader: "url-loader?prefix=font/&limit=5000&mimetype=application/font-woff" },
-      { test: /\.ttf$/,    loader: "file-loader?prefix=font/" },
-      { test: /\.eot$/,    loader: "file-loader?prefix=font/" },
-      { test: /\.svg$/,    loader: "file-loader?prefix=font/" },
+      { test: /\.woff$/, loader: "url-loader?prefix=font/&limit=5000&mimetype=application/font-woff" },
+      { test: /\.ttf$/, loader: "file-loader?prefix=font/" },
+      { test: /\.eot$/, loader: "file-loader?prefix=font/" },
+      { test: /\.svg$/, loader: "file-loader?prefix=font/" },
 
       {
         test: /\.js$/,
