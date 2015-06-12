df324a92f6dc7589c93e4235339ad464fd81fd87
John Doe <speed@actor.im>
2015-06-13 00:11:13 +0300

    feat(web): deactivate old tabs on opening new ones

--- apps/actor-web/src/app/index.js
+++ apps/actor-web/src/app/index.js
@@ -1,6 +1,9 @@
+import crosstab from 'crosstab';
+
 import React from 'react';
 import Router from 'react-router';
 
+import Deactivated from './components/Deactivated.react.js';
 import Login from './components/Login.react.js';
 import Main from './components/Main.react';
 import JoinGroup from './components/JoinGroup.react';
@@ -12,8 +15,19 @@ const DefaultRoute = Router.DefaultRoute;
 const Route = Router.Route;
 const RouteHandler = Router.RouteHandler;
 
+const ActorInitEvent = 'concurrentActorInit';
+
+crosstab.on(ActorInitEvent, (msg) => {
+  if (msg.origin !== crosstab.id && window.location.pathname !== '/deactivated') {
+    window.location.assign('/deactivated');
+  }
+});
+
 window.jsAppLoaded = () => {
-  window.messenger = new window.actor.ActorApp();
+  if (window.location.pathname !== '/deactivated') {
+    crosstab.broadcast(ActorInitEvent, {});
+    window.messenger = new window.actor.ActorApp();
+  }
 
   const App = React.createClass({
     render() {
@@ -26,6 +40,7 @@ window.jsAppLoaded = () => {
       <Route handler={Main} name="main" path="/"/>
       <Route handler={JoinGroup} name="join" path="/join/:token"/>
       <Route handler={Login} name="login" path="/auth"/>
+      <Route handler={Deactivated} name="deactivated" path="/deactivated"/>
       <DefaultRoute handler={Main}/>
     </Route>
   );
