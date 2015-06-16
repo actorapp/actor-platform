4f1a114c2837f2942a6c0fb78a93eee86c6321f0
John Doe <speed@actor.im>
2015-06-16 17:32:56 +0300

    refactor(web): removed obsolete angular app

--- apps/actor-web/app/main.js
+++
@@ -1,5 +0,0 @@
-window.React = require('react');
-
-var Avatar = require('./shared/messages/avatar.jsx');
-var Message = require('./shared/messages/message.jsx');
-var ChatMessage = require('./shared/messages/messages.jsx');
