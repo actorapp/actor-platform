7ae0cc6a8bd5552d8890748c51d9ee34999dc404
John Doe <speed@actor.im>
2015-06-17 20:57:31 +0300

    feat(web): ugly but working group creation :poop:

--- apps/actor-web/src/app/constants/ActorAppConstants.js
+++ apps/actor-web/src/app/constants/ActorAppConstants.js
@@ -7,6 +7,45 @@ export const AuthSteps = {
   COMPLETED: 4
 };
 
+export const ActionTypes = keymirror({
+  APP_HIDDEN: null,
+  APP_VISIBLE: null,
+
+  AUTH_SMS_REQUEST_SUCCESS: null,
+  AUTH_SMS_REQUEST_FAILURE: null,
+
+  SEND_CODE_SUCCESS: null,
+  SEND_CODE_FAILURE: null,
+
+  SEND_SIGNUP_SUCCESS: null,
+  SEND_SIGNUP_FAILURE: null,
+
+  SET_LOGGED_IN: null,
+  START_SIGNUP: null,
+
+  AUTH_WRONG_NUMBER_CLICK: null,
+
+  DIALOGS_CHANGED: null,
+  SELECT_DIALOG_PEER: null,
+  SELECTED_DIALOG_INFO_CHANGED: null,
+
+  SEND_MESSAGE_TEXT: null,
+  SEND_MESSAGE_FILE: null,
+  SEND_MESSAGE_PHOTO: null,
+
+  SHOW_ACTIVITY: null,
+  HIDE_ACTIVITY: null,
+
+  CONTACT_ADD: null,
+  CONTACT_REMOVE: null,
+  CONTACT_LIST_SHOW: null,
+  CONTACT_LIST_HIDE: null,
+  CONTACT_LIST_CHANGED: null,
+
+  CREATE_GROUP_MODAL_OPEN: null,
+  CREATE_GROUP_MODAL_CLOSE: null
+});
+
 export default {
   AuthSteps: AuthSteps,
 
@@ -15,41 +54,7 @@ export default {
     GROUP: 'group'
   },
 
-  ActionTypes: keymirror({
-    APP_HIDDEN: null,
-    APP_VISIBLE: null,
-
-    AUTH_SMS_REQUEST_SUCCESS: null,
-    AUTH_SMS_REQUEST_FAILURE: null,
-
-    SEND_CODE_SUCCESS: null,
-    SEND_CODE_FAILURE: null,
-
-    SEND_SIGNUP_SUCCESS: null,
-    SEND_SIGNUP_FAILURE: null,
-
-    SET_LOGGED_IN: null,
-    START_SIGNUP: null,
-
-    AUTH_WRONG_NUMBER_CLICK: null,
-
-    DIALOGS_CHANGED: null,
-    SELECT_DIALOG_PEER: null,
-    SELECTED_DIALOG_INFO_CHANGED: null,
-
-    SEND_MESSAGE_TEXT: null,
-    SEND_MESSAGE_FILE: null,
-    SEND_MESSAGE_PHOTO: null,
-
-    SHOW_ACTIVITY: null,
-    HIDE_ACTIVITY: null,
-
-    CONTACT_ADD: null,
-    CONTACT_REMOVE: null,
-    CONTACT_LIST_SHOW: null,
-    CONTACT_LIST_HIDE: null,
-    CONTACT_LIST_CHANGED: null
-  }),
+  ActionTypes: ActionTypes,
 
   ActivityTypes: keymirror({
     USER_PROFILE: null,
