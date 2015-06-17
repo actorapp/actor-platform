106a17376bba2252bf8981b658727d7880c6aacc
prettynatty <fear@loathing.in>
2015-06-17 23:12:41 +0300

    fix(messaging): invalidate cache for request completed with an error

--- actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/messaging/MessagingHandlers.scala
+++ actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/messaging/MessagingHandlers.scala
@@ -29,29 +29,41 @@ private[messaging] trait MessagingHandlers {
   private val caches = scala.collection.mutable.Map.empty[Long, Cache[java.lang.Long, Future[HandlerResult[ResponseSeqDate]]]]
 
   override def jhandleSendMessage(outPeer: OutPeer, randomId: Long, message: Message, clientData: ClientData): Future[HandlerResult[ResponseSeqDate]] = {
-    implicit val cache = caches.getOrElseUpdate(clientData.authId, createCache[java.lang.Long, Future[HandlerResult[ResponseSeqDate]]](MaxCacheSize))
-    withCachedResult[java.lang.Long, Future[HandlerResult[ResponseSeqDate]]](randomId) {
-      val authorizedAction = requireAuth(clientData).map { implicit client ⇒
-        withOutPeer(outPeer) {
-          val dateTime = new DateTime
-          val dateMillis = dateTime.getMillis
+    val cache = caches.getOrElseUpdate(clientData.authId, createCache[java.lang.Long, Future[HandlerResult[ResponseSeqDate]]](MaxCacheSize))
 
-          val seqstateAction = outPeer.`type` match {
-            case PeerType.Private ⇒
-              DBIO.from(PrivatePeerManager.sendMessage(outPeer.id, client.userId, client.authId, randomId, dateTime, message))
-            case PeerType.Group ⇒
-              DBIO.from(GroupPeerManager.sendMessage(outPeer.id, client.userId, client.authId, randomId, dateTime, message))
-          }
+    val authorizedAction = requireAuth(clientData).map { implicit client ⇒
+      withOutPeer(outPeer) {
+        val dateTime = new DateTime
+        val dateMillis = dateTime.getMillis
 
-          for (seqstate ← seqstateAction) yield {
-            val fromPeer = Peer(PeerType.Private, client.userId)
-            val toPeer = outPeer.asPeer
-            onMessage(Events.PeerMessage(fromPeer.asModel, toPeer.asModel, randomId, dateMillis, message))
-            Ok(ResponseSeqDate(seqstate._1, seqstate._2, dateMillis))
-          }
+        val seqstateAction = outPeer.`type` match {
+          case PeerType.Private ⇒
+            DBIO.from(PrivatePeerManager.sendMessage(outPeer.id, client.userId, client.authId, randomId, dateTime, message))
+          case PeerType.Group ⇒
+            DBIO.from(GroupPeerManager.sendMessage(outPeer.id, client.userId, client.authId, randomId, dateTime, message))
+        }
+
+        for (seqstate ← seqstateAction) yield {
+          val fromPeer = Peer(PeerType.Private, client.userId)
+          val toPeer = outPeer.asPeer
+          onMessage(Events.PeerMessage(fromPeer.asModel, toPeer.asModel, randomId, dateMillis, message))
+          Ok(ResponseSeqDate(seqstate._1, seqstate._2, dateMillis))
         }
       }
-      db.run(toDBIOAction(authorizedAction))
+    }
+
+    Option(cache getIfPresent (randomId)) match {
+      case Some(resFuture) ⇒
+        resFuture
+      case None ⇒
+        val resFuture = db.run(toDBIOAction(authorizedAction))
+        cache.put(randomId, resFuture)
+
+        resFuture onFailure {
+          case _ ⇒ cache.invalidate(randomId)
+        }
+
+        resFuture
     }
   }
 }
