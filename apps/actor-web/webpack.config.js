7074d7aa38af471e75b389d13004ac6a061f0e81
John Doe <speed@actor.im>
2015-06-18 23:32:09 +0300

    refactor(web): fix eslint warnings;

--- apps/actor-web/webpack.config.js
+++ apps/actor-web/webpack.config.js
@@ -1,19 +1,19 @@
 import minimist from 'minimist';
-import path from "path";
-import webpack from "webpack";
+import path from 'path';
+import webpack from 'webpack';
 
 const argv = minimist(process.argv.slice(2));
 
 const DEBUG = !argv.release;
 
-module.exports = {
+export default {
   cache: DEBUG,
   debug: DEBUG,
   devtool: DEBUG ? 'source-map' : false,
   hotComponents: DEBUG,
   entry: {
     app: [
-      "./src/app"
+      './src/app'
     ],
     styles: DEBUG ? [
       'webpack-dev-server/client?http://localhost:3000',
@@ -22,43 +22,43 @@ module.exports = {
     ] : ['./src/styles']
   },
   output: {
-    path: path.join(__dirname, "dist/assets"),
-    publicPath: "/assets/",
-    filename: "[name].js",
-    chunkFilename: "[chunkhash].js"
+    path: path.join(__dirname, 'dist/assets'),
+    publicPath: '/assets/',
+    filename: '[name].js',
+    chunkFilename: '[chunkhash].js'
   },
   resolve: {
-    root: [path.join(__dirname, "bower_components")]
+    root: [path.join(__dirname, 'bower_components')]
   },
   module: {
     preLoaders: [
-      {test: /\.js$/, loader: "eslint-loader", exclude: /node_modules/}
+      {test: /\.js$/, loader: 'eslint-loader', exclude: /node_modules/}
     ],
 
     loaders: [
       {
         test: /\.scss|\.css$/,
-        loader: "style!css!sass?outputStyle=expanded&indentedSyntax" +
-        "includePaths[]=" +
-        (path.resolve(__dirname, "./bower_components")) + "&" +
-        "includePaths[]=" +
-        (path.resolve(__dirname, "./node_modules"))
+        loader: 'style!css!sass?outputStyle=expanded&indentedSyntax' +
+        'includePaths[]=' +
+        (path.resolve(__dirname, './bower_components')) + '&' +
+        'includePaths[]=' +
+        (path.resolve(__dirname, './node_modules'))
       },
 
       {
         test: /\.png$/,
-        loader: "file-loader"
+        loader: 'file-loader'
       },
 
       // required for bootstrap icons
-      {test: /\.woff$/, loader: "url-loader?prefix=font/&limit=5000&mimetype=application/font-woff"},
-      {test: /\.ttf$/, loader: "file-loader?prefix=font/"},
-      {test: /\.eot$/, loader: "file-loader?prefix=font/"},
-      {test: /\.svg$/, loader: "file-loader?prefix=font/"},
+      {test: /\.woff$/, loader: 'url-loader?prefix=font/&limit=5000&mimetype=application/font-woff'},
+      {test: /\.ttf$/, loader: 'file-loader?prefix=font/'},
+      {test: /\.eot$/, loader: 'file-loader?prefix=font/'},
+      {test: /\.svg$/, loader: 'file-loader?prefix=font/'},
 
       {
         test: /\.js$/,
-        loader: "babel",
+        loader: 'babel',
         exclude: /(node_modules|bower_components)/,
         query: {
           optional: ['strict', 'es7.classProperties']
@@ -68,8 +68,8 @@ module.exports = {
   },
   plugins: [
     new webpack.ResolverPlugin([
-      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("package.json", ["main"]),
-      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('package.json', ['main']),
+      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('bower.json', ['main'])
     ]),
     new webpack.optimize.DedupePlugin(),
     new webpack.HotModuleReplacementPlugin(),
