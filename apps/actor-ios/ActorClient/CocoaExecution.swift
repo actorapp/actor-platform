d2ac79997afe9ec60d676f5ef218eb2f00e8dd85
ex3ndr <me@ex3ndr.com>
2015-06-19 22:21:21 +0300

    feat(iOS): Add join group by URL

--- apps/actor-ios/ActorClient/CocoaExecution.swift
+++ apps/actor-ios/ActorClient/CocoaExecution.swift
@@ -7,28 +7,10 @@ import Foundation
 extension UIViewController {
     
     func execute(command: AMCommand) {
-        execute(command, successBlock: nil, failureBlock: nil)
+        (UIApplication.sharedApplication().delegate as! AppDelegate).execute(command)
     }
     
     func execute(command: AMCommand, successBlock: ((val: Any?) -> Void)?, failureBlock: ((val: Any?) -> Void)?) {
-        var window = UIApplication.sharedApplication().windows[1] as! UIWindow
-        var hud = MBProgressHUD(window: window)
-        hud.mode = MBProgressHUDMode.Indeterminate
-        hud.removeFromSuperViewOnHide = true
-        window.addSubview(hud)
-        window.bringSubviewToFront(hud)
-        hud.show(true)
-        command.startWithCallback(CocoaCallback(result: { (val:Any?) -> () in
-            dispatch_async(dispatch_get_main_queue(), {
-                hud.hide(true)
-                successBlock?(val: val)
-            })
-            }, error: { (val) -> () in
-                dispatch_async(dispatch_get_main_queue(), {
-                    hud.hide(true)
-                    failureBlock?(val: val)
-                })
-        }))
-    }
-    
+        (UIApplication.sharedApplication().delegate as! AppDelegate).execute(command, successBlock: successBlock, failureBlock: failureBlock)
+    }    
 }
