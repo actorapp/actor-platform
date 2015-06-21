352f496cb91f015b7cb292952f28387e6ee26cdc
ex3ndr <me@ex3ndr.com>
2015-06-21 10:56:56 +0300

    chore(*): Restructured library structure, fixing cocoa and web compilation

---
+++ actor-apps/library/core-js/src/main/java/im/actor/model/js/angular/AngularListCallback.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2015 Actor LLC. <https://actor.im>
+ */
+
+package im.actor.model.js.angular;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArray;
+
+import org.timepedia.exporter.client.Export;
+import org.timepedia.exporter.client.ExportClosure;
+import org.timepedia.exporter.client.Exportable;
+
+@Export
+@ExportClosure
+public interface AngularListCallback<T extends JavaScriptObject> extends Exportable {
+    public void onCollectionChanged(JsArray<T> array);
+}
---
+++ actor-build/buildWeb.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+BUILD_DIRECTORY="`pwd`/actor-build"
+APPS_DIRECTORY="`pwd`/actor-apps"
+GRADLEW="${APPS_DIRECTORY}/gradlew"
+
+# Updating configuration parameters
+${BUILD_DIRECTORY}/updateAppsConfig.sh
+
+# Building Release Android app
+cd "${APPS_DIRECTORY}"
+${GRADLEW} ":library:core-js:buildPackage"
