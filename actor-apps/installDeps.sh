41ea847b58144246bcc34313576fd678036c022b
ex3ndr <me@ex3ndr.com>
2015-06-25 08:00:17 +0300

    chore(*): Fixing gitignore

--- actor-apps/installDeps.sh
+++ actor-apps/installDeps.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 echo "Actor Platform apps dependency installation..."
 
@@ -10,17 +11,18 @@ INSTALL_IOS=${IS_OSX}
 INSTALL_WEB=true
 
 if $INSTALL_ANDROID; then
-	echo_w "Installing Android environment..."
+	# echo_w "Installing Android environment..."
 	"$SCRIPT_DIR/build/configureAndroidDeps.sh" "${SCRIPT_DIR}"
 fi
 
 if $INSTALL_IOS; then 
-	echo_w "Installing iOS environment..."
+	# echo_w "Installing iOS environment..."
 	"$SCRIPT_DIR/build/configureCocoaDeps.sh" "${SCRIPT_DIR}"
 fi	
 
 if $INSTALL_WEB; then 
-	echo_w "Installing Web environment..."
+	# echo_w "Installing Web environment..." "${SCRIPT_DIR}"
+	"$SCRIPT_DIR/build/configureWebDeps.sh" "${SCRIPT_DIR}"
 fi
 # echo "Installing Android environment: ${INSTALL_ANDROID}"
 # echo "Installing iOS environment: ${INSTALL_IOS}"
