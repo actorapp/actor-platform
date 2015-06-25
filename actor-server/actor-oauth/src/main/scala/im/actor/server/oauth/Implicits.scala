9587da9782d4373c23c701c2350f4fabc2c862b0
rock_jam <5min4eq.unity@gmail.com>
2015-06-25 17:03:01 +0300

    fix(oauth): make refresh token optional

--- actor-server/actor-oauth/src/main/scala/im/actor/server/oauth/Implicits.scala
+++ actor-server/actor-oauth/src/main/scala/im/actor/server/oauth/Implicits.scala
@@ -2,21 +2,21 @@ package im.actor.server.oauth
 
 import akka.http.scaladsl.unmarshalling.PredefinedFromEntityUnmarshallers._
 import akka.http.scaladsl.unmarshalling._
-import akka.stream.FlowMaterializer
+import akka.stream.Materializer
 import play.api.libs.functional.syntax._
 import play.api.libs.json.{ JsPath, Json, Reads }
 
 trait Implicits {
 
-  implicit val flowMaterializer: FlowMaterializer
+  implicit val materializer: Materializer
 
   implicit val tokenReads: Reads[Token] =
     ((JsPath \ "access_token").read[String] and
       (JsPath \ "token_type").read[String] and
       (JsPath \ "expires_in").read[Long] and
-      (JsPath \ "refresh_token").read[String])(createToken _)
+      (JsPath \ "refresh_token").readNullable[String])(createToken _)
 
-  private def createToken(accessToken: String, tokenType: String, expiresIn: Long, refreshToken: String) =
+  private def createToken(accessToken: String, tokenType: String, expiresIn: Long, refreshToken: Option[String]) =
     Token(accessToken, tokenType, expiresIn, refreshToken)
 
   implicit val toOAuthToken: FromResponseUnmarshaller[Option[Token]] = Unmarshaller { implicit ec ⇒ resp ⇒
