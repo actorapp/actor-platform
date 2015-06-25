c65a75f37d3b9aadef79aaa10177373ab9f6e19c
Steve Kite <steve@actor.im>
2015-06-25 07:35:55 +0300

    chore(*): Built in SBT, restructure of gradle build

---
+++ actor-apps/installDeps.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "Actor Platform apps dependency installation..."
+
+SCRIPT_DIR="$(dirname "$0")"
+source "$SCRIPT_DIR/build/common.sh"
+
+INSTALL_ANDROID=true
+INSTALL_IOS=${IS_OSX}
+INSTALL_WEB=true
+
+if $INSTALL_ANDROID; then
+	echo_w "Installing Android environment..."
+	"$SCRIPT_DIR/build/configureAndroidDeps.sh" "${SCRIPT_DIR}"
+fi
+
+if $INSTALL_IOS; then 
+	echo_w "Installing iOS environment..."
+	"$SCRIPT_DIR/build/configureCocoaDeps.sh" "${SCRIPT_DIR}"
+fi	
+
+if $INSTALL_WEB; then 
+	echo_w "Installing Web environment..."
+fi
+# echo "Installing Android environment: ${INSTALL_ANDROID}"
+# echo "Installing iOS environment: ${INSTALL_IOS}"
+# echo "Installing Web environment: ${INSTALL_WEB}"
