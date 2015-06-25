fa796d3359ba7dda13441c7db0026e81290e506f
ex3ndr <me@ex3ndr.com>
2015-06-25 19:32:35 +0300

    chore(*): Subscript configs

--- actor-apps/updateApi.sh
+++ actor-apps/updateApi.sh
@@ -7,4 +7,9 @@ cd "$SCRIPT_DIR/core/src/main/java/im/actor/model/api/"
 rm -fr "*"
 
 cd "$SCRIPT_DIR/build-tools/codegen-java/"
-"./gradlew" run  "-PappArgs=['$SCRIPT_DIR/../actor-api/actor.json', '$SCRIPT_DIR/core/src/main/java/']" 
+
+if [ "$#" -ne 0 ]; then
+	"./gradlew" run  "-PappArgs=['$SCRIPT_DIR/../actor-api/actor.json', '$1', '$SCRIPT_DIR/core/src/main/java/']" 
+else 
+	"./gradlew" run  "-PappArgs=['$SCRIPT_DIR/../actor-api/actor.json', '$SCRIPT_DIR/core/src/main/java/']" 
+fi
