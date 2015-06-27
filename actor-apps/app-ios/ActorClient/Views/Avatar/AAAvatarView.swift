c252c4409c9f67aa4c75bc8ce0214db48e1f981e
ex3ndr <me@ex3ndr.com>
2015-06-27 17:22:01 +0300

    wip(iOS): Collection view mirgration

--- actor-apps/app-ios/ActorClient/Views/Avatar/AAAvatarView.swift
+++ actor-apps/app-ios/ActorClient/Views/Avatar/AAAvatarView.swift
@@ -249,3 +249,18 @@ class AAAvatarView: UIImageView {
     }
 
 }
+
+class BarAvatarView : AAAvatarView {
+    
+    override init(frameSize: Int, type: AAAvatarType) {
+        super.init(frameSize: frameSize, type: type)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func alignmentRectInsets() -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 36, bottom: 0, right: 8)
+    }
+}
