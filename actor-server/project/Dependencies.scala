932f70fb104d4e79f2d1f1341e59c48bc38cfc1e
Nikolay Tatarinov <rockjam@actor.im>
2015-06-28 22:46:43 +0300

    chore(build): reduce dependency on aws java sdk

--- actor-server/project/Dependencies.scala
+++ actor-server/project/Dependencies.scala
@@ -41,7 +41,7 @@ object Dependencies {
     val flywayCore              = "org.flywaydb"                  %  "flyway-core"                   % "3.1"
     val hikariCP                = "com.zaxxer"                    %  "HikariCP"                      % "2.3.5"
 
-    val amazonaws               = "com.amazonaws"                 %  "aws-java-sdk"                  % "1.9.31"
+    val amazonaws               = "com.amazonaws"                 %  "aws-java-sdk-s3"               % "1.9.31"
     val awsWrap                 = "com.github.dwhjames"           %% "aws-wrap"                      % "0.7.2"
 
     val bcprov                  = "org.bouncycastle"              %  "bcprov-jdk15on"                % "1.50"
