24d2d1d1d0b9b10bb67348e41e3048b5526e2eb0
rock_jam <5min4eq.unity@gmail.com>
2015-06-29 01:56:22 +0300

    feat(http-api): route for serving static files

--- actor-server/actor-http-api/src/main/scala/im/actor/server/api/http/HttpApiConfig.scala
+++ actor-server/actor-http-api/src/main/scala/im/actor/server/api/http/HttpApiConfig.scala
@@ -2,13 +2,14 @@ package im.actor.server.api.http
 
 import com.typesafe.config.Config
 
-case class HttpApiConfig(baseUrl: String, interface: String, port: Int)
+case class HttpApiConfig(baseUrl: String, interface: String, port: Int, staticFilesDirectory: String)
 
 object HttpApiConfig {
   def fromConfig(config: Config): HttpApiConfig =
     HttpApiConfig(
       config.getString("base-uri"),
       config.getString("interface"),
-      config.getInt("port")
+      config.getInt("port"),
+      config.getString("static-files-directory")
     )
 }
