49b4fa9cde9dda5d98d2591d76fb6aa13d43e5dc
rock_jam <5min4eq.unity@gmail.com>
2015-06-29 01:56:22 +0300

    feat(http-api): route for serving static files

--- actor-server/actor-http-api/src/main/scala/im/actor/server/api/http/status/StatusHandler.scala
+++ actor-server/actor-http-api/src/main/scala/im/actor/server/api/http/status/StatusHandler.scala
@@ -3,8 +3,10 @@ package im.actor.server.api.http.status
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 
-class StatusHandler {
-  def routes: Route = path("status") {
+import im.actor.server.api.http.RoutesHandler
+
+class StatusHandler extends RoutesHandler {
+  override def routes: Route = path("status") {
     get {
       complete("")
     }
