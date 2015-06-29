38444e3b14f61b8d67c02dbde7460e5aaa141d7c
prettynatty <fear@loathing.in>
2015-06-30 01:57:58 +0300

    feat(server): new sql configuration

--- actor-server/actor-persist/src/main/scala/im/actor/server/db/DbInit.scala
+++ actor-server/actor-persist/src/main/scala/im/actor/server/db/DbInit.scala
@@ -1,12 +1,31 @@
 package im.actor.server.db
 
+import scala.util.Try
+
+import com.github.kxbmap.configs._
 import com.typesafe.config._
 import slick.driver.PostgresDriver.api.Database
 import slick.jdbc.{ HikariCPJdbcDataSource, JdbcDataSource }
 
 trait DbInit {
-  def initDs(sqlConfig: Config): HikariCPJdbcDataSource = {
-    HikariCPJdbcDataSource.forConfig(sqlConfig, null, "")
+  def initDs(sqlConfig: Config): Try[HikariCPJdbcDataSource] = {
+    for {
+      host ← sqlConfig.get[Try[String]]("host")
+      port ← sqlConfig.get[Try[Int]]("port")
+      db ← sqlConfig.get[Try[String]]("db")
+      _ ← sqlConfig.get[Try[String]]("user")
+      _ ← sqlConfig.get[Try[String]]("password")
+    } yield {
+      val conf = sqlConfig.withFallback(ConfigFactory.parseString(
+        s"""
+          |{
+          |  url: "jdbc:postgresql://${host}:${port}/${db}"
+          |}
+        """.stripMargin
+      ))
+
+      HikariCPJdbcDataSource.forConfig(conf, null, "")
+    }
   }
 
   def initDb(source: JdbcDataSource) =
