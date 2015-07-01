cbcd89b00c408c5f019f0325ed4eaea528d47be4
korka <badgrib@gmail.com>
2015-07-01 20:07:53 +0300

    fix(android): friends top set handle clicks

--- actor-apps/app-android/src/main/java/im/actor/messenger/app/fragment/group/JoinPublicGroupFragment.java
+++ actor-apps/app-android/src/main/java/im/actor/messenger/app/fragment/group/JoinPublicGroupFragment.java
@@ -134,7 +134,7 @@ public class JoinPublicGroupFragment extends BaseFragment {
                             }
                         });
                         PublicGroupSetView topFriendsGroupSetView = new PublicGroupSetView(getActivity(), new PublicGroupSet(topByFriendsGroupsSet, getString(R.string.join_public_group_top_by_friends_title), getString(R.string.join_public_group_top_by_friends_subtitle)), PublicGroupCardView.COUNTER_TYPE_FRIENDS);
-                        topMembersGroupSetView.setOnGroupClickListener(new PublicGroupSetView.GroupClickListener() {
+                        topFriendsGroupSetView.setOnGroupClickListener(new PublicGroupSetView.GroupClickListener() {
                             @Override
                             public void onClick(PublicGroup group) {
                                 openGroup(group);
