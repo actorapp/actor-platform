cc1d97f66fceaa729569be3bf4eda09352fa29ec
korka <badgrib@gmail.com>
2015-07-02 16:52:29 +0300

    chore(android): join public group always popup, all groups separator, ui fixes

--- actor-apps/app-android/src/main/java/im/actor/messenger/app/fragment/group/JoinPublicGroupPopUpFragment.java
+++ actor-apps/app-android/src/main/java/im/actor/messenger/app/fragment/group/JoinPublicGroupPopUpFragment.java
@@ -40,6 +40,7 @@ public class JoinPublicGroupPopUpFragment extends BaseFragment {
     String description;
     String title;
     int members;
+    boolean isMember;
     private CoverAvatarView avatarView;
 
     @Override
@@ -56,6 +57,7 @@ public class JoinPublicGroupPopUpFragment extends BaseFragment {
         description = getArguments().getString("description");
         accessHash = getArguments().getLong("accessHash");
         members = getArguments().getInt("members");
+        isMember = getArguments().getBoolean("isMember");
         View res = inflater.inflate(R.layout.fragment_join_public_group_pop_up, container, false);
 
         // Avatar
@@ -80,30 +82,36 @@ public class JoinPublicGroupPopUpFragment extends BaseFragment {
         final TextView membersCount = (TextView) res.findViewById(R.id.membersCount);
         membersCount.setText(getString(R.string.join_public_group_members_count).concat(Integer.toString(members)));
 
-
-        ((TextView) res.findViewById(R.id.joinButtonText)).setTypeface(Fonts.medium());
+        TextView buttonText = (TextView) res.findViewById(R.id.joinButtonText);
+        buttonText.setTypeface(Fonts.medium());
+        buttonText.setText(getString(isMember ? R.string.open : R.string.join));
 
         res.findViewById(R.id.joinButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                execute(messenger().joinPublicGroup(id, accessHash), R.string.main_fab_join_public_group, new CommandCallback<Integer>() {
-                    @Override
-                    public void onResult(Integer res) {
-                        startActivity(Intents.openDialog(Peer.group(res), false, getActivity()));
-                        getActivity().finish();
-                    }
+                if (isMember) {
+                    startActivity(Intents.openDialog(Peer.group(id), false, getActivity()));
+                    getActivity().finish();
+                } else {
+                    execute(messenger().joinPublicGroup(id, accessHash), R.string.main_fab_join_public_group, new CommandCallback<Integer>() {
+                        @Override
+                        public void onResult(Integer res) {
+                            startActivity(Intents.openDialog(Peer.group(res), false, getActivity()));
+                            getActivity().finish();
+                        }
 
-                    @Override
-                    public void onError(Exception e) {
-                        if (e instanceof RpcException) {
-                            RpcException re = (RpcException) e;
-                            if ("USER_ALREADY_INVITED".equals(re.getTag())) {
-                                startActivity(Intents.openDialog(Peer.group(id), false, getActivity()));
-                                getActivity().finish();
+                        @Override
+                        public void onError(Exception e) {
+                            if (e instanceof RpcException) {
+                                RpcException re = (RpcException) e;
+                                if ("USER_ALREADY_INVITED".equals(re.getTag())) {
+                                    startActivity(Intents.openDialog(Peer.group(id), false, getActivity()));
+                                    getActivity().finish();
+                                }
                             }
                         }
-                    }
-                });
+                    });
+                }
             }
         });
 
