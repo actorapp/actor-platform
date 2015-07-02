53c9532853b27c8849033663c86e7455362b161d
korka <badgrib@gmail.com>
2015-07-02 20:11:17 +0300

    chore(android): oauth dialog to fragment

--- actor-apps/app-android/src/main/java/im/actor/messenger/app/fragment/auth/OAuthFragment.java
+++ actor-apps/app-android/src/main/java/im/actor/messenger/app/fragment/auth/OAuthFragment.java
@@ -1,29 +1,32 @@
 package im.actor.messenger.app.fragment.auth;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
+import im.actor.messenger.R;
+import im.actor.messenger.app.view.KeyboardHelper;
 import im.actor.model.modules.Auth;
 
 import static im.actor.messenger.app.Core.messenger;
 
 /**
- * Created by korka on 23.06.15.
+ * Created by korka on 02.07.15.
  */
-public class OAuthDialogActivity extends Activity {
+public class OAuthFragment extends BaseAuthFragment {
+    WebView wv;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        WebView wv = new WebView(this);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        wv = new WebView(getActivity());
         wv.getSettings().setJavaScriptEnabled(true);
         wv.loadUrl(messenger().getPreferences().getString(Auth.KEY_OAUTH_REDIRECT_URL));
         wv.setWebViewClient(new WebViewClient() {
@@ -37,22 +40,21 @@ public class OAuthDialogActivity extends Activity {
             @Override
             public void onLoadResource(WebView view, String url) {
                 if (url.startsWith("https://actor.im/auth/oauth2callback")) {
+                    goneView(wv);
                     Uri u = Uri.parse(url);
                     String code = u.getQueryParameter("code");
-                    setResult(RESULT_OK, new Intent().putExtra("code", code));
-                    finish();
+                    executeAuth(messenger().requestCompleteOAuth(code), "Sign in");
                 } else {
                     super.onLoadResource(view, url);
                 }
             }
         });
+        return wv;
+    }
 
-        LinearLayout ll = new LinearLayout(this);
-        ll.setGravity(Gravity.CENTER);
-        ll.setOrientation(LinearLayout.VERTICAL);
-        LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        ll.addView(wv, llp);
-
-        setContentView(ll);
+    @Override
+    public void onResume() {
+        super.onResume();
+        setTitle(R.string.auth_email_title);
     }
 }
