69ededd20e7a0d56a33f6f6bd942c9b2749dc2e2
rock_jam <5min4eq.unity@gmail.com>
2015-07-03 22:05:53 +0300

    fix(persist): mark auth codes deleted instead of deleting them directly

--- actor-server/actor-persist/src/main/scala/im/actor/server/persist/AuthSmsCodeObsolete.scala
+++ actor-server/actor-persist/src/main/scala/im/actor/server/persist/AuthSmsCodeObsolete.scala
@@ -5,20 +5,24 @@ import slick.driver.PostgresDriver.api._
 import im.actor.server.models
 
 class AuthSmsCodeObsoleteTable(tag: Tag) extends Table[models.AuthSmsCodeObsolete](tag, "auth_sms_codes_obsolete") {
-  def phoneNumber = column[Long]("phone_number", O.PrimaryKey)
+  def id = column[Long]("id", O.PrimaryKey)
+  def phoneNumber = column[Long]("phone_number")
   def smsHash = column[String]("sms_hash")
   def smsCode = column[String]("sms_code")
+  def isDeleted = column[Boolean]("is_deleted")
 
-  def * = (phoneNumber, smsHash, smsCode) <> (models.AuthSmsCodeObsolete.tupled, models.AuthSmsCodeObsolete.unapply)
+  def * = (id, phoneNumber, smsHash, smsCode, isDeleted) <> (models.AuthSmsCodeObsolete.tupled, models.AuthSmsCodeObsolete.unapply)
 }
 
 object AuthSmsCodeObsolete {
   val codes = TableQuery[AuthSmsCodeObsoleteTable]
 
-  def create(phoneNumber: Long, smsHash: String, smsCode: String) =
-    codes += models.AuthSmsCodeObsolete(phoneNumber, smsHash, smsCode)
+  def create(id: Long, phoneNumber: Long, smsHash: String, smsCode: String) =
+    codes += models.AuthSmsCodeObsolete(id, phoneNumber, smsHash, smsCode)
 
-  def findByPhoneNumber(number: Long) = codes.filter(_.phoneNumber === number).result
+  def findByPhoneNumber(number: Long) =
+    codes.filter(c ⇒ c.phoneNumber === number && c.isDeleted === false).result
 
-  def deleteByPhoneNumber(number: Long) = codes.filter(_.phoneNumber === number).delete
+  def deleteByPhoneNumber(number: Long) =
+    codes.filter(_.phoneNumber === number).map(_.isDeleted).update(true)
 }
