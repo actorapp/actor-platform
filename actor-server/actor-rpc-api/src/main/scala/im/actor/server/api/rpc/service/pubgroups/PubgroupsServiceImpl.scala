b9f4605fc4f61b2e8fd935429238a579b8959af5
Nikolay Tatarinov <rockjam@actor.im>
2015-07-04 01:12:43 +0300

    fix(rpc): wrong pubgroup sorting

--- actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/pubgroups/PubgroupsServiceImpl.scala
+++ actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/pubgroups/PubgroupsServiceImpl.scala
@@ -30,7 +30,7 @@ class PubgroupsServiceImpl(
       for {
         groups ← persist.Group.findPublic
         pubGroupStructs ← DBIO.sequence(groups.view map getPubgroupStructUnsafe)
-        sorted = pubGroupStructs.sortWith((g1, g2) ⇒ g1.friendsCount > g2.friendsCount && g1.membersCount >= g2.membersCount)
+        sorted = pubGroupStructs.sortWith((g1, g2) ⇒ g1.friendsCount >= g2.friendsCount && g1.membersCount >= g2.membersCount)
       } yield Ok(ResponseGetPublicGroups(sorted.toVector))
     }
 
