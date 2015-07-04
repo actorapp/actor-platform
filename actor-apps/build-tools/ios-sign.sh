3b2b948dadd1ee1e11ceaddb1df93aec3a9e4a4d
ex3ndr <me@ex3ndr.com>
2015-07-04 23:37:17 +0300

    chore(iOS): Restructure build scripts for iOS

---
+++ actor-apps/build-tools/ios-sign.sh
@@ -0,0 +1,26 @@
+# !/bin/bash
+
+APP=$1
+CERTIFICATE=$2
+PROVISION_FILE=$3
+
+# Adding Provision
+cp $PROVISION_FILE "build/Applications/$APP.app/embedded.mobileprovision"
+
+# Extracting entitlements.plist
+security cms -D -i $PROVISION_FILE > build/Applications/profile.plist
+# plutil -convert binary1 build/Applications/entitlements.plist
+
+# Sign App
+cd build/Applications
+codesign --force --sign "$CERTIFICATE" --entitlements entitlements.plist "$APP.app/Frameworks/*"
+codesign --force --sign "$CERTIFICATE" --entitlements entitlements.plist "$APP.app"
+cd ../../
+
+# Package app
+xcrun -sdk iphoneos PackageApplication -v "build/Applications/$APP.app" -o "$DEST_IPA"
+
+Acrhive dSYM files
+cd build
+zip -r $APP.dSYM.zip $APP.app.dSYM
+cd ..
