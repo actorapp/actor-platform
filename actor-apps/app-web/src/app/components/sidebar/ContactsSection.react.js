00dea1b77cdcc943e0f3175b592bb7c57df2ad4a
ex3ndr <me@ex3ndr.com>
2015-07-05 03:45:36 +0300

    chore(core): Fix web

---
+++ actor-apps/app-web/src/app/components/sidebar/ContactsSection.react.js
@@ -0,0 +1,57 @@
+import _ from 'lodash';
+
+import React from 'react';
+
+import ContactStore from '../../stores/ContactStore';
+import ContactActionCreators from '../../actions/ContactActionCreators';
+
+import ContactsSectionItem from './ContactsSectionItem.react';
+
+const getStateFromStores = () => {
+  return {
+    contacts: ContactStore.getContacts()
+  };
+};
+
+class ContactsSection extends React.Component {
+  componentWillMount() {
+    ContactActionCreators.showContactList();
+    ContactStore.addChangeListener(this.onChange);
+  }
+
+  componentWillUnmount() {
+    ContactActionCreators.hideContactList();
+    ContactStore.removeChangeListener(this.onChange);
+  }
+
+  constructor() {
+    super();
+
+    this.onChange = this.onChange.bind(this);
+
+    this.state = getStateFromStores();
+  }
+
+  onChange() {
+    this.setState(getStateFromStores());
+  }
+
+  render() {
+    let contacts = this.state.contacts;
+
+    let contactList = _.map(contacts, (contact, i) => {
+      return (
+        <ContactsSectionItem contact={contact} key={i}/>
+      );
+    });
+
+    return (
+      <ul className="sidebar__list sidebar__list--contacts">
+        {contactList}
+      </ul>
+    );
+  }
+}
+
+export default ContactsSection;
+
