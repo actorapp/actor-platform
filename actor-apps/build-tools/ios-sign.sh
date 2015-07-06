26af3582a5032ba4e22454b347fdfe630573b4dc
ex3ndr <me@ex3ndr.com>
2015-07-06 03:40:24 +0300

    chore(iOS): Disable binary entitlements

--- actor-apps/build-tools/ios-sign.sh
+++ actor-apps/build-tools/ios-sign.sh
@@ -11,7 +11,7 @@ cp $PROVISION_FILE "build/Applications/$APP.app/embedded.mobileprovision"
 # Extracting entitlements.plist
 security cms -D -i $PROVISION_FILE > build/Applications/profile.plist
 /usr/libexec/PlistBuddy -x -c 'Print:Entitlements' build/Applications/profile.plist > build/Applications/entitlements.plist
-plutil -convert binary1 build/Applications/entitlements.plist
+# plutil -convert binary1 build/Applications/entitlements.plist
 
 # Sign App
 cd build/Applications
