ffd607ff01ad0c2602943fd6316263fc8e373e3f
John Doe <speed@actor.im>
2015-07-06 21:23:39 +0300

    feat+chore(web): add material-ui components to sidebar;

--- actor-apps/app-web/src/app/components/SidebarSection.react.js
+++ actor-apps/app-web/src/app/components/SidebarSection.react.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
-import Tabs from 'react-simpletabs';
+import { Styles, Tabs, Tab } from 'material-ui';
+import ActorTheme from '../constants/ActorTheme';
 
 import HeaderSection from './sidebar/HeaderSection.react';
 import RecentSection from './sidebar/RecentSection.react';
 import ContactsSection from './sidebar/ContactsSection.react';
 
+const ThemeManager = new Styles.ThemeManager();
+
+
 class SidebarSection extends React.Component {
+  static childContextTypes = {
+    muiTheme: React.PropTypes.object
+  };
+
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme()
+    };
+  }
+
+  componentWillMount() {
+    ThemeManager.setTheme(ActorTheme);
+  }
+
   constructor() {
     super();
   }
@@ -16,6 +34,22 @@ class SidebarSection extends React.Component {
       <aside className="sidebar">
         <HeaderSection/>
 
+        <Tabs className="sidebar__tabs"
+              contentContainerClassName="sidebar__tabs__tab-content"
+              tabItemContainerClassName="sidebar__tabs__tab-items">
+
+          <Tab label="Recent">
+            <RecentSection/>
+          </Tab>
+
+          <Tab label="Contacts">
+            <ContactsSection/>
+          </Tab>
+
+        </Tabs>
+
+
+        {/*
         <Tabs className="sidebar__tabs">
           <Tabs.Panel title="Recent">
             <RecentSection/>
@@ -24,6 +58,7 @@ class SidebarSection extends React.Component {
             <ContactsSection/>
           </Tabs.Panel>
         </Tabs>
+         */}
       </aside>
     );
   }
