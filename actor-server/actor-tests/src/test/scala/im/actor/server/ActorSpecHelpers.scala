ef7e143cfa82ae5289c41596a2aa4b279b6a60e2
John Doe <speed@actor.im>
2015-07-07 22:54:06 +0300

    fix(server): fixed some docker and configuration issues

--- actor-server/actor-tests/src/test/scala/im/actor/server/ActorSpecHelpers.scala
+++ actor-server/actor-tests/src/test/scala/im/actor/server/ActorSpecHelpers.scala
@@ -7,11 +7,9 @@ import im.actor.server.push._
 
 trait ActorSpecHelpers {
   def buildSeqUpdManagerRegion()(implicit system: ActorSystem, db: Database): SeqUpdatesManagerRegion = {
-    val gcmConfig = system.settings.config.getConfig("push.google")
     val appleConfig = ApplePushManagerConfig.load(system.settings.config.getConfig("push.apple"))
 
     implicit val googlePushManager = new GooglePushManager(GooglePushManagerConfig(List.empty))
-
     implicit val applePushManager = new ApplePushManager(appleConfig, system)
 
     SeqUpdatesManager.startRegion()
