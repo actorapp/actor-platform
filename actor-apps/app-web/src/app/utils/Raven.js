49df963a17785219a697f3c2a4f9ce94e9f2f493
John Doe <speed@actor.im>
2015-07-09 12:36:50 +0300

    chore(web): add sourcemaps support;

--- actor-apps/app-web/src/app/utils/Raven.js
+++ actor-apps/app-web/src/app/utils/Raven.js
@@ -1,5 +1,5 @@
 import Raven from 'raven-js';
-import Console from 'raven-js/plugins/console'; // eslint-disable-line
+//import Console from 'raven-js/plugins/console'; // eslint-disable-line
 
 Raven.config('https://f35bd82f5a7a44cea27c0d7f09322c4f@app.getsentry.com/47478', {
   logger: 'javascript',
