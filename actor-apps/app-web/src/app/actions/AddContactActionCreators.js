258dc142764f01b5fce5b8613e204205472e9fa5
John Doe <speed@actor.im>
2015-07-10 22:59:23 +0300

    feat(web): update add contact store, actions and component;

--- actor-apps/app-web/src/app/actions/AddContactActionCreators.js
+++ actor-apps/app-web/src/app/actions/AddContactActionCreators.js
@@ -1,5 +1,8 @@
 import ActorAppDispatcher from '../dispatcher/ActorAppDispatcher';
+import ActorClient from '../utils/ActorClient';
 import { ActionTypes } from '../constants/ActorAppConstants';
+import ContactActionCreators from './ContactActionCreators';
+import DialogActionCreators from './DialogActionCreators';
 
 export default {
   openModal: () => {
@@ -15,9 +18,26 @@ export default {
   },
 
   findUsers: phone => {
-    ActorAppDispatcher.dispatch({
-      type: ActionTypes.CONTACT_ADD_MODAL_FIND_USERS,
-      phone: phone
-    });
+    ActorClient.findUsers(phone)
+      .then(users => {
+        if (users.length > 0) {
+          const uid = users[0].id;
+          const userPeer = ActorClient.getUserPeer(uid);
+
+          ContactActionCreators.addContact(uid);
+          DialogActionCreators.selectDialogPeer(userPeer);
+
+          ActorAppDispatcher.dispatch({
+            type: ActionTypes.CONTACT_ADD_MODAL_FIND_USER_OK
+          });
+
+        } else {
+          ActorAppDispatcher.dispatch({
+            type: ActionTypes.CONTACT_ADD_MODAL_FIND_USER_UNREGISTERED
+          });
+        }
+      }).catch(error => {
+        throw new Error(error);
+      });
   }
 };
