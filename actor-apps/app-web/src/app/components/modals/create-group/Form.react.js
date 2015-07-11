7e7ccc6642ebd96547bf438239241d1444bbc00c
Oleg Shilov <me@olegshilov.com>
2015-07-11 05:18:18 +0300

    feat+refactor(web): implement new create group modal window;

--- actor-apps/app-web/src/app/components/modals/create-group/Form.react.js
+++ actor-apps/app-web/src/app/components/modals/create-group/Form.react.js
@@ -3,12 +3,17 @@ import Immutable from 'immutable';
 import keymirror from 'keymirror';
 
 import React from 'react';
+import { Styles, TextField, FlatButton } from 'material-ui';
 
 import CreateGroupActionCreators from '../../../actions/CreateGroupActionCreators';
 
 import ContactStore from '../../../stores/ContactStore';
 
 import ContactItem from './ContactItem.react';
+
+import ActorTheme from '../../../constants/ActorTheme';
+
+const ThemeManager = new Styles.ThemeManager();
 
 const STEPS = keymirror({
   NAME_INPUT: null,
@@ -18,20 +23,33 @@ const STEPS = keymirror({
 class CreateGroupForm extends React.Component {
   static displayName = 'CreateGroupForm'
 
+  static childContextTypes = {
+    muiTheme: React.PropTypes.object
+  };
+
+  state = {
+    step: STEPS.NAME_INPUT,
+    selectedUserIds: new Immutable.Set()
+  }
+
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme()
+    };
+  }
+
   constructor(props) {
     super(props);
 
-    this.state = {
-      step: STEPS.NAME_INPUT,
-      selectedUserIds: new Immutable.Set()
-    };
-
-    this.onNameChange = this.onNameChange.bind(this);
-    this.onNameSubmit = this.onNameSubmit.bind(this);
-
-    this.onMembersSubmit = this.onMembersSubmit.bind(this);
-
-    this.onContactToggle = this.onContactToggle.bind(this);
+    ThemeManager.setTheme(ActorTheme);
+    ThemeManager.setComponentThemes({
+      textField: {
+        textColor: 'rgba(0,0,0,.87)',
+        focusColor: '#68a3e7',
+        backgroundColor: 'transparent',
+        borderColor: '#68a3e7'
+      }
+    });
   }
 
   render() {
@@ -41,10 +59,23 @@ class CreateGroupForm extends React.Component {
       case STEPS.NAME_INPUT:
         stepForm = (
           <form className="group-name" onSubmit={this.onNameSubmit}>
-            <input onChange={this.onNameChange} placeholder="Group name" value={this.state.name}/>
-            <footer>
-              <button className="button button--primary">Add members</button>
+
+            <div className="modal-new__body">
+              <TextField className="login__form__input"
+                         floatingLabelText="Group name"
+                         fullWidth
+                         onChange={this.onNameChange}
+                         value={this.state.name}/>
+
+            </div>
+
+            <footer className="modal-new__footer text-right">
+              <FlatButton hoverColor="rgba(74,144,226,.12)"
+                          label="Add members"
+                          secondary={true}
+                          type="submit"/>
             </footer>
+
           </form>
         );
         break;
@@ -57,12 +88,21 @@ class CreateGroupForm extends React.Component {
 
         stepForm = (
           <form className="group-members" onSubmit={this.onMembersSubmit}>
-            <ul className="contacts__list">
-              {contactList}
-            </ul>
 
-            <footer>
-              <button className="button button--primary">Create group</button>
+            <div className="count">{this.state.selectedUserIds.size} Members</div>
+
+            <div className="modal-new__body">
+              <ul className="contacts__list">
+                {contactList}
+              </ul>
+            </div>
+
+            <footer className="modal-new__footer text-right">
+              <FlatButton hoverColor="rgba(74,144,226,.12)"
+                          label="Create group"
+                          secondary={true}
+                          type="submit"/>
+
             </footer>
           </form>
         );
@@ -72,7 +112,7 @@ class CreateGroupForm extends React.Component {
     return stepForm;
   }
 
-  onContactToggle(contact, isSelected) {
+  onContactToggle = (contact, isSelected) => {
     if (isSelected) {
       this.setState({selectedUserIds: this.state.selectedUserIds.add(contact.uid)});
     } else {
@@ -80,13 +120,13 @@ class CreateGroupForm extends React.Component {
     }
   }
 
-  onNameChange(event) {
+  onNameChange = event => {
     event.preventDefault();
 
     this.setState({name: event.target.value});
   }
 
-  onNameSubmit(event) {
+  onNameSubmit = event => {
     event.preventDefault();
     if (this.state.name) {
       let name = this.state.name.trim();
@@ -96,7 +136,7 @@ class CreateGroupForm extends React.Component {
     }
   }
 
-  onMembersSubmit(event) {
+  onMembersSubmit =event => {
     event.preventDefault();
     CreateGroupActionCreators.createGroup(this.state.name, null, this.state.selectedUserIds.toJS());
   }
