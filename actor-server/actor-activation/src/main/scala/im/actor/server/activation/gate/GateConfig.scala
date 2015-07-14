657b289511e35a02ab8e29d25acc19fdfcd2d5cc
rock_jam <5min4eq.unity@gmail.com>
2015-07-14 19:43:03 +0300

    feat(server: activation): add auth token to requests

--- actor-server/actor-activation/src/main/scala/im/actor/server/activation/gate/GateConfig.scala
+++ actor-server/actor-activation/src/main/scala/im/actor/server/activation/gate/GateConfig.scala
@@ -5,13 +5,14 @@ import scala.util.Try
 import com.github.kxbmap.configs._
 import com.typesafe.config.{ Config, ConfigFactory }
 
-case class GateConfig(uri: String)
+case class GateConfig(uri: String, authToken: String)
 
 object GateConfig {
   def load(config: Config): Try[GateConfig] =
     for {
       uri ← config.get[Try[String]]("uri")
-    } yield GateConfig(uri)
+      authToken ← config.get[Try[String]]("auth-token")
+    } yield GateConfig(uri, authToken)
 
   def load: Try[GateConfig] = {
     load(ConfigFactory.load().getConfig("services.activation-gate"))
