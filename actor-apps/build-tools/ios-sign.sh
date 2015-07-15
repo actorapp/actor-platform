b1698a4db8f9d02bcb205e094aee684b5bbd261d
ex3ndr <me@ex3ndr.com>
2015-07-15 04:44:01 +0300

    chore(iOS): Trying to fix entitlements on resign

--- actor-apps/build-tools/ios-sign.sh
+++ actor-apps/build-tools/ios-sign.sh
@@ -4,14 +4,14 @@ set -e
 APP=$1
 CERTIFICATE=$2
 PROVISION_FILE=$3
+ENTITLEMENTS_FILE=$4
 
 # Adding Provision
 cp $PROVISION_FILE "build/Applications/$APP.app/embedded.mobileprovision"
 
 # Extracting entitlements.plist
-security cms -D -i $PROVISION_FILE > build/Applications/profile.plist
-/usr/libexec/PlistBuddy -x -c 'Print:Entitlements' build/Applications/profile.plist > build/Applications/entitlements.plist
-# plutil -convert binary1 build/Applications/entitlements.plist
+# security cms -D -i $PROVISION_FILE > build/Applications/profile.plist
+# /usr/libexec/PlistBuddy -x -c 'Print:Entitlements' build/Applications/profile.plist > build/Applications/entitlements.plist
 
 # Sign App
 cd build/Applications
@@ -27,5 +27,5 @@ do
 done
 cd ../../
 
-codesign --force --sign "$CERTIFICATE" --entitlements entitlements.plist "$APP.app"
+codesign --force --sign "$CERTIFICATE" --entitlements "$ENTITLEMENTS_FILE" "$APP.app"
 cd ../../
