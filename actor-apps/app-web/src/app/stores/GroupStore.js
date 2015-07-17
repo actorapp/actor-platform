fb95eae04a7fd51f8d074cde5c84aee2409268b1
Oleg Shilov <me@olegshilov.com>
2015-07-18 00:40:55 +0300

    feat+refactor(web): show integration token in group information panel;

--- actor-apps/app-web/src/app/stores/GroupStore.js
+++ actor-apps/app-web/src/app/stores/GroupStore.js
@@ -1,13 +1,45 @@
-import ActorClient from 'utils/ActorClient';
+import { EventEmitter } from 'events';
+import ActorAppDispatcher from 'dispatcher/ActorAppDispatcher';
+import { ActionTypes } from 'constants/ActorAppConstants';
+import GroupProfileActionCreators from 'actions/GroupProfileActionCreators';
 
-const GroupStore = {
+const CHANGE_EVENT = 'change';
+
+let _integrationToken;
+
+class GroupStore extends EventEmitter {
   getGroup(gid) {
-    return ActorClient.getGroup(gid);
-  },
-
-  getIntegrationToken(gid) {
-    return ActorClient.getIntegrationToken(gid);
+    return GroupProfileActionCreators.getGroup(gid);
   }
-};
 
-export default GroupStore;
+  getIntegrationToken() {
+    return _integrationToken;
+  }
+
+  emitChange() {
+    this.emit(CHANGE_EVENT);
+  }
+
+  addChangeListener(callback) {
+    this.on(CHANGE_EVENT, callback);
+  }
+
+  removeChangeListener(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  }
+}
+
+let GroupStoreInstance = new GroupStore();
+
+GroupStoreInstance.dispatchToken = ActorAppDispatcher.register(action => {
+  switch (action.type) {
+    case ActionTypes.GET_INTEGRATION_TOKEN:
+      _integrationToken = action.token;
+      break;
+    default:
+      return;
+  }
+  GroupStoreInstance.emitChange();
+});
+
+export default GroupStoreInstance;
