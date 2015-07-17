283593ce4c3637c15f45be2fe50e07cb35164057
Oleg Shilov <me@olegshilov.com>
2015-07-18 01:39:09 +0300

    fix+reactor(web): fixed an issue sudden appearance of the activity panel;

--- actor-apps/app-web/src/app/stores/ActivityStore.js
+++ actor-apps/app-web/src/app/stores/ActivityStore.js
@@ -7,31 +7,35 @@ import DialogStore from 'stores/DialogStore';
 import { EventEmitter } from 'events';
 import assign from 'object-assign';
 
-var CHANGE_EVENT = 'change';
+const CHANGE_EVENT = 'change';
 
-var _activity = null;
+let _isOpen = false,
+    _activity = null;
 
 var ActivityStore = assign({}, EventEmitter.prototype, {
-  getActivity: function () {
+  getActivity() {
     return _activity;
   },
 
-  emitChange: function () {
+  isOpen() {
+    return _isOpen;
+  },
+
+  emitChange() {
     this.emit(CHANGE_EVENT);
   },
 
-  addChangeListener: function (callback) {
+  addChangeListener(callback) {
     this.on(CHANGE_EVENT, callback);
   },
 
-  removeChangeListener: function (callback) {
+  removeChangeListener(callback) {
     this.removeListener(CHANGE_EVENT, callback);
   }
 });
 
 
-var _cleanup = function () {
-};
+let _cleanup = () => {};
 
 var _setActivityFromPeer = function () {
   _cleanup();
@@ -52,7 +56,6 @@ var _setActivityFromPeer = function () {
       _cleanup = function () {
         ActorClient.unbindUser(peer.id, change);
       };
-
       ActorClient.bindUser(peer.id, change);
     }
       break;
@@ -76,30 +79,28 @@ var _setActivityFromPeer = function () {
     }
       break;
     default:
+      return;
   }
 };
 
-ActivityStore.dispatchToken = ActorAppDispatcher.register(function (action) {
+ActivityStore.dispatchToken = ActorAppDispatcher.register(action => {
+  console.info(action);
   switch (action.type) {
     case ActionTypes.HIDE_ACTIVITY:
-      _activity = null;
-      ActivityStore.emitChange();
+      _isOpen = false;
       break;
-
     case ActionTypes.SHOW_ACTIVITY:
+      _isOpen = true;
+      break;
+    case ActionTypes.SELECT_DIALOG_PEER:
       ActorAppDispatcher.waitFor([DialogStore.dispatchToken]);
       _setActivityFromPeer();
       break;
 
-    case ActionTypes.SELECT_DIALOG_PEER:
-      if (_activity != null) { // check if it is not hidden
-        ActorAppDispatcher.waitFor([DialogStore.dispatchToken]);
-        _setActivityFromPeer();
-      }
-      break;
-
     default:
+      return;
   }
+  ActivityStore.emitChange();
 });
 
 export default ActivityStore;
