af3cbbdf3af8435b8664dd8aad24ec30f5ff800a
Oleg Shilov <me@olegshilov.com>
2015-07-24 00:06:03 +0300

    feat+fix(web): change favicon on notification;

---
+++ actor-apps/app-web/src/app/stores/FaviconStore.js
@@ -0,0 +1,48 @@
+import { EventEmitter } from 'events';
+import ActorAppDispatcher from 'dispatcher/ActorAppDispatcher';
+import { ActionTypes } from 'constants/ActorAppConstants';
+
+const CHANGE_EVENT = 'change';
+
+const FaviconPath = {
+  DEFAULT: 'assets/img/favicon_logo_32x32.png',
+  NOTIFICATION: 'assets/img/favicon_notification_512x512.png'
+};
+
+let _iconPath = FaviconPath.DEFAULT;
+
+class FaviconStore extends EventEmitter {
+  getFaviconPath() {
+    return _iconPath;
+  }
+
+  emitChange() {
+    this.emit(CHANGE_EVENT);
+  }
+
+  addChangeListener(callback) {
+    this.on(CHANGE_EVENT, callback);
+  }
+
+  removeChangeListener(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  }
+}
+
+let FaviconStoreInstance = new FaviconStore();
+
+FaviconStoreInstance.dispatchToken = ActorAppDispatcher.register(action => {
+  switch(action.type) {
+    case ActionTypes.FAVICON_SET_DEFAULT:
+      _iconPath = FaviconPath.DEFAULT;
+      break;
+    case ActionTypes.FAVICON_SET_NOTIFICATION:
+      _iconPath = FaviconPath.NOTIFICATION;
+      break;
+    default:
+      return;
+  }
+  FaviconStoreInstance.emitChange();
+});
+
+export default FaviconStoreInstance;
