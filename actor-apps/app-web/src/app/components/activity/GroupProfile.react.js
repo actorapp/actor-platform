25dba4cebbb5dc03496ca18ee2cc6247d27fd900
Oleg Shilov <me@olegshilov.com>
2015-07-25 02:05:10 +0300

    feat(web): add new static group profile to activity window with react-ab;

--- actor-apps/app-web/src/app/components/activity/GroupProfile.react.js
+++ actor-apps/app-web/src/app/components/activity/GroupProfile.react.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
+import { Experiment, Variant } from 'react-ab';
+import mixpanel from 'utils/Mixpanel';
+
 import DialogActionCreators from 'actions/DialogActionCreators';
+import GroupProfileActionCreators from 'actions/GroupProfileActionCreators';
 
 import LoginStore from 'stores/LoginStore';
 import PeerStore from 'stores/PeerStore';
@@ -26,15 +30,6 @@ class GroupProfile extends React.Component {
     group: React.PropTypes.object.isRequired
   };
 
-  componentWillUnmount() {
-    DialogStore.removeNotificationsListener(this.onChange);
-    GroupStore.addChangeListener(this.onChange);
-  }
-
-  componentWillReceiveProps(newProps) {
-    this.setState(getStateFromStores(newProps.group.id));
-  }
-
   constructor(props) {
     super(props);
 
@@ -44,20 +39,34 @@ class GroupProfile extends React.Component {
     this.state = getStateFromStores(props.group.id);
   }
 
+  componentWillUnmount() {
+    DialogStore.removeNotificationsListener(this.onChange);
+    GroupStore.addChangeListener(this.onChange);
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.setState(getStateFromStores(newProps.group.id));
+  }
+
   onAddMemberClick = group => {
     InviteUserActions.modalOpen(group);
-  }
+  };
 
   onLeaveGroupClick = groupId => {
     DialogActionCreators.leaveGroup(groupId);
-  }
+  };
 
   onNotificationChange = event => {
     DialogActionCreators.changeNotificationsEnabled(this.state.thisPeer, event.target.checked);
-  }
+  };
 
   onChange = () => {
     this.setState(getStateFromStores(this.props.group.id));
+  };
+
+  onChoice = (experiment, variant) => {
+    console.warn('choose variant', experiment, variant);
+    mixpanel.register({'tagline': variant});
   };
 
   render() {
@@ -65,9 +74,11 @@ class GroupProfile extends React.Component {
     const myId = LoginStore.getMyId();
     const isNotificationsEnabled = this.state.isNotificationsEnabled;
     const integrationToken = this.state.integrationToken;
+    const admin = GroupProfileActionCreators.getUser(group.adminId);
 
-    let memberArea;
-    let adminControls;
+    console.info(group);
+    let memberArea,
+        adminControls;
 
     if (group.adminId === myId) {
       adminControls = (
@@ -80,15 +91,6 @@ class GroupProfile extends React.Component {
     if (DialogStore.isGroupMember(group)) {
       memberArea = (
         <div>
-          <div className="notifications">
-            <label htmlFor="notifications">Enable Notifications</label>
-
-            <div className="switch pull-right">
-              <input checked={isNotificationsEnabled} id="notifications" onChange={this.onNotificationChange.bind(this)} type="checkbox"/>
-              <label htmlFor="notifications"></label>
-            </div>
-          </div>
-
           <GroupProfileMembers groupId={group.id} members={group.members}/>
 
           <ul className="profile__list profile__list--controls">
@@ -98,7 +100,7 @@ class GroupProfile extends React.Component {
             <li className="profile__list__item">
               <a className="link__red" onClick={this.onLeaveGroupClick.bind(this, group.id)}>Leave group</a>
             </li>
-              {adminControls}
+            {adminControls}
           </ul>
 
           <InviteUser/>
@@ -107,20 +109,86 @@ class GroupProfile extends React.Component {
     }
 
     return (
-      <div className="activity__body profile">
-        <div className="profile__name">
-          <AvatarItem image={group.bigAvatar}
-                      placeholder={group.placeholder}
-                      size="medium"
-                      title={group.name}/>
-          <h3>{group.name}</h3>
+      <Experiment name="groupInfo" onChoice={this.onChoice}>
+        <Variant name="old">
+          <div className="activity__body profile">
+            <div className="profile__name">
+              <AvatarItem image={group.bigAvatar}
+                          placeholder={group.placeholder}
+                          size="medium"
+                          title={group.name}/>
+              <h3>{group.name}</h3>
+            </div>
+            <div className="notifications">
+              <label htmlFor="notifications">Enable Notifications</label>
 
-        </div>
+              <div className="switch pull-right">
+                <input checked={isNotificationsEnabled} id="notifications" onChange={this.onNotificationChange.bind(this)} type="checkbox"/>
+                <label htmlFor="notifications"></label>
+              </div>
+            </div>
 
-        {memberArea}
+            {memberArea}
+            {integrationToken}
+          </div>
+        </Variant>
+        <Variant name="new">
+          <div className="activity__body group_profile">
+            <ul className="group_profile__list">
+              <li className="group_profile__list__item group_profile__meta">
+                <AvatarItem image={group.bigAvatar}
+                            placeholder={group.placeholder}
+                            title={group.name}/>
 
-        {integrationToken}
-      </div>
+                <h3 className="group_profile__meta__title">{group.name}</h3>
+
+                <div className="info">
+                  <p>some description heere</p>
+                </div>
+                <div className="info--light">
+                  Created by {admin.name} {/*on Month Day, Year*/}
+                </div>
+
+              </li>
+              <li className="group_profile__list__item">
+                <label htmlFor="notifications">Notifications</label>
+                <div className="switch pull-right">
+                  <input checked={isNotificationsEnabled}
+                         id="notifications"
+                         onChange={this.onNotificationChange}
+                         type="checkbox"/>
+                  <label htmlFor="notifications"></label>
+                </div>
+              </li>
+              <li className="group_profile__list__item group_profile__media">
+                Media
+                <i className="material-icons pull-right">keyboard_arrow_down</i>
+              </li>
+              <li className="group_profile__list__item group_profile__members">
+                Members
+                <i className="material-icons pull-right">keyboard_arrow_down</i>
+
+                <div className="fold">
+                  {memberArea}
+                </div>
+              </li>
+              <li className="group_profile__list__item group_profile__integration">
+                Token
+                <i className="material-icons pull-right">keyboard_arrow_down</i>
+
+                <div className="fold">
+                  <div className="info--light">
+                    If you have programming chops, or know someone who does,
+                    this integration token allow the most flexibility and communication
+                    with your own systems.
+                  </div>
+                  {integrationToken}
+                </div>
+              </li>
+            </ul>
+          </div>
+        </Variant>
+      </Experiment>
     );
   }
 }
