9a9ddf8b1b8d07162ae024081e49b1db8446da88
rock_jam <5min4eq.unity@gmail.com>
2015-07-25 03:42:29 +0300

    fix(server): write to db on message send in GroupOffice, persist Read/Receive events in UserOffice

--- actor-server/actor-peer-managers/src/main/scala/im/actor/server/user/UserOfficeActor.scala
+++ actor-server/actor-peer-managers/src/main/scala/im/actor/server/user/UserOfficeActor.scala
@@ -1,6 +1,6 @@
 package im.actor.server.user
 
-import akka.actor.{ ActorLogging, Props, Status }
+import akka.actor.{ ActorSystem, ActorLogging, Props, Status }
 import akka.pattern.pipe
 import akka.persistence.RecoveryFailure
 import akka.util.Timeout
@@ -33,10 +33,13 @@ object UserOfficeActor {
   ActorSerializer.register(3006, classOf[UserEnvelope.BroadcastUpdateResponse])
   ActorSerializer.register(3007, classOf[UserEnvelope.RemoveAuth])
   ActorSerializer.register(3008, classOf[UserEnvelope.UserInfo])
+  ActorSerializer.register(3009, classOf[UserEnvelope.MessageRead])
 
   ActorSerializer.register(4001, classOf[UserEvents.AuthAdded])
   ActorSerializer.register(4002, classOf[UserEvents.AuthRemoved])
   ActorSerializer.register(4003, classOf[UserEvents.UserInfoAdded])
+  ActorSerializer.register(4004, classOf[UserEvents.MessageReceived])
+  ActorSerializer.register(4005, classOf[UserEvents.MessageRead])
 
   def props(
     implicit
@@ -64,6 +67,7 @@ class UserOfficeActor(
   implicit val region: UserOfficeRegion = UserOfficeRegion(context.parent)
   implicit private val timeout: Timeout = Timeout(10.seconds)
 
+  implicit private val system: ActorSystem = context.system
   implicit private val ec: ExecutionContext = context.dispatcher
 
   private val userId = self.path.name.toInt
@@ -127,10 +131,10 @@ class UserOfficeActor(
       val ownUpdate = UpdateMessageSent(peer, randomId, date.getMillis)
       db.run(persistAndPushUpdate(senderAuthId, ownUpdate, None, isFat)) pipeTo sender()
     case Payload.SendMessage(SendMessage(senderUserId, senderAuthId, accessHash, randomId, message, isFat)) ⇒
-      val isCorrectHash = accessSalt.exists { salt =>
+      val isCorrectHash = accessSalt.exists { salt ⇒
         accessHash == ACLUtils.userAccessHash(senderAuthId, userId, salt)
       }
-      if(isCorrectHash) {
+      if (isCorrectHash) {
         val replyTo = sender()
         context become {
           case MessageSentComplete ⇒
@@ -146,9 +150,9 @@ class UserOfficeActor(
           SeqState(seq, state) ← UserOffice.deliverOwnMessage(senderUserId, privatePeerStruct(userId), senderAuthId, randomId, date, message, isFat)
           _ ← Future.successful(recordRelation(senderUserId, userId))
         } yield {
-            db.run(writeHistoryMessage(models.Peer.privat(senderUserId), models.Peer.privat(userId), date, randomId, message.header, message.toByteArray))
-            SeqStateDate(seq, state, dateMillis)
-          }
+          db.run(writeHistoryMessage(models.Peer.privat(senderUserId), models.Peer.privat(userId), date, randomId, message.header, message.toByteArray))
+          SeqStateDate(seq, state, dateMillis)
+        }
         sendFuture onComplete {
           case Success(seqstate) ⇒
             replyTo ! seqstate
@@ -163,34 +167,38 @@ class UserOfficeActor(
       }
     case Payload.MessageReceived(MessageReceived(receiverUserId, _, date, receivedDate)) ⇒
       if (!lastReceivedDate.exists(_ > date)) {
-        lastReceivedDate = Some(date)
-        val update = UpdateMessageReceived(Peer(PeerType.Private, receiverUserId), date, receivedDate)
+        persist(UserEvents.MessageReceived(date)) { _ ⇒
+          lastReceivedDate = Some(date)
+          val update = UpdateMessageReceived(Peer(PeerType.Private, receiverUserId), date, receivedDate)
 
-        db.run(for {
-          _ ← persistAndPushUpdates(authIds, update, None)
-        } yield {
-          // TODO: report errors
-          db.run(markMessagesReceived(models.Peer.privat(receiverUserId), models.Peer.privat(userId), new DateTime(date)))
-        }) onFailure {
-          case e ⇒
-            log.error(e, "Failed to mark messages received")
+          db.run(for {
+            _ ← persistAndPushUpdates(authIds, update, None)
+          } yield {
+            // TODO: report errors
+            db.run(markMessagesReceived(models.Peer.privat(receiverUserId), models.Peer.privat(userId), new DateTime(date)))
+          }) onFailure {
+            case e ⇒
+              log.error(e, "Failed to mark messages received")
+          }
         }
       }
     case Payload.MessageRead(MessageRead(readerUserId, _, date, readDate)) ⇒
       if (!lastReadDate.exists(_ > date)) {
-        lastReadDate = Some(date)
-        val update = UpdateMessageRead(Peer(PeerType.Private, readerUserId), date, readDate)
-        val readerUpdate = UpdateMessageReadByMe(Peer(PeerType.Private, userId), date)
+        persist(UserEvents.MessageRead(date)) { _ ⇒
+          lastReadDate = Some(date)
+          val update = UpdateMessageRead(Peer(PeerType.Private, readerUserId), date, readDate)
+          val readerUpdate = UpdateMessageReadByMe(Peer(PeerType.Private, userId), date)
 
-        db.run(for {
-          _ ← persistAndPushUpdates(authIds, update, None)
-          _ ← broadcastUserUpdate(readerUserId, readerUpdate, None) //todo: may be replace with MessageReadOwn
-        } yield {
-          // TODO: report errors
-          db.run(markMessagesRead(models.Peer.privat(readerUserId), models.Peer.privat(userId), new DateTime(date)))
-        }) onFailure {
-          case e ⇒
-            log.error(e, "Failed to mark messages read")
+          db.run(for {
+            _ ← persistAndPushUpdates(authIds, update, None)
+            _ ← broadcastUserUpdate(readerUserId, readerUpdate, None) //todo: may be replace with MessageReadOwn
+          } yield {
+            // TODO: report errors
+            db.run(markMessagesRead(models.Peer.privat(readerUserId), models.Peer.privat(userId), new DateTime(date)))
+          }) onFailure {
+            case e ⇒
+              log.error(e, "Failed to mark messages read")
+          }
         }
       }
   }
@@ -202,6 +210,10 @@ class UserOfficeActor(
       authIds -= authId
     case UserEvents.UserInfoAdded(salt) ⇒
       accessSalt = Some(salt)
+    case UserEvents.MessageRead(date) ⇒
+      lastReadDate = Some(date)
+    case UserEvents.MessageReceived(date) ⇒
+      lastReceivedDate = Some(date)
     case RecoveryFailure(e) ⇒
       log.error(e, "Failed to recover")
   }
