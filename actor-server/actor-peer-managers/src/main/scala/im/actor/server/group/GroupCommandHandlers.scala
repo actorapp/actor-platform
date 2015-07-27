47091aa570da21c7bc67f1e48c436bb41dcc5601
John Doe <speed@actor.im>
2015-07-27 20:10:25 +0300

    refactor(server): code cleanup

--- actor-server/actor-peer-managers/src/main/scala/im/actor/server/group/GroupCommandHandlers.scala
+++ actor-server/actor-peer-managers/src/main/scala/im/actor/server/group/GroupCommandHandlers.scala
@@ -1,24 +1,18 @@
 package im.actor.server.group
 
-import scala.concurrent.Future
-import scala.concurrent.forkjoin.ThreadLocalRandom
-
-import akka.actor.{ Status, ActorRef }
-import akka.pattern.pipe
 import org.joda.time.DateTime
-import slick.dbio.DBIO
 import slick.driver.PostgresDriver.api._
 
 import im.actor.api.rpc.groups.UpdateGroupAvatarChanged
 import im.actor.server.api.ApiConversions._
-import im.actor.server.file.{ Avatar, FileErrors, FileLocation }
+import im.actor.server.file.Avatar
 import im.actor.server.group.GroupEvents.AvatarUpdated
 import im.actor.server.push.SeqUpdatesManager._
 import im.actor.server.push.SeqUpdatesManagerRegion
 import im.actor.server.sequence.SeqStateDate
 import im.actor.server.util.ImageUtils._
-import im.actor.server.util.{ FileStorageAdapter, GroupServiceMessages, HistoryUtils }
-import im.actor.server.{ models, persist ⇒ p }
+import im.actor.server.util.{FileStorageAdapter, GroupServiceMessages, HistoryUtils}
+import im.actor.server.{models, persist => p}
 
 private[group] trait GroupCommandHandlers {
   self: GroupOfficeActor ⇒
@@ -30,12 +24,10 @@ private[group] trait GroupCommandHandlers {
 
   protected def updateAvatar(group: Group, clientUserId: Int, clientAuthId: Long, avatarOpt: Option[Avatar], randomId: Long)(
     implicit
-    fsAdapter:           FileStorageAdapter,
-    db:                  Database,
+    fsAdapter: FileStorageAdapter,
+    db: Database,
     seqUpdManagerRegion: SeqUpdatesManagerRegion
-  ): Unit = {
-    val replyTo = sender()
-
+    ): Unit = {
     val date = new DateTime
     val avatarData = avatarOpt map (getAvatarData(models.AvatarData.OfGroup, groupId, _)) getOrElse (models.AvatarData.empty(models.AvatarData.OfGroup, groupId.toLong))
 
@@ -48,17 +40,17 @@ private[group] trait GroupCommandHandlers {
         groupUserIds ← p.GroupUser.findUserIds(groupId)
         (seqstate, _) ← broadcastClientAndUsersUpdate(clientUserId, clientAuthId, groupUserIds.toSet, update, None, isFat = false)
       } yield {
-        db.run(HistoryUtils.writeHistoryMessage(
-          models.Peer.privat(clientUserId),
-          models.Peer.group(groupId),
-          date,
-          randomId,
-          serviceMessage.header,
-          serviceMessage.toByteArray
-        ))
+          db.run(HistoryUtils.writeHistoryMessage(
+            models.Peer.privat(clientUserId),
+            models.Peer.group(groupId),
+            date,
+            randomId,
+            serviceMessage.header,
+            serviceMessage.toByteArray
+          ))
 
-        UpdateAvatarResponse(avatarOpt, SeqStateDate(seqstate.seq, seqstate.state, date.getMillis))
-      })
+          UpdateAvatarResponse(avatarOpt, SeqStateDate(seqstate.seq, seqstate.state, date.getMillis))
+        })
     }
   }
 }
