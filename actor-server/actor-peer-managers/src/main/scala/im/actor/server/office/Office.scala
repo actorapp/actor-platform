f3b297844f4869ee19f6133e344e4538a491d64e
John Doe <speed@actor.im>
2015-07-27 17:44:47 +0300

    feat(server): persistStashingReply helper

--- actor-server/actor-peer-managers/src/main/scala/im/actor/server/office/Office.scala
+++ actor-server/actor-peer-managers/src/main/scala/im/actor/server/office/Office.scala
@@ -2,18 +2,40 @@ package im.actor.server.office
 
 import java.util.concurrent.TimeUnit
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.language.postfixOps
+import scala.util.{ Failure, Success }
 
-import akka.actor.{ PoisonPill, ActorLogging }
+import akka.actor.{ ActorLogging, ActorRef, Status }
 import akka.contrib.pattern.ShardRegion.Passivate
 import akka.persistence.PersistentActor
 
 case object StopOffice
 
-trait Office extends PersistentActor with ActorLogging {
+trait Office[E] extends PersistentActor with ActorLogging {
   private val passivationIntervalMs = context.system.settings.config.getDuration("office.passivation-interval", TimeUnit.MILLISECONDS)
   private implicit val ec = context.dispatcher
+
+  def stashing: Receive = {
+    case msg ⇒ stash()
+  }
+
+  def persistStashingReply(evt: E, replyTo: ActorRef)(onComplete: E ⇒ Unit)(f: E ⇒ Future[Any]): Unit = {
+    context become stashing
+
+    persistAsync(evt) { _ ⇒
+      f(evt) onComplete {
+        case Success(res) ⇒
+          replyTo ! res
+          onComplete(evt)
+        case Failure(e) ⇒
+          log.error(e, "Failed to process event {}", e)
+          replyTo ! Status.Failure(e)
+          onComplete(evt)
+      }
+    }
+  }
 
   if (passivationIntervalMs > 0) {
     log.warning("Passivating in {} ms", passivationIntervalMs)
