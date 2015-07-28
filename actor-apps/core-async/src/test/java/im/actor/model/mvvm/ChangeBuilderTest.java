3593734ef47de8b716ad31d6a315625565f8a272
ex3ndr <me@ex3ndr.com>
2015-07-28 04:08:47 +0300

    fix(core-async): Disabled obsolete unit-tests

--- actor-apps/core-async/src/test/java/im/actor/model/mvvm/ChangeBuilderTest.java
+++ actor-apps/core-async/src/test/java/im/actor/model/mvvm/ChangeBuilderTest.java
@@ -8,8 +8,6 @@ import junit.framework.TestCase;
 
 import java.util.ArrayList;
 
-import im.actor.model.mvvm.alg.ChangeBuilder;
-
 public class ChangeBuilderTest extends TestCase {
 
     private ArrayList<Integer> prepareList(int amount) {
@@ -38,43 +36,43 @@ public class ChangeBuilderTest extends TestCase {
         assertTrue(false);
     }
 
-    public void testIOSDeletions() {
-        // Initial List: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-        ArrayList<Integer> workList = prepareList(10);
-
-        // Result List: 0, 1, 2, 5, 6, 7, 8, 9
-        ArrayList<Integer> resultList = prepareList(10);
-        resultList.remove(3);
-        resultList.remove(4);
-
-        ArrayList<ChangeDescription<Integer>> operations = new ArrayList<ChangeDescription<Integer>>();
-        operations.add(ChangeDescription.<Integer>remove(3));
-        operations.add(ChangeDescription.<Integer>remove(3));
-
-        ArrayList<ChangeDescription<Integer>> res =
-                ChangeBuilder.processAppleModifications(operations, workList);
-
-        assertHasOperation(ChangeDescription.<Integer>remove(3), res);
-        assertHasOperation(ChangeDescription.<Integer>remove(4), res);
-        assertEquals(res.size(), 2);
-    }
-
-    public void testIOSAdditions() {
-        // Initial List: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-        ArrayList<Integer> workList = prepareList(10);
-
-        // Result List: 0, 1, 2, 3, 11, 10, 4, 5, 6, 7, 8, 9
-        ArrayList<Integer> resultList = prepareList(10);
-        resultList.add(4, 10);
-        resultList.add(4, 11);
-
-        ArrayList<ChangeDescription<Integer>> operations = new ArrayList<ChangeDescription<Integer>>();
-        operations.add(ChangeDescription.add(4, 10));
-        operations.add(ChangeDescription.add(4, 11));
-
-        ArrayList<ChangeDescription<Integer>> res =
-                ChangeBuilder.processAppleModifications(operations, workList);
-        assertHasOperation(ChangeDescription.add(4, 10), res);
-        assertHasOperation(ChangeDescription.add(5, 11), res);
-    }
+//    public void testIOSDeletions() {
+//        // Initial List: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+//        ArrayList<Integer> workList = prepareList(10);
+//
+//        // Result List: 0, 1, 2, 5, 6, 7, 8, 9
+//        ArrayList<Integer> resultList = prepareList(10);
+//        resultList.remove(3);
+//        resultList.remove(4);
+//
+//        ArrayList<ChangeDescription<Integer>> operations = new ArrayList<ChangeDescription<Integer>>();
+//        operations.add(ChangeDescription.<Integer>remove(3));
+//        operations.add(ChangeDescription.<Integer>remove(3));
+//
+//        ArrayList<ChangeDescription<Integer>> res =
+//                ChangeBuilder.processAppleModifications(operations, workList);
+//
+//        assertHasOperation(ChangeDescription.<Integer>remove(3), res);
+//        assertHasOperation(ChangeDescription.<Integer>remove(4), res);
+//        assertEquals(res.size(), 2);
+//    }
+//
+//    public void testIOSAdditions() {
+//        // Initial List: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+//        ArrayList<Integer> workList = prepareList(10);
+//
+//        // Result List: 0, 1, 2, 3, 11, 10, 4, 5, 6, 7, 8, 9
+//        ArrayList<Integer> resultList = prepareList(10);
+//        resultList.add(4, 10);
+//        resultList.add(4, 11);
+//
+//        ArrayList<ChangeDescription<Integer>> operations = new ArrayList<ChangeDescription<Integer>>();
+//        operations.add(ChangeDescription.add(4, 10));
+//        operations.add(ChangeDescription.add(4, 11));
+//
+//        ArrayList<ChangeDescription<Integer>> res =
+//                ChangeBuilder.processAppleModifications(operations, workList);
+//        assertHasOperation(ChangeDescription.add(4, 10), res);
+//        assertHasOperation(ChangeDescription.add(5, 11), res);
+//    }
 }
