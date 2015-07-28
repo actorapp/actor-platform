745fe6ab5f90ca90ef1065f9d81958152ba3bc33
Nikolay Tatarinov <rockjam@actor.im>
2015-07-28 22:39:23 +0300

    refactor(server): move all user actions from UserOffice to separate trait

--- actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/auth/ServiceMessages.scala
+++
@@ -1,8 +0,0 @@
-package im.actor.server.api.rpc.service.auth
-
-import im.actor.api.rpc.messaging.{ ServiceExContactRegistered, ServiceMessage }
-
-object ServiceMessages {
-  def contactRegistered(userId: Int) = ServiceMessage("Contact registered", Some(ServiceExContactRegistered(userId)))
-}
-
