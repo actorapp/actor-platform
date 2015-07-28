bcd9790f26c4bb57079f403f25d219e3b3cfb17f
ljshj <jinshanl@gmail.com>
2015-07-28 19:54:05 +0800

    webchinese

--- actor-server/actor-sms/src/main/scala/im/actor/server/sms/TelesignSmsEngine.scala
+++ actor-server/actor-sms/src/main/scala/im/actor/server/sms/TelesignSmsEngine.scala
@@ -14,62 +14,45 @@ import com.typesafe.config._
 import dispatch._
 
 class TelesignSmsEngine(config: Config)(implicit system: ActorSystem) extends AuthSmsEngine {
-  private val apiKey = Base64.getDecoder.decode(config.getString("api-key"))
+  system.log.error("=========================================TelesignSmsEngine")
+  private val apiKey = config.getString("api-key")
   private val customerId = config.getString("customer-id")
-  private val resourceUriString = "/v1/verify/sms"
-  private val resourceUrl = url("https://rest.telesign.com" + resourceUriString)
-  private val dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z")
+  system.log.error("=======================TelesignSmsEngine sendCode apiKey:{} customerId:{}", apiKey, customerId)
+  private val resourceUrl = url("http://utf8.sms.webchinese.cn/")
 
-  private val HmacSha1Algorithm = "HmacSHA1"
   private val Utf8Encoding = "UTF-8"
-  val FormContentType = "application/x-www-form-urlencoded"
+  val FormContentType = "application/x-www-form-urlencoded;charset=utf-8"
 
   private val http = new Http()
 
   implicit val ec: ExecutionContext = system.dispatcher
 
   override def sendCode(phoneNumber: Long, message: String): Future[Unit] = {
+    var mobile = phoneNumber.toString
+    if (mobile.length > 11) {
+      mobile = mobile.substring(mobile.length - 11)
+    }
+    system.log.error("=======================TelesignSmsEngine sendCode phoneNumber:{} message:{}", mobile, message)
     val body = List(
-      "phone_number" → URLEncoder.encode(phoneNumber.toString, Utf8Encoding),
-      "ucid" → "BACS",
-      "verify_code" → URLEncoder.encode(message, Utf8Encoding),
-      "template" → URLEncoder.encode("$$CODE$$ is your Actor code", Utf8Encoding)
+      "smsMob" → URLEncoder.encode(mobile, Utf8Encoding),
+      "Uid" → customerId,
+      "Key" → apiKey,
+      "smsText" → URLEncoder.encode("验证码:" + message, Utf8Encoding)
     ).map(p ⇒ s"${p._1}=${p._2}").mkString("&")
 
     val request = (resourceUrl.POST.setContentType(FormContentType, Utf8Encoding).setBody(body))
 
-    val date = dateFormat.format(new Date)
-
-    val stringToSign =
-      s"""POST
-        |${FormContentType}; charset=${Utf8Encoding}
-        |${date}
-        |${body}
-        |${resourceUriString}""".stripMargin
-
-    val mac = Mac.getInstance(HmacSha1Algorithm)
-    mac.init(new SecretKeySpec(apiKey, HmacSha1Algorithm))
-    val hmac = mac.doFinal(stringToSign.getBytes(Utf8Encoding))
-
-    val signature = Base64.getEncoder.encodeToString(hmac)
-
-    val authValue = s"TSA ${customerId}:${signature}"
-
-    val requestWithAuth = request
-      .addHeader("Date", date)
-      .addHeader("Authorization", authValue)
-
-    val f = http(requestWithAuth)
+    val f = http(request)
 
     f onComplete {
       case Success(v) ⇒
         if (v.getStatusCode > 199 && v.getStatusCode < 300) {
-          system.log.debug("Message sent to Telesign")
+          system.log.error("Message sent to webchinese")
         } else {
-          system.log.error(s"Telesign replied with error ${v.getResponseBody(Utf8Encoding)}")
+          system.log.error(s"webchinese replied with error ${v.getResponseBody(Utf8Encoding)}")
         }
       case Failure(e) ⇒
-        system.log.error(e, "Failed to send sms through Telesign")
+        system.log.error(e, "Failed to send sms through webchinese")
     }
 
     f map (_ ⇒ ())
