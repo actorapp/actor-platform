b468f219727b9499e088a7af680c4cea721d78b2
rock_jam <5min4eq.unity@gmail.com>
2015-07-30 15:10:57 +0300

    fix(server): forbid webhooks in public groups

--- actor-server/actor-http-api/src/main/scala/im/actor/server/api/http/webhooks/WebhooksHandler.scala
+++ actor-server/actor-http-api/src/main/scala/im/actor/server/api/http/webhooks/WebhooksHandler.scala
@@ -1,25 +1,24 @@
 package im.actor.server.api.http.webhooks
 
-import scala.concurrent.ExecutionContext
-import scala.concurrent.duration._
-import scala.concurrent.forkjoin.ThreadLocalRandom
-import scala.util.{ Failure, Success }
-
-import akka.http.scaladsl.model.HttpResponse
 import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.model.{ HttpResponse, StatusCode, StatusCodes }
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.Materializer
 import akka.util.Timeout
-import org.joda.time.DateTime
-import slick.dbio.DBIO
-import slick.driver.PostgresDriver.api._
-
 import im.actor.api.rpc.messaging.{ Message, TextMessage }
 import im.actor.server.api.http.RoutesHandler
 import im.actor.server.api.http.json._
 import im.actor.server.peermanagers.{ GroupPeerManager, GroupPeerManagerRegion }
 import im.actor.server.persist
+import org.joda.time.DateTime
+import slick.dbio.DBIO
+import slick.driver.PostgresDriver.api._
+
+import scala.concurrent.duration._
+import scala.concurrent.forkjoin.ThreadLocalRandom
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.{ Failure, Success }
 
 class WebhooksHandler()(
   implicit
@@ -35,44 +34,50 @@ class WebhooksHandler()(
     post {
       entity(as[Content]) { content ⇒
         onComplete(send(content, token)) {
-          case Success(_) ⇒ complete(HttpResponse(OK))
+          case Success(result) ⇒
+            result match {
+              case Left(statusCode) ⇒ complete(statusCode)
+              case Right(_)         ⇒ complete(HttpResponse(OK))
+            }
           case Failure(e) ⇒ complete(HttpResponse(InternalServerError))
         }
       }
     }
   }
 
-  def send(content: Content, token: String) = {
+  def send(content: Content, token: String): Future[Either[StatusCode, Unit]] = {
     val message: Message = content match {
       case Text(text)    ⇒ TextMessage(text, Vector.empty, None)
       case Document(url) ⇒ throw new NotImplementedError()
       case Image(url)    ⇒ throw new NotImplementedError()
     }
 
-    db.run {
-      for {
-        optBot ← persist.GroupBot.findByToken(token)
-        userAuth ← optBot.map { bot ⇒
-          for {
-            optGroup ← persist.Group.find(bot.groupId)
-            authIds ← persist.AuthId.findByUserId(bot.userId)
-
-            authId ← (optGroup, authIds) match {
-              case (None, _) ⇒ DBIO.successful(None)
-              case (Some(group), auth +: _) ⇒
-                DBIO.from(GroupPeerManager.sendMessage(group.id, bot.userId, auth.id, ThreadLocalRandom.current().nextLong(), DateTime.now, message))
-              case (Some(group), Seq()) ⇒
-                val rng = ThreadLocalRandom.current()
-                val authId = rng.nextLong()
-                for {
-                  _ ← persist.AuthId.create(authId, Some(bot.userId), None)
-                  _ ← DBIO.from(GroupPeerManager.sendMessage(group.id, bot.userId, authId, rng.nextLong(), DateTime.now, message))
-                } yield ()
-            }
-          } yield ()
-        }.getOrElse(DBIO.successful(None))
-      } yield ()
-    }
+    val action: DBIO[Either[ClientError, Unit]] = for {
+      optBot ← persist.GroupBot.findByToken(token)
+      result ← optBot.map { bot ⇒
+        for {
+          optGroup ← persist.Group.find(bot.groupId)
+          authIds ← persist.AuthId.findByUserId(bot.userId)
+          authId ← (optGroup, authIds) match {
+            case (None, _)                          ⇒ DBIO.successful(Left(StatusCodes.NotFound))
+            case (Some(group), _) if group.isPublic ⇒ DBIO.successful(Left(StatusCodes.Forbidden))
+            case (Some(group), auth +: _) ⇒
+              val sendFuture = for {
+                _ ← GroupPeerManager.sendMessage(group.id, bot.userId, auth.id, ThreadLocalRandom.current().nextLong(), DateTime.now, message)
+              } yield Right(())
+              DBIO.from(sendFuture)
+            case (Some(group), Seq()) ⇒
+              val rng = ThreadLocalRandom.current()
+              val authId = rng.nextLong()
+              for {
+                _ ← persist.AuthId.create(authId, Some(bot.userId), None)
+                _ ← DBIO.from(GroupPeerManager.sendMessage(group.id, bot.userId, authId, rng.nextLong(), DateTime.now, message))
+              } yield Right(())
+          }
+        } yield authId
+      } getOrElse DBIO.successful(Left(StatusCodes.BadRequest))
+    } yield result
+    db.run(action)
   }
 
 }
