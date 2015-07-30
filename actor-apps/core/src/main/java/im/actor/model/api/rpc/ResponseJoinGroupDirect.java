6c23d7852c598caeee4567561730eff51ff2cf41
prettynatty <fear@loathing.in>
2015-07-30 16:41:46 +0300

    Revert "feat(common): UpdateUnreadMessages"
    This reverts commit d2714f0c02064efcc7ba2a29e303ebd1428551f8.

--- actor-apps/core/src/main/java/im/actor/model/api/rpc/ResponseJoinGroupDirect.java
+++ actor-apps/core/src/main/java/im/actor/model/api/rpc/ResponseJoinGroupDirect.java
@@ -11,19 +11,27 @@ import im.actor.model.droidkit.bser.BserWriter;
 import im.actor.model.droidkit.bser.DataInput;
 import im.actor.model.droidkit.bser.DataOutput;
 import im.actor.model.droidkit.bser.util.SparseArray;
+
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.NotNull;
+
 import com.google.j2objc.annotations.ObjectiveCName;
+
 import static im.actor.model.droidkit.bser.Utils.*;
+
 import java.io.IOException;
+
 import im.actor.model.network.parser.*;
+
 import java.util.List;
 import java.util.ArrayList;
+
 import im.actor.model.api.*;
 
 public class ResponseJoinGroupDirect extends Response {
 
     public static final int HEADER = 0xc8;
+
     public static ResponseJoinGroupDirect fromBytes(byte[] data) throws IOException {
         return Bser.parse(new ResponseJoinGroupDirect(), data);
     }
@@ -79,7 +87,7 @@ public class ResponseJoinGroupDirect extends Response {
     public void parse(BserValues values) throws IOException {
         this.group = values.getObj(1, new Group());
         List<User> _users = new ArrayList<User>();
-        for (int i = 0; i < values.getRepeatedCount(2); i ++) {
+        for (int i = 0; i < values.getRepeatedCount(2); i++) {
             _users.add(new User());
         }
         this.users = values.getRepeatedObj(2, _users);
