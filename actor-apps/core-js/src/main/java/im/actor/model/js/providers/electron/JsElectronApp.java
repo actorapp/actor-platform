23650b30810ca1af6735f923bfa5dac35af46ff8
Steve Kite <steve@actor.im>
2015-07-31 22:02:35 +0300

    feat(js): Added electron integration

---
+++ actor-apps/core-js/src/main/java/im/actor/model/js/providers/electron/JsElectronApp.java
@@ -0,0 +1,25 @@
+package im.actor.model.js.providers.electron;
+
+/**
+ * Created by ex3ndr on 31.07.15.
+ */
+public class JsElectronApp {
+    public static native boolean isSupported()/*-{
+        return 'require' in $wnd;
+    }-*/;
+
+    public static native void bounce()/*-{
+        var ipc = $wnd.require('ipc');
+        ipc.send('tray-bounce');
+    }-*/;
+
+    public static native void showNewMessages()/*-{
+        var ipc = $wnd.require('ipc');
+        ipc.send('new-messages-show');
+    }-*/;
+
+    public static native void hideNewMessages()/*-{
+        var ipc = $wnd.require('ipc');
+        ipc.send('new-messages-hide');
+    }-*/;
+}
