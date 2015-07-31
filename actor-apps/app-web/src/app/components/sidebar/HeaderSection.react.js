9e984d72a2fb34890a43a7b21a07aeabd073caa6
Oleg Shilov <me@olegshilov.com>
2015-07-31 07:19:08 +0300

    feat(web): add new sidebar header menu, update light button styles;

--- actor-apps/app-web/src/app/components/sidebar/HeaderSection.react.js
+++ actor-apps/app-web/src/app/components/sidebar/HeaderSection.react.js
@@ -58,6 +58,9 @@ class HeaderSection extends React.Component {
       let headerClass = classNames('sidebar__header', 'sidebar__header--clickable', {
         'sidebar__header--opened': this.state.isOpened
       });
+      let menuClass = classNames('dropdown', {
+        'dropdown--opened': this.state.isOpened
+      });
 
       return (
         <header className={headerClass}>
@@ -67,38 +70,38 @@ class HeaderSection extends React.Component {
                         size="tiny"
                         title={user.name} />
             <span className="sidebar__header__user__name col-xs">{user.name}</span>
-            <span className="sidebar__header__user__expand">
-              <i className="material-icons">arrow_drop_down</i>
-            </span>
+            <div className={menuClass}>
+              <span className="dropdown__button">
+                <i className="material-icons">arrow_drop_down</i>
+              </span>
+              <ul className="dropdown__menu dropdown__menu--right">
+                <li className="dropdown__menu__item">
+                  <i className="material-icons">photo_camera</i>
+                  Set Profile Photo
+                </li>
+                <li className="dropdown__menu__item" onClick={this.openMyProfile}>
+                  <i className="material-icons">person</i>
+                  Edit Profile
+                </li>
+                <li className="dropdown__menu__separator"></li>
+                <li className="dropdown__menu__item">
+                  <i className="material-icons">power</i>
+                  Configure Integrations
+                </li>
+                <li className="dropdown__menu__item" onClick={this.openHelpDialog}>
+                  <i className="material-icons">help</i>
+                  Help &amp; Feedback
+                </li>
+                <li className="dropdown__menu__item">
+                  <i className="material-icons">settings</i>
+                  Preferences
+                </li>
+                <li className="dropdown__menu__item dropdown__menu__item--light" onClick={this.setLogout}>
+                  Sign Out
+                </li>
+              </ul>
+            </div>
           </div>
-          <ul className="sidebar__header__menu">
-            <li className="sidebar__header__menu__item" onClick={this.openMyProfile}>
-              <i className="material-icons">person</i>
-              <span>Profile</span>
-            </li>
-            {/*
-            <li className="sidebar__header__menu__item" onClick={this.openCreateGroup}>
-              <i className="material-icons">group_add</i>
-              <span>Create group</span>
-            </li>
-             */}
-            <li className="sidebar__header__menu__item hide">
-              <i className="material-icons">cached</i>
-              <span>Integrations</span>
-            </li>
-            <li className="sidebar__header__menu__item hide">
-              <i className="material-icons">settings</i>
-              <span>Settings</span>
-            </li>
-            <li className="sidebar__header__menu__item" onClick={this.openHelpDialog}>
-              <i className="material-icons">help</i>
-              <span>Help and feedback</span>
-            </li>
-            <li className="sidebar__header__menu__item" onClick={this.setLogout}>
-              <i className="material-icons">power_settings_new</i>
-              <span>Log out</span>
-            </li>
-          </ul>
 
           <MyProfileModal/>
         </header>
