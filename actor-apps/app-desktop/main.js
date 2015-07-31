148f24a8751456fd2fe07f190a2ac83d0665e4a8
ex3ndr <me@ex3ndr.com>
2015-07-31 22:17:46 +0300

    feat(desktop): Added electron files for desktop app

---
+++ actor-apps/app-desktop/main.js
@@ -0,0 +1,192 @@
+var app = require('app');  // Module to control application life.
+var BrowserWindow = require('browser-window');  // Module to create native browser window.
+var Menu = require('menu');
+var MenuItem = require('menu-item');
+var ipc = require('ipc');
+
+// Report crashes to our server.
+// require('crash-reporter').start();
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the javascript object is GCed.
+var mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+function showWindow() {
+  if (mainWindow != null) {
+    return;
+  }
+  // Create the browser window.
+  mainWindow = new BrowserWindow({ 
+    width: 1200, 
+    height: 750,
+    "min-width": 400,
+    "min-height": 300,
+    preload: 'file://' + __dirname + '/preload.js'
+  });
+
+  // and load app
+  mainWindow.loadUrl('https://app.actor.im/');
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has done everything
+// initialization and ready for creating browser windows.
+app.on('ready', function() {
+  // Create the Application's main menu
+  var template = [{
+    label: 'Electron',
+    submenu: [
+      {
+        label: 'About Actor',
+        selector: 'orderFrontStandardAboutPanel:'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Services',
+        submenu: []
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Hide Actor',
+        accelerator: 'Command+H',
+        selector: 'hide:'
+      },
+      {
+        label: 'Hide Others',
+        accelerator: 'Command+Shift+H',
+        selector: 'hideOtherApplications:'
+      },
+      {
+        label: 'Show All',
+        selector: 'unhideAllApplications:'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click: function() { app.quit(); }
+      },
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        label: 'Undo',
+        accelerator: 'Command+Z',
+        selector: 'undo:'
+      },
+      {
+        label: 'Redo',
+        accelerator: 'Shift+Command+Z',
+        selector: 'redo:'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Cut',
+        accelerator: 'Command+X',
+        selector: 'cut:'
+      },
+      {
+        label: 'Copy',
+        accelerator: 'Command+C',
+        selector: 'copy:'
+      },
+      {
+        label: 'Paste',
+        accelerator: 'Command+V',
+        selector: 'paste:'
+      },
+      {
+        label: 'Select All',
+        accelerator: 'Command+A',
+        selector: 'selectAll:'
+      },
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Reload Actor',
+        accelerator: 'Command+R',
+        click: function() { BrowserWindow.getFocusedWindow().reloadIgnoringCache(); }
+      },
+      {
+        label: 'Toggle DevTools',
+        accelerator: 'Alt+Command+I',
+        click: function() { BrowserWindow.getFocusedWindow().toggleDevTools(); }
+      },
+    ]
+  },
+  {
+    label: 'Window',
+    submenu: [
+      {
+        label: 'Minimize',
+        accelerator: 'Command+M',
+        selector: 'performMiniaturize:'
+      },
+      {
+        label: 'Close',
+        accelerator: 'Command+W',
+        selector: 'performClose:'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Bring All to Front',
+        selector: 'arrangeInFront:'
+      },
+    ]
+  },
+  {
+    label: 'Help',
+    submenu: []
+  }];
+
+  menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  showWindow();
+});
+
+app.on('activate-with-no-open-windows', showWindow);
+
+// Adding tray
+
+ipc.on('new-messages-show', function(event, arg) {
+  app.dock.bounce();
+  app.dock.setBadge('.');
+});
+
+ipc.on('new-messages-hide', function(event, arg) {
+  app.dock.setBadge('');
+});
+
+ipc.on('tray-bounce', function(event, arg) {
+  app.dock.bounce();
+});
+  
