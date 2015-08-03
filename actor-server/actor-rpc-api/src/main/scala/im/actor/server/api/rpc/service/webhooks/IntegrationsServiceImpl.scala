e2e86f5312a71c206912d59988adaad689e5b18f
rock_jam <5min4eq.unity@gmail.com>
2015-08-03 23:28:29 +0300

    feat(server): group processor change to TSEvent

--- actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/webhooks/IntegrationsServiceImpl.scala
+++ actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/webhooks/IntegrationsServiceImpl.scala
@@ -1,33 +1,38 @@
 package im.actor.server.api.rpc.service.webhooks
 
-import scala.concurrent.forkjoin.ThreadLocalRandom
-import scala.concurrent.{ ExecutionContext, Future }
-
 import akka.actor.ActorSystem
-import slick.driver.PostgresDriver.api._
-
+import akka.util.Timeout
 import im.actor.api.PeersImplicits
 import im.actor.api.rpc.PeerHelpers._
 import im.actor.api.rpc.integrtions.{ IntegrtionsService, ResponseIntegrationToken }
 import im.actor.api.rpc.peers.OutPeer
 import im.actor.api.rpc.{ ClientData, _ }
-import IntegrationServiceHelpers._
 import im.actor.server.api.http.HttpApiConfig
-import im.actor.server.persist
-import im.actor.server.util.ACLUtils
+import im.actor.server.api.rpc.service.webhooks.IntegrationServiceHelpers._
+import im.actor.server.group.GroupCommands.IntegrationTokenAck
+import im.actor.server.group.GroupErrors.NotAdmin
+import im.actor.server.group.{ GroupOffice, GroupProcessorRegion }
+import slick.driver.PostgresDriver.api._
 
-class IntegrationsServiceImpl(config: HttpApiConfig)(implicit db: Database, actorSystem: ActorSystem) extends IntegrtionsService with PeersImplicits {
+import scala.concurrent.duration._
+import scala.concurrent.{ ExecutionContext, Future }
+
+class IntegrationsServiceImpl(config: HttpApiConfig)(
+  implicit
+  db:                Database,
+  groupOfficeRegion: GroupProcessorRegion,
+  actorSystem:       ActorSystem
+) extends IntegrtionsService with PeersImplicits {
 
   override implicit val ec: ExecutionContext = actorSystem.dispatcher
+  private implicit val timeout = Timeout(10.seconds)
 
   override def jhandleGetIntegrationToken(groupPeer: OutPeer, clientData: ClientData): Future[HandlerResult[ResponseIntegrationToken]] = {
     val authorizedAction = requireAuth(clientData).map { implicit client ⇒
       withOutPeerAsGroupPeer(groupPeer) { groupOutPeer ⇒
-        withOwnGroupMember(groupOutPeer, client.userId) { fullGroup ⇒
-          for {
-            optGroupBot ← persist.GroupBot.findByGroup(fullGroup.id)
-          } yield optGroupBot.map(b ⇒ Ok(ResponseIntegrationToken(b.token, makeUrl(config, b.token)))).getOrElse(Error(TokenNotFound))
-        }
+        for {
+          IntegrationTokenAck(optToken) ← DBIO.from(GroupOffice.getIntegrationToken(groupOutPeer.groupId, client.userId))
+        } yield optToken.map(token ⇒ Ok(ResponseIntegrationToken(token, makeUrl(config, token)))).getOrElse(Error(TokenNotFound))
       }
     }
     db.run(toDBIOAction(authorizedAction))
@@ -36,15 +41,14 @@ class IntegrationsServiceImpl(config: HttpApiConfig)(implicit db: Database, acto
   override def jhandleRevokeIntegrationToken(groupPeer: OutPeer, clientData: ClientData): Future[HandlerResult[ResponseIntegrationToken]] = {
     val authorizedAction = requireAuth(clientData).map { implicit client ⇒
       withOutPeerAsGroupPeer(groupPeer) { groupOutPeer ⇒
-        withGroupAdmin(groupOutPeer) { fullGroup ⇒
-          val newToken = ACLUtils.accessToken(ThreadLocalRandom.current())
-          for {
-            _ ← persist.GroupBot.updateToken(fullGroup.id, newToken)
-          } yield Ok(ResponseIntegrationToken(newToken, makeUrl(config, newToken)))
-        }
+        for {
+          IntegrationTokenAck(optToken) ← DBIO.from(GroupOffice.revokeIntegrationToken(groupOutPeer.groupId, client.userId))
+        } yield optToken.map(token ⇒ Ok(ResponseIntegrationToken(token, makeUrl(config, token)))).getOrElse(Error(FailedToRevokeToken))
       }
     }
-    db.run(toDBIOAction(authorizedAction))
+    db.run(toDBIOAction(authorizedAction)) recover {
+      case NotAdmin ⇒ Error(CommonErrors.forbidden("Only admin can perform this action."))
+    }
   }
 
 }
