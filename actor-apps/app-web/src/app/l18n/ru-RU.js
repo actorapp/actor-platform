24a99b6f329918c8c5e7d79c3b54d8dee5e66d20
John Doe <speed@actor.im>
2015-08-04 01:54:06 +0300

    feat(web): add internationalisation support, translate some strings on Russian.

---
+++ actor-apps/app-web/src/app/l18n/ru-RU.js
@@ -0,0 +1,28 @@
+const russian = {
+  'locales': 'ru-RU',
+  'messages': {
+    // Sidebar menu
+    'setProfilePhoto': 'Установить аватар',
+    'editProfile': 'Редактировать профиль',
+    'configureIntegrations': 'Настройка интеграций',
+    'helpAndFeedback': 'Помощь и обратная связь',
+    'preferences': 'Настройки',
+    'signOut': 'Выход',
+
+    // Profiles
+    'setGroupPhoto': 'Установить фото группы',
+    'addIntegration': 'Добавить интеграцию',
+    'editGroup': 'Редактировать группу',
+    'leaveGroup': 'Покинуть группу',
+    'members':
+      '{numMembers, plural,' +
+        'zero {Нет участников}' +
+        'one {# участник}' +
+        'few {# участника}' +
+        'other {# участников}' +
+      '}'
+
+  }
+};
+
+export default russian;
