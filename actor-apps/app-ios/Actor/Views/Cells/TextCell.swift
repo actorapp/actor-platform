927c210f3cbe16349b930c5cc2340ccd8effb323
ex3ndr <me@ex3ndr.com>
2015-08-05 02:45:45 +0300

    feat(iOS): Added setting nickname

--- actor-apps/app-ios/Actor/Views/Cells/TextCell.swift
+++ actor-apps/app-ios/Actor/Views/Cells/TextCell.swift
@@ -1,11 +1,16 @@
 //
-//  Copyright (c) 2015 Actor LLC. <https://actor.im>
+//  TextCell.swift
+//  ActorApp
+//
+//  Created by Stepan Korshakov on 05.08.15.
+//  Copyright (c) 2015 Actor LLC. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-class TitledCell: CommonCell {
-    
+
+class TextCell: BasicCell {
+
     private var titleLabel: UILabel = UILabel()
     private var contentLabel: UILabel = UILabel()
     
@@ -21,10 +26,12 @@ class TitledCell: CommonCell {
         contentLabel.font = UIFont.systemFontOfSize(17.0)
         contentLabel.text = " "
         contentLabel.textColor = MainAppTheme.list.textColor
+        contentLabel.lineBreakMode = NSLineBreakMode.ByWordWrapping
+        contentLabel.numberOfLines = 0
         contentLabel.sizeToFit()
         contentView.addSubview(contentLabel)
     }
-
+    
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -34,14 +41,23 @@ class TitledCell: CommonCell {
         contentLabel.text = content
     }
     
-    func enableNavigationIcon() {
-        accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
-    }
-    
     override func layoutSubviews() {
         super.layoutSubviews()
         
         titleLabel.frame = CGRect(x: separatorInset.left, y: 7, width: contentView.bounds.width - separatorInset.left - 10, height: titleLabel.bounds.height)
-        contentLabel.frame = CGRect(x: separatorInset.left, y: 27, width: contentView.bounds.width - separatorInset.left - 10, height: contentLabel.bounds.height)
+        contentLabel.frame = CGRect(x: separatorInset.left, y: 27, width: contentView.bounds.width - separatorInset.left - 10, height: 500)
+        contentLabel.sizeToFit()
+    }
+    
+    class func measure(text: String) -> Double {
+        
+        var style = NSMutableParagraphStyle();
+        style.lineBreakMode = NSLineBreakMode.ByWordWrapping;
+        var rect = text.boundingRectWithSize(CGSize(width: 320 - 120, height: 10000),
+            options: NSStringDrawingOptions.UsesLineFragmentOrigin,
+            attributes: [NSFontAttributeName: UIFont.systemFontOfSize(17.0), NSParagraphStyleAttributeName: style],
+            context: nil);
+        
+        return Double(round(rect.height) + 16)
     }
 }
