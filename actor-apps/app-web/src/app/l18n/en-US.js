4d53268348767d92ddc2f246831b9218fe6d9170
John Doe <speed@actor.im>
2015-08-04 17:22:30 +0300

    feat+refactor(web): add more translated strings to app, update and cleanup markup for group profile.

--- actor-apps/app-web/src/app/l18n/en-US.js
+++ actor-apps/app-web/src/app/l18n/en-US.js
@@ -10,10 +10,18 @@ const english = {
     'signOut': 'Sign Out',
 
     // Profiles
+    'createdBy': 'сreated by {admin}',
+    'addPeople': 'Add people',
+    'more': 'More',
+    'actions': 'Actions',
+    'addToContacts': 'Add To Contacts',
+    'removeFromContacts': 'Remove From Contacts',
     'setGroupPhoto': 'Set Group Photo',
     'addIntegration': 'Add a Service Integration',
     'editGroup': 'Edit Group',
+    'deleteGroup': 'Delete Group',
     'leaveGroup': 'Leave Group',
+    'notifications': 'Notifications',
     'members':
       '{numMembers, plural,' +
         'zero {no Members}' +
