a5482b44252a2efd2c92189222fef14437b06c8d
Steve Kite <steve@actor.im>
2015-08-04 03:16:00 +0300

    feat(iOS): Reimplemented group creating

--- actor-apps/app-ios/Actor/Controllers/Compose/GroupMembersController.swift
+++ actor-apps/app-ios/Actor/Controllers/Compose/GroupMembersController.swift
@@ -4,19 +4,26 @@
 
 import UIKit
 
-class GroupMembersController: ContactsBaseViewController, VENTokenFieldDataSource, VENTokenFieldDelegate {
+class GroupMembersController: ContactsBaseViewController, CLTokenInputViewDelegate{
 
-    @IBOutlet weak var tokenField: UIView!
-    @IBOutlet weak var contactsTable: UITableView!
-    var tokenFieldView: VENTokenField!;
-    var selectedNames: Array<AMContact> = []
-    var groupTitle: String!
-    init(title: String) {
-        super.init(contentSection: 0, nibName: "GroupMembersController", bundle: nil)
+    private var groupTitle: String!
+    private var groupImage: UIImage?
+    
+    private var tableView = UITableView()
+    private var tokenView = CLTokenInputView()
+    private var tokenViewHeight: CGFloat = 48
+    
+    private var selected = [TokenRef]()
+    
+    init(title: String, image: UIImage?) {
+        super.init(contentSection: 0, nibName: nil, bundle: nil)
 
-        groupTitle = title
-        navigationItem.title = "Group Members";
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: UIBarButtonItemStyle.Done, target: self, action: "doNext")
+        self.groupTitle = title
+        self.groupImage = image
+        
+        navigationItem.title = localized("CreateGroupMembersTitle")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: localized("NavigationDone"), style: UIBarButtonItemStyle.Done, target: self, action: "doNext")
+        self.extendedLayoutIncludesOpaqueBars = true
     }
 
     required init(coder aDecoder: NSCoder) {
@@ -25,76 +32,93 @@ class GroupMembersController: ContactsBaseViewController, VENTokenFieldDataSourc
     
     override func viewDidLoad() {
         
-        tokenFieldView = VENTokenField(frame: CGRectMake(0, 66, tokenField.frame.width, 48))
-        tokenFieldView.delegate = self
-        tokenFieldView.dataSource = self
-        tokenFieldView.maxHeight = 48
-        tokenFieldView.contentMode = UIViewContentMode.Center
-        tokenFieldView.inputTextFieldTextColor = MainAppTheme.common.tokenFieldText
-        tokenFieldView.setColorScheme(MainAppTheme.common.tokenFieldBg)
-        tokenFieldView.backgroundColor = MainAppTheme.list.backyardColor
-        
-        tokenField.addSubview(tokenFieldView)
-        tokenField.backgroundColor = MainAppTheme.list.backyardColor
         view.backgroundColor = MainAppTheme.list.backyardColor
         
-        bindTable(contactsTable, fade: true)
+        tokenView.delegate = self
+        tokenView.backgroundColor = MainAppTheme.list.backyardColor
+        tokenView.fieldName = ""
+        tokenView.placeholderText = localized("CreateGroupMembersPlaceholders")
+        
+        tableView.keyboardDismissMode = UIScrollViewKeyboardDismissMode.OnDrag
+        
+        self.view.addSubview(tableView)
+        self.view.addSubview(tokenView)
+        
+        bindTable(tableView, fade: true)
         
         super.viewDidLoad()
     }
     
     func doNext() {
-        var res = IOSIntArray(length: UInt(selectedNames.count))
-        for i in 0..<selectedNames.count {
-            res.replaceIntAtIndex(UInt(i), withInt: selectedNames[i].getUid())
+        var res = IOSIntArray(length: UInt(selected.count))
+        for i in 0..<selected.count {
+            res.replaceIntAtIndex(UInt(i), withInt: selected[i].contact.getUid())
         }
         execute(MSG.createGroupCommandWithTitle(groupTitle, withAvatar: nil, withUids: res), successBlock: { (val) -> Void in
-            var gid = val as! JavaLangInteger
-            self.navigateNext(ConversationViewController(peer: AMPeer.groupWithInt(gid.intValue)), removeCurrent: true)
+            var gid = (val as! JavaLangInteger).intValue
+            if self.groupImage != nil {
+                MSG.changeGroupAvatar(gid, image: self.groupImage!)
+            }
+            self.navigateNext(ConversationViewController(peer: AMPeer.groupWithInt(gid)), removeCurrent: true)
         }) { (val) -> Void in
             
         }
     }
     
-    func tokenField(tokenField: VENTokenField!, didChangeText text: String!) {
-        filter(text)
-    }
-    
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
         var contact = objectAtIndexPath(indexPath) as! AMContact
-        var isRemoved = false
-        for i in 0..<selectedNames.count {
-            var n = selectedNames[i]
-            if (n.getUid() == contact.getUid()) {
-                selectedNames.removeAtIndex(i)
-                isRemoved = true
-                break
+        
+        for i in 0..<selected.count {
+            var n = selected[i]
+            if (n.contact.getUid() == contact.getUid()) {
+                selected.removeAtIndex(i)
+                tokenView.removeToken(n.token)
+                return
             }
         }
         
-        if (!isRemoved) {
-            selectedNames.append(contact)
-            filter("")
-        }
-        tokenFieldView?.reloadData()
+        var token = CLToken(displayText: contact.getName(), context: nil)
+        tokenView.addToken(token)
+        selected.append(TokenRef(contact: contact, token: token))
+        filter("")
+    }
+
+    func tokenInputView(view: CLTokenInputView!, didChangeText text: String!) {
+        filter(text)
+    }
+    
+    func tokenInputView(view: CLTokenInputView!, didChangeHeightTo height: CGFloat) {
+        tokenViewHeight = height
         
-        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        self.view.setNeedsLayout()
     }
     
-    func tokenField(tokenField: VENTokenField!, didDeleteTokenAtIndex index: UInt) {
-        self.selectedNames.removeAtIndex(Int(index))
-        tokenFieldView?.reloadData()
+    func tokenInputView(view: CLTokenInputView!, didRemoveToken token: CLToken!) {
+        for i in 0..<selected.count {
+            var n = selected[i]
+            if (n.token == token) {
+                selected.removeAtIndex(i)
+                return
+            }
+        }
     }
     
-    func tokenField(tokenField: VENTokenField!, titleForTokenAtIndex index: UInt) -> String! {
-        return self.selectedNames[Int(index)].getName()
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        tokenView.frame = CGRectMake(0, 64, view.frame.width, tokenViewHeight)
+        tableView.frame = CGRectMake(0, tokenViewHeight, view.frame.width, view.frame.height - tokenViewHeight)
     }
+}
+
+private class TokenRef {
+    var contact: AMContact
+    var token: CLToken
     
-    func tokenFieldCollapsedText(tokenField: VENTokenField!) -> String! {
-        return "selected \(self.selectedNames.count)"
-    }
-    
-    func numberOfTokensInTokenField(tokenField: VENTokenField!) -> UInt {
-        return UInt(self.selectedNames.count)
+    init(contact: AMContact, token: CLToken) {
+        self.contact = contact
+        self.token = token
     }
 }
