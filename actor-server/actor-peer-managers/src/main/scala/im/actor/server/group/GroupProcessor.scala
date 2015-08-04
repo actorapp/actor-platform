45346f31571497e173fd057fcaec143fb1ef94be
John Doe <speed@actor.im>
2015-08-04 03:37:07 +0300

    perf(server): passivate GroupProcessor in 5 hours of inactivity

--- actor-server/actor-peer-managers/src/main/scala/im/actor/server/group/GroupProcessor.scala
+++ actor-server/actor-peer-managers/src/main/scala/im/actor/server/group/GroupProcessor.scala
@@ -142,7 +142,7 @@ private[group] final class GroupProcessor(
 
   override def persistenceId = GroupOffice.persistenceIdFor(groupId)
 
-  context.setReceiveTimeout(1.hour)
+  context.setReceiveTimeout(5.hours)
 
   private val MaxCacheSize = 100L
 
