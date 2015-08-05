ad4d17ac250d892d5f6d4dd8ece9441aba5dbf6c
John Doe <speed@actor.im>
2015-08-05 20:05:50 +0300

    feat+fix+refactor+style(web): add new invite to group popup, hide non implemented elements, cleanup code.

--- actor-apps/app-web/src/app/stores/InviteUserStore.js
+++ actor-apps/app-web/src/app/stores/InviteUserStore.js
@@ -1,49 +1,67 @@
-import Reflux from 'reflux';
+import { EventEmitter } from 'events';
+import ActorAppDispatcher from 'dispatcher/ActorAppDispatcher';
+import { ActionTypes } from 'constants/ActorAppConstants';
+//import DialogStore from './DialogStore';
 
 import ActorClient from 'utils/ActorClient';
 
-import InviteUserActions from 'actions/InviteUserActions';
+const CHANGE_EVENT = 'change';
 
-let
-  _isModalOpen = false,
-  _group = null,
-  _inviteUrl = null;
+let _isModalOpen = false,
+    _group = null,
+    _inviteUrl = null;
 
-export default Reflux.createStore({
-  init () {
-    this.listenTo(InviteUserActions.modalOpen, this.onModalOpen);
-    this.listenTo(InviteUserActions.modalClose, this.onModalClose);
-  },
-
-  isModalOpen () {
-    return _isModalOpen;
-  },
-
-  getGroup () {
-    return _group;
-  },
-
-  getInviteUrl () {
-    return _inviteUrl;
-  },
-
-  onModalOpen (group) {
-    _isModalOpen = true;
-    _group = group;
-
-    ActorClient.getInviteUrl(group.id)
-      .then((url) => {
-        _inviteUrl = url;
-        this.trigger();
-      });
-
-    this.trigger();
-  },
-
-  onModalClose () {
-    _isModalOpen = false;
-    _group = null;
-
-    this.trigger();
+class InviteUserStore extends EventEmitter {
+  emitChange() {
+    this.emit(CHANGE_EVENT);
   }
+
+  addChangeListener(callback) {
+    this.on(CHANGE_EVENT, callback);
+  }
+
+  removeChangeListener(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  }
+
+  isModalOpen() {
+    return _isModalOpen;
+  }
+
+  getGroup() {
+    return _group;
+  }
+
+  getInviteUrl() {
+    return _inviteUrl;
+  }
+}
+
+let InviteUserStoreInstance = new InviteUserStore();
+
+InviteUserStoreInstance.dispatchToken = ActorAppDispatcher.register(action => {
+  switch(action.type) {
+    case ActionTypes.INVITE_USER_MODAL_SHOW:
+      _isModalOpen = true;
+      _group = action.group;
+
+      ActorClient.getInviteUrl(action.group.id)
+        .then((url) => {
+          _inviteUrl = url;
+          InviteUserStoreInstance.emitChange();
+        });
+      break;
+    case ActionTypes.INVITE_USER_MODAL_HIDE:
+      _isModalOpen = false;
+      _group = null;
+      break;
+    case ActionTypes.SELECTED_DIALOG_INFO_CHANGED:
+      _group = action.info;
+      break;
+    default:
+      return;
+  }
+  InviteUserStoreInstance.emitChange();
 });
+
+export default InviteUserStoreInstance;
