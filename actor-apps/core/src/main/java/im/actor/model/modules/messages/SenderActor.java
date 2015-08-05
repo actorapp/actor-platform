4dc583fdb42854987da564bbab32700f684747e8
ex3ndr <me@ex3ndr.com>
2015-08-05 13:49:34 +0300

    fix(core): Better mentions handling

--- actor-apps/core/src/main/java/im/actor/model/modules/messages/SenderActor.java
+++ actor-apps/core/src/main/java/im/actor/model/modules/messages/SenderActor.java
@@ -132,7 +132,8 @@ public class SenderActor extends ModuleActor {
                     User user = getUser(member.getUid());
                     if (user.getNick() != null) {
                         String nick = "@" + user.getNick();
-                        if (text.contains(nick + " ") || text.contains(" " + nick) || text.endsWith(nick) || text.equals(nick)) {
+                        // TODO: Better filtering
+                        if (text.contains(nick + ":") || text.contains(nick + " ") || text.contains(" " + nick) || text.endsWith(nick) || text.equals(nick)) {
                             mentions.add(user.getUid());
                         }
                     }
