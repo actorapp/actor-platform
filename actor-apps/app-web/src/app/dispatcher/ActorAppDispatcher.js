d806ca87c5d3fab99cd9ae3879d6eeaeb4eb898c
John Doe <speed@actor.im>
2015-08-06 05:20:05 +0300

    fix(web): disable dispatched action logging;

--- actor-apps/app-web/src/app/dispatcher/ActorAppDispatcher.js
+++ actor-apps/app-web/src/app/dispatcher/ActorAppDispatcher.js
@@ -2,8 +2,4 @@ import { Dispatcher } from 'flux';
 
 const ActorAppDispatcher = new Dispatcher();
 
-ActorAppDispatcher.register(action => {
-  console.info(action);
-});
-
 export default ActorAppDispatcher;
