55ef16a06bc1c97cf34cb5f3290714d16ab2fd79
ex3ndr <me@ex3ndr.com>
2015-08-07 03:17:10 +0300

    wip(core): Reorganization of core libs

---
+++ actor-apps/core/src/javaThreading/im/actor/model/runtime/threads/JavaAtomicLong.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2015 Actor LLC. <https://actor.im>
+ */
+
+package im.actor.model.runtime.threads;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import im.actor.model.util.AtomicLongCompat;
+
+public class JavaAtomicLong extends AtomicLongCompat {
+    final AtomicLong atomicLong;
+
+    public JavaAtomicLong(long value) {
+        atomicLong = new AtomicLong(value);
+    }
+
+    @Override
+    public long get() {
+        return atomicLong.get();
+    }
+
+    @Override
+    public long incrementAndGet() {
+        return atomicLong.incrementAndGet();
+    }
+
+    @Override
+    public long getAndIncrement() {
+        return atomicLong.getAndIncrement();
+    }
+
+    @Override
+    public void set(long v) {
+        atomicLong.set(v);
+    }
+}
---
+++ actor-apps/core/src/mvvm/im/actor/model/cocoa/CocoaThreadingProvider.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2015 Actor LLC. <https://actor.im>
+ */
+
+package im.actor.model.cocoa;
+
+import im.actor.model.runtime.JavaThreadingProvider;
+
+public class CocoaThreadingProvider extends JavaThreadingProvider {
+}
