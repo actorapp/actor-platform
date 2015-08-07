0b09c4ba6b6ae3557496faa67f4a5a5919306732
rock_jam <5min4eq.unity@gmail.com>
2015-08-07 15:32:09 +0300

    Revert "perf(server): history query optimization"
    This reverts commit ea9d0398b1f2b2f6abf6c0f3b2bcfac69c58e42b.

--- actor-server/actor-persist/src/main/scala/im/actor/server/persist/Dialog.scala
+++ actor-server/actor-persist/src/main/scala/im/actor/server/persist/Dialog.scala
@@ -8,7 +8,7 @@ import com.github.tototoshi.slick.PostgresJodaSupport._
 import org.joda.time.DateTime
 import slick.dbio.Effect.{ Read, Write }
 import slick.driver.PostgresDriver.api._
-import slick.profile.{ SqlStreamingAction, SqlAction, FixedSqlStreamingAction, FixedSqlAction }
+import slick.profile.{ SqlAction, FixedSqlStreamingAction, FixedSqlAction }
 
 import im.actor.server.models
 
@@ -106,10 +106,11 @@ object Dialog {
     }
   }
 
-  def findExistingUserIds(userIds: Set[Int], peer: models.Peer): SqlStreamingAction[Vector[Int], Int, Effect] = {
-    val in = userIds.mkString(",")
-    sql"""SELECT user_id FROM dialogs WHERE peer_type = ${peer.typ.toInt} AND peer_id = ${peer.id} AND user_id IN ($in)"""
-      .as[Int]
+  def findExistingUserIds(userIds: Set[Int], peer: models.Peer): FixedSqlStreamingAction[Seq[Int], Int, Read] = {
+    byPeerC.applied((peer.typ.toInt, peer.id))
+      .filter(_.userId inSetBind userIds)
+      .map(_.userId)
+      .result
   }
 
   def updateLastMessageDates(userIds: Set[Int], peer: models.Peer, lastMessageDate: DateTime)(implicit ec: ExecutionContext) = {
