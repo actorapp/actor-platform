2843a86ce1d69e61500cb7638fdbb3ff08f1c320
John Doe <speed@actor.im>
2015-08-10 05:12:21 +0300

    fix+perf(server): use single datasource for all database-related stuff

--- actor-server/actor-persist/src/main/scala/im/actor/server/db/DbExtension.scala
+++ actor-server/actor-persist/src/main/scala/im/actor/server/db/DbExtension.scala
@@ -1,23 +1,61 @@
 package im.actor.server.db
 
+import java.util
+import javax.naming.{ NamingException, Context, InitialContext }
+import javax.naming.spi.{ InitialContextFactory, InitialContextFactoryBuilder, NamingManager }
+import javax.sql.DataSource
+
+import scala.util.Try
+
 import akka.actor._
+import com.typesafe.config.Config
+import com.github.kxbmap.configs._
+import org.flywaydb.core.Flyway
 import slick.driver.PostgresDriver.api.Database
 import slick.jdbc.{ JdbcDataSource, HikariCPJdbcDataSource }
+
+import im.actor.server.JNDI
 
 trait DbExtension extends Extension {
   val ds: JdbcDataSource
   val db: Database
 }
 
-final class DbExtensionImpl(val ds: HikariCPJdbcDataSource, val db: Database) extends Extension
+final class DbExtensionImpl(val ds: HikariCPJdbcDataSource, val db: Database) extends Extension with FlywayInit {
+  private lazy val flyway: Flyway = initFlyway(ds.ds)
 
-object DbExtension extends ExtensionId[DbExtensionImpl] with ExtensionIdProvider with DbInit {
+  def clean(): Unit = flyway.clean()
+
+  def migrate(): Unit = flyway.migrate()
+}
+
+object DbExtension extends ExtensionId[DbExtensionImpl] with ExtensionIdProvider {
+  private val JndiPath = "DefaultDataSource"
+
   override def lookup = DbExtension
 
-  override def createExtension(system: ExtendedActorSystem) = {
+  override def createExtension(system: ExtendedActorSystem): DbExtensionImpl = {
     val sqlConfig = system.settings.config.getConfig("services.postgresql")
-    val ds = initDs(sqlConfig).toOption.get
+    val ds = initDs(sqlConfig).get
     val db = initDb(ds)
+
+    system.registerOnTermination(db.close())
+
     new DbExtensionImpl(ds, db)
+  }
+
+  private def initDs(sqlConfig: Config): Try[HikariCPJdbcDataSource] = {
+    for {
+      _ ← sqlConfig.get[Try[String]]("host")
+      _ ← sqlConfig.get[Try[Int]]("port")
+      _ ← sqlConfig.get[Try[String]]("db")
+      _ ← sqlConfig.get[Try[String]]("user")
+      _ ← sqlConfig.get[Try[String]]("password")
+    } yield HikariCPJdbcDataSource.forConfig(sqlConfig, null, "main")
+  }
+
+  private def initDb(ds: HikariCPJdbcDataSource): Database = {
+    JNDI.initialContext.rebind(JndiPath, ds.ds)
+    Database.forSource(ds)
   }
 }
