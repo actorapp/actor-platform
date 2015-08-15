88ae57f39e9e6f6476deb93418e70bb12ea50840
ex3ndr <me@ex3ndr.com>
2015-08-15 17:15:15 +0300

    wip(*): Big project refactoring: Cocoa: build improvements

--- actor-apps/runtime-cocoa/src/main/java/im/actor/runtime/NetworkRuntimeProvider.java
+++ actor-apps/runtime-cocoa/src/main/java/im/actor/runtime/NetworkRuntimeProvider.java
@@ -1,7 +1,7 @@
 package im.actor.runtime;
 
-import im.actor.runtime.cocoa.CocoaNetworkRuntime;
+import im.actor.runtime.cocoa.CocoaNetworkProxyProvider;
 
-public class NetworkRuntimeProvider extends CocoaNetworkRuntime {
+public class NetworkRuntimeProvider extends CocoaNetworkProxyProvider {
 
 }
--- actor-apps/runtime-cocoa/src/main/java/im/actor/runtime/cocoa/CocoaNetworkRuntime.java
+++
@@ -1,6 +0,0 @@
-package im.actor.runtime.cocoa;
-
-import im.actor.runtime.generic.GenericNetworkProvider;
-
-public class CocoaNetworkRuntime extends GenericNetworkProvider {
-}
