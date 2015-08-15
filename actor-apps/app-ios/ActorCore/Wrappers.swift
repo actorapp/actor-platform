f8ebb51076fed8b916e627b4e91311c3c211696a
ex3ndr <me@ex3ndr.com>
2015-08-15 21:42:08 +0300

    wip(*): Big project refactoring: Cocoa: Fixing gitignore

---
+++ actor-apps/app-ios/ActorCore/Wrappers.swift
@@ -0,0 +1,96 @@
+//
+//  Copyright (c) 2014-2015 Actor LLC. <https://actor.im>
+//
+
+import Foundation
+
+class CocoaCallback: NSObject, ACCommandCallback {
+    
+    var resultClosure: ((val: AnyObject!) -> ())?;
+    var errorClosure: ((val:JavaLangException!) -> ())?;
+    
+    init<T>(result: ((val:T?) -> ())?, error: ((val:JavaLangException!) -> ())?) {
+        super.init()
+        self.resultClosure = { (val: AnyObject!) -> () in
+            result?(val: val as? T)
+        }
+        self.errorClosure = error
+    }
+    
+    func onResult(res: AnyObject!) {
+        resultClosure?(val: res)
+    }
+    
+    func onError(e: JavaLangException!) {
+        errorClosure?(val: e)
+    }
+}
+
+class CocoaDownloadCallback : NSObject, ACFileCallback {
+    
+    let notDownloaded: (()->())?
+    let onDownloading: ((progress: Double) -> ())?
+    let onDownloaded: ((fileName: String) -> ())?
+    
+    init(notDownloaded: (()->())?, onDownloading: ((progress: Double) -> ())?, onDownloaded: ((reference: String) -> ())?) {
+        self.notDownloaded = notDownloaded;
+        self.onDownloading = onDownloading;
+        self.onDownloaded = onDownloaded;
+    }
+    
+    init(onDownloaded: (reference: String) -> ()) {
+        self.notDownloaded = nil;
+        self.onDownloading = nil;
+        self.onDownloaded = onDownloaded;
+    }
+    
+    func onNotDownloaded() {
+        self.notDownloaded?();
+    }
+    
+    func onDownloading(progress: jfloat) {
+        self.onDownloading?(progress: Double(progress));
+    }
+    
+    func onDownloaded(reference: ARFileSystemReference!) {
+        self.onDownloaded?(fileName: reference!.getDescriptor());
+    }
+}
+
+class CocoaUploadCallback : NSObject, ACUploadFileCallback {
+    
+    let notUploaded: (()->())?
+    let onUploading: ((progress: Double) -> ())?
+    let onUploadedClosure: (() -> ())?
+    
+    init(notUploaded: (()->())?, onUploading: ((progress: Double) -> ())?, onUploadedClosure: (() -> ())?) {
+        self.onUploading = onUploading
+        self.notUploaded = notUploaded
+        self.onUploadedClosure = onUploadedClosure;
+    }
+    
+    func onNotUploading() {
+        self.notUploaded?();
+    }
+    
+    func onUploaded() {
+        self.onUploadedClosure?()
+    }
+    
+    func onUploading(progress: jfloat) {
+        self.onUploading?(progress: Double(progress))
+    }
+}
+
+class CocoaConversationVMCallback: NSObject, ACConversationVMCallback {
+    
+    let closure: ((unreadId: jlong, index: jint)->())?
+    
+    init(loadClosure: ((unreadId: jlong, index: jint)->())) {
+        self.closure = loadClosure
+    }
+    
+    func onLoadedWithLong(unreadId: jlong, withInt index: jint) {
+        
+    }
+}
