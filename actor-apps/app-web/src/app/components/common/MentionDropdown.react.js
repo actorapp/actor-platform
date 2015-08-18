137694d477c816b4181cc4e8706c226097493619
Oleg Shilov <me@olegshilov.com>
2015-08-19 01:08:06 +0300

    wip+fix(web):  handle keydowns and scroll list on mention component.

--- actor-apps/app-web/src/app/components/common/MentionDropdown.react.js
+++ actor-apps/app-web/src/app/components/common/MentionDropdown.react.js
@@ -3,6 +3,11 @@ import classnames from 'classnames';
 
 import { KeyCodes } from 'constants/ActorAppConstants';
 
+import AvatarItem from 'components/common/AvatarItem.react';
+
+let scrollIndex = 0;
+const DROPDOWN_ITEM_HEIGHT = 38; // is this right?
+
 class MentionDropdown extends React.Component {
   static propTypes = {
     mentions: React.PropTypes.array,
@@ -63,14 +68,117 @@ class MentionDropdown extends React.Component {
     this.setState({isShown: false});
   };
 
+  onSelect = (value) => {
+    const { onSelect } = this.props;
+    onSelect(value);
+  };
+
+  handleScroll = (top) => {
+    const menuListNode = React.findDOMNode(this.refs.mentionList);
+    menuListNode.scrollTop = top;
+  };
+
+  onKeyDown = (event) => {
+    const { mentions, onClose } = this.props;
+    const { selectedIndex } = this.state;
+    const visibleItems = 6;
+    let index = selectedIndex;
+
+    console.warn(mentions.length);
+
+    if (index !== null) {
+      switch (event.keyCode) {
+        case KeyCodes.ENTER:
+          event.stopPropagation();
+          event.preventDefault();
+          this.onSelect(mentions[selectedIndex]);
+          break;
+
+        case KeyCodes.ARROW_UP:
+          event.stopPropagation();
+          event.preventDefault();
+
+          if (index > 0) {
+            index -= 1;
+          } else if (index === 0) {
+            index = mentions.length - 1;
+          }
+
+          if (scrollIndex > index) {
+            scrollIndex = index;
+          } else if (index === mentions.length - 1) {
+            scrollIndex = mentions.length - visibleItems;
+          }
+
+          this.handleScroll(scrollIndex * DROPDOWN_ITEM_HEIGHT);
+
+          this.setState({selectedIndex: index});
+          break;
+        case KeyCodes.ARROW_DOWN:
+          event.stopPropagation();
+          event.preventDefault();
+
+          if (index < mentions.length - 1) {
+            index += 1;
+          } else if (index === mentions.length - 1) {
+            index = 0;
+          }
+
+          if (index + 1 > scrollIndex + visibleItems) {
+            scrollIndex = index + 1 - visibleItems;
+          } else if (index === 0) {
+            scrollIndex = 0;
+          }
+
+          this.handleScroll(scrollIndex * DROPDOWN_ITEM_HEIGHT);
+
+          this.setState({selectedIndex: index});
+          break;
+        default:
+      }
+    }
+
+    if (event.keyCode === KeyCodes.ESC) {
+      this.closeMentions();
+      if (onClose) onClose();
+    }
+  };
 
   render() {
     const { className, mentions } = this.props;
-    const { isShown } = this.state;
+    const { isShown, selectedIndex } = this.state;
 
     const mentionClassName = classnames('mention', {
       'mention--opened': isShown
     }, className);
+
+    const mentionsElements = _.map(mentions, (mention, index) => {
+      const itemClassName = classnames('mention__list__item', {
+        'mention__list__item--active': selectedIndex === index
+      });
+      // TODO: fix to use real nicknames
+      const nickname = mention.nickname ? <span className="nickname">{mention.nickname}</span> : null;
+
+      return (
+        <li className={itemClassName}
+            key={index}
+            onClick={() => this.onSelect(mention)}
+            onMouseOver={() => this.setState({selectedIndex: index})}>
+          <AvatarItem image={mention.avatar}
+                      placeholder={mention.placeholder}
+                      size="tiny"
+                      title={mention.title}/>
+          <div className="title">
+            {nickname}
+            <span className="name">{mention.title}</span>
+          </div>
+        </li>
+      );
+    });
+
+
+
+
 
     if (isShown) {
       return (
@@ -78,9 +186,7 @@ class MentionDropdown extends React.Component {
           <div className="mention__wrapper">
             <header className="mention__header">mention__header</header>
             <ul className="mention__list" ref="mentionList">
-              <li className="mention__list__item">mention__list__item</li>
-              <li className="mention__list__item">mention__list__item</li>
-              <li className="mention__list__item">mention__list__item</li>
+              {mentionsElements}
             </ul>
           </div>
         </div>
