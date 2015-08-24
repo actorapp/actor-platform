b0c4d8cd43e917d78266ccd51de6fab8e8af91f8
John Doe <speed@actor.im>
2015-08-24 23:11:16 +0300

    feat(web): set favicon when counter changed

--- actor-apps/app-web/src/app/components/Main.react.js
+++ actor-apps/app-web/src/app/components/Main.react.js
@@ -6,29 +6,20 @@ import PeerUtils from 'utils/PeerUtils';
 
 import DialogActionCreators from 'actions/DialogActionCreators';
 import VisibilityActionCreators from 'actions/VisibilityActionCreators';
-import FaviconActionCreators from 'actions/FaviconActionCreators';
-import FaviconStore from 'stores/FaviconStore';
 
 import SidebarSection from 'components/SidebarSection.react';
 import DialogSection from 'components/DialogSection.react';
-import Favicon from 'components/common/Favicon.react';
 import Banner from 'components/common/Banner.react';
+import Favicon from 'components/common/Favicon.react';
 
 const visibilitychange = 'visibilitychange';
 
 const onVisibilityChange = () => {
   if (!document.hidden) {
     VisibilityActionCreators.createAppVisible();
-    FaviconActionCreators.setDefaultFavicon();
   } else {
     VisibilityActionCreators.createAppHidden();
   }
-};
-
-const getStateFromStores = () => {
-  return {
-    faviconPath: FaviconStore.getFaviconPath()
-  };
 };
 
 class Main extends React.Component {
@@ -43,10 +34,7 @@ class Main extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = getStateFromStores();
-
     document.addEventListener(visibilitychange, onVisibilityChange);
-    FaviconStore.addChangeListener(this.onChange);
 
     if (!document.hidden) {
       VisibilityActionCreators.createAppVisible();
@@ -59,16 +47,12 @@ class Main extends React.Component {
     }
   }
 
-  onChange = () => {
-    this.setState(getStateFromStores());
-  };
-
   render() {
     const peer = PeerUtils.stringToPeer(this.props.params.id);
 
     return (
       <div className="app">
-        <Favicon path={this.state.faviconPath}/>
+        <Favicon/>
         <Banner/>
         <SidebarSection selectedPeer={peer}/>
         <DialogSection peer={peer}/>
