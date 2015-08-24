15142804ccd76eeddfbdd01462fad5b7455e9a87
prettynatty <fear@loathing.in>
2015-08-24 20:22:22 +0300

    feat(web): handle people:// links

--- actor-apps/app-web/src/app/utils/Markdown.js
+++ actor-apps/app-web/src/app/utils/Markdown.js
@@ -3,6 +3,10 @@ import _ from 'lodash';
 import marked from 'marked';
 import hljs from 'highlight.js';
 
+import ActorClient from 'utils/ActorClient';
+
+import DialogActionCreators from 'actions/DialogActionCreators';
+
 const plainTextRenderer = _.assign(new marked.Renderer({
   tables: false
 }), {
@@ -32,10 +36,27 @@ const plainTextRenderer = _.assign(new marked.Renderer({
 
   del: (text) => text,
 
-  link: (href, title, text) => '[' + title + '](' + href + ')',
+  link: (href, title, text) => '[' + (title || '') + (text || '') + '](' + href + ')',
 
-  image: (href, title, text) => '[' + title + '](' + href + ')'
+  image: (href, title, text) => '[' + (title || '') + (text || '') + '](' + href + ')'
 });
+
+const PEOPLE = 'people://';
+
+window.handleMdClick = (e) => {
+  const url = e.target.getAttribute('href');
+
+  if (_.startsWith(url, PEOPLE)) {
+    const userId = parseInt(url.substring(PEOPLE.length));
+    if (userId > 0) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      const peer = ActorClient.getUserPeer(userId);
+      DialogActionCreators.selectDialogPeer(peer);
+    }
+  }
+};
 
 const defaultRenderer = _.assign(new marked.Renderer({
   sanitize: true,
@@ -48,7 +69,7 @@ const defaultRenderer = _.assign(new marked.Renderer({
     let external, newWindow, out;
     external = /^https?:\/\/.+$/.test(href);
     newWindow = external || title === 'newWindow';
-    out = '<a href=\"' + href + '\"';
+    out = '<a href=\"' + href + '\" onclick="window.handleMdClick(event)"';
     if (newWindow) {
       out += ' target="_blank"';
     }
