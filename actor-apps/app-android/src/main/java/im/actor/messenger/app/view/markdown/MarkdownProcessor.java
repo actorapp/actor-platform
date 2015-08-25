162bded115469e75a51a0663260a7d083469a210
Steve Kite <steve@actor.im>
2015-08-25 05:51:01 +0300

    feat(*): Implemented actor markdown parser, displaying new markdown text

--- actor-apps/app-android/src/main/java/im/actor/messenger/app/view/markdown/MarkdownProcessor.java
+++ actor-apps/app-android/src/main/java/im/actor/messenger/app/view/markdown/MarkdownProcessor.java
@@ -1,64 +1,82 @@
 package im.actor.messenger.app.view.markdown;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.view.View;
 
-import im.actor.runtime.markdown.MarkdownDocument;
-import in.uncod.android.bypass.Bypass;
-import in.uncod.android.bypass.Element;
+import im.actor.messenger.app.AppContext;
+import im.actor.messenger.app.fragment.preview.CodePreviewActivity;
+import im.actor.runtime.markdown.MDDocument;
+import im.actor.runtime.markdown.MDRawText;
+import im.actor.runtime.markdown.MDSection;
+import im.actor.runtime.markdown.MDSpan;
+import im.actor.runtime.markdown.MDText;
 
 public class MarkdownProcessor {
 
-    private Bypass bypass;
-
     public MarkdownProcessor() {
-        bypass = new Bypass();
+
     }
 
     public Spannable processText(String markdown) {
+        MDDocument doc = im.actor.runtime.markdown.MarkdownProcessor.processDocument(markdown);
+
         SpannableStringBuilder builder = new SpannableStringBuilder();
-
-//        Document document = bypass.processMarkdown(markdown);
-//
-//        ArrayList<QueueItem> elementQueue = new ArrayList<QueueItem>();
-//        for (int i = 0; i < document.getElementCount(); i++) {
-//            elementQueue.add(i, new QueueItem(document.getElement(i)));
-//        }
-//
-//        while (!elementQueue.isEmpty()) {
-//            QueueItem item = elementQueue.get(0);
-//            Element element = item.element;
-//            if (!item.isExpanded) {
-//                builder.append(element.toString());
-//                item.isExpanded = true;
-//                for (int i = 0; i < element.size(); i++) {
-//                    elementQueue.add(i, new QueueItem(element.getChildren(i)));
-//                }
-//                continue;
-//            }
-//            elementQueue.remove(0);
-//
-//
-//            // Process Element
-//            builder.append(element.getText() + "[/" + element.getType() + "]");
-//        }
-
-        MarkdownDocument doc = im.actor.runtime.markdown.MarkdownProcessor.processDocument(markdown);
-
-        return new SpannableStringBuilder(doc.getRootElement().getText());
+        boolean isFirst = true;
+        for (MDSection s : doc.getSections()) {
+            if (isFirst) {
+                isFirst = false;
+            } else {
+                builder.append("\n");
+            }
+            if (s.getType() == MDSection.TYPE_CODE) {
+                int start = builder.length();
+                builder.append("View Source Code");
+                final String text = s.getCode().getCode();
+                builder.setSpan(new RelativeSizeSpan(1.1f), start, builder.length(),
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                builder.setSpan(new ForegroundColorSpan(Color.RED), start, builder.length(),
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                builder.setSpan(new ClickableSpan() {
+                    @Override
+                    public void onClick(View view) {
+                        AppContext.getContext().startActivity(
+                                new Intent(AppContext.getContext(), CodePreviewActivity.class)
+                                        .putExtra("source_code", text)
+                                        .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                    }
+                }, start, builder.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            } else {
+                writeText(s.getText(), builder);
+            }
+        }
+        return builder;
     }
 
-    private class QueueItem {
-        private Element element;
-        private boolean isExpanded;
-
-        public QueueItem(Element element) {
-            this.element = element;
-            this.isExpanded = false;
-        }
-
-        public void markExpanded() {
-            isExpanded = true;
+    private void writeText(MDText[] texts, SpannableStringBuilder builder) {
+        for (MDText text : texts) {
+            if (text instanceof MDRawText) {
+                builder.append(((MDRawText) text).getRawText());
+            } else if (text instanceof MDSpan) {
+                MDSpan span = (MDSpan) text;
+                int start = builder.length();
+                writeText(span.getChild(), builder);
+                Object spanObj;
+                if (span.getSpanType() == MDSpan.TYPE_BOLD) {
+                    spanObj = new StyleSpan(Typeface.BOLD);
+                } else {
+                    spanObj = new StyleSpan(Typeface.ITALIC);
+                }
+                builder.setSpan(spanObj, start, builder.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            }
         }
     }
 }
---
+++ actor-apps/runtime/src/main/java/im/actor/runtime/markdown/MDText.java
@@ -0,0 +1,5 @@
+package im.actor.runtime.markdown;
+
+public abstract class MDText {
+
+}
