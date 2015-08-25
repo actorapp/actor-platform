8e2d558b732e9c5a1dcebf77272d85000f3577f2
John Doe <speed@actor.im>
2015-08-25 04:10:00 +0300

    wip(web): add state to user invite buttons

--- actor-apps/app-web/src/app/components/modals/invite-user/ContactItem.react.js
+++ actor-apps/app-web/src/app/components/modals/invite-user/ContactItem.react.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import ReactMixin from 'react-mixin';
-import addons from 'react/addons';
+//import ReactMixin from 'react-mixin';
+//import addons from 'react/addons';
 import classnames from 'classnames';
+
+import InviteUserStore from 'stores/InviteUserStore';
+
+import { ChangeState } from 'constants/ActorAppConstants';
 
 import AvatarItem from 'components/common/AvatarItem.react';
 
-const {addons: { PureRenderMixin }} = addons;
+//const {addons: { PureRenderMixin }} = addons;
 
-@ReactMixin.decorate(PureRenderMixin)
+const getStateFromStore = (props) => {
+  const { contact } = props;
+  const group = InviteUserStore.getGroup();
+
+  return {
+    changingState: InviteUserStore.getChangeState(group.id, contact.uid)
+  }
+};
+
+//@ReactMixin.decorate(PureRenderMixin)
 class ContactItem extends React.Component {
   static propTypes = {
     contact: React.PropTypes.object,
@@ -17,22 +30,54 @@ class ContactItem extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = getStateFromStore(props);
+  }
+
+  componentWillUnmount() {
+    const { contact } = this.props;
+    const group = InviteUserStore.getGroup();
+    InviteUserStore.resetChangeState(group.id, contact.uid);
   }
 
   onSelect = () => {
     this.props.onSelect(this.props.contact);
+    InviteUserStore.addChangeListener(this.onChange);
+  };
+
+  onChange = () => {
+    this.setState(getStateFromStore(this.props));
+
+    const { changingState } = this.state;
+
+    if (changingState === ChangeState.SUCCESS && changingState === ChangeState.FAILURE) {
+      InviteUserStore.removeChangeListener(this.onChange);
+    }
   };
 
   render() {
     const { contact, member } = this.props;
+    const { changingState } = this.state;
+
+    let controls;
+    switch (changingState) {
+      case ChangeState.SUCCESS:
+      case ChangeState.INIT:
+        controls = !member
+          ? <a className="material-icons" onClick={this.onSelect}>person_add</a>
+          : <i className="material-icons">check</i>;
+        break;
+      case ChangeState.IN_PROCESS:
+        controls = <i className="material-icons">refresh</i>;
+        break;
+      case ChangeState.FAILURE:
+        controls = <i className="material-icons">warning</i>;
+        break;
+    }
 
     const contactClassName = classnames('contacts__list__item row', {
       'contacts__list__item--member': member
     });
-
-    const controls = !member
-      ? <a className="material-icons" onClick={this.onSelect}>person_add</a>
-      : <i className="material-icons">check</i>;
 
     return (
       <li className={contactClassName}>
