0302fcab9abcc8f1d93aae9acccc189a9f9b2ec5
kor-ka <badgrib@gmail.com>
2015-08-28 23:24:36 +0300

    feat(android): mentions medium font

--- actor-apps/app-android/src/main/java/im/actor/messenger/app/view/MentionSpan.java
+++ actor-apps/app-android/src/main/java/im/actor/messenger/app/view/MentionSpan.java
@@ -1,6 +1,7 @@
 package im.actor.messenger.app.view;
 
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.text.TextPaint;
 import android.view.View;
 
@@ -11,6 +12,8 @@ import im.actor.messenger.app.Intents;
 public class MentionSpan extends BaseUrlSpan {
     int[] colors;
     int userId;
+
+    public static Typeface tf;
 
     public MentionSpan(String nick, int userId, boolean hideUrlStyle) {
         super(nick, hideUrlStyle);
@@ -29,25 +32,30 @@ public class MentionSpan extends BaseUrlSpan {
     @Override
     public void updateDrawState(TextPaint ds) {
         super.updateDrawState(ds);
-        if(hideUrlStyle){
+        if (hideUrlStyle) {
             ds.setUnderlineText(false);
             ds.setColor(Color.BLACK);
         }
 
+        if (tf == null) {
+            tf = Typeface.createFromAsset(AppContext.getContext().getAssets(), "Roboto-Medium.ttf");
+        }
+
         ds.setColor(colors[Math.abs(userId) % colors.length]);
+        ds.setTypeface(tf);
     }
 
     private String url;
 
-    public void setUrl(String s){
+    public void setUrl(String s) {
         this.url = s;
     }
 
     @Override
     public void onClick(View widget) {
-        if(hideUrlStyle){
+        if (hideUrlStyle) {
             //Do nothing
-        }else{
+        } else {
 
             widget.getContext().startActivity(Intents.openProfile(userId, widget.getContext()));
         }
