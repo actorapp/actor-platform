908da45ef029531261b9876bbcfe68d4eee58890
John Doe <speed@actor.im>
2015-08-28 05:57:25 +0300

    fix(server): group members fixes

--- actor-server/actor-runner/src/main/scala/im/actor/server/Main.scala
+++ actor-server/actor-runner/src/main/scala/im/actor/server/Main.scala
@@ -31,7 +31,7 @@ import im.actor.server.dialog.privat.{ PrivateDialog, PrivateDialogExtension }
 import im.actor.server.email.{ EmailConfig, EmailSender }
 import im.actor.server.enrich.{ RichMessageConfig, RichMessageWorker }
 import im.actor.server.group._
-import im.actor.server.migrations.{ IntegrationTokenMigrator, LocalNamesMigrator }
+import im.actor.server.migrations.{ GroupCreatorMemberMigrator, IntegrationTokenMigrator, LocalNamesMigrator }
 import im.actor.server.oauth.{ GoogleProvider, OAuth2GoogleConfig }
 import im.actor.server.dialog.group.{ GroupDialog, GroupDialogExtension }
 import im.actor.server.presences.{ GroupPresenceManager, PresenceManager }
@@ -87,6 +87,7 @@ object Main extends App {
   UserMigrator.migrate()
   GroupMigrator.migrate()
   LocalNamesMigrator.migrate()
+  GroupCreatorMemberMigrator.migrate()
 
   implicit val weakUpdManagerRegion = WeakUpdatesManager.startRegion()
   implicit val presenceManagerRegion = PresenceManager.startRegion()
