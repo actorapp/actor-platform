d4e172d258d5c49957e50bfa9e50bbf12dcc9b99
Gleb Putintsev <kor_ka@actor.im>
2015-09-02 20:38:02 +0300

    feat(android): docs design

--- actor-apps/app-android/src/main/java/im/actor/messenger/app/fragment/media/DocumentsAdapter.java
+++ actor-apps/app-android/src/main/java/im/actor/messenger/app/fragment/media/DocumentsAdapter.java
@@ -3,6 +3,8 @@ package im.actor.messenger.app.fragment.media;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import im.actor.core.entity.Message;
 import im.actor.messenger.R;
@@ -12,6 +14,7 @@ import im.actor.messenger.app.fragment.chat.messages.MessageHolder;
 import im.actor.messenger.app.fragment.chat.messages.MessagesAdapter;
 import im.actor.messenger.app.fragment.chat.messages.MessagesFragment;
 import im.actor.messenger.app.fragment.chat.messages.PreprocessedData;
+import im.actor.messenger.app.fragment.chat.view.BubbleContainer;
 import im.actor.runtime.generic.mvvm.BindedDisplayList;
 
 
@@ -28,20 +31,23 @@ public class DocumentsAdapter extends MessagesAdapter {
 
     @Override
     public MessageHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        return new DocumentHolder(this, inflate(R.layout.adapter_dialog_doc, viewGroup));
+        return new DocumentHolder(this, inflate(R.layout.adapter_document, viewGroup));
     }
 
     private class DocumentHolder extends DocHolder {
 
         public DocumentHolder(MessagesAdapter fragment, View itemView) {
-            super(fragment, itemView);
-
+            super(fragment, itemView, true);
         }
 
         @Override
         protected void bindData(Message message, boolean isUpdated, PreprocessedData preprocessedData) {
             super.bindData(message, isUpdated, preprocessedData);
             container.hideDate();
+            container.setOnClickListener((View.OnClickListener) this);
+            container.setOnClickListener((BubbleContainer.OnAvatarClickListener) this);
+            container.setOnLongClickListener((View.OnLongClickListener) this);
+            container.setOnLongClickListener((BubbleContainer.OnAvatarLongClickListener) this);
         }
     }
 
