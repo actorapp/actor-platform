39a7ce577784a9766e7960ca7254feacfba315b5
Steve Kite <steve@actor.im>
2015-09-05 03:54:54 +0300

    feat(core): Introduction of EventBus for better event processing

--- actor-apps/core/src/main/java/im/actor/core/modules/Module.java
+++
@@ -1,5 +0,0 @@
-package im.actor.core.modules;
-
-public interface Module {
-
-}
--- actor-apps/core/src/main/java/im/actor/core/modules/RunnableModule.java
+++
@@ -1,5 +0,0 @@
-package im.actor.core.modules;
-
-public interface RunnableModule extends Module {
-    void run();
-}
