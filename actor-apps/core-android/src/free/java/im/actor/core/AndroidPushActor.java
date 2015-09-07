7f5ba1eadd117433b6dd763612f4d55b14530425
kor-ka <badgrib@gmail.com>
2015-09-07 14:51:04 +0300

    fix(android): push register

---
+++ actor-apps/core-android/src/free/java/im/actor/core/AndroidPushActor.java
@@ -0,0 +1,16 @@
+package im.actor.core;
+
+import android.content.Context;
+
+import im.actor.core.modules.Modules;
+import im.actor.runtime.actors.Actor;
+
+
+public class AndroidPushActor extends Actor {
+
+
+    public AndroidPushActor(Context context, Modules messenger) {
+    }
+
+
+}
