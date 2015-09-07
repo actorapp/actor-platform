140794fc204ea72f55302aa07487316d2a9b2386
kor-ka <badgrib@gmail.com>
2015-09-07 22:02:43 +0300

    chore(android): push actors comments

--- actor-apps/app-android/src/push/empty/java/im/actor/core/AndroidPushActor.java
+++ actor-apps/app-android/src/push/empty/java/im/actor/core/AndroidPushActor.java
@@ -5,7 +5,11 @@ import android.content.Context;
 import im.actor.core.modules.Modules;
 import im.actor.runtime.actors.Actor;
 
-
+/**
+ * this actor is used for load push token and register it on server
+ *
+ * we are not using pushes for free version of Actor, that's why it's empty
+ */
 public class AndroidPushActor extends Actor {
 
 
