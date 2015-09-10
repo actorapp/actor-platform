8d18d00b94736bbf61546facb3c670ae41c11eea
Steve Kite <steve@actor.im>
2015-09-10 05:54:55 +0300

    wip(iOS): Updating to Swift 2

--- actor-apps/app-ios/ActorSwift/LruCache.swift
+++ actor-apps/app-ios/ActorSwift/LruCache.swift
@@ -90,7 +90,7 @@ class LinkedList<K, V> {
 }
 
 
-class SwiftlyLRU<K : Hashable, V> : Printable {
+class SwiftlyLRU<K : Hashable, V> : CustomStringConvertible {
     
     let capacity: Int
     var length = 0
