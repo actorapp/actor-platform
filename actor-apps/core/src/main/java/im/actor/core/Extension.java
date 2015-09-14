16e8a6f37b75e63d5740a85131eb6bef487360e2
Steve Kite <steve@actor.im>
2015-09-14 21:17:43 +0300

    feat(core): Finding extensions in core by key

--- actor-apps/core/src/main/java/im/actor/core/Extension.java
+++ actor-apps/core/src/main/java/im/actor/core/Extension.java
@@ -8,6 +8,13 @@ import im.actor.core.modules.ModuleContext;
 public interface Extension {
 
     /**
+     * Get unique extension key
+     *
+     * @return extension key
+     */
+    String getExtensionKey();
+
+    /**
      * Performing registration of extension before Messenger run
      *
      * @param context Module context
@@ -18,4 +25,5 @@ public interface Extension {
      * Running extension
      */
     void runExtension();
+
 }
