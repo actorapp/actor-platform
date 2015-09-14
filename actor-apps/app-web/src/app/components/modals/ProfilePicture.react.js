0d34d092aaa36f3a85efb55390e2efcde5ff3204
John Doe <speed@actor.im>
2015-09-14 21:45:21 +0300

    wip(web): handle crop overlay moving

--- actor-apps/app-web/src/app/components/modals/ProfilePicture.react.js
+++ actor-apps/app-web/src/app/components/modals/ProfilePicture.react.js
@@ -4,6 +4,7 @@
 
 import React, { Component } from 'react';
 import { Container } from 'flux/utils';
+import classnames from 'classnames';
 
 import Modal from 'react-modal';
 
@@ -13,12 +14,28 @@ import ProfilePictureActionCreators from 'actions/ProfilePictureActionCreators';
 
 import ProfilePictureStore from 'stores/ProfilePictureStore'
 
+const ProfilePictureStep = {
+  SELECT_SOURCE: 1,
+  SELECT_FROM_DISK: 2,
+  SELECT_FROM_WEBCAM: 3,
+  CROP: 4
+};
+
 class ProfilePictureModal extends Component {
   static getStores = () => [ProfilePictureStore];
 
   static calculateState() {
     return {
-      isOpen: ProfilePictureStore.isOpen()
+      isOpen: ProfilePictureStore.isOpen(),
+      currentStep: ProfilePictureStep.SELECT_SOURCE,
+      cropPosition: {
+        x: 0,
+        y: 0
+      },
+      cropSize: {
+        height: 200,
+        width: 200
+      }
     };
   }
 
@@ -44,33 +61,110 @@ class ProfilePictureModal extends Component {
     }
   };
 
-  onProfilePhotoChange = () => {
-    const input = React.findDOMNode(this.refs.image);
-    const imageForm = React.findDOMNode(this.refs.imageForm);
-    const file = input.files[0];
-    let reader = new FileReader();
+  onStartMoving = (event) => {
+    const { cropPosition } = this.state;
 
+    event.preventDefault();
+
+    const overlay = React.findDOMNode(this.refs.overlay);
+    const overlayRect = overlay.getBoundingClientRect();
+
+    const dragOffset = {
+      x: (event.pageX - overlayRect.left) - cropPosition.x,
+      y: (event.pageY - overlayRect.top) - cropPosition.y
+    };
+    this.setState({dragOffset});
+
+    overlay.addEventListener('mousemove', this.onMoving);
+    overlay.addEventListener('touchmove', this.onMoving);
+  };
+
+  onMoving = (event) => {
+    const { dragOffset, cropSize } = this.state;
+    const overlay = React.findDOMNode(this.refs.overlay);
+    const overlayRect = overlay.getBoundingClientRect();
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    const cropPosition = {
+      x: (event.pageX - overlayRect.left) - dragOffset.x,
+      y: (event.pageY - overlayRect.top) - dragOffset.y
+    };
+
+    if (cropPosition.x < cropSize.width/2) {
+      cropPosition.x = 0;
+    //} else if (cropPosition.x > (overlayRect - cropSize.width)) {
+    //  console.debug('x>or-w/2')
+    }
+
+    this.setState({cropPosition})
+  };
+
+  onEndMoving = (event) => {
+    const overlay = React.findDOMNode(this.refs.overlay);
+
+    event.preventDefault();
+
+    overlay.removeEventListener('mousemove', this.onMoving);
+    overlay.removeEventListener('touchmove', this.onMoving);
+  };
+
+  onProfilePhotoChange = () => {
+    const imageInput = React.findDOMNode(this.refs.imageInput);
+    const imageForm = React.findDOMNode(this.refs.imageForm);
+    const file = imageInput.files[0];
+
+    let reader = new FileReader();
     reader.onload = (event) => {
-      this.setState({profilePhoto: event.target.result});
+      this.setState({
+        profilePhotoSource: event.target.result,
+        currentStep: ProfilePictureStep.CROP
+      });
       imageForm.reset();
     };
     reader.readAsDataURL(file);
   };
 
+  onSelectFromDisk = () => this.setState({currentStep: ProfilePictureStep.SELECT_FROM_DISK});
+  onSelectFromWebcam = () => this.setState({currentStep: ProfilePictureStep.SELECT_FROM_WEBCAM});
+
   render() {
-    const { isOpen, profilePhoto } = this.state;
+    const { isOpen, profilePhotoSource, currentStep, cropPosition } = this.state;
 
-    const modalBody = profilePhoto ? (
-        <img src={profilePhoto}/>
-    ) : [
-      <h3>Chose image to set profile picture</h3>
-    ,
-      <form ref="imageForm">
-        <input type="file" ref="image" onChange={this.onProfilePhotoChange}/>
-      </form>
-    ];
-
-    console.debug(modalBody);
+    let modalBody;
+    switch (currentStep) {
+      case ProfilePictureStep.SELECT_SOURCE:
+        modalBody = (
+          <div style={{padding: 20}}>
+            <h3>select source</h3>
+            <button onClick={this.onSelectFromDisk}>Disk</button> | <button onClick={this.onSelectFromWebcam}>Webcam</button>
+          </div>
+        );
+        break;
+      case ProfilePictureStep.SELECT_FROM_DISK:
+        modalBody = (
+          <form ref="imageForm" style={{padding: 20}}>
+            <h3>Chose image to set profile picture</h3>
+            <input type="file" ref="imageInput" onChange={this.onProfilePhotoChange}/>
+          </form>
+        );
+        break;
+      case ProfilePictureStep.CROP:
+        modalBody = (
+          <div className="crop-wrapper" ref="overlay">
+            <div className="crop-wrapper__overlay"
+                 style={{left: cropPosition.x, top: cropPosition.y}}
+                 onMouseDown={this.onStartMoving}
+                 onMouseUp={this.onEndMoving}>
+              <img src={profilePhotoSource} draggable="false" style={{left: -cropPosition.x, top: -cropPosition.y}}/>
+            </div>
+            <img ref="originalImage" src={profilePhotoSource} className="crop-wrapper__image-original"
+                 draggable="false"/>
+          </div>
+        );
+        break;
+    }
 
     if (isOpen) {
       return (
@@ -80,9 +174,7 @@ class ProfilePictureModal extends Component {
 
           <div className="modal-new__header">
             <i className="modal-new__header__icon material-icons">portrait</i>
-
             <h3 className="modal-new__header__title">Profile picture</h3>
-
             <div className="pull-right">
               <button className="button button--lightblue" onClick={this.onClose}>Done</button>
             </div>
