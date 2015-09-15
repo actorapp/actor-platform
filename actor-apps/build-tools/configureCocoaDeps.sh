65de67e9c6905e990cc3d26a00adb87804c7009d
Steve Kite <steve@actor.im>
2015-09-15 03:07:49 +0300

    feat(iOS): Migrated to patched j2objc-0.9.8.1

--- actor-apps/build-tools/configureCocoaDeps.sh
+++ actor-apps/build-tools/configureCocoaDeps.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Configuration
-J2OBJC_VERSION=0.9.8
+J2OBJC_VERSION=0.9.8.1
 BUILD_DIRECTORY="$1/build-tools"
 J2OBJC_DIR="${BUILD_DIRECTORY}/dist/"
 
@@ -11,12 +11,12 @@ source "$BUILD_DIRECTORY/common.sh"
 if [ ! -d "${J2OBJC_DIR}/j2objc-${J2OBJC_VERSION}" ]; then
 	echo_w "Downloading j2objc..."
 
-	curl -o "${BUILD_DIRECTORY}/j2objc-${J2OBJC_VERSION}.zip" -L https://github.com/google/j2objc/releases/download/${J2OBJC_VERSION}/j2objc-${J2OBJC_VERSION}.zip
+	curl -o "${BUILD_DIRECTORY}/j2objc-${J2OBJC_VERSION}.zip" -L https://bintray.com/artifact/download/ex3ndr/generic/j2objc-${J2OBJC_VERSION}.zip
 
 	echo "Extracting j2objc..."
 	mkdir -p "${J2OBJC_DIR}"
 	unzip -q "${BUILD_DIRECTORY}/j2objc-${J2OBJC_VERSION}.zip" -d "${J2OBJC_DIR}"
 	rm "${BUILD_DIRECTORY}/j2objc-${J2OBJC_VERSION}.zip"
-else 
+else
 	echo_w "Cocoa Dependency already installed. Skipping..."
-fi	
+fi
