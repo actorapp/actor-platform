bc4566206de2de775049a2fff644774de3616e27
Steve Kite <steve@actor.im>
2015-09-17 01:43:05 +0300

    chore(iOS): Updated build scripts

--- actor-apps/build-tools/ios-copy-swift.sh
+++ actor-apps/build-tools/ios-copy-swift.sh
@@ -2,31 +2,31 @@
 set -e
 IPA_NAME=$1
 APP_NAME=$2
-TMP_IPA="build/Applications/${IPA_NAME}_Temp"
+TMP_IPA="build/IPA_Temp"
 DEVELOPER_DIR=`xcode-select --print-path`
 
 # Create dest dir
 if [ -d "${TMP_IPA}" ];
 then
     rm -rf "${TMP_IPA}"
-fi  
+fi
 mkdir ${TMP_IPA}
 
 # Unzip IPA
-unzip -q "build/Applications/${IPA_NAME}" -d "${TMP_IPA}"
+unzip -q "build/${IPA_NAME}" -d "${TMP_IPA}"
 
 # Copy SwitfSupport
 mkdir -p "${TMP_IPA}/SwiftSupport"
-for SWIFT_LIB in $(ls -1 ${TMP_IPA}/Payload/${APP_NAME}.app/Frameworks); do 
+for SWIFT_LIB in $(ls -1 ${TMP_IPA}/Payload/${APP_NAME}.app/Frameworks); do
     cp "${DEVELOPER_DIR}/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/iphoneos/${SWIFT_LIB}" "${TMP_IPA}/SwiftSupport"
 done
 
 # Repack IPA
-rm -rf "build/Applications/${IPA_NAME}"
+rm -rf "build/${IPA_NAME}"
 echo "+ zip --symlinks --verbose --recurse-paths ${IPA} ."
 cd "${TMP_IPA}"
 zip --symlinks --verbose --recurse-paths "${IPA_NAME}" .
-cd ../../../
+cd ../../
 
 # Move to original path
-mv "${TMP_IPA}/${IPA_NAME}" "build/Applications/${IPA_NAME}"
+mv "${TMP_IPA}/${IPA_NAME}" "build/${IPA_NAME}"
