225de5394e43a462b795e3dc1c3d883244cbc49f
Steve Kite <steve@actor.im>
2015-09-17 01:43:05 +0300

    chore(iOS): Updated build scripts

--- actor-apps/build-tools/ios-update-version.sh
+++ actor-apps/build-tools/ios-update-version.sh
@@ -4,19 +4,17 @@ set -e
 VERSION=$1
 PLIST=$2
 
-baseVersion=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "${PLIST}")
+baseVersion=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "${PLIST}")
 
 versionParts=(${baseVersion//./ })
 partsCount=`expr ${#versionParts[@]} - 2`
 version=""
-for i in $(seq 0 ${partsCount}); do 
+for i in $(seq 0 ${partsCount}); do
 	version="${version}${versionParts[$i]}."
 done
 version="${version}${VERSION}"
 
-echo ${version}
-
 /usr/libexec/PlistBuddy -x -c "Set :CFBundleShortVersionString ${version}" "$PLIST"
 /usr/libexec/PlistBuddy -x -c "Set :CFBundleVersion ${version}" "$PLIST"
 
-echo "##teamcity[buildNumber '${version}']"
+echo ${version}
