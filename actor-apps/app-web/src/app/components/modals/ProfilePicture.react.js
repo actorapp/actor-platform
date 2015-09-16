7ad006d591568e66da9357218635dedb84ebe96f
Oleg Shilov <me@olegshilov.com>
2015-09-16 22:16:09 +0300

    wip(web): send image to crop and cropped image back between my profile modal and crop modal

--- actor-apps/app-web/src/app/components/modals/ProfilePicture.react.js
+++ actor-apps/app-web/src/app/components/modals/ProfilePicture.react.js
@@ -14,20 +14,17 @@ import ProfilePictureActionCreators from 'actions/ProfilePictureActionCreators';
 
 import ProfilePictureStore from 'stores/ProfilePictureStore'
 
-const ProfilePictureStep = {
-  SELECT_SOURCE: 1,
-  SELECT_FROM_DISK: 2,
-  SELECT_FROM_WEBCAM: 3,
-  CROP: 4
-};
-
 class ProfilePictureModal extends Component {
+  static propTypes = {
+    onCropFinish: React.PropTypes.func//.isRequired
+  };
+
   static getStores = () => [ProfilePictureStore];
 
   static calculateState() {
     return {
       isOpen: ProfilePictureStore.isOpen(),
-      currentStep: ProfilePictureStep.SELECT_SOURCE,
+      pictureSource: ProfilePictureStore.getPictureSource(),
       cropPosition: {
         x: 0,
         y: 0
@@ -39,23 +36,14 @@ class ProfilePictureModal extends Component {
     };
   }
 
-  componentWillUpdate(nextProps, nextState) {
-    const { isOpen } = nextState;
-
-    if (isOpen) {
-      document.addEventListener('keydown', this.onKeyDown, false);
-    } else {
-      document.removeEventListener('keydown', this.onKeyDown, false);
-    }
+  componentDidMount() {
+    document.addEventListener('keydown', this.onKeyDown, false);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.onKeyDown, false);
   }
 
-  onClose = () => {
-    ProfilePictureActionCreators.hide();
-    this.setState({
-      profilePictureSource: null,
-      currentStep: ProfilePictureStep.SELECT_SOURCE
-    });
-  };
+  onClose = () => ProfilePictureActionCreators.hide();
 
   onKeyDown = (event) => {
     if (event.keyCode === KeyCodes.ESC) {
@@ -81,7 +69,6 @@ class ProfilePictureModal extends Component {
     overlay.addEventListener('mousemove', this.onMoving);
     overlay.addEventListener('touchmove', this.onMoving);
   };
-
   onMoving = (event) => {
     const { dragOffset, cropSize } = this.state;
     const overlay = React.findDOMNode(this.refs.overlay);
@@ -109,7 +96,6 @@ class ProfilePictureModal extends Component {
 
     this.setState({cropPosition})
   };
-
   onEndMoving = (event) => {
     const overlay = React.findDOMNode(this.refs.overlay);
 
@@ -119,27 +105,10 @@ class ProfilePictureModal extends Component {
     overlay.removeEventListener('touchmove', this.onMoving);
   };
 
-  onProfilePhotoChange = () => {
-    const imageInput = React.findDOMNode(this.refs.imageInput);
-    const imageForm = React.findDOMNode(this.refs.imageForm);
-    const file = imageInput.files[0];
-
-    let reader = new FileReader();
-    reader.onload = (event) => {
-      this.setState({
-        profilePictureSource: event.target.result,
-        currentStep: ProfilePictureStep.CROP
-      });
-      imageForm.reset();
-    };
-    reader.readAsDataURL(file);
-  };
-
-  onSelectFromDisk = () => this.setState({currentStep: ProfilePictureStep.SELECT_FROM_DISK});
-  onSelectFromWebcam = () => this.setState({currentStep: ProfilePictureStep.SELECT_FROM_WEBCAM});
-
   onCrop = () => {
     const { cropPosition, cropSize } = this.state;
+    const { onCropFinish } = this.props;
+
     const cropImage = React.findDOMNode(this.refs.cropImage);
     let canvas = document.createElement('canvas');
 
@@ -150,49 +119,13 @@ class ProfilePictureModal extends Component {
     context.drawImage(cropImage, cropPosition.x, cropPosition.y, cropSize.width, cropSize.height, 0, 0, cropSize.width, cropSize.height);
 
     const croppedImage = canvas.toDataURL();
-    this.setState({croppedImage});
-    window.open(croppedImage);
+
+    onCropFinish(croppedImage);
+    this.onClose();
   };
 
   render() {
-    const { isOpen, profilePictureSource, currentStep, cropPosition } = this.state;
-
-    let modalBody;
-    switch (currentStep) {
-      case ProfilePictureStep.SELECT_SOURCE:
-        modalBody = (
-          <div style={{padding: 20}}>
-            <h3>select source</h3>
-            <button onClick={this.onSelectFromDisk}>Disk</button> | <button onClick={this.onSelectFromWebcam}>Webcam</button>
-          </div>
-        );
-        break;
-      case ProfilePictureStep.SELECT_FROM_DISK:
-        modalBody = (
-          <form ref="imageForm" style={{padding: 20}}>
-            <h3>Chose image to set profile picture</h3>
-            <input type="file" ref="imageInput" onChange={this.onProfilePhotoChange}/>
-          </form>
-        );
-        break;
-      case ProfilePictureStep.CROP:
-        modalBody = (
-          <div className="crop-wrapper" ref="overlay">
-            <div className="crop-wrapper__overlay"
-                 style={{left: cropPosition.x, top: cropPosition.y}}
-                 onMouseDown={this.onStartMoving}
-                 onTouchStart={this.onStartMoving}
-                 onMouseUp={this.onEndMoving}
-                 onTouchEnd={this.onEndMoving}
-                 onDoubleClick={this.onCrop}>
-              <img src={profilePictureSource} ref="cropImage" draggable="false" style={{left: -cropPosition.x, top: -cropPosition.y}}/>
-            </div>
-            <img ref="originalImage" src={profilePictureSource} className="crop-wrapper__image-original"
-                 draggable="false"/>
-          </div>
-        );
-        break;
-    }
+    const { isOpen, pictureSource, cropPosition } = this.state;
 
     if (isOpen) {
       return (
@@ -204,12 +137,29 @@ class ProfilePictureModal extends Component {
             <i className="modal-new__header__icon material-icons">portrait</i>
             <h3 className="modal-new__header__title">Profile picture</h3>
             <div className="pull-right">
-              <button className="button button--lightblue" onClick={this.onClose}>Done</button>
+              <button className="button button--lightblue" onClick={this.onCrop}>Done</button>
             </div>
           </div>
 
           <div className="modal-new__body">
-            {modalBody}
+            <div className="crop-wrapper" ref="overlay">
+              <div className="crop-wrapper__overlay"
+                   onMouseDown={this.onStartMoving}
+                   onMouseUp={this.onEndMoving}
+                   onTouchEnd={this.onEndMoving}
+                   onTouchStart={this.onStartMoving}
+                   style={{left: cropPosition.x, top: cropPosition.y}}>
+                <img className="crop-wrapper__image-crop"
+                     draggable="false"
+                     ref="cropImage"
+                     src={pictureSource}
+                     style={{left: -cropPosition.x, top: -cropPosition.y}}/>
+              </div>
+              <img className="crop-wrapper__image-original"
+                   draggable="false"
+                   ref="originalImage"
+                   src={pictureSource}/>
+            </div>
           </div>
         </Modal>
       );
@@ -219,4 +169,4 @@ class ProfilePictureModal extends Component {
   }
 }
 
-export default Container.create(ProfilePictureModal, {pure: false});
+export default Container.create(ProfilePictureModal);
--- actor-apps/app-web/src/app/stores/ProfilePictureStore.js
+++ actor-apps/app-web/src/app/stores/ProfilePictureStore.js
@@ -6,20 +6,28 @@ import { Store } from 'flux/utils';
 import Dispatcher from 'dispatcher/ActorAppDispatcher';
 import { ActionTypes } from 'constants/ActorAppConstants';
 
-let _isOpen = false;
+let _isOpen = false,
+    _pictureSource = null;
 
 class ProfilePictureStore extends Store {
   isOpen() {
     return _isOpen;
   }
+
+  getPictureSource() {
+    return _pictureSource;
+  }
+
   __onDispatch = (action) => {
     switch(action.type) {
       case ActionTypes.PROFILE_PICTURE_MODAL_SHOW:
         _isOpen = true;
+        _pictureSource = action.source;
         this.__emitChange();
         break;
       case ActionTypes.PROFILE_PICTURE_MODAL_HIDE:
         _isOpen = false;
+        _pictureSource = null;
         this.__emitChange();
         break;
       default:
