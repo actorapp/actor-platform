d38e63b1300330c82ff2967c92f529fb2d7c8f46
Steve Kite <steve@actor.im>
2015-09-17 05:37:26 +0300

    feat(desktop): Added desktop build scripts

---
+++ actor-apps/app-desktop/build.sh
@@ -0,0 +1,8 @@
+echo "Building OSX"
+electron-packager src Actor --platform=darwin --arch=x64 --app-bundle-id=im.actor.app --version=0.32.3 --out=build --icon=icon/osx.icns
+
+echo "Building Windows"
+electron-packager src Actor --platform=win32 --arch=all --app-bundle-id=im.actor.app --version=0.32.3 --out=build --icon=icon/windows.icns
+
+echo "Building Linux"
+electron-packager src Actor --platform=linux --arch=all --app-bundle-id=im.actor.app --version=0.32.3 --out=build
