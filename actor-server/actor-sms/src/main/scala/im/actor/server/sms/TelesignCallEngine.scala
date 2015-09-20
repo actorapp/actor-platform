513536e90010a8850a6cd41839c1c997c419a585
prettynatty <fear@loathing.in>
2015-09-20 23:25:22 +0300

    fix(server:auth): wrong method call on call request

--- actor-server/actor-sms/src/main/scala/im/actor/server/sms/TelesignCallEngine.scala
+++ actor-server/actor-sms/src/main/scala/im/actor/server/sms/TelesignCallEngine.scala
@@ -3,5 +3,5 @@ package im.actor.server.sms
 import scala.concurrent.Future
 
 class TelesignCallEngine(telesignClient: TelesignClient) extends AuthCallEngine {
-  override def sendCode(phoneNumber: Long, message: String, language: String): Future[Unit] = telesignClient.sendSmsCode(phoneNumber, message, language)
+  override def sendCode(phoneNumber: Long, code: String, language: String): Future[Unit] = telesignClient.sendCallCode(phoneNumber, code, language)
 }
