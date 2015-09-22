09d967aa032c1fee93074b0c985f2b0f6ca0218b
prettynatty <fear@loathing.in>
2015-09-23 01:55:28 +0300

    chore(server): get the whole version from environment variable

--- actor-server/project/Versioning.scala
+++ actor-server/project/Versioning.scala
@@ -2,16 +2,22 @@ package im.actor
 
 private[actor] trait Versioning {
   protected def getVersion: String = {
-    val version = s"1.0.$getVersionPatch$getVersionPostfix"
+    val buildNumber = getBuildNumber
 
-    if (sys.env.isDefinedAt("TEAMCITY_VERSION")) {
-      println(s"##teamcity[buildNumber '$version']")
+    if (buildNumber.contains(".")) { // already set by previous commands
+      buildNumber
+    } else {
+      val version = s"1.0.$buildNumber$getVersionPostfix"
+
+      if (sys.env.isDefinedAt("TEAMCITY_VERSION")) {
+        println(s"##teamcity[buildNumber '$version']")
+      }
+
+      version
     }
-
-    version
   }
 
-  private def getVersionPatch: String = sys.env.get("VERSION_PATCH").orElse(sys.env.get("BUILD_NUMBER")).getOrElse("0")
+  private def getBuildNumber: String = sys.env.get("BUILD_NUMBER").getOrElse("0")
 
   private def getVersionPostfix: String = sys.env.get("VERSION_POSTFIX").map("-" + _).getOrElse("")
 }
