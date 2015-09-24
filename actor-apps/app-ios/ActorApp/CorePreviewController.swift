09f1cb3eb1ab6b1db2497da19871942556a0f70b
Steve Kite <steve@actor.im>
2015-09-24 17:29:12 +0300

    feat(iOS): New settings page and photo viewer

--- actor-apps/app-ios/ActorApp/CorePreviewController.swift
+++ actor-apps/app-ios/ActorApp/CorePreviewController.swift
@@ -1,9 +1,5 @@
 //
-//  CorePreviewController.swift
-//  ActorApp
-//
-//  Created by Stepan Korshakov on 05.09.15.
-//  Copyright (c) 2015 Actor LLC. All rights reserved.
+//  Copyright (c) 2014-2015 Actor LLC. <https://actor.im>
 //
 
 import Foundation
