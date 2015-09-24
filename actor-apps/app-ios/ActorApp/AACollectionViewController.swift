9b4d26d3f4322e5c0ab7e8dac1e996634855a335
Steve Kite <steve@actor.im>
2015-09-24 21:31:30 +0300

    wip(iOS): Wallpapers collection view

---
+++ actor-apps/app-ios/ActorApp/AACollectionViewController.swift
@@ -0,0 +1,48 @@
+//
+//  AACollectionViewController.swift
+//  ActorApp
+//
+//  Created by Steve Kite on 24.09.15.
+//  Copyright © 2015 Actor LLC. All rights reserved.
+//
+
+import Foundation
+
+class AACollectionViewController: AAViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    var collectionView:UICollectionView!
+    
+    init(collectionLayout: UICollectionViewLayout) {
+        super.init()
+        
+        collectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: collectionLayout)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        super.loadView()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
+        
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: NSLocalizedString("NavigationBack",comment:"Back button"), style: UIBarButtonItemStyle.Plain, target: nil, action: nil)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        collectionView.frame = view.bounds;
+    }
+
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        fatalError("Not implemented!")
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        fatalError("Not implemented!")
+    }
+}
--- actor-apps/app-ios/ActorApp/Controllers Support/AATableViewController.swift
+++ actor-apps/app-ios/ActorApp/Controllers Support/AATableViewController.swift
@@ -6,14 +6,8 @@ import UIKit
 
 class AATableViewController: AAViewController, UITableViewDataSource, UITableViewDelegate {
     
-    // MARK: -
-    // MARK: Public vars
-    
     var tableView: UITableView!
     var tableViewStyle: UITableViewStyle!
-    
-    // MARK: - 
-    // MARK: Constructors
     
     init(style: UITableViewStyle) {
         super.init()
@@ -25,10 +19,13 @@ class AATableViewController: AAViewController, UITableViewDataSource, UITableVie
         super.init(coder: aDecoder)
     }
     
-    // MARK: -
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func loadView() {
+        super.loadView()
+        
+        tableView = UITableView(frame: view.bounds, style: tableViewStyle)
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
         
         navigationItem.backBarButtonItem = UIBarButtonItem(title: NSLocalizedString("NavigationBack",comment:"Back button"), style: UIBarButtonItemStyle.Plain, target: nil, action: nil)
     }
@@ -41,50 +38,27 @@ class AATableViewController: AAViewController, UITableViewDataSource, UITableVie
         super.viewWillAppear(animated)
     }
     
-    override func loadView() {
-        super.loadView()
-        
-        // view.backgroundColor = UIColor.whiteColor()
-        
-        tableView = UITableView(frame: view.bounds, style: tableViewStyle)
-        tableView.delegate = self
-        tableView.dataSource = self
-        view.addSubview(tableView)
-    }
-
-    // MARK: -
-    // MARK: Methods
-    
-    func showPlaceholderWithImage(image: UIImage?, title: String?, subtitle: String?) {
-        placeholder.setImage(image, title: title, subtitle: subtitle)
-        super.showPlaceholder()
-    }
-    
-    // MARK: -
-    // MARK: Setters
-    
-    override func setEditing(editing: Bool, animated: Bool) {
-        super.setEditing(editing, animated: animated)
-        tableView.setEditing(editing, animated: animated)
-    }
-    
-    // MARK: -
-    // MARK: Getters
-    
-    private func placeholderViewFrame() -> CGRect {
-        let navigationBarHeight: CGFloat = 64.0 + Utils.retinaPixel() // TODO: if will be landscape then change to manual calc
-        return CGRect(x: 0, y: navigationBarHeight, width: view.bounds.size.width, height: view.bounds.size.height - navigationBarHeight)
-    }
-    
-    // MARK: -
-    // MARK: Layout
-    
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         
         tableView.frame = view.bounds;
     }
 
+    func showPlaceholderWithImage(image: UIImage?, title: String?, subtitle: String?) {
+        placeholder.setImage(image, title: title, subtitle: subtitle)
+        super.showPlaceholder()
+    }
+
+    override func setEditing(editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        tableView.setEditing(editing, animated: animated)
+    }
+    
+    private func placeholderViewFrame() -> CGRect {
+        let navigationBarHeight: CGFloat = 64.5
+        return CGRect(x: 0, y: navigationBarHeight, width: view.bounds.size.width, height: view.bounds.size.height - navigationBarHeight)
+    }
+    
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 0
     }
