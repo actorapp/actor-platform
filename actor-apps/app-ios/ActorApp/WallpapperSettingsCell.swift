9e5b420cdcf4cebaa013c79edcdb57df21651ddf
Steve Kite <steve@actor.im>
2015-09-25 23:43:41 +0300

    wip(iOS): Implemented custom background selection

---
+++ actor-apps/app-ios/ActorApp/WallpapperSettingsCell.swift
@@ -0,0 +1,64 @@
+//
+//  Copyright (c) 2014-2015 Actor LLC. <https://actor.im>
+//
+
+
+import Foundation
+
+class WallpapperSettingsCell: UATableViewCell {
+    
+    private let wallpapper1 = UIImageView()
+    private let wallpapper2 = UIImageView()
+    private let wallpapper3 = UIImageView()
+    private let label = UILabel()
+    
+    var wallpapperDidTap: ((name: String) -> ())?
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        wallpapper1.image = UIImage(named: "bg_1.jpg")!
+        wallpapper1.clipsToBounds = true
+        wallpapper1.contentMode = .ScaleAspectFill
+        wallpapper1.viewDidTap = { self.wallpapperDidTap?(name: "bg_1.jpg") }
+        wallpapper2.image = UIImage(named: "bg_2.jpg")!
+        wallpapper2.clipsToBounds = true
+        wallpapper2.contentMode = .ScaleAspectFill
+        wallpapper2.viewDidTap = { self.wallpapperDidTap?(name: "bg_2.jpg") }
+        wallpapper3.image = UIImage(named: "bg_3.jpg")!
+        wallpapper3.clipsToBounds = true
+        wallpapper3.contentMode = .ScaleAspectFill
+        wallpapper3.viewDidTap = { self.wallpapperDidTap?(name: "bg_3.jpg") }
+        label.font = UIFont.systemFontOfSize(17)
+        label.applyStyle("label")
+        label.text = localized("SettingsWallpappers")
+        
+        selectionStyle = .None
+        
+        self.contentView.addSubview(wallpapper1)
+        self.contentView.addSubview(wallpapper2)
+        self.contentView.addSubview(wallpapper3)
+        self.contentView.addSubview(label)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let width = contentView.width
+        let height = contentView.height
+        let padding: CGFloat = 15
+        let wPadding: CGFloat = 44
+        let wWidth = (width - padding * 4) / 3
+        let wHeight = height - padding - 44
+        
+        wallpapper1.frame = CGRectMake(padding, wPadding, wWidth, wHeight)
+        wallpapper2.frame = CGRectMake(padding * 2 + wWidth, wPadding, wWidth, wHeight)
+        wallpapper3.frame = CGRectMake(padding * 3 + wWidth * 2, wPadding, wWidth, wHeight)
+        
+        label.frame = CGRectMake(padding, 0, width - padding * 2, 44)
+    }
+}
