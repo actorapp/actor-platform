700654ea2fa008f0c435d9f69bdd6f2dd7da2e1f
prettynatty <fear@loathing.in>
2015-09-26 02:22:09 +0300

    feat(server): ability to customize server name (for sms messages for example)

--- actor-server/actor-sms/src/main/scala/im/actor/server/sms/TelesignClient.scala
+++ actor-server/actor-sms/src/main/scala/im/actor/server/sms/TelesignClient.scala
@@ -17,7 +17,7 @@ import scala.util.Failure
 object TelesignClient {
   private val BaseUrl = "https://rest.telesign.com"
   private val ApiVersion = "v1"
-  val DefaultSmsTemplate: String = "Actor activation code: $$CODE$$"
+  val DefaultSmsTemplate: String = "$$SERVER_NAME$$ activation code: $$CODE$$"
   val DefaultLanguage = "en"
   val CallLanguages: Set[String] = "af sq ar ar-EG hy bn eu be bn-BD bs bg ca ku zh-HK zh-CN zh-TW hr cs da fa-AF nl egx en-AU en-GB en-US et fil fi fr fr-CA gl ka de el gu ha he hi hu is id zu it ja kn kk km sw ko ko ky lv ln lt lb mk ms ml mr ne no or fa pl pt pt-BR pt-PT pa-IN pa-PK ro ru sr sr-BA sd sk sk-SK sl es es-ES es-CL es-419 es-ES sv gsw ta te th tr uk ur vi cy".split(" ").toSet
 }
@@ -43,7 +43,7 @@ final class TelesignClient(config: Config)(implicit system: ActorSystem) {
       "phone_number" → phoneNumber.toString,
       "ucid" → "BACS",
       "verify_code" → code,
-      "template" → template
+      "template" → template.replace("$$SERVER_NAME$$", system.settings.config.getString("name"))
     )) map { _ ⇒
       system.log.debug("Message sent via telesign")
     }
--- actor-server/actor-sms/src/main/scala/im/actor/server/sms/TelesignSmsEngine.scala
+++ actor-server/actor-sms/src/main/scala/im/actor/server/sms/TelesignSmsEngine.scala
@@ -2,6 +2,6 @@ package im.actor.server.sms
 
 import scala.concurrent.Future
 
-class TelesignSmsEngine(telesignClient: TelesignClient) extends AuthSmsEngine {
+final class TelesignSmsEngine(telesignClient: TelesignClient) extends AuthSmsEngine {
   override def sendCode(phoneNumber: Long, message: String): Future[Unit] = telesignClient.sendSmsCode(phoneNumber, message, TelesignClient.DefaultSmsTemplate)
 }
