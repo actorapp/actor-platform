77e475d08acc82b10cb0f28220a6309069028d81
rock_jam <5min4eq.unity@gmail.com>
2015-09-29 16:03:16 +0300

    feat(server:core,rpc): webactions service implementation

---
+++ actor-server/actor-testkit/src/main/scala/im/actor/server/webactions/CorrectWebaction.scala
@@ -0,0 +1,22 @@
+package im.actor.server.webactions
+
+import akka.actor.ActorSystem
+import im.actor.api.rpc.collections.{ ApiStringValue, ApiInt32Value, ApiMapValueItem, ApiMapValue }
+
+import scala.concurrent.Future
+
+object CorrectWebaction {
+  val uri = "https://google.com/"
+  val regex = "https://mail.google.com"
+  val completeUri = "https://mail.google.com/mail/u/0/#inbox"
+}
+
+class CorrectWebaction(system: ActorSystem) extends Webaction(system) {
+  import system.dispatcher
+
+  override def uri(params: ApiMapValue): String = CorrectWebaction.uri
+  override def regex: String = CorrectWebaction.regex
+  override def complete(userId: Int, url: String): Future[ApiMapValue] = Future {
+    ApiMapValue(Vector(ApiMapValueItem("userId", ApiInt32Value(userId)), ApiMapValueItem("url", ApiStringValue(url.reverse))))
+  }
+}
