a8e0ab3c3fcaeb3513622627539cbf5d2e8943f2
John Doe <speed@actor.im>
2015-10-02 02:18:29 +0300

    chore(web): cleanup

--- actor-apps/app-web/src/app/utils/polyfills.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-
-// String.includes
-if (!String.prototype.includes) {
-  String.prototype.includes = () => {
-    'use strict';
-    return String.prototype.indexOf.apply(this, arguments) !== -1;
-  };
-}
