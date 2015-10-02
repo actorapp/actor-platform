c0523376ab94a36c52f2049d604a6ae3d00421af
yumazaki <jack3760@aol.com>
2015-10-02 17:09:25 +0800

    Create zh-CN.js

---
+++ actor-apps/app-web/src/app/l18n/zh-CN.js
@@ -0,0 +1,93 @@
+const chinese = {
+  'locales': 'zh-CN',
+  'messages': {
+    // Sidebar
+    'setProfilePhoto': '设置头像',
+    'editProfile': '个人信息',
+    'configureIntegrations': '配置集成',
+    'helpAndFeedback': 'Help & Feedback',
+    'preferences': '设置',
+    'signOut': '登出',
+    'addContact': '添加联系人',
+    'twitter': 'Our twitter',
+
+    // Toolbar
+    'search': '搜索',
+
+    // Compose
+    'composeSendFile': '发送文件',
+    'composeSendPhote': '发送照片',
+    'composeSend': '发送',
+
+    // Profiles
+    'createdBy': '创建者',
+    'addPeople': '添加成员',
+    'more': '更多',
+    'actions': '操作',
+    'addToContacts': '添加到联系人',
+    'removeFromContacts': '从联系人中删除',
+    'setGroupPhoto': '设置群头像',
+    'addIntegration': '添加一个服务集成',
+    'editGroup': '编辑群',
+    'clearGroup': '清除群',
+    'deleteGroup': '删除群',
+    'clearConversation': '清除会话',
+    'deleteConversation': '删除会话',
+    'leaveGroup': '离开群',
+    'sharedMedia': '已分享的媒体',
+    'notifications': '通知',
+    'integrationTokenCopied': '集成链接已复制',
+    'members':
+      '{numMembers, plural,' +
+        '=0 {没有成员}' +
+        '=1 {# 成员}' +
+        'other {# 成员}' +
+      '}',
+
+    // Modals
+    'inviteModalTitle': '添加更多成员',
+    'inviteModalSearch': '搜索联系人或用户',
+    'inviteModalNotFound': '抱歉，没有找到用户',
+    'inviteByLink': '通过链接邀请至群',
+    'inviteByLinkModalTitle': '通过链接邀请',
+    'inviteByLinkModalDescription': '通过这个链接，所有人都可以进入群 ”{groupName}”:',
+    'inviteByLinkModalCopyButton': '复制链接',
+    'inviteByLinkModalRevokeButton': '撤销链接',
+    'inviteLinkCopied': '邀请链接已复制',
+
+    'preferencesModalTitle': '设置',
+    'preferencesGeneralTab': '通用',
+    'preferencesNotificationsTab': '通知 & 音效',
+    'preferencesSecurityTab': '安全',
+    'preferencesSendMessageTitle': '发送消息',
+    'preferencesSendMessage': '发送消息',
+    'preferencesNewLine': '换行',
+    'preferencesEffectsTitle': '铃声',
+    'preferencesEnableEffects': '启用铃声提醒',
+    'preferencesNotificationsTitle': '通知',
+    'preferencesNotificationsGroup': '启用群通知提醒',
+    'preferencesNotificationsOnlyMention': '启用提到我的通知提醒',
+    'preferencesNotificationsOnlyMentionHint': '当消息中提到我的时候才发送通知提醒',
+    'preferencesPrivacyTitle': '隐私',
+    'preferencesMessagePreview': '消息预览',
+    'preferencesMessagePreviewHint': '从通知提醒中隐藏正文',
+    'preferencesSessionsTitle': '已登陆的会话',
+    'preferencesSessionsCurrentSession': '当前会话',
+    'preferencesSessionsAuthTime': '登陆时间',
+    'preferencesSessionsTerminate': '断开',
+    'preferencesSessionsTerminateAll': '断开所有会话',
+
+    'createGroupModalTitle': '创建群',
+    'createGroupButton': '创建群',
+    'createGroupGroupName': '群名称',
+    'createGroupAddMembers': '添加成员',
+
+    'addContactModalTitle': '添加联系人',
+    'addContactPhoneNumber': '手机号',
+    'addContactAdd': '添加',
+    'addContactNotRegistered': '这个手机号没有在Actor中注册.',
+    'addContactInContacts': '这个用户已经在您的联系人中.'
+  }
+};
+
+export default chinese;
