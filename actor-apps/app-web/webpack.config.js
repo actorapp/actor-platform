fa377df128c23075ab8e29fe5b3dd57741c60176
Oleg Shilov <me@olegshilov.com>
2015-10-05 16:20:43 +0300

    fix(web): fix RangeError: Maximum call stack size exceeded while build.

--- actor-apps/app-web/webpack.config.js
+++ actor-apps/app-web/webpack.config.js
@@ -30,6 +30,7 @@ export default {
     sourceMapFilename: '[name].map'
   },
   resolve: {
+    packageAlias: false,
     modulesDirectories: ['node_modules'],
     root: [
       path.join(__dirname, 'src/app')
