5936b9ec8e3c091632db882b30f768073f50bd8a
John Doe <speed@actor.im>
2015-10-05 21:28:18 +0300

    fix(web): fix mixpanel utils, add locale data to assets.

--- actor-apps/app-web/src/app/l18n/index.js
+++ actor-apps/app-web/src/app/l18n/index.js
@@ -2,44 +2,37 @@
  * Copyright (C) 2015 Actor LLC. <https://actor.im>
  */
 
+import russian from './ru-RU';
+import english from './en-US';
+import spanish from './es-ES';
+
 const language = navigator.language || navigator.browserLanguage;
 
 // Intl polyfill
-if (!window.Intl) {
+if (!global.Intl) {
+  const request = new XMLHttpRequest();
+  const url = window.location.href;
+  const arr = url.split('/');
+  const localeDataPath = arr[0] + '//' + arr[2] + '/assets/locale-data/' + language + '.json';
+
   require('intl');
-  switch (language.toLowerCase()) {
-    case 'ru':
-    case 'ru-ru':
-      require('intl/locale-data/jsonp/ru');
-      break;
-    case 'es':
-    case 'es-es':
-      require('intl/locale-data/jsonp/es');
-      break;
-    case 'en':
-    case 'en-us':
-    default:
-      require('intl/locale-data/jsonp/en');
-      break;
+
+  function addLocaleData() {
+    IntlPolyfill.__addLocaleData(JSON.parse(this.response));
   }
+
+  request.addEventListener('load', addLocaleData);
+  request.open('GET', localeDataPath);
+  request.send();
 }
 
 // Set language data
-let intlData;
-switch (language.toLowerCase()) {
-  case 'ru':
-  case 'ru-ru':
-    intlData = require('./ru-RU');
-    break;
-  case 'es':
-  case 'es-es':
-    intlData = require('./es-ES');
-    break;
-  case 'en':
-  case 'en-us':
-  default:
-    intlData = require('./en-US');
-    break;
-}
+const languageData = {
+  'ru': russian,
+  'en': english,
+  'es': spanish
+};
+
+const intlData = languageData[language] || languageData[language.split('-')[0]] || languageData['en'];
 
 export default { intlData };
