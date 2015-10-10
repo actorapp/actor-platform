b02b336681f187e5bc976c6a2dc190320448f0ee
Steve Kite <steve@actor.im>
2015-10-10 08:19:29 +0300

    feat(sdk): Iterative header conversing, copying only referenced headers

--- actor-apps/actor-sdk/sdk-ios/ActorSDK/convert.py
+++ actor-apps/actor-sdk/sdk-ios/ActorSDK/convert.py
@@ -1,7 +1,44 @@
 import sys
 import os
 
-added = set()
+# print "Starting converting headers"
+
+# Building umbrella paths
+
+def loadIncludes(fileName):
+    res = set()
+    umbrella = open(fileName, 'r').read()
+    for line in umbrella.splitlines():
+        if line.startswith("#include") and '\"' in line:
+            start = line.index('\"')
+            end = line.index('\"', start + 1)
+            includedFile = line[start + 1:end]
+            if includedFile != 'objc/runtime.h':
+                localIncludePath = os.path.join("Sources/", includedFile)
+                if os.path.exists(localIncludePath):
+                    res.add(includedFile.lower())
+        if line.startswith("#import <ActorSDK/"):
+            start = line.index('<') + 9
+            end = line.index('>', start + 1)
+            includedFile = line[start + 1:end]
+            res.add(includedFile.lower())
+    return res
+
+# print "Building required includes"
+
+def processInclude(fileName, paths, level):
+    # print ('|' * level) + "Processing " + fileName
+    res = paths.copy()
+    includes = loadIncludes(fileName)
+    for inc in includes:
+        if inc not in res:
+            res.add(inc)
+            res = processInclude('Sources/' + inc, res, level + 1)
+    return res
+
+paths = loadIncludes(sys.argv[1])
+for p in paths.copy():
+    paths = paths.union(processInclude('Sources/' + p, paths.copy(), 0))
 
 if not os.path.exists('Public'):
     os.makedirs('Public')
@@ -10,7 +47,7 @@ for root, directories, filenames in os.walk('Sources/'):
     for filename in filenames:
 
         # Matching only header files
-        if not filename.endswith(".h"):
+        if not filename.lower().endswith(".h"):
             continue
 
         # File logic path like im/actor/core/Messenger.h
@@ -21,26 +58,44 @@ for root, directories, filenames in os.walk('Sources/'):
         srcFile = os.path.join(root, filename)
         # Converted File path like Sources2/actor/core/Messenger.h
         destFile = os.path.join('Public', path)
+        
+        # Don'c copy unreferenced includes
+        if path.lower() not in paths:
+            continue
 
         # Auto Create directory
         if not os.path.exists(os.path.dirname(destFile)):
             os.makedirs(os.path.dirname(destFile))
                 
         with open(srcFile, 'r') as f:
+            
+            allLines = f.read()
+            destLines = ""
+            
+            for line in allLines.splitlines():
+                if (line.startswith("#include") or line.startswith("#import")) and '\"' in line:
+                    start = line.index('\"')
+                    end = line.index('\"', start + 1)
+                    includedFile = line[start + 1:end]
+                    if includedFile != 'objc/runtime.h':
+                        localIncludePath = os.path.join("Sources/", includedFile)
+                        if os.path.exists(localIncludePath):
+                            line = line[0:start] + "<ActorSDK/" + includedFile + ">" + line[end+1:]
+                        else:
+                            line = line[0:start] + "<j2objc/" + includedFile + ">" + line[end+1:]
+        
+                destLines = destLines + line + "\n"
 
-            with open(destFile, 'w') as d:
-                for line in f:
-                    if (line.startswith("#include") or line.startswith("#import")) and '\"' in line:
+            isUpdated = True
 
-                        start = line.index('\"')
-                        end = line.index('\"', start + 1)
-                        includedFile = line[start + 1:end]
+            if os.path.exists(destFile):
+                with open(destFile, 'rw') as d:
+                    if d.read() == destLines:
+                        # print "Not Updated"
+                        isUpdated = False
 
-                        if includedFile != 'objc/runtime.h':
-                            localIncludePath = os.path.join("Sources/", includedFile)
-                            if os.path.exists(localIncludePath):
-                                line = line[0:start] + "<ActorSDK/" + includedFile + ">" + line[end+1:]
-                            else:
-                                line = line[0:start] + "<j2objc/" + includedFile + ">" + line[end+1:]
+            if isUpdated:
+                with open(destFile, 'w') as d:
+                    d.write(destLines)
 
-                    d.write(line)
+
