b6a98a732e7dcc26d28d09e938f0a6f848cf29ea
Steve Kite <steve@actor.im>
2015-10-10 10:01:47 +0300

    wip(sdk): Removing obsolete crypto

--- actor-apps/actor-sdk/runtime/main/src/main/java/org/bouncycastle/math/ec/endo/GLVEndomorphism.java
+++
@@ -1,8 +0,0 @@
-package org.bouncycastle.math.ec.endo;
-
-import java.math.BigInteger;
-
-public interface GLVEndomorphism extends ECEndomorphism
-{
-    BigInteger[] decomposeScalar(BigInteger k);
-}
