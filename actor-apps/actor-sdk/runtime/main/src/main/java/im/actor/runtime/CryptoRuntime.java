d2cd80b98c14492e7c17e73bf1731fb10f832782
Steve Kite <steve@actor.im>
2015-10-10 10:01:47 +0300

    wip(sdk): Removing obsolete crypto

--- actor-apps/actor-sdk/runtime/main/src/main/java/im/actor/runtime/CryptoRuntime.java
+++ actor-apps/actor-sdk/runtime/main/src/main/java/im/actor/runtime/CryptoRuntime.java
@@ -17,43 +17,6 @@ import im.actor.runtime.crypto.RsaEncryptCipher;
 public interface CryptoRuntime {
 
     /**
-     * Generation of RSA 1024 bit key pair
-     *
-     * @return generated key pair
-     */
-    @ObjectiveCName("generateRSA1024KeyPair")
-    CryptoKeyPair generateRSA1024KeyPair();
-
-    /**
-     * Create RSA encryption cipher
-     *
-     * @param publicKey public rsa key
-     * @return the RSA encryption cipher for publicKey
-     */
-    @ObjectiveCName("createRSAOAEPSHA1CipherWithPublicKey:")
-    RsaEncryptCipher createRSAOAEPSHA1Cipher(byte[] publicKey);
-
-    /**
-     * Create RSA encryption/decryption cipher
-     *
-     * @param publicKey  public rsa key
-     * @param privateKey private rsa key
-     * @return the RSA encryption/decryption cipher for key pair
-     */
-    @ObjectiveCName("createRSAOAEPSHA1CipherWithPublicKey:withPrivateKey:")
-    RsaCipher createRSAOAEPSHA1Cipher(byte[] publicKey, byte[] privateKey);
-
-    /**
-     * Create AES cipher
-     *
-     * @param key AES Key
-     * @param iv  AES IV
-     * @return the AES cipher for keys
-     */
-    @ObjectiveCName("createAESCBCPKS7CipherWithKey:withIv:")
-    AesCipher createAESCBCPKS7Cipher(byte[] key, byte[] iv);
-
-    /**
      * Calculate MD5
      *
      * @param data source data
