8ce724da7c1da4be2721dd68c647e6a5a3387d51
Steve Kite <steve@actor.im>
2015-10-11 18:30:29 +0300

    feat(sdk): App with extension

---
+++ actor-sdk/sdk-app/ios/ActorApp/ActorApp-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  ActorApp-Bridging-Header.h
+//  ActorApp
+//
+//  Created by Steve Kite on 11.10.15.
+//  Copyright © 2015 Steve Kite. All rights reserved.
+//
+
+#ifndef ActorApp_Bridging_Header_h
+#define ActorApp_Bridging_Header_h
+
+#include "im/actor/extension/SampleExtension.h"
+
+#endif /* ActorApp_Bridging_Header_h */
