407db01d387c85d919d8dee905fd465ced0b93b3
Steve Kite <steve@actor.im>
2015-10-11 17:22:31 +0300

    feat(core): Added extensions support

---
+++ actor-sdk/sdk-core/core/shared/src/main/java/im/actor/core/network/parser/ApiParserConfig.java
@@ -0,0 +1,30 @@
+package im.actor.core.network.parser;
+
+import java.io.IOException;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import im.actor.core.api.parser.RpcParser;
+import im.actor.core.api.parser.UpdatesParser;
+
+public class ApiParserConfig {
+
+    private RpcParser rpcRarser = new RpcParser();
+    private UpdatesParser updatesParser = new UpdatesParser();
+    private CopyOnWriteArrayList<ParsingExtension> extensions = new CopyOnWriteArrayList<ParsingExtension>();
+
+    public void addExtension(ParsingExtension extension) {
+        extensions.add(extension);
+    }
+
+    public RpcScope parseRpc(int header, byte[] content) throws IOException {
+        for (ParsingExtension ex : extensions) {
+            try {
+                return ex.getRpcScopeParser().read(header, content);
+            } catch (Exception e) {
+                // Ignore
+            }
+        }
+
+        return rpcRarser.read(header, content);
+    }
+}
