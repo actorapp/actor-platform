6ddf061c3ba1abf1a4e8c3652e1ebae57551fbcd
Steve Kite <steve@actor.im>
2015-10-11 18:30:29 +0300

    feat(sdk): App with extension

--- actor-sdk/sdk-app/ios/ActorApp/Extension/convert.py
+++ actor-sdk/sdk-app/ios/ActorApp/Extension/convert.py
@@ -1,47 +1,47 @@
 import sys
 import os
-
-# print "Starting converting headers"
-
-# Building umbrella paths
-
-def loadIncludes(fileName):
-    res = set()
-    umbrella = open(fileName, 'r').read()
-    for line in umbrella.splitlines():
-        if line.startswith("#include") and '\"' in line:
-            start = line.index('\"')
-            end = line.index('\"', start + 1)
-            includedFile = line[start + 1:end]
-            if includedFile != 'objc/runtime.h':
-                localIncludePath = os.path.join("Sources/", includedFile)
-                if os.path.exists(localIncludePath):
-                    res.add(includedFile.lower())
-        if line.startswith("#import <ActorSDK/"):
-            start = line.index('<') + 9
-            end = line.index('>', start + 1)
-            includedFile = line[start + 1:end]
-            res.add(includedFile.lower())
-    return res
-
-# print "Building required includes"
-
-def processInclude(fileName, paths, level):
-    # print ('|' * level) + "Processing " + fileName
-    res = paths.copy()
-    includes = loadIncludes(fileName)
-    for inc in includes:
-        if inc not in res:
-            res.add(inc)
-            res = processInclude('Sources/' + inc, res, level + 1)
-    return res
-
-paths = loadIncludes(sys.argv[1])
-for p in paths.copy():
-    paths = paths.union(processInclude('Sources/' + p, paths.copy(), 0))
-
-if not os.path.exists('Public'):
-    os.makedirs('Public')
+#
+## print "Starting converting headers"
+#
+## Building umbrella paths
+#
+#def loadIncludes(fileName):
+#    res = set()
+#    umbrella = open(fileName, 'r').read()
+#    for line in umbrella.splitlines():
+#        if line.startswith("#include") and '\"' in line:
+#            start = line.index('\"')
+#            end = line.index('\"', start + 1)
+#            includedFile = line[start + 1:end]
+#            if includedFile != 'objc/runtime.h':
+#                localIncludePath = os.path.join("Sources/", includedFile)
+#                if os.path.exists(localIncludePath):
+#                    res.add(includedFile.lower())
+#        if line.startswith("#import <ActorSDK/"):
+#            start = line.index('<') + 9
+#            end = line.index('>', start + 1)
+#            includedFile = line[start + 1:end]
+#            res.add(includedFile.lower())
+#    return res
+#
+## print "Building required includes"
+#
+#def processInclude(fileName, paths, level):
+#    # print ('|' * level) + "Processing " + fileName
+#    res = paths.copy()
+#    includes = loadIncludes(fileName)
+#    for inc in includes:
+#        if inc not in res:
+#            res.add(inc)
+#            res = processInclude('Sources/' + inc, res, level + 1)
+#    return res
+#
+#paths = loadIncludes(sys.argv[1])
+#for p in paths.copy():
+#    paths = paths.union(processInclude('Sources/' + p, paths.copy(), 0))
+#
+#if not os.path.exists('Public'):
+#    os.makedirs('Public')
 
 for root, directories, filenames in os.walk('Sources/'):
     for filename in filenames:
@@ -58,10 +58,6 @@ for root, directories, filenames in os.walk('Sources/'):
         srcFile = os.path.join(root, filename)
         # Converted File path like Sources2/actor/core/Messenger.h
         destFile = os.path.join('Public', path)
-        
-        # Don'c copy unreferenced includes
-        if path.lower() not in paths:
-            continue
 
         # Auto Create directory
         if not os.path.exists(os.path.dirname(destFile)):
@@ -80,7 +76,7 @@ for root, directories, filenames in os.walk('Sources/'):
                     if includedFile != 'objc/runtime.h':
                         localIncludePath = os.path.join("Sources/", includedFile)
                         if os.path.exists(localIncludePath):
-                            line = line[0:start] + "<ActorSDK/" + includedFile + ">" + line[end+1:]
+                            line = line[0:start] + "\"" + includedFile + "\"" + line[end+1:]
                         else:
                             line = line[0:start] + "<j2objc/" + includedFile + ">" + line[end+1:]
         
