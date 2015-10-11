bf4a07b472fbf2ab0709f6a63c5a815c2cdd2a2b
Steve Kite <steve@actor.im>
2015-10-11 18:04:35 +0300

    feat(sdk): SDK-based app with extension

---
+++ actor-sdk/sdk-app/shared/src/java/im/actor/extension/SampleExtension.java
@@ -0,0 +1,9 @@
+package im.actor.extension;
+
+import im.actor.runtime.Log;
+
+public class SampleExtension {
+    public static void perform() {
+        Log.d("SampleExtension", "Perform");
+    }
+}
