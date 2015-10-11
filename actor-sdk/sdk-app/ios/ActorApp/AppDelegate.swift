d865c2873008ab794f5ed285301b6b4bce34e7db
Steve Kite <steve@actor.im>
2015-10-11 18:30:29 +0300

    feat(sdk): App with extension

--- actor-sdk/sdk-app/ios/ActorApp/AppDelegate.swift
+++ actor-sdk/sdk-app/ios/ActorApp/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
         // Override point for customization after application launch.
         return true
     }
