e52abd0b90acd19cc7031edd87b962138767840d
Steve Kite <steve@actor.im>
2015-10-13 11:44:59 +0300

    feat(sdk): New methods in storage engines

--- actor-sdk/sdk-core/runtime/shared/src/main/java/im/actor/runtime/storage/KeyValueStorage.java
+++ actor-sdk/sdk-core/runtime/shared/src/main/java/im/actor/runtime/storage/KeyValueStorage.java
@@ -9,21 +9,69 @@ import com.google.j2objc.annotations.ObjectiveCName;
 import java.util.List;
 
 public interface KeyValueStorage {
+
+    /**
+     * Adding new or replacing old record
+     *
+     * @param key  record key
+     * @param data record raw data
+     */
     @ObjectiveCName("addOrUpdateItemWithKey:withData:")
     void addOrUpdateItem(long key, byte[] data);
 
+    /**
+     * Adding new or replacing old records
+     *
+     * @param values records
+     */
     @ObjectiveCName("addOrUpdateItems:")
     void addOrUpdateItems(List<KeyValueRecord> values);
 
+    /**
+     * Removing record by key
+     *
+     * @param key record key
+     */
     @ObjectiveCName("removeItemWithKey:")
     void removeItem(long key);
 
+    /**
+     * Removing records by keys
+     *
+     * @param keys record keys
+     */
     @ObjectiveCName("removeItemsWithKeys:")
     void removeItems(long[] keys);
 
+    /**
+     * Loading item by key
+     *
+     * @param key record key
+     * @return result or null if not found
+     */
+    @ObjectiveCName("loadItemWithKey:")
+    byte[] loadItem(long key);
+
+    /**
+     * Loading items by keys
+     *
+     * @param keys record keys
+     * @return all loaded items
+     */
+    @ObjectiveCName("loadItems:")
+    List<KeyValueRecord> loadItems(long[] keys);
+
+    /**
+     * Loading all items from storage
+     *
+     * @return all items
+     */
+    @ObjectiveCName("loadAllItems")
+    List<KeyValueRecord> loadAllItems();
+
+    /**
+     * Clearing storage
+     */
     @ObjectiveCName("clear")
     void clear();
-
-    @ObjectiveCName("getValueWithKey:")
-    byte[] getValue(long key);
 }
