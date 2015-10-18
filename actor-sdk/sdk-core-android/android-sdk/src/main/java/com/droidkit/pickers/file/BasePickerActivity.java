e66d9cc3c04b796004f79182f6a79a969af3543e
Steve Kite <steve@actor.im>
2015-10-18 16:48:15 +0300

    fix(android): Removing old SearchView references

--- actor-sdk/sdk-core-android/android-sdk/src/main/java/com/droidkit/pickers/file/BasePickerActivity.java
+++ actor-sdk/sdk-core-android/android-sdk/src/main/java/com/droidkit/pickers/file/BasePickerActivity.java
@@ -4,7 +4,6 @@ import android.animation.ValueAnimator;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
