740ae120f56b3c4ec7ce13757179d4a53f5144e5
John Doe <speed@actor.im>
2015-10-20 21:36:47 +0300

    feat(server:bots): UpdateAvatar method; support of all message types

---
+++ actor-server/actor-bots/src/main/scala/im/actor/server/bot/BotToInternalConversions.scala
@@ -0,0 +1,20 @@
+package im.actor.server.bot
+
+import im.actor.bots.BotMessages
+import im.actor.server.file.{ FileLocation, AvatarImage, Avatar }
+
+import scala.language.implicitConversions
+
+trait BotToInternalConversions {
+  implicit def toFileLocation(fl: BotMessages.FileLocation): FileLocation =
+    FileLocation(fl.fileId, fl.accessHash)
+
+  implicit def toAvatarImage(image: BotMessages.AvatarImage): AvatarImage =
+    AvatarImage(image.fileLocation, image.width, image.height, image.fileSize.toLong)
+
+  implicit def toAvatarImageOpt(imageOpt: Option[BotMessages.AvatarImage]): Option[AvatarImage] =
+    imageOpt map toAvatarImage
+
+  implicit def toAvatar(avatar: BotMessages.Avatar): Avatar =
+    Avatar(avatar.small, avatar.large, avatar.full)
+}
