304e7220caef22a50d3b7e753e84e5f81cdea00c
Steve Kite <steve@actor.im>
2015-10-20 14:35:45 +0300

    fix(iOS): Fixing invite url processing

--- actor-sdk/sdk-core-ios/ActorSDK/Sources/ActorSDK.swift
+++ actor-sdk/sdk-core-ios/ActorSDK/Sources/ActorSDK.swift
@@ -72,6 +72,9 @@ public class ActorSDK {
     /// Invite url scheme
     public var inviteUrlScheme: String? = nil
     
+    /// Web Invite Domain host
+    public var inviteUrlHost: String? = nil
+    
     /// Extensions
     private var extensions = [ActorExtension]()
     
@@ -292,6 +295,58 @@ public class ActorSDK {
         presentMessengerInWindow(window)
         window.makeKeyAndVisible()
     }
+
+    //
+    // Data Processing
+    //
+    
+    /// Handling URL Opening in application
+    func openUrl(url: String) {
+        if let u = NSURL(string: url) {
+            
+            // Handle web invite url
+            if (u.scheme.lowercaseString == "http" || u.scheme.lowercaseString == "https") &&  inviteUrlHost != nil {
+                
+                if u.host == inviteUrlHost {
+                    if let token = u.lastPathComponent {
+                        joinGroup(token)
+                        return
+                    }
+                }
+            }
+            
+            // Handle custom scheme invite url
+            if (u.scheme.lowercaseString == inviteUrlScheme?.lowercaseString) {
+                if (u.host == "invite") {
+                    let token = u.query?.componentsSeparatedByString("=")[1]
+                    if token != nil {
+                        joinGroup(token!)
+                        return
+                    }
+                }
+            }
+            
+            UIApplication.sharedApplication().openURL(u)
+        }
+    }
+    
+    /// Handling joining group by token
+    func joinGroup(token: String) {
+        let alert = UIAlertController(title: nil, message: AALocalized("GroupJoinMessage"), preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: AALocalized("AlertNo"), style: .Cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: AALocalized("GroupJoinAction"), style: .Default){ (action) -> Void in
+            AAExecutions.execute(Actor.joinGroupViaLinkCommandWithUrl(token), type: .Safe, ignore: [], successBlock: { (val) -> Void in
+                let groupId = val as! JavaLangInteger
+                if let controller = self.bindedToWindow.rootViewController {
+                    let tabBarController = controller as! UITabBarController
+                    let index = tabBarController.selectedIndex
+                    let navController = tabBarController.viewControllers![index] as! UINavigationController
+                    navController.pushViewController(ConversationViewController(peer: ACPeer.groupWithInt(groupId.intValue)), animated: true)
+                }
+            }, failureBlock: nil)
+        })
+        bindedToWindow.rootViewController?.presentViewController(alert, animated: true, completion: nil)
+    }
     
     //
     // File System
@@ -448,42 +503,27 @@ public class ActorSDK {
         }
         self.completionHandler = completionHandler
     }
-    
+
     //
     // Handling invite url
     //
+    
     func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
         
-        if (url.scheme == "actor") {
-            if (url.host == "invite") {
-                if (Actor.isLoggedIn()) {
-                    let token = url.query?.componentsSeparatedByString("=")[1]
-                    if token != nil {
-//                        UIAlertView.showWithTitle(nil, message: AALocalized("GroupJoinMessage"), cancelButtonTitle: localized("AlertNo"), otherButtonTitles: [AALocalized("GroupJoinAction")], tapBlock: { (view, index) -> Void in
-//                            if (index == view.firstOtherButtonIndex) {
-//                                Executions.execute(Actor.joinGroupViaLinkCommandWithUrl(token), successBlock: { (val) -> Void in
-//                                    let groupId = val as! JavaLangInteger
-//                                    self.openChat(ACPeer.groupWithInt(groupId.intValue))
-//                                    }, failureBlock: { (val) -> Void in
-//                                        
-//                                        if let res = val as? ACRpcException {
-//                                            if res.getTag() == "USER_ALREADY_INVITED" {
-//                                                UIAlertView.showWithTitle(nil, message: localized("ErrorAlreadyJoined"), cancelButtonTitle: localized("AlertOk"), otherButtonTitles: nil, tapBlock: nil)
-//                                                return
-//                                            }
-//                                        }
-//                                        
-//                                        UIAlertView.showWithTitle(nil, message: localized("ErrorUnableToJoin"), cancelButtonTitle: localized("AlertOk"), otherButtonTitles: nil, tapBlock: nil)
-//                                })
-//                            }
-//                        })
-                    }
-                }
-                
-                return true
-            }
+        dispatchOnUi { () -> Void in
+            self.openUrl(url.absoluteString)
         }
-        return false
+
+        return true
+    }
+    
+    public func application(application: UIApplication, handleOpenURL url: NSURL) -> Bool {
+        
+        dispatchOnUi { () -> Void in
+            self.openUrl(url.absoluteString)
+        }
+        
+        return true
     }
 }
 
