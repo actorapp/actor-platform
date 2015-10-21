c084c55458fa1751fa02a761215d721fbfd29727
Oleg Shilov <me@olegshilov.com>
2015-10-21 20:54:42 +0300

    feat(web): add selection by keys, open dialog on select.

--- actor-apps/app-web/src/app/components/modals/FastSwitcher.react.js
+++ actor-apps/app-web/src/app/components/modals/FastSwitcher.react.js
@@ -12,10 +12,14 @@ import classnames from 'classnames';
 import { KeyCodes } from 'constants/ActorAppConstants';
 
 import FastSwitcherActionCreators from 'actions/FastSwitcherActionCreators';
+import DialogActionCreators from 'actions/DialogActionCreators';
 
 import FastSwitcherStore from 'stores/FastSwitcherStore';
 
 import AvatarItem from 'components/common/AvatarItem.react';
+
+const RESULT_ITEM_HEIGHT = 44;
+let scrollIndex = 0;
 
 class FastSwitcher extends Component {
   static getStores = () => [FastSwitcherStore];
@@ -48,7 +52,7 @@ class FastSwitcher extends Component {
       return (
         <li className={resultClassName}
             key={index}
-            onClick={() => this.handleSelect(result.peer)}
+            onClick={() => this.handleSelect(result.peer.peer)}
             onMouseOver={() => this.setState({selectedIndex: index})}>
           <AvatarItem image={result.peer.avatar}
                       placeholder={result.peer.placeholder}
@@ -82,7 +86,7 @@ class FastSwitcher extends Component {
                    ref="query"/>
           </div>
 
-          <ul className="results">
+          <ul className="results" ref="results">
             {resultsList}
           </ul>
 
@@ -93,23 +97,85 @@ class FastSwitcher extends Component {
     }
   }
 
-  handleClose = () => FastSwitcherActionCreators.hide();
-  handleSearch = (event) => FastSwitcherActionCreators.search(event.target.value);
   setFocus = () => {
     setTimeout(() => {
       React.findDOMNode(this.refs.query).focus();
     }, 0);
   };
 
+  handleClose = () => FastSwitcherActionCreators.hide();
+
+  handleSearch = (event) => FastSwitcherActionCreators.search(event.target.value);
+
   handleSelect = (peer) => {
-    console.debug(peer);
+    DialogActionCreators.selectDialogPeer(peer);
+    this.handleClose();
   };
 
   handleKeyDown = (event) => {
-    if (event.keyCode === KeyCodes.ESC) {
-      event.preventDefault();
-      this.handleClose();
+    const { results, selectedIndex } = this.state;
+    const visibleItems = 8;
+    let index = selectedIndex;
+
+    switch (event.keyCode) {
+      case KeyCodes.ENTER:
+        event.stopPropagation();
+        event.preventDefault();
+        this.handleSelect(results[selectedIndex].peer.peer);
+        break;
+
+      case KeyCodes.ARROW_UP:
+        event.stopPropagation();
+        event.preventDefault();
+
+        if (index > 0) {
+          index -= 1;
+        } else if (index === 0) {
+          index = results.length - 1;
+        }
+
+        if (scrollIndex > index) {
+          scrollIndex = index;
+        } else if (index === results.length - 1) {
+          scrollIndex = results.length - visibleItems;
+        }
+
+        this.handleScroll(scrollIndex * RESULT_ITEM_HEIGHT);
+        this.setState({selectedIndex: index});
+        break;
+      case KeyCodes.ARROW_DOWN:
+      case KeyCodes.TAB:
+        event.stopPropagation();
+        event.preventDefault();
+
+        if (index < results.length - 1) {
+          index += 1;
+        } else if (index === results.length - 1) {
+          index = 0;
+        }
+
+        if (index + 1 > scrollIndex + visibleItems) {
+          scrollIndex = index + 1 - visibleItems;
+        } else if (index === 0) {
+          scrollIndex = 0;
+        }
+
+        this.handleScroll(scrollIndex * RESULT_ITEM_HEIGHT);
+        this.setState({selectedIndex: index});
+        break;
+
+      case KeyCodes.ESC:
+        event.preventDefault();
+        this.handleClose();
+        break;
+
+      default:
     }
+  };
+
+  handleScroll = (top) => {
+    const resultsNode = React.findDOMNode(this.refs.results);
+    resultsNode.scrollTop = top;
   };
 }
 
