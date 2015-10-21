43d82e847a6555a300e426cb50df927e7eb107ff
John Doe <speed@actor.im>
2015-10-21 21:46:08 +0300

    feat(server:botkit): java-friendly optional

--- actor-server/actor-bots-shared/src/main/scala/im/actor/bots/macros/BotInterface.scala
+++ actor-server/actor-bots-shared/src/main/scala/im/actor/bots/macros/BotInterface.scala
@@ -15,21 +15,30 @@ private object BotInterfaceImpl {
 
     def extractCaseClassFields(clazz: ClassSymbol) = {
       clazz.typeSignature.decls collect {
-        case m: MethodSymbol if m.isCaseAccessor ⇒ internal.valDef(m)
+        case m: MethodSymbol if m.isCaseAccessor ⇒ m //internal.valDef(m)
       } toList
     }
 
     def extractClassName(clazz: ClassSymbol) = clazz.name.decodedName.toString
 
-    val requests = c.mirror.staticClass("im.actor.bots.BotMessages.RequestBody").knownDirectSubclasses collect {
-      case cs: ClassSymbol ⇒ cs
+    def javaFields(fields: List[MethodSymbol]) = {
+      fields.foldLeft((false, List.empty[c.universe.Tree])) {
+        case ((hasJavaField, fields), field) ⇒
+          println("===")
+          println(field.typeSignature)
+
+          field.typeSignature.typeSymbol match {
+            case s if s == c.mirror.staticClass("scala.Option") ⇒
+              val typ = field.typeSignature.resultType.typeArgs.head
+              val jfield = q"""${field.name}: java.util.Optional[$typ]"""
+              (true, fields :+ jfield)
+            case _ ⇒
+              (hasJavaField, fields :+ internal.valDef(field))
+          }
+      }
     }
 
-    val updates = c.mirror.staticClass("im.actor.bots.BotMessages.UpdateBody").knownDirectSubclasses collect {
-      case cs: ClassSymbol ⇒ cs
-    }
-
-    val rqDefs = requests map { decl ⇒
+    def rqDef(decl: c.universe.ClassSymbol): List[c.universe.Tree] = {
       val rqClassName = extractClassName(decl)
       val methodName = s"request${rqClassName}"
       val method = TermName(methodName)
@@ -41,21 +50,40 @@ private object BotInterfaceImpl {
         else
           q"""${decl.companion}"""
 
-      q"""
-        def $method(..$fields) = request($rqConst)
-      """
-    } toList
+      val (hasjFields, jfields) = javaFields(fields)
+
+      val sfields = fields map (internal.valDef(_))
+
+      if (hasjFields) {
+        List(
+          q"""def $method(..$jfields) = request($rqConst)""",
+          q"""def $method(..$sfields) = request($rqConst)"""
+        )
+      } else {
+        List(q"""def $method(..$sfields) = request($rqConst)""")
+      }
+    }
+
+    val requests = c.mirror.staticClass("im.actor.bots.BotMessages.RequestBody").knownDirectSubclasses collect {
+      case cs: ClassSymbol ⇒ cs
+    }
+
+    val updates = c.mirror.staticClass("im.actor.bots.BotMessages.UpdateBody").knownDirectSubclasses collect {
+      case cs: ClassSymbol ⇒ cs
+    }
 
     val (updCallbacks, updDefs) = updates map { decl ⇒
       val methodName = s"on${decl.name.decodedName.toString}"
       val method = TermName(methodName)
-      ((decl, method), q"def ${method}(upd: $decl): Unit")
+      ((decl, method), q"def $method(upd: $decl): Unit")
     } unzip
 
     val requestDef =
       q"""
         def request[T <: im.actor.bots.BotMessages.RequestBody](body: T): scala.concurrent.Future[body.Response]
         """
+
+    val rqDefs = requests flatMap (rqDef(_))
 
     val updCases = updCallbacks map {
       case (upd, method) ⇒
@@ -76,6 +104,9 @@ private object BotInterfaceImpl {
         c.Expr[Any](
           q"""
                $mods class $name(..$args) extends ..$parents {
+                 import scala.compat.java8.OptionConverters._
+                 import scala.language.implicitConversions
+                 implicit def optionalToOption[A](optional: java.util.Optional[A]): Option[A] = optional.asScala
                  $requestDef
                  ..$rqDefs
                  ..$updDefs
