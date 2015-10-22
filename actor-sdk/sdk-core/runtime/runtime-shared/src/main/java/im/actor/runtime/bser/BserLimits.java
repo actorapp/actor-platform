4069a1cd6de10563ee5bd9db6b67633defd9cad9
Steve Kite <steve@actor.im>
2015-10-22 16:15:54 +0300

    fix(iOS): Fixing excluded files, fix compilation error in runtime

--- actor-sdk/sdk-core/runtime/runtime-shared/src/main/java/im/actor/runtime/bser/BserLimits.java
+++ actor-sdk/sdk-core/runtime/runtime-shared/src/main/java/im/actor/runtime/bser/BserLimits.java
@@ -4,10 +4,11 @@
 
 package im.actor.runtime.bser;
 
-final class Limits {
+final class BserLimits {
+    
     public static final int MAX_BLOCK_SIZE = 1024 * 1024;// 1 MB
     public static final int MAX_PROTO_REPEATED = 1024 * 1024;
 
-    private Limits() {
+    private BserLimits() {
     }
 }
