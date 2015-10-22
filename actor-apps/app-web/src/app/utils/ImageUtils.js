6845dd5e571d3c67bddafa482fa7d036399d9530
John Doe <speed@actor.im>
2015-10-22 22:53:06 +0300

    feat(web): open big avatars by click on avatar from activity panel

--- actor-apps/app-web/src/app/utils/ImageUtils.js
+++ actor-apps/app-web/src/app/utils/ImageUtils.js
@@ -1,8 +1,15 @@
 /*
  * Copyright (C) 2015 Actor LLC. <https://actor.im>
  */
+import Lightbox from 'jsonlylightbox';
 
-export const dataURItoBlob = (dataURI) => {
+const lightbox = new Lightbox();
+lightbox.load({
+  animation: false,
+  controlClose: '<i class="material-icons">close</i>'
+});
+
+const dataURItoBlob = (dataURI) => {
   const byteString = atob(dataURI.split(',')[1]);
   const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
   const buffer = new ArrayBuffer(byteString.length);
@@ -13,4 +20,9 @@ export const dataURItoBlob = (dataURI) => {
   }
 
   return new Blob([view], {type: mimeString});
+};
+
+export {
+  lightbox,
+  dataURItoBlob
 };
