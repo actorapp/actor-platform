ac1cc3b9b708555ec753ebc788ca2db41078ed03
Steve Kite <steve@actor.im>
2015-10-24 06:03:01 +0300

    feat(bots): Updated example

--- actor-sdk/sdk-bot/bot-example/src/main/java/im/actor/Main.java
+++ actor-sdk/sdk-bot/bot-example/src/main/java/im/actor/Main.java
@@ -7,17 +7,27 @@ import im.actor.botkit.RemoteBot;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+
+        // Reading Bot's token from console
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter bot token: ");
         System.out.flush();
         String token = scanner.nextLine();
         scanner.close();
 
+        // Creating Akka Actor system: soup where Bots live
         ActorSystem system = ActorSystem.create();
 
-        system.actorOf(Props.create(HelloBot.class, token, RemoteBot.DefaultEndpoint()), "HelloBot");
+        // Creating Actor of our Hello Bot
+        // Information about creating bot
+        // 1) Bot Class 2) Bot Token 3) Server Endpoint
+        Props botProps = Props.create(HelloBot.class, token, RemoteBot.DefaultEndpoint());
+        // Create Bot in ActorSystem
+        system.actorOf(botProps);
 
+        // Infinite loop
         system.awaitTermination();
     }
 }
