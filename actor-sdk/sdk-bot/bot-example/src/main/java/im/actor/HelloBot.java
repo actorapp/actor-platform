db9dc07cc66e945d733492c23c8da73b91c5a012
Steve Kite <steve@actor.im>
2015-10-24 06:03:01 +0300

    feat(bots): Updated example

--- actor-sdk/sdk-bot/bot-example/src/main/java/im/actor/HelloBot.java
+++ actor-sdk/sdk-bot/bot-example/src/main/java/im/actor/HelloBot.java
@@ -1,48 +1,72 @@
 package im.actor;
 
-import akka.util.Timeout;
-import im.actor.bots.BotMessages;
+import java.util.Optional;
+
 import im.actor.botkit.RemoteBot;
-import scala.concurrent.Future;
+import im.actor.bots.BotMessages;
 import shardakka.ShardakkaExtension;
 import shardakka.keyvalue.SimpleKeyValueJava;
 
-import java.util.Optional;
-
-import static scala.compat.java8.JFunction.proc;
-
+/**
+ * Example Hello Bot: Entry point of development bots for Actor Platform
+ */
 public class HelloBot extends RemoteBot {
 
-    SimpleKeyValueJava<String> msgsKv;
-    Timeout timeout = new Timeout(1000);
+    /**
+     * Local persistent key-value. Useful for storing bot's data.
+     */
+    private SimpleKeyValueJava<String> localKeyValue;
 
     public HelloBot(String token, String endpoint) {
         super(token, endpoint);
 
-        msgsKv = ShardakkaExtension.get(context().system()).simpleKeyValue("msgs", context().system()).asJava();
+        // Creating KeyValue. Don't try to understand this, this is not necessary.
+        // We will improve this in future versions
+        // "msgs" is name of storage, asJava is required for better Java API.
+        localKeyValue = ShardakkaExtension.get(context().system()).simpleKeyValue("msgs").asJava();
     }
 
+
     @Override
-    public void onTextMessage(BotMessages.TextMessage tm) {
-        if (tm.getText().startsWith("hello")) {
-            requestSendTextMessage(
-                    tm.getSender().asOutPeer(),
-                    nextRandomId(),
-                    "Hi, " + getUser(tm.getSender().getId()).getName() + "!");
-        } else if (tm.getText().startsWith("last")) {
-            Future<Optional<String>> future = msgsKv
-                    .get("last", timeout);
+    public void onMessage(BotMessages.Message message) {
+        if (message.message() instanceof BotMessages.TextMessage) {
+            // If Message is Text Message
+            BotMessages.TextMessage text = (BotMessages.TextMessage) message.message();
 
-            future.foreach(proc(s -> {
-                String msg = s
-                        .map(text -> "Last message I received was: " + text)
-                        .orElse("I'm alone :'(");
-                requestSendTextMessage(tm.getSender().asOutPeer(), nextRandomId(), msg);
-            }), context().dispatcher());
-        } else {
-            requestSendTextMessage(tm.getSender().asOutPeer(), nextRandomId(), "Please, say hello");
+
+            if (text.text().trim().toLowerCase().startsWith("hello")) {
+                // If Message starts from hello: Sending response
+
+                // Get User of sender
+                BotMessages.User user = getUser(message.sender().id());
+
+                // Sending welcome message
+                requestSendMessage(message.peer(),
+                        nextRandomId(), new BotMessages.TextMessage("Hi, " + user.name() + "!"));
+            } else if (text.text().trim().toLowerCase().startsWith("last")) {
+                // If Message starts from last: Sending last sent message
+
+                // Reading last_message from disc
+                Optional<String> res = localKeyValue.syncGet("last_message");
+
+                if (res.isPresent()) {
+
+                    // If present: send it back
+                    requestSendMessage(message.peer(),
+                            nextRandomId(), new BotMessages.TextMessage("Last message:  " + res.get()));
+                } else {
+
+                    requestSendMessage(message.peer(),
+                            nextRandomId(), new BotMessages.TextMessage("I'm alone :'("));
+                }
+            } else {
+
+                // Else ask person about saying hello
+                requestSendMessage(message.peer(), nextRandomId(), new BotMessages.TextMessage("Please, say hello"));
+            }
+
+            // Store last message
+            localKeyValue.syncUpsert("last_message", text.text());
         }
-
-        msgsKv.upsert("last", tm.getText(), timeout);
     }
 }
