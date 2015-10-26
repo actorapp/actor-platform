875a1485ef08dd5868821b87a1ca0677ae52a0f5
Gleb Putintsev <kor_ka@actor.im>
2015-10-26 21:34:45 +0300

    feat(android): settings extends over delegate

---
+++ actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/controllers/fragment/settings/ActorSettingsCategory.java
@@ -0,0 +1,10 @@
+package im.actor.sdk.controllers.fragment.settings;
+
+import android.content.Context;
+import android.view.View;
+
+public interface ActorSettingsCategory {
+    String getCategoryName();
+
+    View getView(Context context);
+}
