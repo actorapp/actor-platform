f61529cb60479989260c0204eaa67f1e94686b37
kor_ka <badgrib@gmmail.com>
2015-10-26 21:34:45 +0300

    feat(android): settings extends over delegate

--- actor-sdk/sdk-core-android/android-app/src/main/java/im/actor/Application.java
+++ actor-sdk/sdk-core-android/android-app/src/main/java/im/actor/Application.java
@@ -1,7 +1,12 @@
 package im.actor;
 
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
+
 import im.actor.sdk.ActorSDK;
 import im.actor.sdk.BaseActorSDKDelegate;
+import im.actor.sdk.controllers.fragment.settings.ActorSettingsCategory;
 
 /**
  * Created by badgr on 16.10.2015.
@@ -16,6 +21,66 @@ public class Application extends android.app.Application{
     }
 
     private class ActorSDKDelegate extends BaseActorSDKDelegate {
+        @Override
+        public View getBeforeNickSettingsView(Context context) {
+            TextView tv = new TextView(context);
+            tv.setText("BeforeNickSettingsView");
+            return tv;
+        }
 
+        @Override
+        public View getAfterPhoneSettingsView(Context context) {
+            TextView tv = new TextView(context);
+            tv.setText("AfterPhoneSettingsView");
+            return tv;
+        }
+
+        @Override
+        public View getSettingsTopView(Context context) {
+            TextView tv = new TextView(context);
+            tv.setText("SettingsTopView");
+            return tv;
+        }
+
+        @Override
+        public View getSettingsBottomView(Context context) {
+            TextView tv = new TextView(context);
+            tv.setText("SettingsBottomView");
+            return tv;
+        }
+
+        @Override
+        public ActorSettingsCategory getBeforeSettingsCategory() {
+            return new ActorSettingsCategory() {
+                @Override
+                public String getCategoryName() {
+                    return "BeforeSettingsCategory";
+                }
+
+                @Override
+                public View getView(Context context) {
+                    TextView tv = new TextView(context);
+                    tv.setText("BeforeSettings");
+                    return tv;
+                }
+            };
+        }
+
+        @Override
+        public ActorSettingsCategory getAfterSettingsCategory() {
+            return new ActorSettingsCategory() {
+                @Override
+                public String getCategoryName() {
+                    return "AfterSettingsCategory";
+                }
+
+                @Override
+                public View getView(Context context) {
+                    TextView tv = new TextView(context);
+                    tv.setText("AfterSettings");
+                    return tv;
+                }
+            };
+        }
     }
 }
--- actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/BaseActorSDKDelegate.java
+++ actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/BaseActorSDKDelegate.java
@@ -1,8 +1,12 @@
 package im.actor.sdk;
 
+import android.content.Context;
+import android.view.View;
+
 import im.actor.core.AuthState;
 import im.actor.sdk.controllers.fragment.auth.BaseAuthFragment;
 import im.actor.sdk.controllers.fragment.auth.SignPhoneFragment;
+import im.actor.sdk.controllers.fragment.settings.ActorSettingsCategory;
 import im.actor.sdk.intents.ActorIntent;
 
 public class BaseActorSDKDelegate implements ActorSDKDelegate {
@@ -31,4 +35,36 @@ public class BaseActorSDKDelegate implements ActorSDKDelegate {
     public BaseAuthFragment getSignFragment() {
         return new SignPhoneFragment();
     }
+
+    @Override
+    public View getBeforeNickSettingsView(Context context) {
+        return null;
+    }
+
+    @Override
+    public View getAfterPhoneSettingsView(Context context) {
+        return null;
+    }
+
+    @Override
+    public View getSettingsTopView(Context context) {
+        return null;
+    }
+
+    @Override
+    public View getSettingsBottomView(Context context) {
+        return null;
+    }
+
+    @Override
+    public ActorSettingsCategory getBeforeSettingsCategory() {
+        return null;
+    }
+
+    @Override
+    public ActorSettingsCategory getAfterSettingsCategory() {
+        return null;
+    }
+
+
 }
