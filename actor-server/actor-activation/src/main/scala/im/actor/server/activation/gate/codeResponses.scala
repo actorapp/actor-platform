f1c7eecfde7cc28ef5d9ba5f4dd9784f1d7729f9
rockjam <5min4eq.unity@gmail.com>
2015-10-26 23:40:07 +0300

    fix(server:activation): make activation API more preceise

--- actor-server/actor-activation/src/main/scala/im/actor/server/activation/gate/codeResponses.scala
+++
@@ -1,5 +0,0 @@
-package im.actor.server.activation.gate
-
-sealed trait CodeResponse
-final case class CodeHash(hash: String) extends CodeResponse
-final case class CodeError(message: String) extends CodeResponse
