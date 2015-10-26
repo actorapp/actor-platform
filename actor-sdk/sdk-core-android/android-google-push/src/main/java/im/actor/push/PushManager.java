f5d1810828172215c4f0017b3a2c358044f4f16e
kor_ka <badgrib@gmmail.com>
2015-10-26 19:47:29 +0300

    chore(android): move push register logic to lob

--- actor-sdk/sdk-core-android/android-google-push/src/main/java/im/actor/push/PushManager.java
+++ actor-sdk/sdk-core-android/android-google-push/src/main/java/im/actor/push/PushManager.java
@@ -10,9 +10,57 @@ import im.actor.runtime.Log;
 import im.actor.sdk.ActorSDK;
 import im.actor.sdk.core.ActorPushManager;
 
-public class PushManager extends ActorPushManager {
+public class PushManager implements ActorPushManager {
+    private static final String TAG = "im.actor.push.PushManager";
+    private boolean isRegistered;
+
     @Override
-    public String tryRegisterPush(Context context) {
+    public void registerPush(final Context context) {
+        isRegistered = ActorSDK.sharedActor().getMessenger().getPreferences().getBool("push_registered", false);
+
+        if (!isRegistered) {
+            Log.d(TAG, "Requesting push token...");
+
+            // TODO: Add backoff
+            new Thread() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            String regId = tryRegisterPush(context);
+                            if (regId != null) {
+                                Log.d(TAG, "Token loaded");
+                                onPushRegistered(regId);
+                                return;
+                            } else {
+                                Log.d(TAG, "Unable to load Token");
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        Log.d(TAG, "Waiting for next attempt");
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e1) {
+                            e1.printStackTrace();
+                            return;
+                        }
+                    }
+                }
+            }.start();
+        } else {
+            Log.d(TAG, "Already registered token");
+        }
+    }
+
+    private void onPushRegistered(String token) {
+        isRegistered = true;
+        ActorSDK.sharedActor().getMessenger().getPreferences().putBool("push_registered", true);
+        ActorSDK.sharedActor().getMessenger().registerGooglePush(ActorSDK.sharedActor().getPushId(), token);
+
+    }
+
+    private String tryRegisterPush(Context context) {
         GoogleCloudMessaging cloudMessaging = GoogleCloudMessaging.getInstance(context);
         Log.d("Actor GCM", "Requesting push token iteration...");
         try {
@@ -21,6 +69,6 @@ public class PushManager extends ActorPushManager {
             e.printStackTrace();
             return null;
         }
-
     }
+
 }
