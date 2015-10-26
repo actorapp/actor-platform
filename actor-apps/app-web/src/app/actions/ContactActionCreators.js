94dcd11007ebf1ac3497862ed5dd4923f952a8dd
Oleg Shilov <me@olegshilov.com>
2015-10-26 16:56:59 +0300

    feat(web): change action spinner position

--- actor-apps/app-web/src/app/actions/ContactActionCreators.js
+++ actor-apps/app-web/src/app/actions/ContactActionCreators.js
@@ -3,33 +3,18 @@ import { ActionTypes } from 'constants/ActorAppConstants';
 import mixpanel from 'utils/Mixpanel';
 
 export default {
-  showContactList: () => {
-    ActorAppDispatcher.dispatch({
-      type: ActionTypes.CONTACT_LIST_SHOW
-    });
-  },
-
-  hideContactList: () => {
-    ActorAppDispatcher.dispatch({
-      type: ActionTypes.CONTACT_LIST_HIDE
-    });
-  },
+  showContactList: () => dispatch(ActionTypes.CONTACT_LIST_SHOW),
+  hideContactList: () => dispatch(ActionTypes.CONTACT_LIST_HIDE),
 
   setContacts: (contacts) => dispatch(ActionTypes.CONTACT_LIST_CHANGED, { contacts }),
 
   addContact: (uid) => {
     mixpanel.track('Add user to contacts');
-    ActorAppDispatcher.dispatch({
-      type: ActionTypes.CONTACT_ADD,
-      uid: uid
-    });
+    dispatch(ActionTypes.CONTACT_ADD, { uid });
   },
 
   removeContact: (uid) => {
     mixpanel.track('Remove user from contacts');
-    ActorAppDispatcher.dispatch({
-      type: ActionTypes.CONTACT_REMOVE,
-      uid: uid
-    });
+    dispatch(ActionTypes.CONTACT_REMOVE, { uid });
   }
 };
