1901984dfaae2fd1e961b314f65d7b33804299bf
kor_ka <badgrib@gmmail.com>
2015-10-29 20:42:18 +0300

    style(android): dialogs colors -> ActorStyle

--- actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/ActorStyle.java
+++ actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/ActorStyle.java
@@ -3,18 +3,52 @@ package im.actor.sdk;
 import android.graphics.Color;
 
 public class ActorStyle {
-    private int mainColor = Color.parseColor("#4d74a6");
     private int toolBarColor = 0;
     private int fabColor = 0;
     private int fabColorPressed = 0;
     private int categoryTextColor = 0;
     private int recordIconTintColor = 0;
     private int avatarBackgroundColor = 0;
-    private int mainBackground = Color.parseColor("#ffffff");
-    private int backyardBackground = 0;
     private int actionShareColor = 0;
     private int actionAddContactColor = 0;
-    private int contctFastTitleColor = 0;
+    private int contactFastTitleColor = 0;
+
+    //Dialogs
+    private int dialogsStatePendingColor = Color.parseColor("#40000000");
+    private int dialogsStateSentColor = Color.parseColor("#40000000");
+    private int dialogsStateDeliveredColor = Color.parseColor("#40000000");
+    private int dialogsStateReadColor = Color.parseColor("#ff7ea8ef");
+    private int dialogsStateErrorColor = Color.parseColor("#d24a43");
+    private int dialogsCounterTextColor = 0;
+    private int dialogsCounterBackgroundColor = 0;
+    private int dialogsDivider = 0;
+    private int dialogsTitle = 0;
+    private int dialogsText = 0;
+    private int dialogsTime = 0;
+    private int dialogsTyping = 0;
+
+    //Root colors
+    private int mainColor = Color.parseColor("#4d74a6");
+    private int mainBackground = Color.parseColor("#ffffff");
+    private int backyardBackground = 0;
+
+    private int primaryAlt = Color.parseColor("#4ca665");
+    private int primaryAltHovered = Color.parseColor("#ff3d8652");
+    private int primaryAltPressed = Color.parseColor("#ff5cca7b");
+
+    //Dividers
+    private int divider = Color.parseColor("#1e000000");
+    private int divider_inv = Color.parseColor("#1effffff");
+
+    //Text colors
+    private int textPrimaryInv = Color.parseColor("#DEffffff");
+    private int textPrimary = Color.parseColor("#DE000000");
+    private int textSecondary = Color.parseColor("#7A000000");
+    private int textHint = Color.parseColor("#42000000");
+    private int textSubHeader = Color.parseColor("#6F000000");
+    private int textSecondaryInv = Color.parseColor("#8Affffff");
+    private int textHintInv = Color.parseColor("#42ffffff");
+    private int textSubheaderInv = Color.parseColor("#8Fffffff");
 
     public int getMainColor() {
         return mainColor;
@@ -141,16 +175,249 @@ public class ActorStyle {
         this.actionAddContactColor = actionAddContactColor;
     }
 
-    public int getContctFastTitleColor() {
-        if (contctFastTitleColor != 0) {
-            return contctFastTitleColor;
+    public int getContactFastTitleColor() {
+        if (contactFastTitleColor != 0) {
+            return contactFastTitleColor;
         } else {
             return getMainColor();
         }
     }
 
-    public void setContctFastTitleColor(int contctFastTitleColor) {
-        this.contctFastTitleColor = contctFastTitleColor;
+    public void setContactFastTitleColor(int contactFastTitleColor) {
+        this.contactFastTitleColor = contactFastTitleColor;
+    }
+
+    public int getDialogsCounterTextColor() {
+        if (dialogsCounterTextColor != 0) {
+            return dialogsCounterTextColor;
+        } else {
+            return getTextPrimaryInv();
+        }
+    }
+
+    public void setDialogsCounterTextColor(int dialogsCounterTextColor) {
+        this.dialogsCounterTextColor = dialogsCounterTextColor;
+    }
+
+    public int getDialogsCounterBackgroundColor() {
+        if (dialogsCounterBackgroundColor != 0) {
+            return dialogsCounterBackgroundColor;
+        } else {
+            return getPrimaryAlt();
+        }
+    }
+
+    public void setDialogsCounterBackgroundColor(int dialogsCounterBackgroundColor) {
+        this.dialogsCounterBackgroundColor = dialogsCounterBackgroundColor;
+    }
+
+    public int getTextPrimaryInv() {
+        return textPrimaryInv;
+    }
+
+    public void setTextPrimaryInv(int textPrimaryInv) {
+        this.textPrimaryInv = textPrimaryInv;
+    }
+
+    public int getPrimaryAlt() {
+        return primaryAlt;
+    }
+
+    public void setPrimaryAlt(int primaryAlt) {
+        this.primaryAlt = primaryAlt;
+    }
+
+    public int getPrimaryAltHovered() {
+        return primaryAltHovered;
+    }
+
+    public void setPrimaryAltHovered(int primaryAltHovered) {
+        this.primaryAltHovered = primaryAltHovered;
+    }
+
+    public int getPrimaryAltPressed() {
+        return primaryAltPressed;
+    }
+
+    public void setPrimaryAltPressed(int primaryAltPressed) {
+        this.primaryAltPressed = primaryAltPressed;
+    }
+
+    public int getDialogsStatePendingColor() {
+        return dialogsStatePendingColor;
+    }
+
+    public void setDialogsStatePendingColor(int dialogsStatePendingColor) {
+        this.dialogsStatePendingColor = dialogsStatePendingColor;
+    }
+
+    public int getDialogsStateSentColor() {
+        return dialogsStateSentColor;
+    }
+
+    public void setDialogsStateSentColor(int dialogsStateSentColor) {
+        this.dialogsStateSentColor = dialogsStateSentColor;
+    }
+
+    public int getDialogsStateDeliveredColor() {
+        return dialogsStateDeliveredColor;
+    }
+
+    public void setDialogsStateDeliveredColor(int dialogsStateDeliveredColor) {
+        this.dialogsStateDeliveredColor = dialogsStateDeliveredColor;
+    }
+
+    public int getDialogsStateReadColor() {
+        return dialogsStateReadColor;
+    }
+
+    public void setDialogsStateReadColor(int dialogsStateReadColor) {
+        this.dialogsStateReadColor = dialogsStateReadColor;
+    }
+
+    public int getDialogsStateErrorColor() {
+        return dialogsStateErrorColor;
+    }
+
+    public void setDialogsStateErrorColor(int dialogsStateErrorColor) {
+        this.dialogsStateErrorColor = dialogsStateErrorColor;
+    }
+
+    public int getDialogsDivider() {
+        if (dialogsDivider != 0) {
+            return dialogsDivider;
+        } else {
+            return getDividerColor();
+        }
+    }
+
+    public void setDialogsDivider(int dialogsDivider) {
+        this.dialogsDivider = dialogsDivider;
+    }
+
+    public int getDialogsTitle() {
+        if (dialogsTitle != 0) {
+            return dialogsTitle;
+        } else {
+            return getTextPrimary();
+        }
+
+    }
+
+    public void setDialogsTitle(int dialogsTitle) {
+        this.dialogsTitle = dialogsTitle;
+    }
+
+    public int getDialogsText() {
+        if (dialogsText != 0) {
+            return dialogsText;
+        } else {
+            return getTextSecondary();
+        }
+    }
+
+    public void setDialogsText(int dialogsText) {
+        this.dialogsText = dialogsText;
+    }
+
+    public int getDialogsTime() {
+        if (dialogsTime != 0) {
+            return dialogsTime;
+        } else {
+            return getTextSecondary();
+        }
+    }
+
+    public void setDialogsTime(int dialogsTime) {
+        this.dialogsTime = dialogsTime;
+    }
+
+    public int getDialogsTyping() {
+        if (dialogsTyping != 0) {
+            return dialogsTyping;
+        } else {
+            return getMainColor();
+        }
+    }
+
+    public void setDialogsTyping(int dialogsTyping) {
+        this.dialogsTyping = dialogsTyping;
+    }
+
+    public int getDividerColor() {
+        return divider;
+    }
+
+    public int getDivider_inv() {
+        return divider_inv;
+    }
+
+    public void setDivider_inv(int divider_inv) {
+        this.divider_inv = divider_inv;
+    }
+
+    public int getDivider() {
+        return divider;
+    }
+
+    public void setDivider(int divider) {
+        this.divider = divider;
+    }
+
+    public int getTextPrimary() {
+        return textPrimary;
+    }
+
+    public void setTextPrimary(int textPrimary) {
+        this.textPrimary = textPrimary;
+    }
+
+    public int getTextSecondary() {
+        return textSecondary;
+    }
+
+    public void setTextSecondary(int textSecondary) {
+        this.textSecondary = textSecondary;
+    }
+
+    public int getTextHint() {
+        return textHint;
+    }
+
+    public void setTextHint(int textHint) {
+        this.textHint = textHint;
+    }
+
+    public int getTextSubHeader() {
+        return textSubHeader;
+    }
+
+    public void setTextSubHeader(int textSubHeader) {
+        this.textSubHeader = textSubHeader;
+    }
+
+    public int getTextSecondaryInv() {
+        return textSecondaryInv;
+    }
+
+    public void setTextSecondaryInv(int textSecondaryInv) {
+        this.textSecondaryInv = textSecondaryInv;
+    }
+
+    public int getTextHintInv() {
+        return textHintInv;
+    }
+
+    public void setTextHintInv(int textHintInv) {
+        this.textHintInv = textHintInv;
+    }
+
+    public int getTextSubheaderInv() {
+        return textSubheaderInv;
+    }
+
+    public void setTextSubheaderInv(int textSubheaderInv) {
+        this.textSubheaderInv = textSubheaderInv;
     }
 
     private int getDarkenArgb(int color, double percent) {
