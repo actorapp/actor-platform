31377cdfc4d2a75929e317bcaf0e9d0d33858626
John Doe <speed@actor.im>
2015-10-30 21:04:15 +0300

    refactor(server:messaging): get rid of DBIOs

--- actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/messaging/MessagingHandlers.scala
+++ actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/messaging/MessagingHandlers.scala
@@ -1,17 +1,17 @@
 package im.actor.server.api.rpc.service.messaging
 
 import akka.util.Timeout
-import im.actor.api.rpc.DBIOResult._
+import im.actor.api.rpc.FutureResultRpc._
 import im.actor.api.rpc._
 import im.actor.api.rpc.messaging._
 import im.actor.api.rpc.misc._
 import im.actor.api.rpc.peers._
+import im.actor.config.ActorConfig
 import im.actor.server.dialog.privat.PrivateDialogErrors
 import im.actor.server.group.GroupErrors
 import im.actor.server.sequence.SeqStateDate
 
 import scala.concurrent._
-import scala.concurrent.duration._
 
 private[messaging] trait MessagingHandlers {
   self: MessagingServiceImpl ⇒
@@ -19,10 +19,10 @@ private[messaging] trait MessagingHandlers {
   import im.actor.api.rpc.Implicits._
 
   override implicit val ec: ExecutionContext = actorSystem.dispatcher
-  private implicit val timeout: Timeout = Timeout(5.seconds) // TODO: configurable
+  private implicit val timeout: Timeout = ActorConfig.defaultTimeout
 
   override def jhandleSendMessage(outPeer: ApiOutPeer, randomId: Long, message: ApiMessage, clientData: ClientData): Future[HandlerResult[ResponseSeqDate]] = {
-    val authorizedAction = requireAuth(clientData).map { implicit client ⇒
+    authorized(clientData) { implicit client ⇒
       val accessHashCheck = outPeer.`type` match {
         case ApiPeerType.Private ⇒ userExt.checkAccessHash(outPeer.id, client.authId, outPeer.accessHash)
         case ApiPeerType.Group   ⇒ groupExt.checkAccessHash(outPeer.id, outPeer.accessHash)
@@ -44,12 +44,10 @@ private[messaging] trait MessagingHandlers {
         val toPeer = outPeer.asPeer
         onMessage(Events.PeerMessage(fromPeer.asModel, toPeer.asModel, randomId, date, message))
         ResponseSeqDate(seq, state.toByteArray, date)
-      }).run
-    }
-
-    db.run(toDBIOAction(authorizedAction)) recover {
-      case GroupErrors.NotAMember            ⇒ Error(CommonErrors.forbidden("You are not a group member."))
-      case PrivateDialogErrors.MessageToSelf ⇒ Error(CommonErrors.forbidden("Sending messages to self is not allowed."))
+      }).run recover {
+        case GroupErrors.NotAMember            ⇒ Error(CommonErrors.forbidden("You are not a group member."))
+        case PrivateDialogErrors.MessageToSelf ⇒ Error(CommonErrors.forbidden("Sending messages to self is not allowed."))
+      }
     }
   }
 }
