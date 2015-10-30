e60472dafd89b051b1116502e93a9f2c0c34e99c
John Doe <speed@actor.im>
2015-10-30 05:30:42 +0300

    feat(server:search): peer search

---
+++ actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/search/SearchServiceImpl.scala
@@ -0,0 +1,145 @@
+package im.actor.server.api.rpc.service
+
+import akka.actor.ActorSystem
+import im.actor.api.rpc.{ ClientData, _ }
+import im.actor.api.rpc.groups.ApiGroup
+import im.actor.api.rpc.peers.{ ApiPeer, ApiPeerType }
+import im.actor.api.rpc.search._
+import im.actor.api.rpc.users.ApiUser
+import im.actor.concurrent.FutureExt
+import im.actor.server.db.DbExtension
+import im.actor.server.group.{ GroupExtension, GroupUtils }
+import im.actor.server.persist.contact.UserContactRepo
+import im.actor.server.persist.{ DialogRepo, GroupRepo }
+import im.actor.server.user.UserExtension
+
+import scala.concurrent.{ ExecutionContext, Future }
+
+final class SearchServiceImpl(implicit system: ActorSystem) extends SearchService {
+  override implicit protected val ec: ExecutionContext = system.dispatcher
+
+  private val db = DbExtension(system).db
+  private val userExt = UserExtension(system)
+  private val groupExt = GroupExtension(system)
+
+  override def jhandlePeerSearch(query: IndexedSeq[ApiSearchCondition], clientData: ClientData): Future[HandlerResult[ResponsePeerSearch]] = {
+    authorized(clientData) { implicit client ⇒
+      val (peerTypes, texts) = query.foldLeft(Set.empty[ApiSearchPeerType.Value], Set.empty[String]) {
+        case ((pts, txts), ApiSearchPieceText(t))          ⇒ (pts, txts + t)
+        case ((pts, txts), ApiSearchPeerTypeCondition(pt)) ⇒ (pts + pt, txts)
+      }
+
+      texts.toList match {
+        case text :: Nil ⇒ searchResult(peerTypes.toVector, Some(text))
+        case Nil         ⇒ searchResult(peerTypes.toVector, None)
+        case _           ⇒ Future.successful(Error(RpcError(400, "INVALID_QUERY", "Invalid query.", canTryAgain = false, None)))
+      }
+    }
+  }
+
+  private def searchResult(pts: IndexedSeq[ApiSearchPeerType.Value], text: Option[String])(implicit client: AuthorizedClientData): Future[HandlerResult[ResponsePeerSearch]] = {
+    for {
+      results ← FutureExt.ftraverse(pts)(search(_, text)).map(_.reduce(_ ++ _))
+      (groupIds, userIds) = results.view.map(_.peer).foldLeft(Vector.empty[Int], Vector.empty[Int]) {
+        case ((gids, uids), ApiPeer(pt, pid)) ⇒
+          pt match {
+            case ApiPeerType.Private ⇒ (gids, uids :+ pid)
+            case ApiPeerType.Group   ⇒ (gids :+ pid, uids)
+          }
+      }
+      (groups, users) ← GroupUtils.getGroupsUsers(groupIds, userIds, client.userId, client.authId)
+    } yield Ok(ResponsePeerSearch(results, users.toVector, groups.toVector))
+  }
+
+  private def search(pt: ApiSearchPeerType.Value, text: Option[String])(implicit clientData: AuthorizedClientData): Future[IndexedSeq[ApiPeerSearchResult]] = {
+    pt match {
+      case ApiSearchPeerType.Contacts ⇒
+        for {
+          users ← searchContacts(text)
+        } yield users map result
+      case ApiSearchPeerType.Groups ⇒
+        for {
+          groups ← searchGroups(text)
+        } yield groups map (result(_, isPublic = false))
+      case ApiSearchPeerType.Public ⇒
+        for {
+          groups ← searchGroups(text)
+        } yield groups map (result(_, isPublic = true))
+    }
+  }
+
+  private def result(apiUser: ApiUser): ApiPeerSearchResult =
+    ApiPeerSearchResult(
+      peer = ApiPeer(ApiPeerType.Private, apiUser.id),
+      title = apiUser.localName.getOrElse(apiUser.name),
+      description = None,
+      membersCount = None,
+      dateCreated = None,
+      creator = None,
+      isPublic = None,
+      isJoined = None
+    )
+
+  private def result(apiGroup: ApiGroup, isPublic: Boolean): ApiPeerSearchResult =
+    ApiPeerSearchResult(
+      peer = ApiPeer(ApiPeerType.Group, apiGroup.id),
+      title = apiGroup.title,
+      description = apiGroup.about,
+      membersCount = Some(apiGroup.members.size),
+      dateCreated = Some(apiGroup.createDate),
+      creator = Some(apiGroup.creatorUserId),
+      isPublic = Some(isPublic),
+      isJoined = Some(apiGroup.isMember)
+    )
+
+  private def searchContacts(text: Option[String])(implicit client: AuthorizedClientData): Future[IndexedSeq[ApiUser]] = {
+    for {
+      userIds ← db.run(UserContactRepo.findContactIdsActive(client.userId))
+      users ← FutureExt.ftraverse(userIds)(userExt.getApiStruct(_, client.userId, client.authId))
+    } yield filterUsers(users.toVector, text)
+  }
+
+  private def filterUsers(users: IndexedSeq[ApiUser], textOpt: Option[String]): IndexedSeq[ApiUser] =
+    textOpt match {
+      case Some(text) ⇒
+        val lotext = text.toLowerCase
+        users filter { user ⇒
+          user.name.toLowerCase.contains(lotext) ||
+            user.localName.exists(_.toLowerCase.contains(lotext))
+        }
+      case None ⇒ users
+    }
+
+  // TODO: rewrite it using async
+  private def searchGroups(text: Option[String])(implicit client: AuthorizedClientData): Future[IndexedSeq[ApiGroup]] = {
+    for {
+      ids ← db.run(DialogRepo.findGroupIds(client.userId))
+      groupOpts ← FutureExt.ftraverse(ids) { id ⇒
+        groupExt.isPublic(id) flatMap { isPublic ⇒
+          if (isPublic) Future.successful(None)
+          else groupExt.getApiStruct(id, client.userId).map(Some(_))
+        }
+      }
+    } yield filterGroups(groupOpts.flatten.toVector, text)
+  }
+
+  private def searchPublic(text: Option[String])(implicit client: AuthorizedClientData): Future[IndexedSeq[ApiGroup]] = {
+    for {
+      groups ← db.run(GroupRepo.findPublic)
+      groups ← FutureExt.ftraverse(groups)(g ⇒ groupExt.getApiStruct(g.id, client.userId))
+    } yield filterGroups(groups.toVector, text)
+  }
+
+  private def filterGroups(groups: IndexedSeq[ApiGroup], textOpt: Option[String]): IndexedSeq[ApiGroup] = {
+    textOpt match {
+      case Some(text) ⇒
+        groups.view.filter { group ⇒
+          val lotext = text.toLowerCase
+          group.title.toLowerCase.contains(lotext) ||
+            group.about.exists(_.toLowerCase.contains(lotext)) ||
+            group.theme.exists(_.toLowerCase.contains(lotext))
+        }.force
+      case None ⇒ groups
+    }
+  }
+}
