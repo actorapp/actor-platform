0ee86b5c33853573035a616e64cf4be4ae9040cf
Steve Kite <steve@actor.im>
2015-11-02 17:34:12 +0300

    feat(android): Remove obsolete project

--- actor-apps/app-android/src/main/java/im/actor/messenger/app/view/keyboard/KeyboardStatusListener.java
+++
@@ -1,9 +0,0 @@
-package im.actor.sdk.view.emoji.keyboard;
-
-/**
- * Created by Jesus Christ. Amen.
- */
-public interface KeyboardStatusListener {
-    public void onDismiss();
-    public void onShow();
-}
