5ae0c496a3e7e68052e25fe5f1bb765f723891b0
Steve Kite <steve@actor.im>
2015-11-02 11:42:07 +0300

    fix(core): Fix crash on receiving updates before auth

--- actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/ActorSDKDelegate.java
+++ actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/ActorSDKDelegate.java
@@ -8,6 +8,9 @@ import im.actor.sdk.controllers.fragment.auth.BaseAuthFragment;
 import im.actor.sdk.controllers.fragment.settings.ActorSettingsCategory;
 import im.actor.sdk.intents.ActorIntent;
 
+/**
+ * SDK Delegate. Used for delegating some work to
+ */
 public interface ActorSDKDelegate {
 
     /**
@@ -31,19 +34,34 @@ public interface ActorSDKDelegate {
      */
     ActorIntent getStartIntent();
 
+    /**
+     * If not null returned, overrides settings activity intent
+     *
+     * @return Actor Intent
+     */
+    ActorIntent getSettingsIntent();
+
+    @Deprecated
     AuthState getAuthStartState();
 
+    @Deprecated
     BaseAuthFragment getSignFragment();
 
+    @Deprecated
     View getBeforeNickSettingsView(Context context);
 
+    @Deprecated
     View getAfterPhoneSettingsView(Context context);
 
+    @Deprecated
     View getSettingsTopView(Context context);
 
+    @Deprecated
     View getSettingsBottomView(Context context);
 
+    @Deprecated
     ActorSettingsCategory[] getBeforeSettingsCategories();
 
+    @Deprecated
     ActorSettingsCategory[] getAfterSettingsCategories();
 }
