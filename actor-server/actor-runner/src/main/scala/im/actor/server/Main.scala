42383589c4cd646a3cefb611768800295b3070c1
prettynatty <fear@loathing.in>
2015-11-03 18:07:12 +0300

    feat(server): ActorServer.start()

--- actor-server/actor-runner/src/main/scala/im/actor/server/Main.scala
+++ actor-server/actor-runner/src/main/scala/im/actor/server/Main.scala
@@ -45,120 +45,126 @@ import im.actor.server.social.SocialExtension
 import im.actor.server.user._
 
 object Main extends App {
-  SessionMessage.register()
-  CommonSerialization.register()
-  UserProcessor.register()
-  GroupProcessor.register()
-  DialogProcessor.register()
+  ActorServer.start()
+}
 
-  val serverConfig = ActorConfig.load()
+object ActorServer {
+  def start(): Unit = {
+    SessionMessage.register()
+    CommonSerialization.register()
+    UserProcessor.register()
+    GroupProcessor.register()
+    DialogProcessor.register()
 
-  implicit val system = ActorSystem(serverConfig.getString("actor-system-name"), serverConfig)
+    val serverConfig = ActorConfig.load()
 
-  try {
+    implicit val system = ActorSystem(serverConfig.getString("actor-system-name"), serverConfig)
 
-    // FIXME: get rid of unsafe get's
-    val activationConfig = ActivationConfig.load.get
-    val emailConfig = EmailConfig.fromConfig(serverConfig.getConfig("services.email")).toOption.get
-    val gateConfig = GateConfig.load.get
-    val groupInviteConfig = GroupInviteConfig.load(serverConfig.getConfig("modules.messaging.groups.invite"))
-    val webappConfig = HttpApiConfig.load(serverConfig.getConfig("webapp")).toOption.get
-    val oauth2GoogleConfig = OAuth2GoogleConfig.load(serverConfig.getConfig("services.google.oauth"))
-    val richMessageConfig = RichMessageConfig.load(serverConfig.getConfig("modules.enricher")).get
-    val sequenceConfig = SequenceServiceConfig.load().get
-    implicit val sessionConfig = SessionConfig.load(serverConfig.getConfig("session"))
+    try {
 
-    if (system.settings.config.getList("akka.cluster.seed-nodes").isEmpty) {
-      system.log.info("Going to a single-node cluster mode")
-      val self = Address(
-        "akka.tcp",
-        system.name,
-        InetAddress.getLocalHost.getHostAddress,
-        2552
+      // FIXME: get rid of unsafe get's
+      val activationConfig = ActivationConfig.load.get
+      val emailConfig = EmailConfig.fromConfig(serverConfig.getConfig("services.email")).toOption.get
+      val gateConfig = GateConfig.load.get
+      val groupInviteConfig = GroupInviteConfig.load(serverConfig.getConfig("modules.messaging.groups.invite"))
+      val webappConfig = HttpApiConfig.load(serverConfig.getConfig("webapp")).toOption.get
+      val oauth2GoogleConfig = OAuth2GoogleConfig.load(serverConfig.getConfig("services.google.oauth"))
+      val richMessageConfig = RichMessageConfig.load(serverConfig.getConfig("modules.enricher")).get
+      val sequenceConfig = SequenceServiceConfig.load().get
+      implicit val sessionConfig = SessionConfig.load(serverConfig.getConfig("session"))
+
+      if (system.settings.config.getList("akka.cluster.seed-nodes").isEmpty) {
+        system.log.info("Going to a single-node cluster mode")
+        val self = Address(
+          "akka.tcp",
+          system.name,
+          InetAddress.getLocalHost.getHostAddress,
+          2552
+        )
+
+        Cluster(system).joinSeedNodes(List(self))
+      }
+
+      implicit val executor = system.dispatcher
+      implicit val materializer = ActorMaterializer()
+
+      implicit val db = DbExtension(system).db
+
+      UserMigrator.migrate()
+      GroupMigrator.migrate()
+      LocalNamesMigrator.migrate()
+      GroupCreatorMemberMigrator.migrate()
+      HiddenGroupMigrator.migrate()
+      LocalNamesFromKVMigrator.migrate()
+
+      val weakUpdatesExt = WeakUpdatesExtension(system)
+      val presenceExt = PresenceExtension(system)
+      val groupPresenceExt = GroupPresenceExtension(system)
+      implicit val socialManagerRegion = SocialExtension(system).region
+      implicit val userProcessorRegion = UserExtension(system).processorRegion
+      implicit val userViewRegion = UserExtension(system).viewRegion
+      implicit val groupProcessorRegion = GroupExtension(system).processorRegion
+      implicit val groupViewRegion = GroupExtension(system).viewRegion
+      val groupDialogRegion = DialogExtension(system).groupRegion
+      val privateDialogRegion = DialogExtension(system).privateRegion
+
+      IntegrationTokenMigrator.migrate()
+
+      val activationContext = serverConfig.getString("services.activation.default-service") match {
+        case "internal" ⇒
+          val telesignClient = new TelesignClient(serverConfig.getConfig("services.telesign"))
+          InternalCodeActivation.newContext(
+            activationConfig,
+            new TelesignSmsEngine(telesignClient),
+            new TelesignCallEngine(telesignClient),
+            new SmtpEmailSender(emailConfig)
+          )
+        case "actor-activation" ⇒ new GateCodeActivation(gateConfig)
+        case _                  ⇒ throw new Exception("""Invalid activation.default-service value provided: valid options: "internal", actor-activation""")
+      }
+
+      implicit val sessionRegion = Session.startRegion(Session.props)
+
+      RichMessageWorker.startWorker(richMessageConfig)
+      ReverseHooksListener.startSingleton()
+
+      implicit val oauth2Service = new GoogleProvider(oauth2GoogleConfig)
+
+      val services = Seq(
+        new AuthServiceImpl(activationContext),
+        new ContactsServiceImpl,
+        MessagingServiceImpl(),
+        new GroupsServiceImpl(groupInviteConfig),
+        new PubgroupsServiceImpl,
+        new SequenceServiceImpl(sequenceConfig),
+        new WeakServiceImpl,
+        new UsersServiceImpl,
+        new FilesServiceImpl,
+        new ConfigsServiceImpl,
+        new PushServiceImpl,
+        new ProfileServiceImpl,
+        new IntegrationsServiceImpl(s"${webappConfig.scheme}://${webappConfig.host}"),
+        new WebactionsServiceImpl,
+        new DeviceServiceImpl,
+        new SearchServiceImpl
       )
 
-      Cluster(system).joinSeedNodes(List(self))
+      system.log.warning("Starting ActorBot")
+      ActorBot.start()
+
+      RpcApiExtension(system).register(services)
+
+      ActorCliService.start(system)
+
+      Frontend.start(serverConfig)
+      HttpApiFrontend.start(serverConfig)
+    } catch {
+      case e: ConfigException ⇒
+        system.log.error(e, "Failed to load server configuration")
+        throw e
+      case e: Throwable ⇒
+        system.log.error(e, "Server failed to start up")
+        throw e
     }
-
-    implicit val executor = system.dispatcher
-    implicit val materializer = ActorMaterializer()
-
-    implicit val db = DbExtension(system).db
-
-    UserMigrator.migrate()
-    GroupMigrator.migrate()
-    LocalNamesMigrator.migrate()
-    GroupCreatorMemberMigrator.migrate()
-    HiddenGroupMigrator.migrate()
-    LocalNamesFromKVMigrator.migrate()
-
-    val weakUpdatesExt = WeakUpdatesExtension(system)
-    val presenceExt = PresenceExtension(system)
-    val groupPresenceExt = GroupPresenceExtension(system)
-    implicit val socialManagerRegion = SocialExtension(system).region
-    implicit val userProcessorRegion = UserExtension(system).processorRegion
-    implicit val userViewRegion = UserExtension(system).viewRegion
-    implicit val groupProcessorRegion = GroupExtension(system).processorRegion
-    implicit val groupViewRegion = GroupExtension(system).viewRegion
-    val groupDialogRegion = DialogExtension(system).groupRegion
-    val privateDialogRegion = DialogExtension(system).privateRegion
-
-    IntegrationTokenMigrator.migrate()
-
-    val activationContext = serverConfig.getString("services.activation.default-service") match {
-      case "internal" ⇒
-        val telesignClient = new TelesignClient(serverConfig.getConfig("services.telesign"))
-        InternalCodeActivation.newContext(
-          activationConfig,
-          new TelesignSmsEngine(telesignClient),
-          new TelesignCallEngine(telesignClient),
-          new SmtpEmailSender(emailConfig)
-        )
-      case "actor-activation" ⇒ new GateCodeActivation(gateConfig)
-      case _                  ⇒ throw new Exception("""Invalid activation.default-service value provided: valid options: "internal", actor-activation""")
-    }
-
-    implicit val sessionRegion = Session.startRegion(Session.props)
-
-    RichMessageWorker.startWorker(richMessageConfig)
-    ReverseHooksListener.startSingleton()
-
-    implicit val oauth2Service = new GoogleProvider(oauth2GoogleConfig)
-
-    val services = Seq(
-      new AuthServiceImpl(activationContext),
-      new ContactsServiceImpl,
-      MessagingServiceImpl(),
-      new GroupsServiceImpl(groupInviteConfig),
-      new PubgroupsServiceImpl,
-      new SequenceServiceImpl(sequenceConfig),
-      new WeakServiceImpl,
-      new UsersServiceImpl,
-      new FilesServiceImpl,
-      new ConfigsServiceImpl,
-      new PushServiceImpl,
-      new ProfileServiceImpl,
-      new IntegrationsServiceImpl(s"${webappConfig.scheme}://${webappConfig.host}"),
-      new WebactionsServiceImpl,
-      new DeviceServiceImpl,
-      new SearchServiceImpl
-    )
-
-    system.log.warning("Starting ActorBot")
-    ActorBot.start()
-
-    RpcApiExtension(system).register(services)
-
-    ActorCliService.start(system)
-
-    Frontend.start(serverConfig)
-    HttpApiFrontend.start(serverConfig)
-  } catch {
-    case e: ConfigException ⇒
-      system.log.error(e, "Failed to load server configuration")
-      throw e
-    case e: Throwable ⇒
-      system.log.error(e, "Server failed to start up")
-      throw e
   }
 }
