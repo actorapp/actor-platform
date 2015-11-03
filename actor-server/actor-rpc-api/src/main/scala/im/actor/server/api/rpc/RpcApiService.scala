90a37967e27cc8aa9d21097facfc6f0a8f5792df
John Doe <speed@actor.im>
2015-11-03 17:27:00 +0300

    feat(server:rpc): ability to register services on-the-fly

--- actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/RpcApiService.scala
+++ actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/RpcApiService.scala
@@ -1,27 +1,29 @@
 package im.actor.server.api.rpc
 
-import scala.concurrent._
-import scalaz._
-
 import akka.actor._
 import akka.pattern.pipe
-import scodec.bits._
-
 import im.actor.api.rpc._
 import im.actor.api.rpc.codecs._
+import scodec.bits._
+
+import scala.concurrent._
+import scalaz._
 
 object RpcApiService {
 
   @SerialVersionUID(1L)
-  case class HandleRpcRequest(messageId: Long, requestBytes: BitVector, clientData: ClientData)
+  final case class HandleRpcRequest(messageId: Long, requestBytes: BitVector, clientData: ClientData)
 
   @SerialVersionUID(1L)
-  case class RpcResponse(messageId: Long, responseBytes: BitVector)
+  final case class RpcResponse(messageId: Long, responseBytes: BitVector)
 
-  def props(services: Seq[Service]) = Props(classOf[RpcApiService], services)
+  @SerialVersionUID(1L)
+  private[rpc] case object RefreshChain
+
+  private[rpc] def props(services: Seq[Service]) = Props(classOf[RpcApiService], services)
 }
 
-final class RpcApiService(services: Seq[Service]) extends Actor with ActorLogging {
+private[rpc] final class RpcApiService(services: Seq[Service]) extends Actor with ActorLogging {
 
   import RpcApiService._
 
@@ -32,57 +34,49 @@ final class RpcApiService(services: Seq[Service]) extends Actor with ActorLoggin
 
   private implicit val ec: ExecutionContext = context.dispatcher
 
-  def receive: Receive = {
-    log.debug("Services list changed: {}", services)
+  private val rpcApiExt = RpcApiExtension(context.system)
+  private var chain: Chain = PartialFunction.empty
 
-    val chain: Chain =
-      if (services.isEmpty) {
-        PartialFunction.empty
-      } else {
-        services.map(_.handleRequestPartial).reduce { (a, b) ⇒
-          a.orElse(b)
+  def receive = {
+    case RefreshChain ⇒
+      chain = rpcApiExt.getChain
+    case msg @ HandleRpcRequest(messageId, requestBytes, clientData) ⇒
+      val replyTo = sender()
+
+      try {
+        RequestCodec.decode(requestBytes).require map {
+          case Request(rpcRequest) ⇒
+            log.debug("Request: {}, MessageId: {}, Client: {}", rpcRequest, messageId, clientData)
+
+            val result =
+              if (chain.isDefinedAt(rpcRequest)) {
+                chain(rpcRequest)(clientData)
+              } else {
+                log.error("Unsupported request {}", rpcRequest)
+                Future.successful(Error(CommonErrors.UnsupportedRequest))
+              }
+
+            result
+              .map { res ⇒
+                log.debug("Response: {}, Client: {}", res, clientData)
+                res.fold(err ⇒ err, ok ⇒ ok)
+              }
+              .recover {
+                case e: Throwable ⇒
+                  log.error(e, "Failed to handle messageId: {} rpcRequest: {}", messageId, rpcRequest)
+                  RpcInternalError(true, DefaultErrorDelay)
+              }
+              .map(result ⇒ RpcResponse(messageId, RpcResultCodec.encode(result).require))
+              .pipeTo(replyTo)
+          case _ ⇒
+            Future.successful(CommonErrors.UnsupportedRequest)
         }
+      } catch {
+        case e: Exception ⇒
+          log.error(e, "Failure in RpcApiService while handling messageId: {}", messageId)
+          replyTo ! RpcResponse(messageId, RpcResultCodec.encode(RpcInternalError(true, DefaultErrorDelay)).require) // TODO: configurable delay
+        case e: Throwable ⇒
+          log.error(e, "Failed to handle {}", msg)
       }
-
-    {
-      case msg @ HandleRpcRequest(messageId, requestBytes, clientData) ⇒
-        val replyTo = sender()
-
-        try {
-          RequestCodec.decode(requestBytes).require map {
-            case Request(rpcRequest) ⇒
-              log.debug("Request: {}, MessageId: {}, Client: {}", rpcRequest, messageId, clientData)
-
-              val result =
-                if (chain.isDefinedAt(rpcRequest)) {
-                  chain(rpcRequest)(clientData)
-                } else {
-                  log.error("Unsupported request {}", rpcRequest)
-                  Future.successful(Error(CommonErrors.UnsupportedRequest))
-                }
-
-              result
-                .map { res ⇒
-                  log.debug("Response: {}, Client: {}", res, clientData)
-                  res.fold(err ⇒ err, ok ⇒ ok)
-                }
-                .recover {
-                  case e: Throwable ⇒
-                    log.error(e, "Failed to handle messageId: {} rpcRequest: {}", messageId, rpcRequest)
-                    RpcInternalError(true, DefaultErrorDelay)
-                }
-                .map(result ⇒ RpcResponse(messageId, RpcResultCodec.encode(result).require))
-                .pipeTo(replyTo)
-            case _ ⇒
-              Future.successful(CommonErrors.UnsupportedRequest)
-          }
-        } catch {
-          case e: Exception ⇒
-            log.error(e, "Failure in RpcApiService while handling messageId: {}", messageId)
-            replyTo ! RpcResponse(messageId, RpcResultCodec.encode(RpcInternalError(true, DefaultErrorDelay)).require) // TODO: configurable delay
-          case e: Throwable ⇒
-            log.error(e, "Failed to handle {}", msg)
-        }
-    }
   }
 }
