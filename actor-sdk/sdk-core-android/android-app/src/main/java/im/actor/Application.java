8d09e72668ee78fe3a112948858364edefb4d90e
Gleb Putintsev <kor_ka@actor.im>
2015-11-04 13:38:44 +0300

    fix(android): fix fragment_dialogs layout

--- actor-sdk/sdk-core-android/android-app/src/main/java/im/actor/Application.java
+++ actor-sdk/sdk-core-android/android-app/src/main/java/im/actor/Application.java
@@ -12,8 +12,11 @@ import android.widget.Toast;
 import im.actor.sdk.ActorSDK;
 import im.actor.sdk.ActorStyle;
 import im.actor.sdk.BaseActorSDKDelegate;
+import im.actor.sdk.controllers.fragment.auth.BaseAuthFragment;
+import im.actor.sdk.controllers.fragment.auth.SignEmailFragment;
 import im.actor.sdk.controllers.fragment.settings.ActorSettingsCategory;
 import im.actor.sdk.controllers.fragment.settings.ActorSettingsField;
+import im.actor.sdk.intents.ActorIntent;
 
 public class Application extends android.app.Application{
     @Override
@@ -24,154 +27,19 @@ public class Application extends android.app.Application{
         ActorSDK.sharedActor().createActor(this);
 
         ActorStyle style = ActorSDK.sharedActor().style;
-//        style.setMainColor(Color.parseColor("#529a88"));
-        style.setMainColor(Color.CYAN);
-        style.setMainBackgroundColor(Color.DKGRAY);
-        style.setTextPrimaryColor(Color.GREEN);
-        style.setTextSecondaryColor(Color.RED);
-        style.setSettingsIconColor(Color.GREEN);
-        style.setDividerColor(Color.WHITE);
+        style.setMainColor(Color.parseColor("#529a88"));
+//        style.setMainColor(Color.CYAN);
+//        style.setMainBackgroundColor(Color.DKGRAY);
+//        style.setTextPrimaryColor(Color.GREEN);
+//        style.setTextSecondaryColor(Color.RED);
+//        style.setSettingsIconColor(Color.GREEN);
+//        style.setDividerColor(Color.WHITE);
     }
 
     private class ActorSDKDelegate extends BaseActorSDKDelegate {
         @Override
-        public View getBeforeNickSettingsView(Context context) {
-            TextView tv = new TextView(context);
-            tv.setText("BeforeNickSettingsView");
-            return tv;
-        }
-
-        @Override
-        public View getAfterPhoneSettingsView(Context context) {
-            TextView tv = new TextView(context);
-            tv.setText("AfterPhoneSettingsView");
-            return tv;
-        }
-
-        @Override
-        public View getSettingsTopView(Context context) {
-            TextView tv = new TextView(context);
-            tv.setText("SettingsTopView");
-            return tv;
-        }
-
-        @Override
-        public View getSettingsBottomView(Context context) {
-            TextView tv = new TextView(context);
-            tv.setText("SettingsBottomView");
-            return tv;
-        }
-
-        @Override
-        public ActorSettingsCategory[] getBeforeSettingsCategories() {
-            ActorSettingsCategory[] settings = new ActorSettingsCategory[3];
-            for (int i = 0; i < 3; i++) {
-                final int finalI = i;
-                settings[i] = new ActorSettingsCategory() {
-                    @Override
-                    public String getCategoryName() {
-                        return "BeforeSettingsCategory" + finalI;
-                    }
-
-                    @Override
-                    public View getView(Context context) {
-                        TextView tv = new TextView(context);
-                        tv.setText("BeforeSettings" + finalI);
-                        return tv;
-                    }
-                };
-            }
-            return settings;
-        }
-
-        @Override
-        public ActorSettingsCategory[] getAfterSettingsCategories() {
-            return new ActorSettingsCategory[]{
-                    new ActorSettingsCategory() {
-                        @Override
-                        public String getCategoryName() {
-                            return "AfterSettingsCategory";
-                        }
-
-                        @Override
-                        public ActorSettingsField[] getFields() {
-
-                            final ActorSettingsField zero = new ActorSettingsField() {
-                                @Override
-                                public View getRightView(Context context) {
-                                    return new CheckBox(context);
-                                }
-                            }
-                                    .setIconResourceId(R.drawable.ic_notifications_white_18dp)
-                                    .setName("Zero!");
-                            zero.setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-                                    CheckBox chb = (CheckBox) zero.getRightView();
-                                    chb.setChecked(!chb.isChecked());
-                                }
-                            });
-
-                            return new ActorSettingsField[]{
-
-                                    zero,
-
-                                    new ActorSettingsField("Field", new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-                                            Toast.makeText(getApplicationContext(), "Yo", Toast.LENGTH_SHORT).show();
-                                        }
-                                    }, R.drawable.ic_cloud_download_white_36dp, ActorSDK.sharedActor().style.getAccentColor()),
-
-                                    new ActorSettingsField() {
-
-                                        @Override
-                                        public View getView(Context context) {
-                                            TextView tv = new TextView(context);
-                                            tv.setText("Custom");
-                                            return tv;
-                                        }
-                                    },
-
-                                    new ActorSettingsField("Field 3", new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-                                            Toast.makeText(getApplicationContext(), "Yo 3", Toast.LENGTH_SHORT).show();
-                                        }
-                                    }, R.drawable.ic_camera_alt_white_24dp, getResources().getColor(R.color.action)) {
-
-                                        @Override
-                                        public View getRightView(Context context) {
-                                            return new SwitchCompat(context);
-                                        }
-                                    },
-
-                                    new ActorSettingsField(false) {
-
-                                        @Override
-                                        public String getName() {
-                                            return "Field 4";
-                                        }
-
-                                        @Override
-                                        public View.OnClickListener getOnclickListener() {
-                                            return new View.OnClickListener() {
-                                                @Override
-                                                public void onClick(View v) {
-                                                    Toast.makeText(getApplicationContext(), "Yo 4", Toast.LENGTH_SHORT).show();
-                                                }
-                                            };
-                                        }
-
-                                        @Override
-                                        public View getRightView(Context context) {
-                                            return new AppCompatCheckBox(context);
-                                        }
-                                    },
-                            };
-                        }
-                    }
-            };
+        public BaseAuthFragment getSignFragment() {
+            return new SignEmailFragment();
         }
     }
 }
