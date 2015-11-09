89d5b2a7d23997d87c3d6b59b82c67d8ae9f1341
John Doe <speed@actor.im>
2015-11-09 07:14:22 +0300

    perf(server:sequence): multi-threaded user sequence migrator

---
+++ actor-server/actor-core/src/main/scala/im/actor/server/migrations/FillUserSequenceMigrator.scala
@@ -0,0 +1,19 @@
+package im.actor.server.migrations
+
+import akka.actor.ActorSystem
+import slick.driver.PostgresDriver
+import sql.migration.V20151108011300__FillUserSequence
+
+import scala.concurrent.duration._
+import scala.concurrent.{ Future, ExecutionContext }
+
+object FillUserSequenceMigrator extends Migration {
+  override protected def migrationName: String = "2015-11-08-FillUserSequence"
+
+  override protected def migrationTimeout: Duration = 1.hour
+
+  override protected def startMigration()(implicit system: ActorSystem, db: PostgresDriver.api.Database, ec: ExecutionContext): Future[Unit] = {
+    val migration = new V20151108011300__FillUserSequence(system)
+    Future(migration.migrate())
+  }
+}
