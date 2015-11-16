faf118e6fa055a33de2ea527c6d918fd9608ca0d
prettynatty <fear@loathing.in>
2015-11-16 21:02:04 +0300

    fix(server:messaging): fixed group reactions

--- actor-server/actor-tests/src/test/scala/im/actor/server/api/rpc/service/messaging/ReactionsSpec.scala
+++ actor-server/actor-tests/src/test/scala/im/actor/server/api/rpc/service/messaging/ReactionsSpec.scala
@@ -1,19 +1,25 @@
 package im.actor.server.api.rpc.service.messaging
 
-import im.actor.api.rpc.messaging.{ ApiMessageReaction, ResponseLoadHistory, ResponseReactionsResponse }
+import im.actor.api.rpc.messaging.{ UpdateReactionsUpdate, ApiMessageReaction, ResponseLoadHistory, ResponseReactionsResponse }
 import im.actor.api.rpc._
-import im.actor.server.{ ImplicitSessionRegion, ImplicitAuthService, MessagingSpecHelpers, BaseAppSuite }
+import im.actor.api.rpc.peers.{ ApiPeerType, ApiOutPeer }
+import im.actor.server._
+import im.actor.server.api.rpc.service.groups.{ GroupInviteConfig, GroupsServiceImpl }
 
 final class ReactionsSpec
   extends BaseAppSuite
   with MessagingSpecHelpers
+  with GroupsServiceHelpers
+  with SeqUpdateMatchers
   with ImplicitAuthService
   with ImplicitSessionRegion {
-  "SetReactions" should "set reactions" in set
+  "SetReactions" should "set reactions in private" in setInPrivate
+  it should "set reactions in group" in setInGroup
 
   private implicit lazy val service = MessagingServiceImpl()
+  private implicit lazy val groupService = new GroupsServiceImpl(GroupInviteConfig(""))
 
-  def set() = {
+  def setInPrivate() = {
     val (alice, aliceAuthId, aliceAuthSid, _) = createUser()
     val (bob, bobAuthId, bobAuthSid, _) = createUser()
 
@@ -45,6 +51,40 @@ final class ReactionsSpec
           case Ok(ResponseLoadHistory(history, _)) ⇒
             history.head.reactions should be(Vector(ApiMessageReaction(Vector(bob.id), "like")))
         }
+      }
+    }
+  }
+
+  def setInGroup() = {
+    val (alice, aliceAuthId, aliceAuthSid, _) = createUser()
+    val (bob, bobAuthId, bobAuthSid, _) = createUser()
+
+    val aliceClient = ClientData(aliceAuthId, 1, Some(AuthData(alice.id, aliceAuthSid)))
+    val bobClient = ClientData(bobAuthId, 2, Some(AuthData(bob.id, bobAuthSid)))
+
+    val (peer, randomId) = {
+      implicit val clientData = aliceClient
+      val group = createGroup("Alice and Bob", Set(bob.id))
+      val randomId = sendMessageToGroup(group.groupPeer.groupId, textMessage("Do you like me?"))
+      (ApiOutPeer(ApiPeerType.Group, group.groupPeer.groupId, group.groupPeer.accessHash), randomId)
+    }
+
+    {
+      implicit val clientData = bobClient
+      whenReady(service.handleMessageSetReaction(peer, randomId, "like")) { resp ⇒
+        inside(resp) {
+          case Ok(ResponseReactionsResponse(_, _, reactions)) ⇒
+            reactions.size should ===(1)
+            reactions.head.users should ===(Seq(bob.id))
+            reactions.head.code should ===("like")
+        }
+      }
+    }
+
+    {
+      implicit val clientData = aliceClient
+      expectUpdate(classOf[UpdateReactionsUpdate]) { upd ⇒
+        upd.reactions.head.users should ===(Seq(bob.id))
       }
     }
   }
