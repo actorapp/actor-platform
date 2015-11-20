d04aa1aed1b50ee8cfcf850991b502339dac174b
kor_ka <badgrib@gmmail.com>
2015-11-20 14:52:21 +0300

    feat(android): send location

--- actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/controllers/pickers/map/MapPickerActivity.java
+++ actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/controllers/pickers/map/MapPickerActivity.java
@@ -343,16 +343,6 @@ public class MapPickerActivity extends AppCompatActivity
     private void setUpMap() {
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         for (String provider : locationManager.getAllProviders()) {
-            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for Activity#requestPermissions for more details.
-                return;
-            }
             currentLocation = locationManager.getLastKnownLocation(provider);
             if (currentLocation != null) {
                 break;
