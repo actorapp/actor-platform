07c1a83e492a2a202da84504e527eaec4a663884
John Doe <speed@actor.im>
2015-11-25 19:43:12 +0300

    feat(web): add binding for quick search and update quick search modal.

--- actor-apps/app-web/src/app/actions/QuickSearchActionCreators.js
+++ actor-apps/app-web/src/app/actions/QuickSearchActionCreators.js
@@ -15,6 +15,10 @@ export default {
     dispatch(ActionTypes.QUICK_SEARCH_HIDE);
   },
 
+  setQuickSearchList(list) {
+    dispatch(ActionTypes.QUICK_SEARCH_CHANGED, { list });
+  },
+
   search(query) {
     dispatch(ActionTypes.QUICK_SEARCH, { query });
   }
