7b99fafaaa6745bab0a42c41d6c46e673a73faaa
John Doe <speed@actor.im>
2015-12-03 21:37:44 +0300

    feat+fix+chore(web): fix source maps in sdk, add simple custom login component to sdk example, rename webpack config files and readme, fix dependencies, add eslint task to npm scripts, add repositories to package.json

---
+++ actor-sdk/sdk-web/example/webpack.prod.config.js
@@ -0,0 +1,73 @@
+import path from 'path';
+import webpack from 'webpack';
+
+export default {
+  devtool: 'source-map',
+  entry: {
+    app: ['./src/index.js'],
+    styles: ['./src/styles']
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: './',
+    filename: '[name].js',
+    chunkFilename: '[chunkhash].js',
+    sourceMapFilename: '[name].map'
+  },
+  resolve: {
+    modulesDirectories: ['node_modules'],
+    root: [
+      path.join(__dirname, 'src')
+    ]
+  },
+  module: {
+    preLoaders: [{
+      test: /\.js$/,
+      loaders: ['eslint'],
+      exclude: /(node_modules)/
+    }, {
+      test: /\.js$/,
+      loaders: ['source-map'],
+      include: [path.resolve(__dirname, 'node_modules/actor-sdk')]
+    }],
+    loaders: [{
+      test: /\.(scss|css)$/,
+      loaders: [
+        'style',
+        'css',
+        'autoprefixer?browsers=last 3 versions',
+        'sass?outputStyle=compressed&includePaths[]=' + path.resolve(__dirname, 'node_modules')
+      ]
+    }, {
+      test: /\.js$/,
+      loaders: ['babel'],
+      exclude: /(node_modules)/
+    }, {
+      test: /\.json$/,
+      loaders: ['json']
+    }, {
+      test: /\.(png|mp3|svg)$/,
+      loaders: ['file?name=assets/[name].[ext]']
+    }, {
+      test: /\.(ttf|eot|svg|woff|woff2)$/,
+      loaders: ['file?name=assets/fonts/[name].[ext]']
+    }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.ResolverPlugin([
+      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('package.json', ['main'])
+    ]),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ]
+};
