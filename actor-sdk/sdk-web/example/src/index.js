4cbf0e570a11de9f0cd3b6281689c5671b82928f
Oleg Shilov <me@olegshilov.com>
2015-12-03 21:37:44 +0300

    feat+fix+chore(web): fix source maps in sdk, add simple custom login component to sdk example, rename webpack config files and readme, fix dependencies, add eslint task to npm scripts, add repositories to package.json

--- actor-sdk/sdk-web/example/src/index.js
+++ actor-sdk/sdk-web/example/src/index.js
@@ -2,12 +2,18 @@
  * Copyright (C) 2015 Actor LLC. <https://actor.im>
  */
 
-import { ActorSDK, ActorSDKDelegate } from '../../index';
+import { ActorSDK, ActorSDKDelegate } from 'actor-sdk';
 import config from './app.json';
 
-const delegate = new ActorSDKDelegate();
+import Login from './components/Login.react';
+
 const endpoints = config.endpoints;
 const bugsnagApiKey = config.bugsnag;
 const mixpanelAPIKey = config.mixpanel;
+
+const delegate = new ActorSDKDelegate({
+  loginComponent: Login,
+  sidebarComponent: null
+});
 
 new ActorSDK({endpoints, delegate, bugsnagApiKey, mixpanelAPIKey}).startApp();
