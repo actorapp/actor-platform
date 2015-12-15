ff16b533d89163737c119b3a19c5eb2ed7ddff14
Oleg Shilov <me@olegshilov.com>
2015-12-15 15:19:22 +0300

    Update NotificationsActionCreators.js

--- actor-sdk/sdk-web/src/actions/NotificationsActionCreators.js
+++ actor-sdk/sdk-web/src/actions/NotificationsActionCreators.js
@@ -4,7 +4,7 @@
 
 import { dispatch } from '../dispatcher/ActorAppDispatcher';
 import { ActionTypes } from '../constants/ActorAppConstants';
-import ActorClient form '../utils/ActorClient';
+import ActorClient from '../utils/ActorClient';
 
 const DialogInfoActionCreators = {
   changeNotificationsEnabled(peer, isEnabled) {
