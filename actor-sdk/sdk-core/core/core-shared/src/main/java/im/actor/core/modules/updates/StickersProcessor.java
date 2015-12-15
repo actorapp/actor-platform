4a7abfecfb0c241c3739188008b23e00e0f05f57
kor_ka <badgrib@gmmail.com>
2015-12-16 00:01:39 +0300

    fix(android): save all packs using KeyValueStorage

--- actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/updates/StickersProcessor.java
+++ actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/updates/StickersProcessor.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import im.actor.core.api.ApiStickerCollection;
-import im.actor.core.entity.content.internal.Sticker;
+import im.actor.core.api.updates.UpdateOwnStickersChanged;
 import im.actor.core.entity.content.internal.StickersPack;
 import im.actor.core.modules.AbsModule;
 import im.actor.core.modules.ModuleContext;
@@ -21,55 +21,31 @@ public class StickersProcessor extends AbsModule {
     }
 
     public void onOwnStickerCollectionsChanged(List<ApiStickerCollection> updated) {
-        ArrayList<StickersPack> add = new ArrayList<StickersPack>();
-        ArrayList<StickersPack> remove = new ArrayList<StickersPack>();
-        for (int i = 0; i < stickerPacksList().getCount(); i++) {
-            remove.add(stickerPacksList().getValue(i));
-        }
-        for (ApiStickerCollection pack : updated) {
-            add.add(new StickersPack(pack));
-        }
 
-        boolean needrebuild = false;
-        if (add.size() > 0) {
-            stickerPacksList().addOrUpdateItems(add);
-            stickerPacksKeyValue().addOrUpdateItems(add);
-            needrebuild = true;
-        }
-
-        if (remove.removeAll(add)) {
-            for (StickersPack pack : remove) {
-                stickerPacksList().removeItem(pack.getEngineId());
-                stickerPacksKeyValue().removeItem(pack.getEngineId());
+        stickerPacksStorage().addOrUpdateItem(0, new UpdateOwnStickersChanged(updated).toByteArray());
+        context().getEvents().post(new Event() {
+            @Override
+            public String getType() {
+                return "sticker_collections_changed";
             }
-            needrebuild = true;
-        }
-
-        if (needrebuild) {
-            context().getEvents().post(new Event() {
-                @Override
-                public String getType() {
-                    return "sticker_collections_changed";
-                }
-            });
-        }
+        });
     }
 
-    public void onStickerCollectionsChanged(List<ApiStickerCollection> updated) {
-        ArrayList<StickersPack> add = new ArrayList<StickersPack>();
-        for (ApiStickerCollection pack : updated) {
-            StickersPack p = stickerPacksKeyValue().getValue(pack.getId());
-            if (p != null) {
-                add.add(new StickersPack(pack));
-                stickerPacksKeyValue().addOrUpdateItem(p.updateStickers(pack.getStickers()));
-            }
-        }
-
-        if (add.size() > 0) {
-            stickerPacksList().addOrUpdateItems(add);
-
-        }
-
-
-    }
+//    public void onStickerCollectionsChanged(List<ApiStickerCollection> updated) {
+//        ArrayList<StickersPack> add = new ArrayList<StickersPack>();
+//        for (ApiStickerCollection pack : updated) {
+//            StickersPack p = stickerPacksStorage().getValue(pack.getId());
+//            if (p != null) {
+//                add.add(new StickersPack(pack));
+//                stickerPacksStorage().addOrUpdateItem(p.updateStickers(pack.getStickers()));
+//            }
+//        }
+//
+//        if (add.size() > 0) {
+//            stickerPacksList().addOrUpdateItems(add);
+//
+//        }
+//
+//
+//    }
 }
