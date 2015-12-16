79a28e89e2508a5bb82806fbaaadd95bdebbefb1
Oleg Shilov <me@olegshilov.com>
2015-12-16 21:30:07 +0300

    chore(web): add custom text message component to example

--- actor-sdk/sdk-web/example/src/index.js
+++ actor-sdk/sdk-web/example/src/index.js
@@ -5,6 +5,7 @@
 import { ActorSDK, ActorSDKDelegate } from 'actor-sdk';
 
 import ToolbarSection from './components/ToolbarSection.react';
+import TextMessage from './components/dialog/messages/TextMessage.react';
 
 const endpoints = [
   'wss://front1-ws-mtproto-api-rev2.actor.im',
@@ -15,13 +16,17 @@ const bugsnagApiKey = 'cd24ee53326e06669a36c637b29660c3';
 
 const components = {
   dialog: {
-    toolbar: ToolbarSection
+    toolbar: ToolbarSection,
+    messages: {
+      text: TextMessage
+    }
   }
 };
 
-const actions = {}
+const actions = {};
+const l18n = {};
 
-const delegate = new ActorSDKDelegate(components, actions);
+const delegate = new ActorSDKDelegate(components, actions, l18n);
 
 const app = new ActorSDK({endpoints, delegate, bugsnagApiKey, mixpanelAPIKey});
 app.startApp();
