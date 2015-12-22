79b44cada791e08ecee7bed13d6cdf879dd86f85
Steve Kite <steve@actor.im>
2015-12-22 22:30:35 +0300

    feat(android+core): Fixing localizations

--- actor-sdk/sdk-core/runtime/runtime-android/src/main/java/im/actor/runtime/android/AndroidLocaleProvider.java
+++ actor-sdk/sdk-core/runtime/runtime-android/src/main/java/im/actor/runtime/android/AndroidLocaleProvider.java
@@ -2,16 +2,21 @@ package im.actor.runtime.android;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 import im.actor.runtime.LocaleRuntime;
 
-/**
- * Created by ex3ndr on 10.08.15.
- */
 public class AndroidLocaleProvider implements LocaleRuntime {
 
     @Override
     public String getCurrentLocale() {
+        Locale current = AndroidContext.getContext().getResources().getConfiguration().locale;
+        if (current != null) {
+            String res = current.getLanguage();
+            if (res != null) {
+                return res.substring(0, 1).toUpperCase() + res.substring(1, 2).toLowerCase();
+            }
+        }
         return null;
     }
 
