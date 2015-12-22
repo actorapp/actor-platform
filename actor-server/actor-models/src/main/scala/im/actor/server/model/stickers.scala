6f84e1319933b8d4107bfa6b10d1fc9b5986efea
Nikolay Tatarinov <rockjam@actor.im>
2015-12-22 05:02:30 +0300

    fix(server:stickers): pass size of stickers everywhere

--- actor-server/actor-models/src/main/scala/im/actor/server/model/stickers.scala
+++ actor-server/actor-models/src/main/scala/im/actor/server/model/stickers.scala
@@ -7,12 +7,18 @@ case class StickerData(
   image128FileId:   Long,
   image128FileHash: Long,
   image128FileSize: Long,
+  image128Width:    Int,
+  image128Height:   Int,
   image256FileId:   Option[Long],
   image256FileHash: Option[Long],
   image256FileSize: Option[Long],
+  image256Width:    Option[Int],
+  image256Height:   Option[Int],
   image512FileId:   Option[Long],
   image512FileHash: Option[Long],
-  image512FileSize: Option[Long]
+  image512FileSize: Option[Long],
+  image512Width:    Option[Int],
+  image512Height:   Option[Int]
 )
 
 case class StickerPack(id: Int, accessSalt: String, ownerUserId: Int, isDefault: Boolean)
