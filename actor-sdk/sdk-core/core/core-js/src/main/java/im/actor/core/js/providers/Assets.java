d1ad26b87bf2a002fe357671b05c7cafd05da870
Steve Kite <steve@actor.im>
2015-12-24 02:55:17 +0300

    fix(js): Trying to fix i18n

--- actor-sdk/sdk-core/core/core-js/src/main/java/im/actor/core/js/providers/Assets.java
+++ actor-sdk/sdk-core/core/core-js/src/main/java/im/actor/core/js/providers/Assets.java
@@ -11,6 +11,36 @@ public interface Assets extends ClientBundleWithLookup {
     @Source("AppText.properties")
     TextResource AppText_properties();
 
+    @Source("AppText_Ar.properties")
+    TextResource AppText_Ar_properties();
+
+    @Source("AppText_Cn.properties")
+    TextResource AppText_Cn_properties();
+
+    @Source("AppText_Es.properties")
+    TextResource AppText_Es_properties();
+
+    @Source("AppText_Pt.properties")
+    TextResource AppText_Pt_properties();
+
+    @Source("AppText_Ru.properties")
+    TextResource AppText_Ru_properties();
+
     @Source("Months.properties")
     TextResource Months_properties();
+
+    @Source("Months_Ar.properties")
+    TextResource Months_Ar_properties();
+
+    @Source("Months_Cn.properties")
+    TextResource Months_Cn_properties();
+
+    @Source("Months_Es.properties")
+    TextResource Months_Es_properties();
+
+    @Source("Months_Pt.properties")
+    TextResource Months_Pt_properties();
+
+    @Source("Months_Ru.properties")
+    TextResource Months_Ru_properties();
 }
