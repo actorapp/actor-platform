5856475893e348ae1e4fd39f600ff1ca3c8ed1d2
Nikolay Tatarinov <rockjam@actor.im>
2015-12-24 23:26:38 +0300

    fix(server:tests): use /tmp as local file storage for tests.

--- actor-server/actor-bots/src/main/scala/im/actor/server/bot/BotExtension.scala
+++ actor-server/actor-bots/src/main/scala/im/actor/server/bot/BotExtension.scala
@@ -59,7 +59,7 @@ private[bot] final class BotExtension(_system: ActorSystem) extends Extension {
     shardakka.simpleKeyValue(BotExtension.whUserTokensKV(userId))
 
   HttpApi(system).registerHook("bots") { implicit system ⇒
-    Future.successful(new BotsHttpHandler().routes)
+    Future.successful(new BotsHttpHandler(this).routes)
   }
 
   /**
