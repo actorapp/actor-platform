a11ea2521ad26e7b4dbe6e659416d491a233ecca
prettynatty <fear@loathing.in>
2015-12-25 05:46:38 +0300

    feat(server): password auth

--- actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/auth/AuthErrors.scala
+++ actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/auth/AuthErrors.scala
@@ -1,6 +1,7 @@
 package im.actor.server.api.rpc.service.auth
 
 import im.actor.api.rpc.RpcError
+import im.actor.server.acl.ACLUtils
 import im.actor.server.activation.{ BadRequest, SendFailure, CodeFailure }
 
 object AuthErrors {
@@ -15,12 +16,16 @@ object AuthErrors {
   val EmailUnoccupied = RpcError(400, "EMAIL_UNOCCUPIED", "", false, None)
   val EmailCodeExpired = RpcError(400, "EMAIL_CODE_EXPIRED", "Code is expired.", false, None)
   val EmailCodeInvalid = RpcError(400, "EMAIL_CODE_INVALID", "Invalid code.", false, None)
+  val UsernameCodeExpired = RpcError(400, "USERNAME_CODE_EXPIRED", "Codeode is expired.", false, None)
+  val UsernameCodeInvalid = RpcError(400, "USERNAME_CODE_INVALID", "Invalid code.", false, None)
+  val UsernameUnoccupied = RpcError(400, "USERNAME_UNOCCUPIED", "", false, None)
   val RedirectUrlInvalid = RpcError(400, "REDIRECT_URL_INVALID", "", false, None)
   val NotValidated = RpcError(400, "NOT_VALIDATED", "", false, None) //todo: proper name
   val FailedToGetOAuth2Token = RpcError(400, "FAILED_GET_OAUTH2_TOKEN", "Authorization server error.", false, None)
   val OAuthUserIdDoesNotMatch = RpcError(400, "WRONG_OAUTH2_USER_ID", "Email does not match one provided on the first step.", false, None)
   val ActivationServiceError = RpcError(500, "ACTIVATION_SERVICE_ERROR", "Error occured in activation service. Try again later.", true, None)
   val InvalidAuthCodeHash = RpcError(400, "CODE_HASH_INVALID", "", false, None)
+  val PasswordInvalid = RpcError(400, "PASSWORD_INVALID", s"Password have to be more than ${ACLUtils.PasswordMinLength} and less than ${ACLUtils.PasswordMaxLength}", false, None)
 
   def activationFailure(failure: CodeFailure) = failure match {
     case SendFailure(message) ⇒ RpcError(500, "GATE_ERROR", message, true, None)
