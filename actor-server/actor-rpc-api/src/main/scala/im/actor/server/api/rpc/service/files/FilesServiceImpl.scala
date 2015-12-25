7229ec1c59a22bc5109570f4d6284f573b0d574c
prettynatty <fear@loathing.in>
2015-12-25 16:45:46 +0300

    feat(server:files): GetFileUrls handler

--- actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/files/FilesServiceImpl.scala
+++ actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/files/FilesServiceImpl.scala
@@ -1,18 +1,22 @@
 package im.actor.server.api.rpc.service.files
 
 import akka.actor._
+import cats.data.Xor
+import cats.std.all._
+import cats.syntax.traverse._
 import im.actor.api.rpc.FileHelpers.Errors
 import im.actor.api.rpc.files._
 import im.actor.api.rpc._
+import im.actor.concurrent.FutureExt
 import im.actor.server.acl.ACLUtils
 import im.actor.server.db.DbExtension
 import im.actor.server.file._
 import im.actor.server.persist
+import im.actor.server.persist.FileRepo
 import slick.driver.PostgresDriver.api._
 
 import scala.concurrent.duration._
 import scala.concurrent.{ ExecutionContext, Future }
-import scala.languageFeature.implicitConversions
 
 class FilesServiceImpl(implicit actorSystem: ActorSystem) extends FilesService {
 
@@ -25,11 +29,30 @@ class FilesServiceImpl(implicit actorSystem: ActorSystem) extends FilesService {
 
   override def jhandleGetFileUrl(location: ApiFileLocation, clientData: ClientData): Future[HandlerResult[ResponseGetFileUrl]] =
     authorized(clientData) { client ⇒
-      val timeout = 1.day
       (for {
         file ← fromFutureOption(Errors.LocationInvalid)(db.run(persist.FileRepo.find(location.fileId)))
         url ← fromFutureOption(Errors.LocationInvalid)(fsAdapter.getFileDownloadUrl(file, location.accessHash))
-      } yield ResponseGetFileUrl(url, timeout.toSeconds.toInt)).value map (_.toScalaz)
+      } yield ResponseGetFileUrl(url, FileStorageAdapter.UrlExpirationTimeout.toSeconds.toInt)).value map (_.toScalaz)
+    }
+
+  override def jhandleGetFileUrls(files: IndexedSeq[ApiFileLocation], clientData: ClientData): Future[HandlerResult[ResponseGetFileUrls]] =
+    authorized(clientData) { client ⇒
+      val idsHashes = files.map(fl ⇒ fl.fileId → fl.accessHash).toMap
+      (for {
+        models ← fromFuture(db.run(FileRepo.fetch(idsHashes.keySet)))
+        attempts ← fromFuture(FutureExt.ftraverse(models) { model ⇒
+          val accessHash = idsHashes.getOrElse(model.id, throw new RuntimeException("Db returned file"))
+          (for {
+            url ← fromFutureOption(Errors.LocationInvalid)(fsAdapter.getFileDownloadUrl(model, accessHash))
+          } yield ApiFileUrlDescription(model.id, url, FileStorageAdapter.UrlExpirationTimeout.toSeconds.toInt)).value
+        })
+        // FIXME: fail-fast here
+        urlDescs ← fromEither((e: RpcError) ⇒ e)(attempts.foldLeft(Xor.Right(Nil): Xor[RpcError, List[ApiFileUrlDescription]]) {
+          case (Xor.Right(acc), Xor.Right(fd)) ⇒ Xor.Right(fd :: acc)
+          case (l: Xor.Left[_], _)             ⇒ l
+          case (_, l: Xor.Left[_])             ⇒ l
+        })
+      } yield ResponseGetFileUrls(urlDescs.toVector)).value map (_.toScalaz)
     }
 
   override def jhandleGetFileUploadUrl(expectedSize: Int, clientData: ClientData): Future[HandlerResult[ResponseGetFileUploadUrl]] =
@@ -61,7 +84,4 @@ class FilesServiceImpl(implicit actorSystem: ActorSystem) extends FilesService {
         _ ← fromFuture(db.run(persist.FileRepo.setUploaded(file.id, fileName)))
       } yield ResponseCommitFileUpload(ApiFileLocation(file.id, ACLUtils.fileAccessHash(file.id, file.accessSalt)))).value map (_.toScalaz)
     }
-
-  override def jhandleGetFileUrls(files: IndexedSeq[ApiFileLocation], clientData: ClientData): Future[HandlerResult[ResponseGetFileUrls]] =
-    Future.failed(new RuntimeException("Not implemented"))
 }
