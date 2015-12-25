3b9284b71df644a4c8147cca7c12e189d9787dd2
John Doe <speed@actor.im>
2015-12-25 19:45:51 +0300

    feat+chore(web): add isContact helper function to UserStore, remove deprecated components.

--- actor-sdk/sdk-web/src/stores/UserStore.js
+++ actor-sdk/sdk-web/src/stores/UserStore.js
@@ -5,12 +5,33 @@
 import ActorClient from '../utils/ActorClient';
 
 const UserStore = {
+  /**
+   * Get user information
+   *
+   * @param uid {number} User id
+   * @returns {object} User information
+   */
   getUser(uid) {
     return ActorClient.getUser(uid);
   },
 
+  /**
+   * Get current user id
+   *
+   * @returns {number} User id
+   */
   getMyId() {
     return ActorClient.getUid();
+  },
+
+  /**
+   * Returns true if user is in contact
+   *
+   * @param uid {number} User id
+   * @returns {boolean}
+   */
+  isContact(uid) {
+    return this.getUser(uid).isContact
   }
 };
 
