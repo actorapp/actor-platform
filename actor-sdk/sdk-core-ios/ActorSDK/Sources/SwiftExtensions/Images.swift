fb7c47782b6b04049d050453d3d1391e32042225
iOS Dev <pro@pro.com>
2015-12-28 18:44:32 +0300

    fix(iOS): fix date background in media cell

--- actor-sdk/sdk-core-ios/ActorSDK/Sources/SwiftExtensions/Images.swift
+++ actor-sdk/sdk-core-ios/ActorSDK/Sources/SwiftExtensions/Images.swift
@@ -117,6 +117,25 @@ public extension UIImage {
         UIGraphicsEndImageContext()
         return image;
     }
+    
+    func imageWithColor(color1: UIColor) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        
+        let context  = UIGraphicsGetCurrentContext()
+        CGContextTranslateCTM(context, 0, self.size.height)
+        CGContextScaleCTM(context, 1.0, -1.0);
+        CGContextSetBlendMode(context, CGBlendMode.Normal)
+        
+        let rect     = CGRectMake(0, 0, self.size.width, self.size.height) as CGRect
+        CGContextClipToMask(context, rect, self.CGImage)
+        color1.setFill()
+        CGContextFillRect(context, rect)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext() as UIImage
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
 }
 
 public class Imaging {
