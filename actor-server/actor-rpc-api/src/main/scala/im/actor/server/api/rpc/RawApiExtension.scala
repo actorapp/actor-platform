7f30cfac7e52ae8389c9721c21f8ff60294bf277
Nikolay Tatarinov <rockjam@actor.im>
2015-12-29 04:31:27 +0300

    feat(server:rpc): raw service + spec

--- actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/RawApiExtension.scala
+++ actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/RawApiExtension.scala
@@ -2,34 +2,37 @@ package im.actor.server.api.rpc
 
 import akka.actor._
 import cats.data.Xor
+import im.actor.api.rpc.collections.ApiRawValue
+import im.actor.api.rpc.FutureResultRpcCats
 import im.actor.api.rpc.{ AuthorizedClientData, CommonErrors, RpcError }
-import im.actor.rpc.raw.RawServiceHandler
+import im.actor.rpc.raw.RawApiService
 
 import scala.collection.concurrent.TrieMap
+import scala.concurrent.Future
 
 sealed trait RawApiExtension extends Extension
 
 private[rpc] final class RawApiExtensionImpl(system: ExtendedActorSystem) extends RawApiExtension {
+  import FutureResultRpcCats._
+  import system.dispatcher
 
-  private val services = TrieMap.empty[String, RawServiceHandler]
+  private val services = TrieMap.empty[String, RawApiService]
 
-  def register(name: String, clazz: Class[_ <: RawServiceHandler]): Unit = {
-    val service = system.dynamicAccess.createInstanceFor[RawServiceHandler](clazz, List(classOf[ActorSystem] → system)).get
+  def register(name: String, clazz: Class[_ <: RawApiService]): Unit = {
+    val service = system.dynamicAccess.createInstanceFor[RawApiService](clazz, List(classOf[ActorSystem] → system)).get
     register(name, service)
   }
 
-  def register(name: String, service: RawServiceHandler): Unit = services.putIfAbsent(name, service)
+  def register(name: String, service: RawApiService): Unit = services.putIfAbsent(name, service)
 
-  def register(serviceSeq: Seq[(String, RawServiceHandler)]): Unit = services ++= serviceSeq
+  def register(serviceSeq: Seq[(String, RawApiService)]): Unit = services ++= serviceSeq
 
-  def getHandlingFunction(service: String, method: String)(implicit client: AuthorizedClientData): RpcError Xor RawServiceHandler#RawAuthorizedFunction = {
-    val optFunction: Option[RawServiceHandler#RawAuthorizedFunction] =
-      for {
-        handler ← services.get(service)
-        rawFunc ← handler.handle.lift(method)
-      } yield rawFunc(client)
-    Xor.fromOption(optFunction, CommonErrors.UnsupportedRequest)
-  }
+  def handle(service: String, method: String, params: Option[ApiRawValue])(implicit client: AuthorizedClientData): Future[RpcError Xor ApiRawValue] =
+    (for {
+      serviceHandler ← fromOption(CommonErrors.UnsupportedRequest)(services.get(service))
+      response ← fromOption(CommonErrors.UnsupportedRequest)(serviceHandler.handleRequests(client)(params).lift(method))
+      result ← fromFutureEither(response)
+    } yield result).value
 }
 
 object RawApiExtension extends ExtensionId[RawApiExtensionImpl] with ExtensionIdProvider {
