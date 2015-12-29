cf8476302f53b21ca86b5fce6e18f1d2a17ff42f
John Doe <speed@actor.im>
2015-12-29 13:29:53 +0300

    fix(web): fix isExperimental flag setup

--- actor-sdk/sdk-web/src/components/dialog/messages/MessageItem.react.js
+++ actor-sdk/sdk-web/src/components/dialog/messages/MessageItem.react.js
@@ -52,7 +52,7 @@ class MessageItem extends Component {
 
   static contextTypes = {
     delegate: PropTypes.object,
-    isExperemental: PropTypes.bool
+    isExperimental: PropTypes.bool
   };
 
   constructor(props) {
@@ -114,7 +114,7 @@ class MessageItem extends Component {
   render() {
     const { message, isSameSender, onVisibilityChange, peer, isThisLastMessage, isSelected } = this.props;
     const { isThisMyMessage, isActionsShown } = this.state;
-    const { delegate, isExperemental } = this.context;
+    const { delegate, isExperimental } = this.context;
 
     let Service, Text, Modern, Photo, Document, Voice, Contact, Location, Sticker;
     if (delegate.components.dialog !== null && delegate.components.dialog.messages) {
@@ -260,7 +260,7 @@ class MessageItem extends Component {
           </div>
 
           {
-             isExperemental
+            isExperimental
               ? <div className="message__actions__selector" onClick={this.toggleMessageSelection}>
                   <i className="icon material-icons">check</i>
                 </div>
