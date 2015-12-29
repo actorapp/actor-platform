8ab25f6e69c15f027d4229da977b33660c5bb4aa
Gleb Putintsev <kor_ka@actor.im>
2015-12-29 18:50:14 +0300

    feat(core+android): custom Json message with content description

--- actor-sdk/sdk-core-android/android-app/src/main/java/im/actor/Application.java
+++ actor-sdk/sdk-core-android/android-app/src/main/java/im/actor/Application.java
@@ -1,29 +1,37 @@
 package im.actor;
 
 import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 
-import im.actor.core.api.ApiMessage;
-import im.actor.core.api.ApiTextMessage;
+import im.actor.core.entity.Peer;
 import im.actor.core.entity.content.AbsContent;
 import im.actor.core.entity.content.ContentConverter;
-import im.actor.core.entity.content.TextContent;
+import im.actor.core.entity.content.JsonContent;
 import im.actor.core.entity.content.internal.AbsContentContainer;
 import im.actor.core.entity.content.internal.ContentRemoteContainer;
+import im.actor.runtime.json.JSONException;
 import im.actor.sdk.ActorSDK;
 import im.actor.sdk.ActorSDKApplication;
 import im.actor.sdk.ActorStyle;
 import im.actor.sdk.BaseActorSDKDelegate;
+import im.actor.sdk.controllers.conversation.messages.BaseCustomHolder;
 import im.actor.sdk.controllers.conversation.messages.MessageHolder;
 import im.actor.sdk.controllers.conversation.messages.MessagesAdapter;
+import im.actor.sdk.controllers.conversation.messages.MessagesFragment;
 import im.actor.sdk.controllers.fragment.settings.ActorSettingsCategory;
 import im.actor.sdk.controllers.fragment.settings.ActorSettingsField;
+import im.actor.sdk.controllers.fragment.settings.BaseActorChatActivity;
 import im.actor.sdk.controllers.fragment.settings.BaseActorSettingsActivity;
 import im.actor.sdk.controllers.fragment.settings.BaseActorSettingsFragment;
+import im.actor.sdk.intents.ActorIntent;
 import im.actor.sdk.intents.ActorIntentFragmentActivity;
+
+import static im.actor.sdk.util.ActorSDKMessenger.messenger;
 
 public class Application extends ActorSDKApplication {
 
@@ -41,40 +49,104 @@ public class Application extends ActorSDKApplication {
         AbsContent.registerConverter(new ContentConverter() {
             @Override
             public AbsContent convert(AbsContentContainer container) {
-                if (container instanceof ContentRemoteContainer) {
-                    ApiMessage msg = ((ContentRemoteContainer) container).getMessage();
-                    if (msg instanceof ApiTextMessage) {
-                        return new TextContent((ContentRemoteContainer) container);
-                    }
-                }
-                return null;
+                return JsonContent.convert(container, Custom.class);
             }
 
             @Override
             public Class destinationType() {
-                return TextExContent.class;
+                return Custom.class;
+            }
+        });
+        AbsContent.registerConverter(new ContentConverter() {
+            @Override
+            public AbsContent convert(AbsContentContainer container) {
+                return JsonContent.convert(container, CustomTwo.class);
+            }
+
+            @Override
+            public Class destinationType() {
+                return CustomTwo.class;
             }
         });
     }
 
-    public class TextExContent extends TextContent {
+    public static class Custom extends JsonContent {
 
-        public TextExContent(ContentRemoteContainer remoteContainer) {
-            super(remoteContainer);
+        public String getText() {
+            return getSimpleStringData();
+        }
+
+        @Override
+        public String getDataType() {
+            return "customJson";
+        }
+
+        @Override
+        public String getContentDescriptionEn() {
+            return "custom json msg";
+        }
+
+        @Override
+        public String getContentDescriptionRu() {
+            return " бла бла бла";
         }
     }
+
+    public static class CustomTwo extends JsonContent {
+
+        public String getText() {
+            return getSimpleStringData();
+        }
+
+        @Override
+        public String getDataType() {
+            return "customTwoJson";
+        }
+
+        @Override
+        public String getContentDescriptionEn() {
+            return "custom json msg";
+        }
+
+        @Override
+        public String getContentDescriptionRu() {
+            return " бла бла бла";
+        }
+    }
+
+
 
     private class ActorSDKDelegate extends BaseActorSDKDelegate {
 
         @Override
-        public MessageHolder getCustomMessageViewHolder(Class<AbsContent> content, MessagesAdapter messagesAdapter, ViewGroup viewGroup) {
-            if (content.isAssignableFrom(TextExContent.class)) {
-                return new TextExHolder(messagesAdapter, LayoutInflater
-                        .from(viewGroup.getContext())
-                        .inflate(R.layout.adapter_dialog_text, viewGroup, false));
+        public ActorIntent getChatIntent() {
+            return new BaseActorChatActivity() {
+                @Override
+                public MessagesFragment getChatFragment(Peer peer) {
+                    return new MessagesFragment(peer) {
+                        @Override
+                        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//                            messenger().sendCustomJsonMessage(getPeer(), JsonContent.create(Custom.class, System.currentTimeMillis()+"One"));
+//                            messenger().sendCustomJsonMessage(getPeer(), JsonContent.create(CustomTwo.class, System.currentTimeMillis()+"Two"));
+//                            messenger().sendCustomJsonMessage(getPeer(), JsonContent.create(CustomTwo.class, System.currentTimeMillis()+"Two"));
+                            return super.onCreateView(inflater, container, savedInstanceState);
+                        }
+                    };
+                }
+            };
+        }
+
+        @Override
+        public BaseCustomHolder getCustomMessageViewHolder(Class<AbsContent> content, MessagesAdapter messagesAdapter, ViewGroup viewGroup) {
+            if (content.equals(Custom.class)) {
+                return new CustomHolder(messagesAdapter, viewGroup, R.layout.custom_holder, false);
+            } else if (content.equals(CustomTwo.class)) {
+                return new CustomTwoHolder(messagesAdapter, viewGroup, R.layout.custom_holder, false);
+
             } else {
                 return null;
             }
+
         }
 
         @Override
