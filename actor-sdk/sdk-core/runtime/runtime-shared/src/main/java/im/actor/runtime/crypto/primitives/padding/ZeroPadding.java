c0bb6829f38f822d7532359d0379b2339970882d
Steve Kite <steve@actor.im>
2016-01-07 16:43:48 +0300

    feat(runtime): Adding crypto to runtime

---
+++ actor-sdk/sdk-core/runtime/runtime-shared/src/main/java/im/actor/runtime/crypto/primitives/padding/ZeroPadding.java
@@ -0,0 +1,27 @@
+package im.actor.runtime.crypto.primitives.padding;
+
+import im.actor.runtime.crypto.primitives.Padding;
+
+/**
+ * Padding with zero bytes
+ *
+ * @author Steve Kite (steve@actor.im)
+ */
+public class ZeroPadding implements Padding {
+
+    @Override
+    public void padding(byte[] src, int offset, int length) {
+        for (int i = 0; i < length; i++) {
+            src[i + offset] = 0;
+        }
+    }
+
+    @Override
+    public boolean validate(byte[] src, int offset, int length) {
+        boolean isOk = true;
+        for (int i = 0; i < length; i++) {
+            isOk &= src[i + offset] == 0;
+        }
+        return isOk;
+    }
+}
