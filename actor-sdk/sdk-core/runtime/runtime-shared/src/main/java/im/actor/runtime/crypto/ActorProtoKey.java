425c4151fd143daba86e5fe1707bd1ec3c7d8315
Steve Kite <steve@actor.im>
2016-01-07 16:43:48 +0300

    feat(runtime): Adding crypto to runtime

---
+++ actor-sdk/sdk-core/runtime/runtime-shared/src/main/java/im/actor/runtime/crypto/ActorProtoKey.java
@@ -0,0 +1,37 @@
+package im.actor.runtime.crypto;
+
+import im.actor.runtime.crypto.primitives.util.ByteStrings;
+
+/**
+ * Actor's MTProto V2 keys
+ */
+public class ActorProtoKey {
+
+    private byte[] clientMacKey;
+    private byte[] serverMacKey;
+    private byte[] clientKey;
+    private byte[] serverKey;
+
+    public ActorProtoKey(byte[] masterKey) {
+        clientMacKey = ByteStrings.substring(masterKey, 0, 32);
+        serverMacKey = ByteStrings.substring(masterKey, 32, 32);
+        clientKey = ByteStrings.substring(masterKey, 64, 32);
+        serverKey = ByteStrings.substring(masterKey, 96, 32);
+    }
+
+    public byte[] getClientMacKey() {
+        return clientMacKey;
+    }
+
+    public byte[] getServerMacKey() {
+        return serverMacKey;
+    }
+
+    public byte[] getClientKey() {
+        return clientKey;
+    }
+
+    public byte[] getServerKey() {
+        return serverKey;
+    }
+}
