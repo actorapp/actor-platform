5a8fc90c519a92341a8a118c799689b14a37a3d7
Steve Kite <steve@actor.im>
2016-01-07 16:43:48 +0300

    feat(runtime): Adding crypto to runtime

---
+++ actor-sdk/sdk-core/runtime/runtime-shared/src/main/java/im/actor/runtime/crypto/primitives/digest/SHA512.java
@@ -0,0 +1,28 @@
+package im.actor.runtime.crypto.primitives.digest;
+
+import im.actor.runtime.crypto.primitives.Digest;
+
+public class SHA512 implements Digest {
+
+    private SHA512Digest sha512Digest = new SHA512Digest();
+
+    @Override
+    public void reset() {
+        sha512Digest.reset();
+    }
+
+    @Override
+    public void update(byte[] src, int offset, int length) {
+        sha512Digest.update(src, offset, length);
+    }
+
+    @Override
+    public void doFinal(byte[] dest, int destOffset) {
+        sha512Digest.doFinal(dest, destOffset);
+    }
+
+    @Override
+    public int getDigestSize() {
+        return sha512Digest.getDigestSize();
+    }
+}
---
+++ actor-sdk/sdk-core/runtime/runtime-shared/src/main/java/im/actor/runtime/crypto/primitives/hmac/HMAC.java
@@ -0,0 +1,52 @@
+package im.actor.runtime.crypto.primitives.hmac;
+
+import im.actor.runtime.crypto.primitives.Digest;
+import im.actor.runtime.crypto.primitives.util.ByteStrings;
+
+public class HMAC {
+
+    private Digest digest;
+
+    public HMAC(Digest digest) {
+        this.digest = digest;
+    }
+
+    public void calculate(byte[] secret, byte[] message, int offset, int length, byte[] dest, int destOffset) {
+        byte[] fixedSecret = new byte[digest.getDigestSize()];
+        if (secret.length > digest.getDigestSize()) {
+            digest.reset();
+            digest.update(secret, 0, secret.length);
+            digest.doFinal(fixedSecret, 0);
+        } else if (secret.length < digest.getDigestSize()) {
+            ByteStrings.write(fixedSecret, 0, secret, 0, secret.length);
+            for (int i = secret.length; i < fixedSecret.length; i++) {
+                fixedSecret[i] = 0;
+            }
+        } else {
+            fixedSecret = secret;
+        }
+
+        // Paddings
+        byte[] outerKeyPad = new byte[digest.getDigestSize()];
+        byte[] innerKeyPad = new byte[digest.getDigestSize()];
+        for (int i = 0; i < outerKeyPad.length; i++) {
+            outerKeyPad[i] = (byte) (0x5c ^ fixedSecret[i]);
+            innerKeyPad[i] = (byte) (0x36 ^ fixedSecret[i]);
+        }
+
+        // Inner digest
+        // digest(i_key_pad ∥ message)
+        byte[] innnerHash = new byte[digest.getDigestSize()];
+        digest.reset();
+        digest.update(innerKeyPad, 0, innerKeyPad.length);
+        digest.update(message, 0, message.length);
+        digest.doFinal(innnerHash, 0);
+
+        // Outer digest
+        // digest(o_key_pad ∥ digest(i_key_pad ∥ message))
+        digest.reset();
+        digest.update(outerKeyPad, 0, outerKeyPad.length);
+        digest.update(innnerHash, 0, innnerHash.length);
+        digest.doFinal(dest, destOffset);
+    }
+}
