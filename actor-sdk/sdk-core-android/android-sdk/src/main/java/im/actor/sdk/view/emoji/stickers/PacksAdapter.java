769a0e25a42f7df20cb6c35df3664177d1d6f249
Gleb Putintsev <kor_ka@actor.im>
2016-01-07 16:41:21 +0300

    chore(android): stickers sync refactor

--- actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/view/emoji/stickers/PacksAdapter.java
+++ actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/view/emoji/stickers/PacksAdapter.java
@@ -1,15 +1,18 @@
 package im.actor.sdk.view.emoji.stickers;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.GridLayoutManager;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import im.actor.core.api.ApiStickerDescriptor;
 import im.actor.core.entity.content.internal.Sticker;
 import im.actor.core.entity.content.internal.StickersPack;
+import im.actor.runtime.Log;
 import im.actor.runtime.android.view.BindedListAdapter;
 import im.actor.runtime.android.view.BindedViewHolder;
 import im.actor.runtime.generic.mvvm.BindedDisplayList;
@@ -21,12 +24,15 @@ public class PacksAdapter extends BindedListAdapter<StickersPack, PacksAdapter.S
 
     private Context context;
     private StickersAdapter stickersAdapter;
+    LinearLayout packsSwitchContainer;
+    int selectedPostion = -1;
 
-    public PacksAdapter(BindedDisplayList<StickersPack> displayList, Context context, StickersAdapter stickersAdapter) {
+    public PacksAdapter(BindedDisplayList<StickersPack> displayList, Context context, StickersAdapter stickersAdapter, LinearLayout stickerIndicatorContainer) {
         super(displayList);
+        this.packsSwitchContainer = stickerIndicatorContainer;
         this.context = context;
         this.stickersAdapter = stickersAdapter;
-
+        stickersAdapter.setPacksAdapter(this);
     }
 
     @Override
@@ -37,7 +43,7 @@ public class PacksAdapter extends BindedListAdapter<StickersPack, PacksAdapter.S
 
     @Override
     public void onBindViewHolder(StickerHolder holder, int index, StickersPack item) {
-        holder.bind(item);
+        holder.bind(item, index);
     }
 
     @Override
@@ -50,30 +56,39 @@ public class PacksAdapter extends BindedListAdapter<StickersPack, PacksAdapter.S
         private StickerView sv;
         private StickersPack sp;
         private Sticker s;
+        private int position;
+        FrameLayout fl;
 
         public StickerHolder(Context context, FrameLayout fl) {
             super(fl);
+            this.fl = fl;
             sv = new StickerView(context);
             int padding = Screen.dp(2);
             sv.setPadding(padding, padding, padding, padding);
-            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(Screen.dp(20), Screen.dp(20), Gravity.CENTER);
+            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(packsSwitchContainer.getHeight(), packsSwitchContainer.getHeight(), Gravity.CENTER);
             fl.addView(sv, params);
             fl.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     if (sp != null) {
-                        stickersAdapter.scrollToSticker(new Sticker(null, 0, s.getLocalCollectionId(), 0, true));
+                        selectedPostion = position;
+                        stickersAdapter.scrollToSticker(new Sticker(null, 0, sp.getLocalId(), 0, true));
                     }
                 }
             });
         }
 
 
-        public void bind(StickersPack sp) {
+        public void bind(StickersPack sp, int position) {
+            this.position = position;
             this.sp = sp;
             s = sp.getStickers().get(0);
             sv.bind(s, StickerView.STICKER_SMALL);
-
+            if (selectedPostion == position) {
+                fl.setBackgroundColor(Color.LTGRAY);
+            } else {
+                fl.setBackgroundColor(Color.TRANSPARENT);
+            }
         }
 
         public void unbind() {
@@ -81,6 +96,17 @@ public class PacksAdapter extends BindedListAdapter<StickersPack, PacksAdapter.S
         }
 
 
+    }
+
+    public void selectPack(int localPackId) {
+        for (int i = 0; i < getItemCount(); i++) {
+            StickersPack p = getItem(i);
+            if (p.getLocalId() == localPackId) {
+                selectedPostion = i;
+                notifyDataSetChanged();
+                break;
+            }
+        }
     }
 
 
