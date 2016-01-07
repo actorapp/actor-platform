58607c3e05c5152aa4b626534f8e61eb60413491
Steve Kite <steve@actor.im>
2016-01-07 16:43:48 +0300

    feat(runtime): Adding crypto to runtime

---
+++ actor-sdk/sdk-core/runtime/runtime-shared/src/main/java/im/actor/runtime/crypto/Curve25519.java
@@ -0,0 +1,113 @@
+package im.actor.runtime.crypto;
+
+import java.security.SecureRandom;
+
+import im.actor.runtime.crypto.primitives.curve25519.Sha512;
+import im.actor.runtime.crypto.primitives.curve25519.curve_sigs;
+import im.actor.runtime.crypto.primitives.curve25519.scalarmult;
+import im.actor.runtime.crypto.primitives.digest.SHA256;
+import im.actor.runtime.crypto.primitives.digest.SHA512;
+
+public class Curve25519 {
+
+    private SecureRandom random = new SecureRandom();
+
+    /**
+     * Generating KeyPair
+     *
+     * @return generated key pair
+     */
+    public synchronized Curve25519KeyPair keyGen() {
+        byte[] privateKey = keyGenPrivate();
+        byte[] publicKey = keyGenPublic(privateKey);
+        return new Curve25519KeyPair(publicKey, privateKey);
+    }
+
+    /**
+     * Generating private key. Source: https://cr.yp.to/ecdh.html
+     *
+     * @return generated private key
+     */
+    public synchronized byte[] keyGenPrivate() {
+
+        // Hashing Random Bytes instead of using random bytes directly
+        // Just in case as reference ed255519 implementation do same
+        byte[] randomBytes = new byte[32];
+        random.nextBytes(randomBytes);
+        byte[] privateKey = new byte[32];
+        SHA256 sha256 = new SHA256();
+        sha256.update(randomBytes, 0, 32);
+        sha256.doFinal(privateKey, 0);
+
+        // Performing bit's flipping
+        privateKey[0] &= 248;
+        privateKey[31] &= 127;
+        privateKey[31] |= 64;
+
+        return privateKey;
+    }
+
+    /**
+     * Building public key with private key
+     *
+     * @param privateKey private key
+     * @return generated public key
+     */
+    public synchronized byte[] keyGenPublic(byte[] privateKey) {
+        byte[] publicKey = new byte[32];
+        curve_sigs.curve25519_keygen(publicKey, privateKey);
+        return publicKey;
+    }
+
+    /**
+     * Calculating DH agreement
+     *
+     * @param ourPrivate  Our Private Key
+     * @param theirPublic Theirs Public key
+     * @return calculated agreement
+     */
+    public synchronized byte[] calculateAgreement(byte[] ourPrivate, byte[] theirPublic) {
+        byte[] agreement = new byte[32];
+        scalarmult.crypto_scalarmult(agreement, ourPrivate, theirPublic);
+        return agreement;
+    }
+
+    /**
+     * Calculating signature
+     *
+     * @param random     random seed for signature
+     * @param privateKey private key for signature
+     * @param message    message to sign
+     * @return signature
+     */
+    public synchronized byte[] calculateSignature(byte[] random, byte[] privateKey, byte[] message) {
+        byte[] result = new byte[64];
+
+        if (curve_sigs.curve25519_sign(SHA512Provider, result, privateKey, message, message.length, random) != 0) {
+            throw new IllegalArgumentException("Message exceeds max length!");
+        }
+
+        return result;
+    }
+
+    /**
+     * Verification of signature
+     *
+     * @param publicKey public key of signature
+     * @param message   message
+     * @param signature signature of a message
+     * @return true if signature correct
+     */
+    public synchronized boolean verifySignature(byte[] publicKey, byte[] message, byte[] signature) {
+        return curve_sigs.curve25519_verify(SHA512Provider, signature, publicKey, message, message.length) == 0;
+    }
+
+    private final Sha512 SHA512Provider = new Sha512() {
+        @Override
+        public void calculateDigest(byte[] out, byte[] in, long length) {
+            SHA512 sha512 = new SHA512();
+            sha512.update(in, 0, (int) length);
+            sha512.doFinal(out, 0);
+        }
+    };
+}
