e1254c6eebbc8c32e713c2efd9ff0193ae7c42e3
Steve Kite <steve@actor.im>
2016-01-09 01:53:25 +0300

    feat(iOS): Added new default endpoints and added trusted keys

--- actor-sdk/sdk-core-ios/ActorSDK/Sources/ActorSDK.swift
+++ actor-sdk/sdk-core-ios/ActorSDK/Sources/ActorSDK.swift
@@ -40,7 +40,25 @@ public class ActorSDK {
     /// Server Endpoints
     public var endpoints = [
         "tls://front1-mtproto-api-rev2.actor.im",
-        "tls://front2-mtproto-api-rev2.actor.im"
+        "tls://front2-mtproto-api-rev2.actor.im",
+        
+        "tcp://front1-mtproto-api-rev3.actor.im:443",
+        "tcp://front2-mtproto-api-rev3.actor.im:443",
+        "tcp://front3-mtproto-api-rev3.actor.im:443"
+    ] {
+        didSet {
+            trustedKeys = []
+        }
+    }
+    
+    /// Trusted Server Keys
+    public var trustedKeys = [
+        "d9d34ed487bd5b434eda2ef2c283db587c3ae7fb88405c3834d9d1a6d247145b",
+        "4bd5422b50c585b5c8575d085e9fae01c126baa968dab56a396156759d5a7b46",
+        "ff61103913aed3a9a689b6d77473bc428d363a3421fdd48a8e307a08e404f02c",
+        "20613ab577f0891102b1f0a400ca53149e2dd05da0b77a728b62f5ebc8095878",
+        "fc49f2f2465f5b4e038ec7c070975858a8b5542aa6ec1f927a57c4f646e1c143",
+        "6709b8b733a9f20a96b9091767ac19fd6a2a978ba0dccc85a9ac8f6b6560ac1a"
     ]
     
     //
@@ -145,6 +163,9 @@ public class ActorSDK {
         let appTitle = "Actor iOS"
         for url in endpoints {
             builder.addEndpoint(url)
+        }
+        for key in trustedKeys {
+            builder.addTrustedKey(key)
         }
         builder.setApiConfiguration(ACApiConfiguration(appTitle: appTitle, withAppId: jint(apiId), withAppKey: apiKey, withDeviceTitle: deviceName, withDeviceId: deviceKey))
         
