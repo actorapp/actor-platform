97ff0cbb8ab96ea4676c19f1411305e029825fad
Steve Kite <steve@actor.im>
2016-01-09 00:35:52 +0300

    feat(core): Implemented proto encryption

---
+++ actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/network/mtp/entity/EncryptedCBCPackage.java
@@ -0,0 +1,42 @@
+package im.actor.core.network.mtp.entity;
+
+import java.io.IOException;
+
+import im.actor.runtime.bser.DataInput;
+import im.actor.runtime.bser.DataOutput;
+
+public class EncryptedCBCPackage extends ProtoObject {
+
+    private byte[] iv;
+    private byte[] encryptedContent;
+
+    public EncryptedCBCPackage(byte[] iv, byte[] encryptedContent) {
+        this.iv = iv;
+        this.encryptedContent = encryptedContent;
+    }
+
+    public EncryptedCBCPackage(DataInput stream) throws IOException {
+        super(stream);
+    }
+
+    public byte[] getIv() {
+        return iv;
+    }
+
+    public byte[] getEncryptedContent() {
+        return encryptedContent;
+    }
+
+    @Override
+    public void writeObject(DataOutput bs) throws IOException {
+        bs.writeProtoBytes(iv, 0, iv.length);
+        bs.writeProtoBytes(encryptedContent, 0, encryptedContent.length);
+    }
+
+    @Override
+    public ProtoObject readObject(DataInput bs) throws IOException {
+        iv = bs.readProtoBytes();
+        encryptedContent = bs.readProtoBytes();
+        return this;
+    }
+}
