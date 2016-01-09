954fc6e3d5046c999133e337e3d3678814c6c1f2
Steve Kite <steve@actor.im>
2016-01-09 17:49:16 +0300

    fix(iOS): Fixing preferences storage saving nulls as bytes, fixing cocoa random provider support

--- actor-sdk/sdk-core/runtime/runtime-cocoa/src/main/java/im/actor/runtime/RandomRuntimeProvider.java
+++ actor-sdk/sdk-core/runtime/runtime-cocoa/src/main/java/im/actor/runtime/RandomRuntimeProvider.java
@@ -1,33 +1,7 @@
 package im.actor.runtime;
 
-import java.math.BigInteger;
+import im.actor.runtime.cocoa.CocoaRandomProvider;
 
-/**
- * Created by ex3ndr on 07.08.15.
- */
-public class RandomRuntimeProvider implements RandomRuntime {
-    @Override
-    public byte[] randomBytes(int length) {
-        throw new RuntimeException("Dumb");
-    }
+public class RandomRuntimeProvider extends CocoaRandomProvider {
 
-    @Override
-    public int randomInt(int maxValue) {
-        throw new RuntimeException("Dumb");
-    }
-
-    @Override
-    public void nextBytes(byte[] data) {
-        throw new RuntimeException("Dumb");
-    }
-
-    @Override
-    public BigInteger generateBigInteger(int numBits) {
-        throw new RuntimeException("Dumb");
-    }
-
-    @Override
-    public BigInteger generateBigInteger(int numBits, int certanity) {
-        throw new RuntimeException("Dumb");
-    }
 }
