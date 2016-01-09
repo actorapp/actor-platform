24ee20617ad83c8e654355e6b011dbb4afb123ef
Steve Kite <steve@actor.im>
2016-01-09 18:25:04 +0300

    fix(iOS): CocoaRandomProvider now uses GenericRandomProvider

--- actor-sdk/sdk-core/runtime/runtime-cocoa/src/main/java/im/actor/runtime/cocoa/CocoaRandomProvider.java
+++ actor-sdk/sdk-core/runtime/runtime-cocoa/src/main/java/im/actor/runtime/cocoa/CocoaRandomProvider.java
@@ -1,38 +1,7 @@
 package im.actor.runtime.cocoa;
 
-import java.math.BigInteger;
-import java.security.SecureRandom;
+import im.actor.runtime.generic.GenericRandomProvider;
 
-import im.actor.runtime.crypto.bouncycastle.RandomProvider;
+public class CocoaRandomProvider extends GenericRandomProvider {
 
-public class CocoaRandomProvider implements RandomProvider {
-
-    private SecureRandom secureRandom = new SecureRandom();
-
-    @Override
-    public synchronized byte[] randomBytes(int length) {
-        byte[] res = new byte[length];
-        secureRandom.nextBytes(res);
-        return res;
-    }
-
-    @Override
-    public synchronized int randomInt(int maxValue) {
-        return secureRandom.nextInt(maxValue);
-    }
-
-    @Override
-    public synchronized void nextBytes(byte[] data) {
-        secureRandom.nextBytes(data);
-    }
-
-    @Override
-    public synchronized BigInteger generateBigInteger(int numBits) {
-        throw new RuntimeException("Not supported");
-    }
-
-    @Override
-    public synchronized BigInteger generateBigInteger(int numBits, int certanity) {
-        throw new RuntimeException("Not supported");
-    }
 }
