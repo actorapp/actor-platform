73913cf06886abd14042f2734a53ef0152d3f286
Oleg Shilov <me@olegshilov.com>
2016-01-11 22:01:21 +0300

    feat(web): add service worker for caching

---
+++ actor-sdk/sdk-web/src/workers/index.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('offline-worker.js')
+    .then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    })
+    .catch(function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+}
