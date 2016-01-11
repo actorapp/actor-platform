0f13c654aed7bc1183b0dbbad14d20723ab246ba
prettynatty <fear@loathing.in>
2016-01-12 00:40:01 +0300

    feat(server:http): DataEntity and DataEntities usable from Circe

--- actor-server/actor-http-api/src/main/scala/im/actor/server/api/http/model/DataEntity.scala
+++ actor-server/actor-http-api/src/main/scala/im/actor/server/api/http/model/DataEntity.scala
@@ -1,11 +1,5 @@
 package im.actor.server.api.http.model
 
-import play.api.libs.json.{ JsArray, Json, JsObject, Writes }
+final case class DataEntity[A](data: A)
 
-final case class DataEntity[A](data: A) {
-  def toJson(implicit writes: Writes[A]) = JsObject(Map("data" → Json.toJson(data)))
-}
-
-final case class DataEntities[A](datas: Seq[A]) {
-  def toJson(implicit writes: Writes[A]) = JsObject(Map("data" → JsArray(datas map (Json.toJson(_)))))
-}
+final case class DataEntities[A](data: Seq[A])
