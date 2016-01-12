7110ab92ebd81c7c8ae94d492bdf449c7e3e1444
Oleg Shilov <me@olegshilov.com>
2016-01-12 07:30:45 +0300

    fix(web): register cache service worker only on production.

--- actor-sdk/sdk-web/src/workers/index.js
+++ actor-sdk/sdk-web/src/workers/index.js
@@ -1,9 +1,11 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('offline-worker.js')
-    .then(function(registration) {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    })
-    .catch(function(err) {
-      console.log('ServiceWorker registration failed: ', err);
-    });
+if (process.env.NODE_ENV == 'production') {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('offline-worker.js')
+      .then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch(function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  }
 }
