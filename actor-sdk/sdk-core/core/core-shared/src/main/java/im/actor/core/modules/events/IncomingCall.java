7d4b6fa2b5eab7816cb990437fdebeda70910ad6
kor_ka <badgrib@gmmail.com>
2016-01-14 00:07:45 +0300

    fix(android): incoming call event - add uid

--- actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/events/IncomingCall.java
+++ actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/events/IncomingCall.java
@@ -7,13 +7,19 @@ public class IncomingCall extends Event {
     public static final String EVENT = "incoming_call";
 
     private long callId;
+    private int uid;
 
-    public IncomingCall(long callId) {
+    public IncomingCall(long callId, int uid) {
         this.callId = callId;
+        this.uid = uid;
     }
 
     public long getCall() {
         return callId;
+    }
+
+    public int getUid() {
+        return uid;
     }
 
     @Override
