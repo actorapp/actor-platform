0614063b2617ce2da3e614142ac7077720411873
Oleg Shilov <me@olegshilov.com>
2016-01-14 16:01:29 +0300

    feat+chore(web): update SDK development workflow

--- actor-sdk/sdk-web/gulpfile.babel.js
+++ actor-sdk/sdk-web/gulpfile.babel.js
@@ -4,31 +4,20 @@ import minimist from 'minimist';
 
 import webpack from 'webpack';
 import webpackDevServer from 'webpack-dev-server';
-import webpackProdConfig from './webpack.prod.config.js';
-import webpackDevConfig from './webpack.dev.config.js';
+import webpackConfig from './webpack.config.js';
 
 import gulp from 'gulp';
 import gutil from 'gulp-util';
 import gulpif from 'gulp-if';
 import image from 'gulp-image';
+import shell from 'gulp-shell';
+import rename from 'gulp-rename';
 
 const argv = minimist(process.argv.slice(2));
 const isProduction = argv.release || false;
 
-gulp.task('webpack:build', (callback) => {
-  webpack(webpackProdConfig, (err, stats) => {
-    if (err) {
-      throw new gutil.PluginError('[webpack:build]', err);
-    }
-    gutil.log('[webpack:build]', stats.toString({
-      colors: true
-    }));
-    callback();
-  });
-});
-
 gulp.task('webpack:dev', () => {
-  new webpackDevServer(webpack(webpackDevConfig), {
+  new webpackDevServer(webpack(webpackConfig), {
     publicPath: '/',
     contentBase: './dist',
     hot: true,
@@ -44,40 +33,48 @@ gulp.task('webpack:dev', () => {
   });
 });
 
-gulp.task('assets', ['sounds', 'images']);
+gulp.task('assets', ['html', 'sounds', 'images']);
 
-gulp.task('sounds', () => {
-  gulp.src(['../build/assets/sound/**/*'])
+gulp.task('sounds', ['sdk'], () => {
+  gulp.src(['build/assets/sound/**/*'])
     .pipe(gulp.dest('./dist/assets/sound'));
 });
 
-gulp.task('images', () => {
-  gulp.src(['../build/assets/images/**/*'])
+gulp.task('images', ['sdk'], () => {
+  gulp.src(['build/assets/images/**/*'])
     .pipe(gulpif(isProduction, image({svgo: false})))
     .pipe(gulp.dest('./dist/assets/images'));
 });
 
 gulp.task('html', () => {
-  gulp.src(['src/index.html'])
+  gulp.src(['devapp/index.html'])
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('workers', () => {
+gulp.task('workers', ['sdk'], () => {
   gulp.src([
-    '../build/workers/offline-worker.*',
-    '../build/workers/serviceworker-cache-polyfill.*',
+    'build/workers/offline-worker.*',
+    'build/workers/serviceworker-cache-polyfill.*',
     'node_modules/opus-recorder/libopus.js',
     'node_modules/opus-recorder/oggopusDecoder.js',
     'node_modules/opus-recorder/oggopusEncoder.js',
-    'node_modules/opus-recorder/resampler.js',
+    'node_modules/opus-recorder/resampler.js'
   ])
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('static', ['html', 'assets', 'workers']);
+gulp.task('lib:build', shell.task('./gradlew :actor-sdk:sdk-core:core:core-js:buildPackage', {cwd: '../..'}));
+gulp.task('lib:copy', ['lib:build'], () => {
+  gulp.src(['../sdk-core/core/core-js/build/package/actor.nocache.js'])
+    .pipe(rename('actor.js'))
+    .pipe(gulp.dest('./node_modules/actor-js/'));
+});
+gulp.task('lib', ['lib:build', 'lib:copy']);
+
+gulp.task('sdk', shell.task('npm run build'));
+
+gulp.task('static', ['lib', 'assets', 'workers']);
 
 gulp.task('dev', ['static', 'webpack:dev']);
 
-gulp.task('build', ['static', 'webpack:build']);
-
-gulp.task('default', ['build']);
+gulp.task('default', ['dev']);
