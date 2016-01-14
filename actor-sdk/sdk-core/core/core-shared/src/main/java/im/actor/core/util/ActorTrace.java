ab332f6f30ca9a5180ce6656ece19a4dde6a600c
Steve Kite <steve@actor.im>
2016-01-14 16:33:13 +0300

    feat(runtime): Enable logging on actor's die

--- actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/util/ActorTrace.java
+++ actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/util/ActorTrace.java
@@ -46,8 +46,8 @@ public class ActorTrace implements TraceInterface {
 
     @Override
     public void onActorDie(ActorRef ref, Envelope envelope, Exception e) {
-        Log.w(TAG, "Die(" + ref.getPath() + ") by " + envelope.getMessage());
-        //Log.e(TAG, e);
-        e.printStackTrace();
+        Log.w(TAG, "Die(" + ref.getPath() + ") by " + envelope.getMessage() + " with " + e.getMessage());
+        Log.e(TAG, e);
+        // e.printStackTrace();
     }
 }
