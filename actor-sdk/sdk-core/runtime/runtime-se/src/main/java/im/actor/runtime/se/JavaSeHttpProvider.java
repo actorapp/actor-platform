1cd7b242f0259736177490ff3eea8d605f663346
Steve Kite <steve@actor.im>
2016-01-17 20:38:04 +0300

    fix(se): Fixing SE libraries compilation error

--- actor-sdk/sdk-core/runtime/runtime-se/src/main/java/im/actor/runtime/se/JavaSeHttpProvider.java
+++ actor-sdk/sdk-core/runtime/runtime-se/src/main/java/im/actor/runtime/se/JavaSeHttpProvider.java
@@ -38,7 +38,7 @@ public class JavaSeHttpProvider implements HttpRuntime {
             public void onFailure(Request request, IOException e) {
                 Log.d(TAG, "Downloading part error: " + request.toString());
                 e.printStackTrace();
-                callback.onDownloadFailure();
+                callback.onDownloadFailure(0, 0);
             }
 
             @Override
@@ -47,7 +47,7 @@ public class JavaSeHttpProvider implements HttpRuntime {
                 if (response.code() == 206) {
                     callback.onDownloaded(response.body().bytes());
                 } else {
-                    callback.onDownloadFailure();
+                    callback.onDownloadFailure(0, 0);
                 }
             }
         });
@@ -65,7 +65,7 @@ public class JavaSeHttpProvider implements HttpRuntime {
             public void onFailure(Request request, IOException e) {
                 Log.d(TAG, "Uploading part error: " + request.toString());
                 e.printStackTrace();
-                callback.onUploadFailure();
+                callback.onUploadFailure(0, 0);
             }
 
             @Override
@@ -74,7 +74,7 @@ public class JavaSeHttpProvider implements HttpRuntime {
                 if (response.code() == 200) {
                     callback.onUploaded();
                 } else {
-                    callback.onUploadFailure();
+                    callback.onUploadFailure(0, 0);
                 }
             }
         });
