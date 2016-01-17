d0698d3be6217753f1f72db2c2c9b6a44e67e8bd
prettynatty <fear@loathing.in>
2016-01-17 16:30:17 +0300

    perf(server:push): async google push via HTTP

--- actor-server/actor-core/src/main/scala/im/actor/server/sequence/GooglePushProvider.scala
+++ actor-server/actor-core/src/main/scala/im/actor/server/sequence/GooglePushProvider.scala
@@ -1,29 +1,16 @@
 package im.actor.server.sequence
 
 import akka.actor.ActorSystem
-import akka.event.Logging
-import com.google.android.gcm.server.{ Sender, Message }
 import im.actor.server.model.push.GooglePushCredentials
 
-import scala.concurrent.{ ExecutionContext, Future, blocking }
-
 private[sequence] final class GooglePushProvider(userId: Int, googlePushManager: GooglePushManager, system: ActorSystem) extends PushProvider {
-  import system.dispatcher
-
-  private val Retries = 3
-
-  private val log = Logging(system, getClass)
-
   def deliverInvisible(seq: Int, creds: GooglePushCredentials): Unit = {
-    withMgr(creds.projectId) { implicit mgr ⇒
-      val message =
-        new Message.Builder()
-          .collapseKey(s"seq-invisible-${userId.toString}")
-          .addData("seq", seq.toString)
-          .build()
+    val message = GooglePushMessage(
+      collapseKey = Some(s"seq-invisible-${userId.toString}"),
+      data = Some(Map("seq" → seq.toString))
+    )
 
-      send(message, creds.regId, Retries)
-    }
+    googlePushManager.send(creds.projectId, creds.regId, message)
   }
 
   def deliverVisible(
@@ -34,33 +21,17 @@ private[sequence] final class GooglePushProvider(userId: Int, googlePushManager:
     isSoundEnabled:     Boolean,
     isVibrationEnabled: Boolean
   ): Unit = {
-    withMgr(creds.projectId) { implicit mgr ⇒
-      val builder = new Message.Builder()
-        .collapseKey(s"seq-visible-${userId.toString}")
-        .addData("seq", seq.toString)
-
-      val message =
+    val message = GooglePushMessage(
+      collapseKey = Some(s"seq-visible-${userId.toString}"),
+      data = Some(Map("seq" → seq.toString) ++ (
         data.text match {
           case text if text.nonEmpty && isTextEnabled ⇒
-            builder
-              .addData("message", text)
-              .build()
-          case _ ⇒ builder.build()
+            Map("message" → text)
+          case _ ⇒ Map.empty
         }
+      ))
+    )
 
-      send(message, creds.regId, Retries)
-    }
-  }
-
-  private def withMgr[A](projectId: Long)(f: Sender ⇒ A) =
-    googlePushManager.getInstance(projectId) match {
-      case Some(mgr) ⇒ f(mgr)
-      case None      ⇒ log.warning("No google push configured for project-id: {}", projectId)
-    }
-
-  private def send(message: Message, regId: String, retries: Int)(implicit mgr: Sender, ec: ExecutionContext): Unit = Future {
-    blocking {
-      mgr.send(message, regId, Retries)
-    }
+    googlePushManager.send(creds.projectId, creds.regId, message)
   }
 }
