a4b230eba09ee4e493187e914d91759fee22570a
kor_ka <badgrib@gmmail.com>
2016-01-18 19:11:47 +0300

    feat(android): localize call service messages

---
+++ actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/entity/content/ServiceCallMissed.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2015 Actor LLC. <https://actor.im>
+ */
+
+package im.actor.core.entity.content;
+
+import im.actor.core.entity.content.internal.ContentRemoteContainer;
+
+public class ServiceCallMissed extends ServiceContent {
+
+
+    public ServiceCallMissed(ContentRemoteContainer contentContainer) {
+        super(contentContainer);
+    }
+}
