4d351316045957031cc09f90d2e5d575707c5202
John Doe <speed@actor.im>
2016-01-19 04:22:50 +0300

    feat(web): add scrollbar component and use it at sidebar;

---
+++ actor-sdk/sdk-web/src/components/common/Scrollbar.react.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2016 Actor LLC. <https://actor.im>
+ */
+
+import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
+import tinyscrollbar from 'tinyscrollbar/lib/tinyscrollbar';
+import SimpleScroll from 'simple-scrollbar';
+
+class Scrollbar extends Component {
+  static propTypes = {
+    children: PropTypes.oneOfType([
+      PropTypes.element,
+      PropTypes.array
+    ]),
+    className: PropTypes.string,
+
+    onScroll: PropTypes.func
+  };
+
+  constructor(props) {
+    super(props);
+    this.scrollbar = new SimpleScroll();
+  }
+
+  componentDidMount() {
+    const scrollNode = React.findDOMNode(this.refs.scroll);
+    this.scrollbar.initElement(scrollNode);
+  }
+
+  handleScroll = (event) => {
+    const { onScroll } = this.props;
+    onScroll && onScroll(event);
+  };
+
+  render() {
+    const { children, className } = this.props;
+    const wrapperClassName = classnames('scroll-wrapper', className);
+
+    return (
+      <div className={wrapperClassName} ref="scroll" onScroll={this.handleScroll}>
+        {children}
+      </div>
+    );
+  }
+}
+
+export default Scrollbar;
