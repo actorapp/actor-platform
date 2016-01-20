b834d96657f796a029b12bc9b6520183b1988528
Steve Kite <steve@actor.im>
2016-01-21 00:01:39 +0300

    feat(core): Async updates handling

--- actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/api/SequenceHandlerActor.java
+++ actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/api/SequenceHandlerActor.java
@@ -1,10 +1,18 @@
 package im.actor.core.modules.api;
 
+import java.io.IOException;
+import java.util.List;
+
+import im.actor.core.api.ApiGroup;
+import im.actor.core.api.ApiUser;
+import im.actor.core.api.parser.UpdatesParser;
 import im.actor.core.modules.ModuleContext;
 import im.actor.core.modules.updates.UpdateProcessor;
 import im.actor.core.util.ModuleActor;
 import im.actor.core.network.parser.Update;
+import im.actor.runtime.Log;
 import im.actor.runtime.actors.Future;
+import im.actor.runtime.actors.Promise;
 
 public class SequenceHandlerActor extends ModuleActor {
 
@@ -18,19 +26,63 @@ public class SequenceHandlerActor extends ModuleActor {
         this.processor = processor;
     }
 
+    private void onWeakUpdateReceived(Update update, long date) {
+        Log.d(TAG, "Processing weak update: " + update);
+        this.processor.processWeakUpdate(update, date);
+    }
+
+    private void onSeqUpdate(int type, byte[] body, List<ApiUser> users,
+                             List<ApiGroup> groups, Future future) {
+
+        Update update;
+        try {
+            update = new UpdatesParser().read(type, body);
+        } catch (IOException e) {
+            Log.w(TAG, "Unable to parse update: ignoring");
+            Log.e(TAG, e);
+            future.onResult();
+            return;
+        }
+
+        if (groups == null || users == null) {
+            if (processor.isCausesInvalidation(update)) {
+                Log.w(TAG, "Difference is required");
+                future.onError(new RuntimeException("Difference is required"));
+                return;
+            }
+        }
+
+        // Processing update
+        Log.d(TAG, "Processing update: " + update);
+
+        if (groups != null && users != null) {
+            processor.applyRelated(users, groups, false);
+        }
+
+        processor.processUpdate(update);
+
+        if (groups != null && users != null) {
+            processor.applyRelated(users, groups, true);
+        }
+
+        Log.d(TAG, "Processing update success");
+        future.onResult();
+    }
+
     @Override
     public boolean onAsk(Object message, Future future) {
         if (message instanceof WeakUpdate) {
             WeakUpdate weakUpdate = (WeakUpdate) message;
             onWeakUpdateReceived(weakUpdate.getUpdate(), weakUpdate.getDate());
             return true;
+        } else if (message instanceof SeqUpdate) {
+            SeqUpdate seqUpdate = (SeqUpdate) message;
+            onSeqUpdate(seqUpdate.type, seqUpdate.body,
+                    seqUpdate.users, seqUpdate.groups, future);
+            return false;
         } else {
             return super.onAsk(message, future);
         }
-    }
-
-    private void onWeakUpdateReceived(Update update, long date) {
-        this.processor.processWeakUpdate(update, date);
     }
 
     public static class WeakUpdate {
@@ -49,6 +101,37 @@ public class SequenceHandlerActor extends ModuleActor {
 
         public Update getUpdate() {
             return update;
+        }
+    }
+
+    public static class SeqUpdate {
+
+        private int type;
+        private byte[] body;
+        private List<ApiUser> users;
+        private List<ApiGroup> groups;
+
+        public SeqUpdate(int type, byte[] body, List<ApiUser> users, List<ApiGroup> groups) {
+            this.type = type;
+            this.body = body;
+            this.users = users;
+            this.groups = groups;
+        }
+
+        public int getType() {
+            return type;
+        }
+
+        public byte[] getBody() {
+            return body;
+        }
+
+        public List<ApiUser> getUsers() {
+            return users;
+        }
+
+        public List<ApiGroup> getGroups() {
+            return groups;
         }
     }
 }
