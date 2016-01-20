793c6ec0d64f557bf447bb0cb66f1d86b0bf8e39
Steve Kite <steve@actor.im>
2016-01-21 01:18:02 +0300

    wip(core): Adding Encryption State holder

--- actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/internal/EncryptionModule.java
+++ actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/internal/EncryptionModule.java
@@ -1,8 +1,12 @@
 package im.actor.core.modules.internal;
 
+import java.util.HashMap;
+
 import im.actor.core.modules.AbsModule;
 import im.actor.core.modules.ModuleContext;
+import im.actor.core.modules.internal.encryption.EncryptedStateActor;
 import im.actor.core.modules.internal.encryption.KeyManagerActor;
+import im.actor.runtime.actors.Actor;
 import im.actor.runtime.actors.ActorCreator;
 import im.actor.runtime.actors.ActorRef;
 import im.actor.runtime.actors.Props;
@@ -12,13 +16,13 @@ import static im.actor.runtime.actors.ActorSystem.system;
 public class EncryptionModule extends AbsModule {
 
     private ActorRef keyManager;
+    private HashMap<Integer, ActorRef> encryptedStates = new HashMap<Integer, ActorRef>();
 
     public EncryptionModule(ModuleContext context) {
         super(context);
     }
 
     public void run() {
-
         keyManager = system().actorOf(Props.create(KeyManagerActor.class,
                 new ActorCreator<KeyManagerActor>() {
                     @Override
@@ -26,5 +30,23 @@ public class EncryptionModule extends AbsModule {
                         return new KeyManagerActor(context());
                     }
                 }), "encryption/keys");
+    }
+
+    public ActorRef getKeyManager() {
+        return keyManager;
+    }
+
+    public ActorRef getEncryptedChatManager(final int uid) {
+        synchronized (encryptedStates) {
+            if (!encryptedStates.containsKey(uid)) {
+                encryptedStates.put(uid, system().actorOf(Props.create(EncryptedStateActor.class, new ActorCreator<EncryptedStateActor>() {
+                    @Override
+                    public EncryptedStateActor create() {
+                        return new EncryptedStateActor(uid, context());
+                    }
+                }), "encryption/uid_" + uid));
+            }
+            return encryptedStates.get(uid);
+        }
     }
 }
---
+++ actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/internal/encryption/entity/EncryptedBoxKey.java
@@ -0,0 +1,26 @@
+package im.actor.core.modules.internal.encryption.entity;
+
+public class EncryptedBoxKey {
+
+    private final int uid;
+    private final int keyGroupId;
+    private final byte[] encryptedKey;
+
+    public EncryptedBoxKey(int uid, int keyGroupId, byte[] encryptedKey) {
+        this.uid = uid;
+        this.keyGroupId = keyGroupId;
+        this.encryptedKey = encryptedKey;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public int getKeyGroupId() {
+        return keyGroupId;
+    }
+
+    public byte[] getEncryptedKey() {
+        return encryptedKey;
+    }
+}
