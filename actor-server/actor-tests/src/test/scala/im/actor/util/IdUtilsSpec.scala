d17261d907cb4dde79c798db552ffbe784e897e4
prettynatty <fear@loathing.in>
2016-01-24 03:53:14 +0300

    fix(server): fixed compilation error

--- actor-server/actor-tests/src/test/scala/im/actor/util/IdUtilsSpec.scala
+++ actor-server/actor-tests/src/test/scala/im/actor/util/IdUtilsSpec.scala
@@ -4,14 +4,9 @@ import im.actor.util.misc.IdUtils
 import org.scalacheck.Prop._
 import org.scalacheck.Properties
 
-import scala.concurrent.forkjoin.ThreadLocalRandom
-
 object IdUtilsSpec extends Properties("Id Utils") {
-  val rng = ThreadLocalRandom.current()
-
   property("In range") = forAll { a: Unit ⇒
-    val id = IdUtils.nextIntId(rng)
+    val id = IdUtils.nextIntId()
     id > 1000 && id <= Int.MaxValue
   }
-
 }
