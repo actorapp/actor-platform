516214ff695e83632ffd6571fec42ff8037765b3
Steve Kite <steve@actor.im>
2016-01-24 06:33:14 +0300

    feat(android): Working Actor Push

--- actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/receivers/SDKPushReceiver.java
+++ actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/receivers/SDKPushReceiver.java
@@ -1,13 +1,19 @@
 package im.actor.sdk.receivers;
 
-import android.util.Log;
-
+import im.actor.runtime.json.JSONObject;
+import im.actor.sdk.ActorSDK;
 import im.actor.sdk.push.ActorPushReceiver;
 
 public class SDKPushReceiver extends ActorPushReceiver {
 
     @Override
     public void onPushReceived(String payload) {
-        Log.d("TestPushReceiver", "On push received: " + payload);
+        try {
+            JSONObject object = new JSONObject(payload);
+            int seq = object.getJSONObject("data").getInt("seq");
+            ActorSDK.sharedActor().getMessenger().onPushReceived(seq);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
