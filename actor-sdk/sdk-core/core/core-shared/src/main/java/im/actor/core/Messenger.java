d4224213be013651c1f870ca5dfcb7b39917ba46
kor_ka <badgrib@gmmail.com>
2016-01-25 17:41:39 +0300

    fix(core): end call if already on call

--- actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/Messenger.java
+++ actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/Messenger.java
@@ -1009,6 +1009,14 @@ public class Messenger {
     //             Calls
     //////////////////////////////////////
 
+    /**
+     * Command for doing call
+     *
+     * @param uid          user you want to call
+     * @param callCallback ui callback
+     * @return command to execute
+     */
+    @ObjectiveCName("doCallWithUid:withCallback")
     public Command<ResponseDoCall> doCall(int uid, CallsModule.CallCallback callCallback) {
         return modules.getCallsModule().makeCall(uid, callCallback);
     }
@@ -1017,10 +1025,23 @@ public class Messenger {
         modules.getCallsModule().endCall(callId);
     }
 
-    public void answerCall(long callId, CallsModule.CallCallback callback) {
+    /**
+     * Handle call in ui - <b>should<b/> be called, when receiving IncomingCallEvent
+     * @param callId
+     * @param callback
+     */
+    @ObjectiveCName("handleCallWithCallId:withCallback:")
+    public void handleCall(long callId, CallsModule.CallCallback callback) {
         modules.getCallsModule().handleCall(callId, callback);
     }
 
+    /**
+     * Send call signaling
+     *
+     * @param callId call id
+     * @param data   signal object to send
+     */
+    @ObjectiveCName("sendCallSignalWithCallId:withData:")
     public void sendCallSignal(long callId, AbsSignal data) {
         modules.getCallsModule().sendSignal(callId, data);
     }
