a15ea57fe3c2523f4e9563122b17bce999e0b983
rockjam <5min4eq.unity@gmail.com>
2016-01-28 18:01:02 +0300

    fix(server:core): guarantee unique send message date in dialog

--- actor-server/actor-tests/src/test/scala/im/actor/server/dialog/DialogProcessorSpec.scala
+++ actor-server/actor-tests/src/test/scala/im/actor/server/dialog/DialogProcessorSpec.scala
@@ -9,6 +9,7 @@ import im.actor.server.sequence.SeqStateDate
 import im.actor.server.{ MessageParsing, BaseAppSuite, ImplicitAuthService, ImplicitSessionRegion }
 
 import scala.concurrent.Future
+import scala.language.postfixOps
 
 final class DialogProcessorSpec extends BaseAppSuite
   with ImplicitAuthService
@@ -18,13 +19,15 @@ final class DialogProcessorSpec extends BaseAppSuite
 
   behavior of "Dialog Processor"
 
-  it should "not allow time out when there are highly frequent messages" in e1()
+  it should "not allow time out when there are highly frequent messages" in noTimeout()
+
+  it should "not allow duplicated timestamp in messages" in uniqueTimestamp()
 
   private val dialogExt = DialogExtension(system)
 
   private val messService = MessagingServiceImpl()
 
-  def e1() = {
+  def noTimeout() = {
     val (alice, aliceAuthId, aliceAuthSid, _) = createUser()
     val (bob, bobAuthId, bobAuthSid, _) = createUser()
 
@@ -49,7 +52,7 @@ final class DialogProcessorSpec extends BaseAppSuite
       whenReady(messService.handleLoadHistory(aliceOutPeer, 0L, Int.MaxValue)) { resp ⇒
         inside(resp) {
           case Ok(ResponseLoadHistory(messages, _)) ⇒
-            val (hello, hello2) = messages map { mess ⇒
+            val (aliceMessages, bobsMessages) = messages map { mess ⇒
               val parsed = parseMessage(mess.message.toByteArray)
               parsed.isRight shouldEqual true
               val message = parsed.right.toOption.get
@@ -58,11 +61,46 @@ final class DialogProcessorSpec extends BaseAppSuite
                 case _                          ⇒ fail()
               }
             } partition (mess ⇒ mess startsWith "Hello you back")
-            hello should have length 50
-            hello2 should have length 50
+            aliceMessages should have length 50
+            bobsMessages should have length 50
         }
       }
     }
 
   }
+
+  def uniqueTimestamp() = {
+    val (alice, aliceAuthId, aliceAuthSid, _) = createUser()
+    val (bob, bobAuthId, bobAuthSid, _) = createUser()
+
+    val alicePeer = ApiPeer(ApiPeerType.Private, alice.id)
+    val bobPeer = ApiPeer(ApiPeerType.Private, bob.id)
+
+    def sendMessageToBob(text: String): Future[SeqStateDate] =
+      dialogExt.sendMessage(bobPeer, alice.id, aliceAuthSid, ACLUtils.randomLong(), textMessage(text))
+
+    def sendMessageToAlice(text: String): Future[SeqStateDate] =
+      dialogExt.sendMessage(alicePeer, bob.id, bobAuthSid, ACLUtils.randomLong(), textMessage(text))
+
+    val toAlice = for (i ← 1 to 50) yield sendMessageToAlice(s"Hello $i")
+    val toBob = for (i ← 1 to 50) yield sendMessageToBob(s"Hello you back $i")
+
+    toAlice foreach { whenReady(_)(identity) }
+    toBob foreach { whenReady(_)(identity) }
+
+    {
+      implicit val clientData = ClientData(bobAuthId, 2, Some(AuthData(bob.id, bobAuthSid)))
+      val aliceOutPeer = whenReady(ACLUtils.getOutPeer(alicePeer, bobAuthId))(identity)
+      whenReady(messService.handleLoadHistory(aliceOutPeer, 0L, Int.MaxValue)) { resp ⇒
+        inside(resp) {
+          case Ok(ResponseLoadHistory(messages, _)) ⇒
+            val (aliceMessages, bobsMessages) = messages partition (m ⇒ m.senderUserId == alice.id)
+            (aliceMessages map (_.date) distinct) should have length 50
+            (bobsMessages map (_.date) distinct) should have length 50
+          //            (messages map (_.date) distinct) should have length 100 // todo: this one is future goal. Ensure uniqueness across dialog participants
+        }
+      }
+    }
+  }
+
 }
