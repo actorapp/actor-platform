9b170e7be22b37c1f68b33e9a29ca1ed39a7904c
Nikolay Tatarinov <rockjam@actor.im>
2016-01-29 17:48:30 +0300

    perf(server:core): optimize update counters query

--- actor-server/actor-persist/src/main/scala/im/actor/server/persist/dialog/UserDialogOperations.scala
+++ actor-server/actor-persist/src/main/scala/im/actor/server/persist/dialog/UserDialogOperations.scala
@@ -3,7 +3,6 @@ package im.actor.server.persist.dialog
 import com.github.tototoshi.slick.PostgresJodaSupport._
 import im.actor.server.db.ActorPostgresDriver.api._
 import im.actor.server.model.{ UserDialog, PeerType, Peer }
-import im.actor.server.persist.GroupRepo
 
 import org.joda.time.DateTime
 
@@ -15,11 +14,9 @@ object UserDialogRepo {
   val byPKC = Compiled(byPK _)
   val idByPeerTypeC = Compiled(idByPeerType _)
 
-  val notArchived = userDialogs joinLeft GroupRepo.groups on (_.peerId === _.id) filter {
-    case (usedDialog, groupOpt) ⇒ usedDialog.isArchived === false && groupOpt.map(!_.isHidden).getOrElse(true)
-  } map (_._1)
+  val notArchived = userDialogs.filter(_.isArchived === false)
 
-  val notHiddenNotArchived = notArchived.filter(_.shownAt.isDefined)
+  val notArchivedVisible = notArchived.filter(_.shownAt.isDefined)
 
   private def byPK(userId: Rep[Int], peerType: Rep[Int], peerId: Rep[Int]) =
     userDialogs.filter(u ⇒ u.userId === userId && u.peerType === peerType && u.peerId === peerId)
@@ -34,7 +31,7 @@ object UserDialogRepo {
 trait UserDialogOperations {
   import UserDialogRepo._
 
-  def findUsersVisible(userId: Rep[Int]) = notHiddenNotArchived.filter(_.userId === userId)
+  def findUsersVisible(userId: Rep[Int]) = notArchivedVisible.filter(_.userId === userId)
 
   def findGroupIds(userId: Int) =
     idByPeerTypeC((userId, PeerType.Group.value)).result
