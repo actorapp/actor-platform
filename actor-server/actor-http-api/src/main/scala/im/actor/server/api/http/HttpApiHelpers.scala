f0958539cf76dde3c485c1b76d02ee1c618da617
prettynatty <fear@loathing.in>
2016-01-30 05:51:42 +0300

    fix(server): work-around for cors akka bug

--- actor-server/actor-http-api/src/main/scala/im/actor/server/api/http/HttpApiHelpers.scala
+++ actor-server/actor-http-api/src/main/scala/im/actor/server/api/http/HttpApiHelpers.scala
@@ -1,7 +1,6 @@
 package im.actor.server.api.http
 
-import akka.http.scaladsl.model.HttpMethods._
-import akka.http.scaladsl.model.headers.{ `Access-Control-Allow-Credentials`, `Access-Control-Allow-Headers`, `Access-Control-Allow-Methods`, `Access-Control-Allow-Origin` }
+import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 
@@ -9,10 +8,10 @@ object HttpApiHelpers extends HttpApiHelpers
 
 trait HttpApiHelpers {
   val corsHeaders = List(
-    `Access-Control-Allow-Origin`.`*`,
-    `Access-Control-Allow-Methods`(GET, POST, PUT),
-    `Access-Control-Allow-Headers`("Content-Type", "Access-Control-Allow-Headers", "Authorization"),
-    `Access-Control-Allow-Credentials`(true)
+    headers.RawHeader("Access-Control-Allow-Origin", "*"),
+    headers.RawHeader("Access-Control-Allow-Methods", "GET, POST, PUT"),
+    headers.RawHeader("Access-Control-Allow-Headers", "*"),
+    headers.RawHeader("Access-Control-Allow-Credentials", "true")
   )
 
   //todo: find better solution with Directive
