278606358c02b4d2e226b0573a0456fb33d21411
Steve Kite <steve@actor.im>
2016-01-31 12:04:40 +0300

    fix(core): Fixing compilation errors

--- actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/updates/MessagesProcessor.java
+++ actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/updates/MessagesProcessor.java
@@ -11,6 +11,7 @@ import java.util.List;
 import im.actor.core.api.ApiDialog;
 import im.actor.core.api.ApiDialogGroup;
 import im.actor.core.api.ApiMessage;
+import im.actor.core.api.ApiMessageContainer;
 import im.actor.core.api.ApiMessageReaction;
 import im.actor.core.api.ApiPeer;
 import im.actor.core.api.ApiAppCounters;
@@ -327,7 +328,7 @@ public class MessagesProcessor extends AbsModule {
     public void onMessagesLoaded(Peer peer, ResponseLoadHistory historyResponse) {
         ArrayList<Message> messages = new ArrayList<Message>();
         long maxLoadedDate = Long.MAX_VALUE;
-        for (ApiHistoryMessage historyMessage : historyResponse.getHistory()) {
+        for (ApiMessageContainer historyMessage : historyResponse.getHistory()) {
 
             maxLoadedDate = Math.min(historyMessage.getDate(), maxLoadedDate);
 
