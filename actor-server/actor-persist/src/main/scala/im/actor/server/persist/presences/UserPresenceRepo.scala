0f05759768aebc6a58f9f9d7f2412b13f677e002
prettynatty <fear@loathing.in>
2016-02-03 20:38:47 +0300

    feat(server:presences): store authId with presence

--- actor-server/actor-persist/src/main/scala/im/actor/server/persist/presences/UserPresenceRepo.scala
+++ actor-server/actor-persist/src/main/scala/im/actor/server/persist/presences/UserPresenceRepo.scala
@@ -11,9 +11,11 @@ import com.github.tototoshi.slick.PostgresJodaSupport._
 final class UserPresenceTable(tag: Tag) extends Table[UserPresenceModel](tag, "user_presences") {
   def userId = column[Int]("user_id", O.PrimaryKey)
 
+  def authId = column[Long]("auth_id", O.PrimaryKey)
+
   def lastSeenAt = column[Option[DateTime]]("last_seen_at")
 
-  def * = (userId, lastSeenAt) <> (UserPresenceModel.tupled, UserPresenceModel.unapply)
+  def * = (userId, authId, lastSeenAt) <> (UserPresenceModel.tupled, UserPresenceModel.unapply)
 }
 
 object UserPresenceRepo {
@@ -22,6 +24,14 @@ object UserPresenceRepo {
   def createOrUpdate(userPresence: UserPresenceModel): FixedSqlAction[Int, NoStream, Write] =
     presences.insertOrUpdate(userPresence)
 
-  def find(userId: Int): SqlAction[Option[UserPresenceModel], NoStream, Read] =
-    presences.filter(_.userId === userId).result.headOption
+  private def last(userId: Rep[Int]) =
+    presences
+      .filter(_.userId === userId)
+      .distinctOn(_.userId)
+      .sortBy(_.lastSeenAt)
+      .take(1)
+
+  private val lastC = Compiled(last _)
+
+  def find(userId: Int): SqlAction[Option[UserPresenceModel], NoStream, Read] = lastC(userId).result.headOption
 }
