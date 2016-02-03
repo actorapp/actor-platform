2fada83997c5d1493d1e9ff355142777eaf436f1
John Doe <speed@actor.im>
2016-02-04 01:21:31 +0300

    fix(server): fixed migration

--- actor-server/actor-persist/src/main/resources/sql/migration/V20160203202648__PinPresenceToAuthId.sql
+++ actor-server/actor-persist/src/main/resources/sql/migration/V20160203202648__PinPresenceToAuthId.sql
@@ -1,3 +1,3 @@
-ALTER TABLE user_presences ADD COLUMN auth_id BIGINT NOT NULL;
+ALTER TABLE user_presences ADD COLUMN auth_id BIGINT NOT NULL DEFAULT 0;
 CREATE INDEX ON user_presences (user_id, last_seen_at);
 CREATE INDEX ON user_presences (user_id, auth_id);
