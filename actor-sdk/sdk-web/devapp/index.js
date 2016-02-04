9e932a40c4645af44bf5b851601f009061baa6b2
John Doe <speed@actor.im>
2016-02-04 03:21:48 +0300

    feat+refactor(web): add forceLocale option to sdk, move render method in DialogSection to bottom

--- actor-sdk/sdk-web/devapp/index.js
+++ actor-sdk/sdk-web/devapp/index.js
@@ -5,16 +5,19 @@
 import ActorSDK from '../src/sdk/actor-sdk';
 import ActorSDKDelegate from '../src/sdk/actor-sdk-delegate';
 
-const endpoints = [
-  'wss://front1-ws-mtproto-api-rev2.actor.im',
-  'wss://front2-ws-mtproto-api-rev2.actor.im'
-];
-
 const components = {};
 const actions = {};
 const l18n = {};
 
-const delegate = new ActorSDKDelegate(components, actions, l18n);
+const options = {
+  endpoints: [
+    'wss://front1-ws-mtproto-api-rev2.actor.im',
+    'wss://front2-ws-mtproto-api-rev2.actor.im'
+  ],
+  delegate: new ActorSDKDelegate(components, actions, l18n),
+  isExperimental: true
+};
 
-const app = new ActorSDK({endpoints, delegate, isExperimental: true});
+const app = new ActorSDK(options);
+
 app.startApp();
