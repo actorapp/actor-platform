50bd459b341ec53b08a05f9aa097d7fdf61d5f0d
Steve Kite <steve@actor.im>
2016-02-05 20:34:52 +0300

    wip(core): Refactoring of calls

---
+++ actor-sdk/sdk-core/core/core-js/src/main/java/im/actor/core/js/providers/JsWebRTCProvider.java
@@ -0,0 +1,31 @@
+package im.actor.core.js.providers;
+
+import im.actor.core.WebRTCProvider;
+
+public class JsWebRTCProvider implements WebRTCProvider {
+
+    @Override
+    public void onIncomingCall() {
+
+    }
+
+    @Override
+    public void onOutgoingCall() {
+
+    }
+
+    @Override
+    public void onCallStarted() {
+
+    }
+
+    @Override
+    public void onSignalingReceived(byte[] data) {
+
+    }
+
+    @Override
+    public void onCallEnded() {
+
+    }
+}
