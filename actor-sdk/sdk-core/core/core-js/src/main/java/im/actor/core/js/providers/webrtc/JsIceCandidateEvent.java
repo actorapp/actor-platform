5b00d240e65f2c5421e7ab00d8cf792768c18922
Steve Kite <steve@actor.im>
2016-02-06 14:46:41 +0300

    wip(js): Working one-way audio in webrtc

---
+++ actor-sdk/sdk-core/core/core-js/src/main/java/im/actor/core/js/providers/webrtc/JsIceCandidateEvent.java
@@ -0,0 +1,24 @@
+package im.actor.core.js.providers.webrtc;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+public class JsIceCandidateEvent extends JavaScriptObject {
+    protected JsIceCandidateEvent() {
+
+    }
+
+    public final native String getId()/*-{
+        if (this.id == undefined) {
+            return "";
+        }
+        return this.id;
+    }-*/;
+
+    public final native int getLabel()/*-{
+        return this.label;
+    }-*/;
+
+    public final native String getCandidate()/*-{
+        return this.candidate;
+    }-*/;
+}
