abde6b469d822c3439e778f149a0f85cb13b0717
Steve Kite <steve@actor.im>
2016-02-07 12:54:12 +0300

    feat(js): Voice capture cancelling

--- actor-sdk/sdk-core/core/core-js/src/main/java/im/actor/core/js/providers/webrtc/JsAudio.java
+++ actor-sdk/sdk-core/core/core-js/src/main/java/im/actor/core/js/providers/webrtc/JsAudio.java
@@ -1,12 +1,30 @@
 package im.actor.core.js.providers.webrtc;
 
-public class JsAudio {
-    public static native void playStream(JsMediaStream stream)/*-{
-        var audio = $wnd.document.createElement('AUDIO');
-        $wnd.console.warn(audio);
-        var url = URL.createObjectURL(stream);;
-        $wnd.console.warn(url);
-        audio.src = URL.createObjectURL(stream);
-        audio.play();
+import com.google.gwt.core.client.JavaScriptObject;
+
+public class JsAudio extends JavaScriptObject {
+
+    public static native JsAudio create()/*-{
+        return {audio: $wnd.document.createElement('AUDIO')};
+    }-*/;
+
+    protected JsAudio() {
+
+    }
+
+    public final native void setStream(JsMediaStream mediaStream)/*-{
+        this.audio.src = URL.createObjectURL(mediaStream);
+    }-*/;
+
+    public final native void play()/*-{
+        this.audio.play();
+    }-*/;
+
+    public final native void pause()/*-{
+        this.audio.pause();
+    }-*/;
+
+    public final native void reset()/*-{
+        this.audio.src = ""
     }-*/;
 }
