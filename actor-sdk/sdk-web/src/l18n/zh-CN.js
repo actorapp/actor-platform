95bb1c1d5450ef51bc0d3ac1ec7cd811bf899b50
Oleg Shilov <me@olegshilov.com>
2016-02-08 20:42:03 +0300

    feat(web): add Spanish translation, remove untranslated strings

--- actor-sdk/sdk-web/src/l18n/zh-CN.js
+++ actor-sdk/sdk-web/src/l18n/zh-CN.js
@@ -1,23 +1,6 @@
 export default {
   'locales': 'zh-CN',
   'messages': {
-    // Login
-    'login': {
-      'signIn': 'Sign in',
-      'wrong': 'Wrong?',
-      'phone': 'Phone number',
-      'email': 'Email address',
-      'authCode': 'Auth code',
-      'yourName': 'Your name',
-      'errors': {
-        'numberInvalid': 'Invalid number',
-        'nameInvalid': 'Invalid name',
-        'codeInvalid': 'Invalid code',
-        'codeExpired': 'Phone code is expired',
-        'codeWait': 'Try to request code later'
-      }
-    },
-
     // Menus
     'menu': {
       // Sidebar menu
@@ -25,144 +8,38 @@ export default {
       'addToContacts': '添加联系人',
       'createGroup': '创建群',
       'configureIntegrations': '配置集成',
-      'helpAndFeedback': 'Help & Feedback',
-      'twitter': 'Our twitter',
       'preferences': '设置',
       'signOut': '登出'
     },
 
     // Buttons
     'button': {
-      'ok': 'Ok',
-      'cancel': 'Cancel',
-      'done': 'Done',
-      'requestCode': 'Request code',
-      'checkCode': 'Check code',
-      'signUp': 'Sign up',
       'add': '添加',
       'send': '发送',
-      'sendAll': 'Send all',
       'createGroup': '创建群',
-      'addMembers': '添加成员',
-      'quickSearch': 'Quick search',
-      'close': 'Close'
-    },
-
-    // Messages
-    'message': {
-      'pin': 'Pin',
-      'reply': 'Reply',
-      'forward': 'Forward',
-      'download': 'Download',
-      'delete': 'Delete',
-      'uploading': 'Uploading...',
-      'welcome': {
-        'private': '<p>This is the very beginning of your private conversation with <strong>{name}</strong>.</p><p>All messages here are private between the two of you.</p>',
-        'group': {
-          'main': '<p>This is the very beginning of <strong>{name}</strong> group conversation which created by {creator}.</p>',
-          'you': 'You',
-          'actions': {
-            'start': 'You can ',
-            'end': ' to this conversation.',
-            'invite': 'invite more users'
-          }
-        }
-      }
+      'addMembers': '添加成员'
     },
 
     // Compose
     'compose': {
-      'attach': 'Attachment',
       'sendFile': '发送文件',
       'sendPhoto': '发送照片',
-      'send': '发送',
-      'markdown': {
-        'bold': 'bold',
-        'italic': 'italics',
-        'preformatted': 'preformatted'
-      },
-      'dropzone': 'Drop here to send.'
+      'send': '发送'
     },
 
     // Modals
     'modal': {
-      'profile': {
-        'title': 'Profile',
-        'name': 'Full name',
-        'nick': 'Nickname',
-        'phone': 'Phone number',
-        'email': 'Email',
-        'about': 'About',
-        'avatarChange': 'Change avatar',
-        'avatarRemove': 'Remove'
-      },
-      'group': {
-        'title': 'Edit group',
-        'name': 'Group title',
-        'about': 'Group about',
-        'avatarChange': 'Change avatar',
-        'avatarRemove': 'Remove'
-      },
-      'crop': {
-        'title': 'Crop picture'
-      },
-      'contacts': {
-        'title': 'People',
-        'search': 'Search contacts',
-        'notFound': 'Sorry, no users found.'
-      },
-      'groups': {
-        'title': 'Groups',
-        'search': 'Search',
-        'loading': 'Loading',
-        'notFound': 'No matches found for <strong>{query}</strong>.'
-      },
-      'attachments': {
-        'title': 'Send file',
-        'name': 'Filename',
-        'type': 'Type',
-        'size': 'Size',
-        'extra': 'Extra',
-        'sendAsPicture': 'Send as picture'
-      },
       'addContact': {
         'title': '添加联系人',
-        'query': 'Email, nickname or phone',
-        'phone': '手机号',
-        'notFound': 'User with such data is not found',
-        'empty': 'Start typing to search people',
-        'searching': 'Search for "{query}"'
+        'phone': '手机号'
       },
       'createGroup': {
         'title': '创建群',
         'groupName': '群名称'
-      },
-      'quickSearch': {
-        'title': 'Search everywhere',
-        'placeholder': 'Start typing',
-        'toNavigate': 'to navigate',
-        'toSelect': 'to select',
-        'toClose': 'to close',
-        'openDialog': 'Open conversation',
-        'startDialog': 'Start new conversation',
-        'notFound': 'No matches found for <strong>{query}</strong>.<br/>Have you spelled it correctly?'
-      },
-      'confirm': {
-        'logout': 'Do you really want to leave?',
-        'leave': 'Do you really want to leave this conversation?',
-        'clear': 'Do you really want to clear this conversation?',
-        'delete': 'Do you really want to delete this conversation?',
-        'removeContact': 'You really want to remove {name} from your contacts?',
-        'kick': 'Are you sure you want kick {name}'
       }
     },
 
     // Profiles
-    'profile': {
-      'email': 'email',
-      'phone': 'phone',
-      'nickname': 'nickname'
-    },
     'createdBy': '创建者',
     'addPeople': '添加成员',
     'more': '更多',
@@ -185,10 +62,6 @@ export default {
       '=1 {# 成员}' +
       'other {# 成员}' +
     '}',
-    'kick': 'Kick',
-    'integrationToken': 'Integration Token',
-    'integrationTokenHint': 'If you have programming chops, or know someone who does, this integration token allow the most flexibility and communication with your own systems.',
-    'integrationTokenHelp': 'Learn how to integrate',
 
     // Modals
     'inviteModalTitle': '添加更多成员',
