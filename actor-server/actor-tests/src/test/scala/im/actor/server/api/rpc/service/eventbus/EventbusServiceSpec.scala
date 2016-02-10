000b35879b10c0ec1ba6dfccea1121b5aa2ebe82
prettynatty <fear@loathing.in>
2016-02-11 02:09:04 +0300

    fix(server:eventbus): start EventbusService on startup

--- actor-server/actor-tests/src/test/scala/im/actor/server/api/rpc/service/eventbus/EventbusServiceSpec.scala
+++ actor-server/actor-tests/src/test/scala/im/actor/server/api/rpc/service/eventbus/EventbusServiceSpec.scala
@@ -1,7 +1,7 @@
 package im.actor.server.api.rpc.service.eventbus
 
-import im.actor.api.rpc.eventbus.{ApiEventBusDestination, ResponseCreateNewEventBus}
-import im.actor.server.{SeqUpdateMatchers, ImplicitSessionRegion, ImplicitAuthService, BaseAppSuite}
+import im.actor.api.rpc.eventbus.{ ApiEventBusDestination, ResponseCreateNewEventBus }
+import im.actor.server.{ SeqUpdateMatchers, ImplicitSessionRegion, ImplicitAuthService, BaseAppSuite }
 import im.actor.api.rpc._
 
 final class EventbusServiceSpec extends BaseAppSuite with ImplicitAuthService with ImplicitSessionRegion with SeqUpdateMatchers {
@@ -20,7 +20,7 @@ final class EventbusServiceSpec extends BaseAppSuite with ImplicitAuthService wi
       implicit val clientData = aliceClientData
 
       whenReady(service.handleCreateNewEventBus(None, None)) {
-        case Ok(ResponseCreateNewEventBus(id, deviceId)) => (id, deviceId)
+        case Ok(ResponseCreateNewEventBus(id, deviceId)) ⇒ (id, deviceId)
       }
     }
 
@@ -28,7 +28,7 @@ final class EventbusServiceSpec extends BaseAppSuite with ImplicitAuthService wi
       implicit val clientData = bobClientData
 
       whenReady(service.handleJoinEventBus(id, None)) {
-        case Ok(_) =>
+        case Ok(_) ⇒
       }
 
       service.handlePostToEventBus(id, Vector(ApiEventBusDestination(alice.id, Vector(aliceDeviceId))), Array[Byte](123))
