4d02d218f6e041fb1b964af6b8d45538be2eb392
Nikolay Tatarinov <rockjam@actor.im>
2016-02-10 18:14:10 +0300

    fix(server:messaging): keep right order of messages

--- actor-server/actor-core/src/main/scala/im/actor/server/user/UserOffice.scala
+++ actor-server/actor-core/src/main/scala/im/actor/server/user/UserOffice.scala
@@ -1,10 +1,5 @@
 package im.actor.server.user
 
-import scala.util.control.NoStackTrace
-
 object UserOffice {
-
-  case object InvalidAccessHash extends Exception with NoStackTrace
-
   def persistenceIdFor(userId: Int): String = s"User-${userId}"
 }
