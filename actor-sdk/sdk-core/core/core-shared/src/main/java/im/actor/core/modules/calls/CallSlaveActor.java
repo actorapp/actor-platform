0a999299c5b290e9c732c793784c744aff4efdb3
Steve Kite <steve@actor.im>
2016-02-13 08:53:01 +0300

    feat(webrtc): Added call answering support

--- actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/calls/CallSlaveActor.java
+++ actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/calls/CallSlaveActor.java
@@ -1,35 +1,112 @@
 package im.actor.core.modules.calls;
 
+import im.actor.core.api.ApiAnswerCall;
 import im.actor.core.api.ApiNeedOffer;
+import im.actor.core.api.ApiSwitchMaster;
 import im.actor.core.api.ApiWebRTCSignaling;
+import im.actor.core.api.rpc.RequestGetCallInfo;
+import im.actor.core.api.rpc.ResponseGetCallInfo;
 import im.actor.core.modules.ModuleContext;
-import im.actor.runtime.Log;
+import im.actor.runtime.function.Consumer;
 
 public class CallSlaveActor extends CallActor {
 
-    private static final String TAG = "CallSlaveActor";
+    private MasterNode masterNode;
+    private boolean isAnswerPending = false;
+    private long callId;
 
-    private final String busId;
-
-    public CallSlaveActor(String busId, ModuleContext context) {
+    public CallSlaveActor(long callId, ModuleContext context) {
         super(context);
-        this.busId = busId;
+        this.callId = callId;
     }
 
     @Override
     public void preStart() {
         super.preStart();
-        joinBus(busId);
+        api(new RequestGetCallInfo(callId)).then(new Consumer<ResponseGetCallInfo>() {
+            @Override
+            public void apply(final ResponseGetCallInfo responseGetCallInfo) {
+                joinBus(responseGetCallInfo.getEventBusId());
+            }
+        }).failure(new Consumer<Exception>() {
+            @Override
+            public void apply(Exception e) {
+                shutdown();
+            }
+        }).done(self());
     }
+
+    @Override
+    public void onBusStarted() {
+        super.onBusStarted();
+    }
+
+    public void onMasterNodeChanged(int fromUid, long fromDeviceId) {
+        masterNode = new MasterNode(fromUid, fromDeviceId);
+        if (isAnswerPending) {
+            isAnswerPending = false;
+            sendSignalingMessage(masterNode.getUid(), masterNode.getDeviceId(), new ApiAnswerCall());
+        }
+    }
+
+    public void onNeedOffer(int destUid, long destDeviceId) {
+        getPeer(destUid, destDeviceId).send(new PeerConnectionActor.OnOfferNeeded());
+    }
+
+    public void doAnswer() {
+        if (masterNode == null) {
+            isAnswerPending = true;
+        } else {
+            sendSignalingMessage(masterNode.getUid(), masterNode.getDeviceId(), new ApiAnswerCall());
+        }
+    }
+
+
+    //
+    // Messages
+    //
 
     @Override
     public void onSignalingMessage(int fromUid, long fromDeviceId, ApiWebRTCSignaling signaling) {
         if (signaling instanceof ApiNeedOffer) {
             ApiNeedOffer needOffer = (ApiNeedOffer) signaling;
-            Log.w(TAG, "Need offer for: " + needOffer.getUid());
-            getPeer(needOffer.getUid(), needOffer.getDevice()).send(new PeerConnectionActor.OnOfferNeeded());
+            onNeedOffer(needOffer.getUid(), needOffer.getDevice());
+        } else if (signaling instanceof ApiSwitchMaster) {
+            onMasterNodeChanged(fromUid, fromDeviceId);
         } else {
             super.onSignalingMessage(fromUid, fromDeviceId, signaling);
         }
+    }
+
+    @Override
+    public void onReceive(Object message) {
+        if (message instanceof DoAnswer) {
+            doAnswer();
+        } else {
+            super.onReceive(message);
+        }
+    }
+
+    private class MasterNode {
+
+        private int uid;
+        private long deviceId;
+
+        public MasterNode(int uid, long deviceId) {
+            this.uid = uid;
+            this.deviceId = deviceId;
+        }
+
+        public int getUid() {
+            return uid;
+        }
+
+        public long getDeviceId() {
+            return deviceId;
+        }
+    }
+
+    public static class DoAnswer {
+
     }
 }
