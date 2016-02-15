d6e7b7cc369d78cf884e0ef865c9b4ad4d13b950
Steve Kite <steve@actor.im>
2016-02-16 00:30:37 +0300

    feat(webrtc): Codec parameters optimization

--- actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/calls/PeerConnectionActor.java
+++ actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/calls/PeerConnectionActor.java
@@ -287,13 +287,17 @@ public class PeerConnectionActor extends ModuleActor {
             for (SDPMedia m : sdpScheme.getMediaLevel()) {
 
                 // Disabling media streams
-                m.setMode(SDPMediaMode.INACTIVE);
+                // m.setMode(SDPMediaMode.INACTIVE);
 
                 // Optimizing opus
                 if ("audio".equals(m.getType())) {
                     for (SDPCodec codec : m.getCodecs()) {
                         if ("opus".equals(codec.getName())) {
-
+                            codec.getFormat().put("maxcodedaudiobandwidth", "16000");
+                            codec.getFormat().put("maxaveragebitrate", "20000");
+                            codec.getFormat().put("stereo", "0");
+                            codec.getFormat().put("useinbandfec", "1");
+                            codec.getFormat().put("usedtx", "1");
                         }
                     }
                 }
