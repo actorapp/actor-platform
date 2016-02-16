9f1ad0e36f9811a76124ff2516e609f97cd9d8a1
prettynatty <fear@loathing.in>
2016-02-17 01:42:10 +0300

    fix(server): removed unsupported and probably not-working TLS support

--- actor-server/actor-frontend/src/main/scala/im/actor/server/frontend/TcpFrontend.scala
+++ actor-server/actor-frontend/src/main/scala/im/actor/server/frontend/TcpFrontend.scala
@@ -5,14 +5,13 @@ import akka.event.Logging
 import akka.stream.Materializer
 import akka.stream.scaladsl._
 import im.actor.server.session.SessionRegion
-import im.actor.tls.{ Tls, TlsContext }
 
 import scala.concurrent.duration._
 
 object TcpFrontend extends Frontend("tcp") {
   val IdleTimeout = 30.minutes
 
-  def start(host: String, port: Int, serverKeys: Seq[ServerKey], tlsContext: Option[TlsContext])(
+  def start(host: String, port: Int, serverKeys: Seq[ServerKey])(
     implicit
     sessionRegion: SessionRegion,
     system:        ActorSystem,
@@ -26,7 +25,7 @@ object TcpFrontend extends Frontend("tcp") {
           log.debug("New TCP connection from {}", localAddress)
 
           val mtProto = mtProtoBlueprint(serverKeys)
-          (tlsContext map (Tls.connection(_, flow)) getOrElse flow).joinMat(mtProto)(Keep.right).run()
+          flow.joinMat(mtProto)(Keep.right).run()
       })
       .run()
   }
