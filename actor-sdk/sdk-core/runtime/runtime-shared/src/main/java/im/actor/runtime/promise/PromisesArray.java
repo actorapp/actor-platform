6b915d2f76c1164914f4a19e2e3c0e55e1f072d0
kor_ka <badgrib@gmmail.com>
2016-02-16 19:31:32 +0300

    fix(core) promise to array

--- actor-sdk/sdk-core/runtime/runtime-shared/src/main/java/im/actor/runtime/promise/PromisesArray.java
+++ actor-sdk/sdk-core/runtime/runtime-shared/src/main/java/im/actor/runtime/promise/PromisesArray.java
@@ -77,7 +77,7 @@ public class PromisesArray<T> {
     public static <T> PromisesArray<T> ofPromises(Promise<T>... items) {
         ArrayList<Promise<T>> res = new ArrayList<>();
         Collections.addAll(res, items);
-        final Promise[] promises = (Promise[]) res.toArray();
+        final Promise[] promises = res.toArray(new Promise[res.size()]);
         return new PromisesArray<>(new PromiseFunc<Promise<T>[]>() {
             @Override
             public void exec(PromiseResolver<Promise<T>[]> executor) {
