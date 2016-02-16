687d99558bc12c82b7bfe61db236dc3ecc778f32
prettynatty <fear@loathing.in>
2016-02-16 18:35:56 +0300

    fix(server:webrtc): fixed error handling

--- actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/webrtc/WebrtcServiceImpl.scala
+++ actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/webrtc/WebrtcServiceImpl.scala
@@ -43,6 +43,7 @@ final class WebrtcServiceImpl(implicit system: ActorSystem, sessionRegion: Sessi
 
   override def onFailure: PartialFunction[Throwable, RpcError] = {
     case WebrtcCallErrors.CallAlreadyStarted ⇒ WebrtcErrors.CallAlreadyStareted
+    case WebrtcCallErrors.CallNotStarted => WebrtcErrors.CallNotStarted
+    case WebrtcCallErrors.NotAParticipant => WebrtcErrors.NotAParticipant
   }
-
 }
