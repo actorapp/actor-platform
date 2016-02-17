62572afcd379358d31680ee7a80614efb164b7e4
Steve Kite <steve@actor.im>
2016-02-17 07:23:30 +0300

    ref(runtime): Strip obsolete bouncy castle packages

---
+++ actor-sdk/sdk-core/runtime/runtime-generic/src/main/java/im/actor/runtime/generic/GenericCryptoProvider.java
@@ -0,0 +1,53 @@
+package im.actor.runtime.generic;
+
+import im.actor.runtime.*;
+import im.actor.runtime.Runtime;
+import im.actor.runtime.crypto.primitives.kuznechik.KuznechikFastEngine;
+
+public class GenericCryptoProvider implements CryptoRuntime {
+
+    private static boolean isLoaded = false;
+    private static final Object LOCk = new Object();
+
+    public GenericCryptoProvider() {
+        Runtime.dispatch(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+                byte[] tables = Assets.loadBinAsset("kuz_tables.bin");
+                if (tables != null) {
+                    KuznechikFastEngine.initDump(tables);
+                } else {
+                    KuznechikFastEngine.initCalc();
+                }
+                synchronized (LOCk) {
+                    isLoaded = true;
+                    LOCk.notifyAll();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void waitForCryptoLoaded() {
+        if (isLoaded) {
+            return;
+        }
+        synchronized (LOCk) {
+            if (isLoaded) {
+                return;
+            }
+            try {
+                LOCk.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
