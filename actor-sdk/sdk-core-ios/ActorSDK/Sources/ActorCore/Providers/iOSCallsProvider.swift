b81c556a6daa56c5804fed689f88be00c8d7c5e1
Steve Kite <steve@actor.im>
2016-02-17 15:15:32 +0300

    feat(core+ios): Incoming call ringtone

--- actor-sdk/sdk-core-ios/ActorSDK/Sources/ActorCore/Providers/iOSCallsProvider.swift
+++ actor-sdk/sdk-core-ios/ActorSDK/Sources/ActorCore/Providers/iOSCallsProvider.swift
@@ -8,20 +8,77 @@ import AVFoundation
 class iOSCallsProvider: NSObject, ACCallsProvider {
     
     var beepPlayer: AVAudioPlayer! = nil
+    var ringtonePlayer: AVAudioPlayer! = nil
+    var latestNotification: UILocalNotification!
     
     func onCallStartWithCallId(callId: jlong) {
         dispatchOnUi() {
+            
+            if ((Actor.getCallWithCallId(callId).state.get() as! ACCallState).toNSEnum() == ACCallState_Enum.CALLING_INCOMING) {
+                self.startRingtone()
+            } else {
+                self.stopRingtone()
+            }
+            
             let rootController = ActorSDK.sharedActor().bindedToWindow.rootViewController!
             rootController.presentViewController(AACallViewController(callId: callId), animated: true, completion: nil)
         }
     }
     
-    func onCallEndWithCallId(callId: jlong) {
+    func onCallAnsweredWithCallId(callId: jlong) {
         dispatchOnUi() {
-            
+            self.stopRingtone()
         }
     }
     
+    func onCallEndWithCallId(callId: jlong) {
+        dispatchOnUi() {
+            self.stopRingtone()
+        }
+    }
+    
+    private func startRingtone() {
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategorySoloAmbient, withOptions:
+                AVAudioSessionCategoryOptions.MixWithOthers)
+            try AVAudioSession.sharedInstance().setMode(AVAudioSessionModeDefault)
+            try AVAudioSession.sharedInstance().overrideOutputAudioPort(AVAudioSessionPortOverride.Speaker)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch let error as NSError {
+            print("Error: \(error.description)")
+        }
+        
+        if (self.ringtonePlayer == nil) {
+            do {
+                self.ringtonePlayer = try AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.framework.pathForResource("ringtone", ofType: "m4a")!))
+                self.ringtonePlayer.prepareToPlay()
+                self.ringtonePlayer.numberOfLoops = -1
+            } catch {
+                
+            }
+        }
+        
+        AudioServicesPlaySystemSound(1352)
+        
+        self.ringtonePlayer.play()
+        
+        if (self.latestNotification != nil) {
+            UIApplication.sharedApplication().cancelLocalNotification(self.latestNotification)
+            self.latestNotification = nil
+        }
+        self.latestNotification = UILocalNotification()
+        self.latestNotification.alertBody = "Answer it!"
+        UIApplication.sharedApplication().presentLocalNotificationNow(self.latestNotification)
+    }
+    
+    private func stopRingtone() {
+        ringtonePlayer.pause()
+        if (self.latestNotification != nil) {
+            UIApplication.sharedApplication().cancelLocalNotification(self.latestNotification)
+            self.latestNotification = nil
+        }
+    }
     
     func startOutgoingBeep() {
         do {
