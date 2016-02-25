51cbbb7ecccb243ca161ab1e67667ea40e04b933
Oleg Shilov <me@olegshilov.com>
2016-02-25 20:59:34 +0300

    feat(web): make recent dialogs component dumb, get dialogs list from store in sidebar component and send this list to recent dialogs component as props

--- actor-sdk/sdk-web/src/components/Sidebar.react.js
+++ actor-sdk/sdk-web/src/components/Sidebar.react.js
@@ -3,15 +3,26 @@
  */
 
 import React, { Component, PropTypes } from 'react';
+import { Container } from 'flux/utils';
 
 import DefaultHeaderSection from './sidebar/HeaderSection.react';
 import DefaultRecent from './sidebar/Recent.react';
 import QuickSearchButton from './sidebar/QuickSearchButton.react';
 
+import AllDialogsStore from '../stores/AllDialogsStore';
+
 class SidebarSection extends Component {
   constructor(props){
     super(props);
   }
+
+  static getStores = () => [AllDialogsStore];
+
+  static calculateState() {
+    return {
+      allDialogs: AllDialogsStore.getAllDialogs()
+    }
+  };
 
   static contextTypes = {
     delegate: PropTypes.object
@@ -19,6 +30,7 @@ class SidebarSection extends Component {
 
   render() {
     const { delegate } = this.context;
+    const { allDialogs } = this.state;
 
     let HeaderSection, Recent, FooterSection;
     if (delegate.components.sidebar !== null && typeof delegate.components.sidebar !== 'function') {
@@ -34,11 +46,11 @@ class SidebarSection extends Component {
     return (
       <aside className="sidebar">
         <HeaderSection/>
-        <Recent/>
+        <Recent dialogs={allDialogs}/>
         <FooterSection/>
       </aside>
     );
   }
 }
 
-export default SidebarSection;
+export default Container.create(SidebarSection, {pure: false});
