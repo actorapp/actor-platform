2a2c632c7429ef1d730ae4394eb8fd45433daf88
Gleb Putintsev <kor_ka@actor.im>
2016-02-29 12:25:39 +0300

    feat(android) implement wakeLock

--- actor-sdk/sdk-core/runtime/runtime-android/src/main/java/im/actor/runtime/android/power/AndroidWakeLock.java
+++ actor-sdk/sdk-core/runtime/runtime-android/src/main/java/im/actor/runtime/android/power/AndroidWakeLock.java
@@ -1,11 +1,33 @@
 package im.actor.runtime.android.power;
 
+import android.content.Context;
+import android.os.PowerManager;
+
+import im.actor.runtime.android.AndroidContext;
 import im.actor.runtime.power.WakeLock;
 
 public class AndroidWakeLock implements WakeLock {
 
+    private static PowerManager.WakeLock wakeLock;
+
+    static {
+        PowerManager powerManager = (PowerManager) AndroidContext.getContext().getSystemService(Context.POWER_SERVICE);
+        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                "ActorWakelock");
+    }
+
+    public AndroidWakeLock() {
+        acquire();
+    }
+
     @Override
     public void releaseLock() {
-        // TODO: Implement
+        wakeLock.release();
+    }
+
+    private static void acquire(){
+        if(!wakeLock.isHeld()){
+            wakeLock.acquire();
+        }
     }
 }
