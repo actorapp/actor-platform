69fe06b8a491ec8050b5ca1f7d2f2ce8068f3d4d
Steve Kite <steve@actor.im>
2016-02-29 05:59:17 +0300

    fix(runtime): Fixing compilation error

--- actor-sdk/sdk-core/runtime/runtime-shared/src/template/java/im/actor/runtime/LifecycleRuntimeProvider.java
+++ actor-sdk/sdk-core/runtime/runtime-shared/src/template/java/im/actor/runtime/LifecycleRuntimeProvider.java
@@ -1,8 +1,15 @@
 package im.actor.runtime;
 
+import im.actor.runtime.power.WakeLock;
+
 public class LifecycleRuntimeProvider implements LifecycleRuntime {
     @Override
     public void killApp() {
+        throw new RuntimeException("Dumb");
+    }
+
+    @Override
+    public WakeLock makeWakeLock() {
         throw new RuntimeException("Dumb");
     }
 }
