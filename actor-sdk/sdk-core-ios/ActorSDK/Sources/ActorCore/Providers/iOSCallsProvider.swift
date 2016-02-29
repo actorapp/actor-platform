b17b56a4b59a11564f76959a7255b4559d3f85e7
Steve Kite <steve@actor.im>
2016-02-29 05:49:12 +0300

    feat(ios): Restored vibration and sound from background notification

--- actor-sdk/sdk-core-ios/ActorSDK/Sources/ActorCore/Providers/iOSCallsProvider.swift
+++ actor-sdk/sdk-core-ios/ActorSDK/Sources/ActorCore/Providers/iOSCallsProvider.swift
@@ -12,7 +12,13 @@ class iOSCallsProvider: NSObject, ACCallsProvider {
     var latestNotification: UILocalNotification!
     
     func onCallStartWithCallId(callId: jlong) {
+        
         AAAudioManager.sharedAudio().callStart(Actor.getCallWithCallId(callId).isOutgoing)
+       
+        if !Actor.getCallWithCallId(callId).isOutgoing {
+            showNotification(callId)
+        }
+        
         dispatchOnUi() {
             let rootController = ActorSDK.sharedActor().bindedToWindow.rootViewController!
             rootController.presentViewController(AACallViewController(callId: callId), animated: true, completion: nil)
@@ -21,23 +27,45 @@ class iOSCallsProvider: NSObject, ACCallsProvider {
     
     func onCallAnsweredWithCallId(callId: jlong) {
         AAAudioManager.sharedAudio().callAnswered()
+        hideNotification()
     }
     
     func onCallEndWithCallId(callId: jlong) {
         AAAudioManager.sharedAudio().callEnd()
+        hideNotification()
+    }
+    
+    private func showNotification(callId: jlong) {
+        dispatchOnUi() {
+            let callVm = Actor.getCallWithCallId(callId)
+            if (self.latestNotification != nil) {
+                UIApplication.sharedApplication().cancelLocalNotification(self.latestNotification)
+                self.latestNotification = nil
+            }
+            self.latestNotification = UILocalNotification()
+            if callVm.peer.isGroup {
+                let groupName = Actor.getGroupWithGid(callVm.peer.peerId).getNameModel().get()
+                self.latestNotification.alertBody = "Group Call \(groupName)"
+            } else if callVm.peer.isPrivate {
+                let userName = Actor.getUserWithUid(callVm.peer.peerId).getNameModel().get()
+                self.latestNotification.alertBody = "Call from \(userName)"
+            }
+            self.latestNotification.soundName = "ringtone.m4a"
+            UIApplication.sharedApplication().presentLocalNotificationNow(self.latestNotification)
+        }
+    }
+    
+    private func hideNotification() {
+        dispatchOnUi() {
+            if (self.latestNotification != nil) {
+                UIApplication.sharedApplication().cancelLocalNotification(self.latestNotification)
+                self.latestNotification = nil
+            }
+        }
     }
     
     func startOutgoingBeep() {
-//        do {
-//            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayAndRecord, withOptions:
-//                AVAudioSessionCategoryOptions.AllowBluetooth)
-//            try AVAudioSession.sharedInstance().setMode(AVAudioSessionModeVoiceChat)
-//            try AVAudioSession.sharedInstance().overrideOutputAudioPort(AVAudioSessionPortOverride.None)
-//            try AVAudioSession.sharedInstance().setActive(true)
-//        } catch let error as NSError {
-//            print("Error: \(error.description)")
-//        }
-//        
+        
         if (beepPlayer == nil) {
             do {
                 beepPlayer = try AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.framework.pathForResource("tone", ofType: "m4a")!))
@@ -53,14 +81,5 @@ class iOSCallsProvider: NSObject, ACCallsProvider {
     
     func stopOutgoingBeep() {
         beepPlayer.stop()
-        
-//        do {
-//            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayAndRecord)
-//            try AVAudioSession.sharedInstance().setMode(AVAudioSessionModeVoiceChat)
-//            try AVAudioSession.sharedInstance().overrideOutputAudioPort(AVAudioSessionPortOverride.None)
-//            try AVAudioSession.sharedInstance().setActive(true)
-//        } catch let error as NSError {
-//            print("Error: \(error.description)")
-//        }
     }
 }
