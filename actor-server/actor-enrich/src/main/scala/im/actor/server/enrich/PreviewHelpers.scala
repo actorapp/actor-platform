b4716eacc74efbce1bd9f634a2eaeb3dfcdaf962
Nikolay Tatarinov <rockjam@actor.im>
2016-03-03 02:14:24 +0300

    refactor(server): use single implementation of message updating; improve RichMessages implementation

--- actor-server/actor-enrich/src/main/scala/im/actor/server/enrich/PreviewHelpers.scala
+++ actor-server/actor-enrich/src/main/scala/im/actor/server/enrich/PreviewHelpers.scala
@@ -1,52 +1,54 @@
 package im.actor.server.enrich
 
-import scala.concurrent.duration._
-import scala.concurrent.{ ExecutionContext, Future }
-import scala.util.{ Failure, Success, Try }
-
-import akka.http.scaladsl.HttpExt
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{ HttpEntity, HttpRequest, HttpResponse }
 import akka.stream.Materializer
 import akka.stream.scaladsl._
 import akka.stream.stage.{ Context, PushStage }
 import akka.util.ByteString
-
 import im.actor.server.enrich.PreviewMaker.Failures._
 import im.actor.server.enrich.PreviewMaker._
 
-object PreviewHelpers {
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.{ Failure, Success, Try }
 
-  def withRequest(request: ⇒ HttpRequest, handler: UpdateHandler)(function: HttpResponse ⇒ Future[PreviewResult])(implicit http: HttpExt, materializer: Materializer, ec: ExecutionContext): Future[PreviewResult] = {
+trait PreviewHelpers {
+
+  protected implicit val system: ActorSystem
+  protected implicit val ec: ExecutionContext
+  protected implicit val mat: Materializer
+
+  def withRequest(request: ⇒ HttpRequest, randomId: Long)(function: HttpResponse ⇒ Future[PreviewResult])(implicit system: ActorSystem): Future[PreviewResult] = {
     Try(request) match {
-      case Success(v) ⇒ http.singleRequest(v).flatMap(function).recover { case e: Exception ⇒ failedToMakePreview(handler, e.getMessage) }
-      case Failure(_) ⇒ Future.successful(failedToMakePreview(handler))
+      case Success(v) ⇒ Http().singleRequest(v).flatMap(function) recover { case e: Exception ⇒ failedToMakePreview(randomId, e.getMessage) }
+      case Failure(_) ⇒ Future.successful(failedToMakePreview(randomId))
     }
   }
 
-  def downloadDefault(entity: HttpEntity.Default, fileName: Option[String], handler: UpdateHandler, config: RichMessageConfig)(implicit materializer: Materializer, ec: ExecutionContext): Future[PreviewResult] = {
+  def downloadDefault(entity: HttpEntity.Default, fileName: Option[String], gp: GetPreview, maxSize: Long): Future[PreviewResult] = {
     val mediaType = entity.contentType.mediaType
     val contentLength = entity.contentLength
     (mediaType.isImage, contentLength) match {
-      case (true, length) if length <= config.maxSize ⇒
-        entity
-          .toStrict(10.seconds)
-          .map { body ⇒ PreviewSuccess(body.data, fileName, mediaType.value, handler) }
-          .recover { case e: Exception ⇒ failedToMakePreview(handler, e.getMessage) }
-      case (true, _)  ⇒ Future.successful(contentTooLong(handler))
-      case (false, _) ⇒ Future.successful(notAnImage(handler))
+      case (true, length) if length <= maxSize ⇒
+        entity.dataBytes.runFold(ByteString.empty)(_ ++ _)
+          .map { body ⇒ PreviewSuccess(body, fileName, mediaType.value, gp.clientUserId, gp.peer, gp.randomId) }
+          .recover { case e: Exception ⇒ failedToMakePreview(gp.randomId, e.getMessage) }
+      case (true, _)  ⇒ Future.successful(contentTooLong(gp.randomId))
+      case (false, _) ⇒ Future.successful(notAnImage(gp.randomId))
     }
   }
 
-  def downloadChunked(entity: HttpEntity.Chunked, fileName: Option[String], handler: UpdateHandler, config: RichMessageConfig)(implicit materializer: Materializer, ec: ExecutionContext): Future[PreviewResult] = {
+  def downloadChunked(entity: HttpEntity.Chunked, fileName: Option[String], gp: GetPreview, maxSize: Long): Future[PreviewResult] = {
     val mediaType = entity.contentType.mediaType
     mediaType.isImage match {
       case true ⇒
         entity.dataBytes
-          .via(sizeBoundingFlow(config.maxSize))
-          .runWith(Sink.fold(ByteString.empty) { (acc, el) ⇒ acc ++ el })
-          .map { body ⇒ PreviewSuccess(body, fileName, mediaType.value, handler) }
-          .recover { case e: Exception ⇒ failedToMakePreview(handler, e.getMessage) }
-      case false ⇒ Future.successful(notAnImage(handler))
+          .via(sizeBoundingFlow(maxSize))
+          .runFold(ByteString.empty)(_ ++ _)
+          .map { body ⇒ PreviewSuccess(body, fileName, mediaType.value, gp.clientUserId, gp.peer, gp.randomId) }
+          .recover { case e: Exception ⇒ failedToMakePreview(gp.randomId, e.getMessage) }
+      case false ⇒ Future.successful(notAnImage(gp.randomId))
     }
   }
 
