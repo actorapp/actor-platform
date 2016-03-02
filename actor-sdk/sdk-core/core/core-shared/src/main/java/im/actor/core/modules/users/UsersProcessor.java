b35596b12bdadd2ba0d7a79d9745c18555b78f08
Steve Kite <steve@actor.im>
2016-03-03 01:40:13 +0300

    fix(core): Mismatched access hash logging

--- actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/users/UsersProcessor.java
+++ actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/users/UsersProcessor.java
@@ -18,6 +18,7 @@ import im.actor.core.modules.sequence.Processor;
 import im.actor.core.modules.internal.contacts.ContactsSyncActor;
 import im.actor.core.modules.internal.messages.DialogsActor;
 import im.actor.core.modules.internal.messages.GroupedDialogsActor;
+import im.actor.runtime.Log;
 import im.actor.runtime.annotations.Verified;
 
 import static im.actor.core.util.JavaUtil.equalsE;
@@ -44,6 +45,12 @@ public class UsersProcessor extends AbsModule implements Processor {
                 if (!upd.getName().equals(saved.getName()) ||
                         !equalsE(upd.getAvatar(), saved.getAvatar())) {
                     onUserDescChanged(upd);
+                }
+            }
+
+            if (saved != null) {
+                if (saved.getAccessHash() != u.getAccessHash()) {
+                    Log.w("UsersProcessor", "User's Access Hash changed!");
                 }
             }
         }
