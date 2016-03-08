3aed2c91360cf71bd7985321bff42eafe3adbfec
Gleb Putintsev <kor_ka@actor.im>
2016-03-08 21:07:39 +0300

    feat(android): add ConversationActor delegate, custom display lists

---
+++ actor-sdk/sdk-core-android/android-app/src/main/java/im/actor/allmessages/OverHandlerActor.java
@@ -0,0 +1,119 @@
+package im.actor.allmessages;
+
+import java.util.ArrayList;
+
+import im.actor.core.entity.Message;
+import im.actor.core.entity.Peer;
+import im.actor.core.entity.PeerType;
+import im.actor.runtime.actors.Actor;
+import im.actor.runtime.storage.ListEngine;
+
+import static im.actor.sdk.util.ActorSDKMessenger.messenger;
+
+public class OverHandlerActor extends Actor {
+    ListEngine<MessageEx> allMessages = messenger().getCustomConversationEngine(new Peer(PeerType.PRIVATE, 1), "all", MessageEx.CREATOR);
+
+    public void onIncoming(Peer peer, ArrayList<Message> msgs) {
+        ArrayList<MessageEx> msgex= new ArrayList<MessageEx>();
+        for (Message m:msgs) {
+            msgex.add(new MessageEx(m, peer));
+        }
+        allMessages.addOrUpdateItems(msgex);
+    }
+
+    public void onIncoming(Peer peer, Message msg) {
+        allMessages.addOrUpdateItem(new MessageEx(msg, peer));
+    }
+
+    public void onUpdate(Peer peer, Message msg) {
+        allMessages.addOrUpdateItem(new MessageEx(msg, peer));
+    }
+
+    public void onDelete(Peer peer, long[] rids) {
+        allMessages.removeItems(rids);
+    }
+
+    @Override
+    public void onReceive(Object message) {
+        if(message instanceof Incoming){
+            ArrayList<Message> msgs = ((Incoming) message).getMsgs();
+            if(msgs!=null){
+                onIncoming(((Incoming) message).getPeer(), msgs);
+            } else {
+                onIncoming(((Incoming) message).getPeer(), ((Incoming) message).getMsg());
+            }
+        }else if(message instanceof Delete){
+            onDelete(((Delete) message).getPeer(), ((Delete) message).getRids());
+        }else if(message instanceof Update){
+            onUpdate(((Update) message).getPeer(), ((Update) message).getMsg());
+        }
+    }
+
+    public static class Incoming{
+        Peer peer;
+        Message msg;
+
+        ArrayList<Message> msgs;
+
+        public Incoming(Peer peer, Message msg) {
+            this.peer = peer;
+            this.msg = msg;
+        }
+
+        public Incoming(Peer peer, ArrayList<Message> msgs) {
+            this.peer = peer;
+            this.msgs = msgs;
+        }
+
+        public Peer getPeer() {
+            return peer;
+        }
+
+        public ArrayList<Message> getMsgs() {
+            return msgs;
+        }
+
+        public Message getMsg() {
+            return msg;
+        }
+    }
+
+    public static class Delete{
+        Peer peer;
+        long[] rids;
+
+        public Delete(Peer peer, long[] rids) {
+            this.peer = peer;
+            this.rids = rids;
+        }
+
+        public Peer getPeer() {
+            return peer;
+        }
+
+        public long[] getRids() {
+            return rids;
+        }
+    }
+
+
+    public static class Update{
+        Peer peer;
+        Message msg;
+
+        public Update(Peer peer, Message msg) {
+            this.peer = peer;
+            this.msg = msg;
+        }
+
+        public Peer getPeer() {
+            return peer;
+        }
+
+        public Message getMsg() {
+            return msg;
+        }
+    }
+
+
+}
