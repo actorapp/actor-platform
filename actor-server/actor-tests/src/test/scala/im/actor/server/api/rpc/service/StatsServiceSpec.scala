9e2e30a1ed1a4d328d2144385468e2e2a8850aea
rockjam <5min4eq.unity@gmail.com>
2016-03-11 23:14:25 +0300

    feat(server:rpc): StatsService implementation

---
+++ actor-server/actor-tests/src/test/scala/im/actor/server/api/rpc/service/StatsServiceSpec.scala
@@ -0,0 +1,65 @@
+package im.actor.server.api.rpc.service
+
+import im.actor.api.rpc.collections.{ ApiInt32Value, ApiMapValue, ApiMapValueItem, ApiStringValue }
+import im.actor.api.rpc.stats.{ ApiAppVisibleChanged, ApiUntypedEvent }
+import im.actor.api.rpc.{ AuthData, ClientData }
+import im.actor.server.api.rpc.service.stats.StatsServiceImpl
+import im.actor.server.persist.ClientStatsRepo
+import im.actor.server.{ BaseAppSuite, ImplicitAuthService, ImplicitSessionRegion }
+import play.api.libs.json.Json
+
+class StatsServiceSpec extends BaseAppSuite
+  with ImplicitSessionRegion
+  with ImplicitAuthService {
+
+  behavior of "StatsService"
+
+  it should "store events in database" in storeEvents()
+
+  it should "store params as json" in storeParamsAsJson()
+
+  val service = new StatsServiceImpl
+
+  def storeEvents() = {
+    val (user, userAuthId, userAuthSid, _) = createUser()
+    implicit val aliceClientData = ClientData(userAuthId, 1, Some(AuthData(user.id, userAuthSid)))
+
+    whenReady(service.handleStoreEvents(Vector(
+      ApiAppVisibleChanged(visible = true),
+      ApiAppVisibleChanged(visible = false)
+    )))(identity)
+
+    whenReady(db.run(ClientStatsRepo.findByUserId(user.id))) { stats ⇒
+      stats should have length 2
+      stats foreach { s ⇒
+        s.userId shouldEqual user.id
+        s.authId shouldEqual userAuthId
+        assert(s.event.contains("VisibleChanged"), "Event should contain VisibleChanged")
+      }
+    }
+  }
+
+  def storeParamsAsJson() = {
+    val (user, userAuthId, userAuthSid, _) = createUser()
+    implicit val aliceClientData = ClientData(userAuthId, 1, Some(AuthData(user.id, userAuthSid)))
+
+    whenReady(service.handleStoreEvents(Vector(
+      ApiUntypedEvent("AppCrash", Some(ApiMapValue(Vector(
+        ApiMapValueItem("exception", ApiStringValue("NullPointerException")),
+        ApiMapValueItem("line", ApiInt32Value(24))
+      ))))
+    )))(identity)
+
+    whenReady(db.run(ClientStatsRepo.findByUserId(user.id))) { stats ⇒
+      stats should have length 1
+      val stat = stats.head
+
+      val parts = stat.event split ";"
+      parts(0) shouldEqual "AppCrash"
+      val json = Json.parse(parts(1))
+      (json \ "exception").validate[String].asOpt shouldEqual Some("NullPointerException")
+      (json \ "line").validate[Int].asOpt shouldEqual Some(24)
+    }
+  }
+
+}
