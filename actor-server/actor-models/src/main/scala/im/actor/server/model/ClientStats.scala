5dd1ea6a0fc3d3b9d1c11b69844fe32405343eef
Nikolay Tatarinov <rockjam@actor.im>
2016-03-11 23:14:25 +0300

    feat(server:rpc): StatsService implementation

---
+++ actor-server/actor-models/src/main/scala/im/actor/server/model/ClientStats.scala
@@ -0,0 +1,6 @@
+package im.actor.server.model
+
+/**
+ * @param event event type, followed by all other fields, separated by semicolon(;)
+ */
+final case class ClientStats(id: Long, userId: Int, authId: Long, event: String)
---
+++ actor-server/actor-persist/src/main/scala/im/actor/server/persist/ClientStatsRepo.scala
@@ -0,0 +1,28 @@
+package im.actor.server.persist
+
+import im.actor.server.db.ActorPostgresDriver.api._
+import im.actor.server.model.ClientStats
+
+final class ClientStatsTable(tag: Tag) extends Table[ClientStats](tag, "client_stats") {
+  def id = column[Long]("id", O.PrimaryKey)
+  def userId = column[Int]("user_id")
+  def authId = column[Long]("auth_id")
+  def event = column[String]("event")
+
+  def * = (id, userId, authId, event) <> (ClientStats.tupled, ClientStats.unapply)
+}
+
+object ClientStatsRepo {
+  val stats = TableQuery[ClientStatsTable]
+
+  private def byUserId(userId: Rep[Int]) = stats.filter(_.userId === userId)
+  private def byAuthId(authId: Rep[Long]) = stats.filter(_.authId === authId)
+
+  private val byUserIdC = Compiled(byUserId _)
+  private val byAuthIdC = Compiled(byAuthId _)
+
+  def create(s: Seq[ClientStats]) = stats ++= s
+
+  def findByUserId(userId: Int): DBIO[Seq[ClientStats]] = byUserIdC(userId).result
+  def findByAuthId(authId: Long): DBIO[Seq[ClientStats]] = byAuthIdC(authId).result
+}
