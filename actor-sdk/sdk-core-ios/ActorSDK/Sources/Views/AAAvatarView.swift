79b64137db71b374c665cf44ac58c793b6487c2b
Steve Kite <steve@actor.im>
2016-03-14 12:18:28 +0300

    wip(core+ios): Better Avatar prformance

--- actor-sdk/sdk-core-ios/ActorSDK/Sources/Views/AAAvatarView.swift
+++ actor-sdk/sdk-core-ios/ActorSDK/Sources/Views/AAAvatarView.swift
@@ -10,7 +10,7 @@ public enum AAAvatarType {
     case Square
 }
 
-public class AAAvatarView: UIView, YYAsyncLayerDelegate {
+public class AAAvatarView: UIView, YYAsyncLayerDelegate, ACFileEventCallback {
     
     var avatarTyle: AAAvatarType = .Rounded {
         didSet(v) {
@@ -36,6 +36,7 @@ public class AAAvatarView: UIView, YYAsyncLayerDelegate {
     
     private var title: String?
     private var id: Int?
+    private var fileId: jlong?
     private var fileName: String?
     private var showPlaceholder: Bool = false
     
@@ -47,10 +48,25 @@ public class AAAvatarView: UIView, YYAsyncLayerDelegate {
         self.backgroundColor = UIColor.clearColor()
         self.opaque = false
         self.contentMode = .Redraw;
+        Actor.subscribeToDownloads(self)
     }
 
     public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        Actor.unsubscribeFromDownloads(self)
+    }
+    
+    public func onDownloadedWithLong(fileId: jlong) {
+        if self.fileId == fileId {
+            dispatchOnUi {
+                if self.fileId == fileId {
+                    self.layer.setNeedsDisplay()
+                }
+            }
+        }
     }
     
     //
@@ -82,141 +98,24 @@ public class AAAvatarView: UIView, YYAsyncLayerDelegate {
 //        }
     }
     
-    public func bind(title: String, id: Int, avatar: ACAvatar!, clearPrev: Bool = true) {
+    public func bind(title: String, id: Int, avatar: ACAvatar?, clearPrev: Bool = true) {
         
         
         self.title = title
         self.id = id
         self.fileName = nil
-        self.showPlaceholder = false
+        
+        if avatar?.smallImage != nil {
+            let fileRef = avatar!.smallImage.fileReference!
+            self.fileId = fileRef.getFileId()
+            self.showPlaceholder = false
+            Actor.startDownloadingWithReference(fileRef)
+        } else {
+           self.fileId = nil
+            self.showPlaceholder = true
+        }
         
         self.layer.setNeedsDisplay()
-        
-//        
-//        title = title.smallValue()
-//        
-//        let needSmallAvatar: Bool = frameSize < 100
-//        
-//        var fileLocation: ACFileReference?
-//        if needSmallAvatar == true {
-//            fileLocation = avatar?.smallImage?.fileReference
-//        } else {
-//            fileLocation = avatar?.smallImage?.fileReference
-//        }
-//        
-//        if (bindedId != nil && bindedId == id) {
-//            var notChanged = true;
-//            
-//            // Is Preview changed
-//            notChanged = notChanged && bindedTitle == title
-//            
-//            // Is avatar changed
-//            if (fileLocation == nil) {
-//                if (bindedFileId != nil) {
-//                    notChanged = false
-//                }
-//            } else if (bindedFileId == nil) {
-//                if (fileLocation != nil) {
-//                    notChanged = false
-//                }
-//            } else {
-//                if (bindedFileId != fileLocation?.getFileId()) {
-//                    notChanged = false
-//                }
-//            }
-//            
-//            if (notChanged) {
-//                return
-//            }
-//        }
-//        
-//        unbind(clearPrev)
-//        
-//        self.bindedId = id
-//        self.bindedTitle = title
-//        
-//        if (fileLocation == nil) {
-//            
-//            requestId++
-//            
-//            self.image = nil
-//            let callbackRequestId = requestId
-//            let callbackBindedId = bindedId
-//            dispatchBackground() {
-//                if callbackRequestId == self.requestId {
-//                    
-//                    let image = Placeholders.avatarPlaceholder(callbackBindedId, size: self.frameSize, title: title, rounded: self.avatarType == .Rounded)
-//                    
-//                    dispatchOnUi() {
-//                        if callbackRequestId == self.requestId {
-//                            self.image = image
-//                        }
-//                    }
-//                }
-//            }
-//            
-//            return
-//        }
-//        
-//        // Load avatar
-//        
-//        let cached = checkCache(frameSize, id: Int64(fileLocation!.getFileId()))
-//        if (cached != nil) {
-//            self.image = cached
-//            return
-//        }
-//        
-//        if needSmallAvatar == false {
-//            let smallFileLocation = avatar?.smallImage?.fileReference
-//            var smallAvatarCached = checkCache(40, id: Int64(smallFileLocation!.getFileId()))
-//            if smallAvatarCached == nil {
-//                smallAvatarCached = checkCache(48, id: Int64(smallFileLocation!.getFileId()))
-//            }
-//            if smallAvatarCached != nil {
-//                image = smallAvatarCached
-//            }
-//        }
-//        
-//        requestId++
-//        
-//        let callbackRequestId = requestId
-//        self.bindedFileId = fileLocation?.getFileId()
-//        self.callback = AAFileCallback(onDownloaded: { (reference) -> () in
-//            
-//            if (callbackRequestId != self.requestId) {
-//                return;
-//            }
-//            
-//            var image = UIImage(contentsOfFile: CocoaFiles.pathFromDescriptor(reference));
-//            
-//            if (image == nil) {
-//                return
-//            }
-//            
-//            if (self.avatarType == .Rounded) {
-//                image = image!.roundImage(self.frameSize)
-//            }
-//            
-//            dispatchOnUi {
-//                if (callbackRequestId != self.requestId) {
-//                    return;
-//                }
-//                
-//                self.putToCache(self.frameSize, id: Int64(self.bindedFileId!), image: image!)
-//                if (self.enableAnimation) {
-//                    UIView.transitionWithView(self, duration: 0.4, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: { () -> Void in
-//                        self.image = image;
-//                    }, completion: nil)
-//                } else {
-//                    self.image = image;
-//                }
-//            }
-//        })
-//        let fl = fileLocation
-//        let c = callback
-//        dispatchBackground {
-//            Actor.bindRawFileWithReference(fl, autoStart: true, withCallback: c)
-//        }
     }
     
     public func unbind() {
@@ -274,102 +173,167 @@ public class AAAvatarView: UIView, YYAsyncLayerDelegate {
         
         let _id = id!
         let _title = title!
+        let _fileName = fileName
+        let _fileId = fileId
+        let _showPlaceholder = showPlaceholder
         
         res.display = { (context: CGContext,  size: CGSize, isCancelled: () -> Bool) -> () in
+            
             let r = min(size.width, size.height) / 2
-            let colors = ActorSDK.sharedActor().style.avatarColors
-            let color = colors[_id % colors.count].CGColor
+            let filePath: String?
+            if _fileName != nil {
+                filePath = _fileName
+            } else if _fileId != nil {
+                let desc = Actor.findDownloadedDescriptorWithFileId(_fileId!)
+                if desc != nil {
+                    filePath = CocoaFiles.pathFromDescriptor(desc!)
+                } else {
+                    filePath = nil
+                }
+            } else {
+                filePath = nil
+            }
             
-            // Background
+            if isCancelled() {
+                return
+            }
             
-            CGContextSetFillColorWithColor(context, color)
             
-            CGContextAddArc(context, r, r, r, CGFloat(M_PI * 0), CGFloat(M_PI * 2), 0)
-
-            CGContextDrawPath(context, .Fill)
+            if filePath == nil && _showPlaceholder {
             
-            // Text
+                let colors = ActorSDK.sharedActor().style.avatarColors
+                let color = colors[_id % colors.count].CGColor
+                
+                // Background
+                
+                CGContextSetFillColorWithColor(context, color)
+                
+                CGContextAddArc(context, r, r, r, CGFloat(M_PI * 0), CGFloat(M_PI * 2), 0)
+                
+                CGContextDrawPath(context, .Fill)
+                
+                // Text
+                
+                UIColor.whiteColor().set()
+                
+                let font = UIFont.systemFontOfSize(r)
+                var rect = CGRectMake(0, 0, r * 2, r * 2)
+                rect.origin.y = round(CGFloat(r * 2 * 47 / 100) - font.pointSize / 2)
+                
+                let style : NSMutableParagraphStyle = NSParagraphStyle.defaultParagraphStyle().mutableCopy() as! NSMutableParagraphStyle
+                style.alignment = NSTextAlignment.Center
+                style.lineBreakMode = NSLineBreakMode.ByWordWrapping
+                
+                let short = _title.trim().smallValue()
+                
+                short.drawInRect(rect, withAttributes: [NSParagraphStyleAttributeName:style, NSFontAttributeName:font,
+                    NSForegroundColorAttributeName:ActorSDK.sharedActor().style.avatarTextColor])
+                
+                if isCancelled() {
+                    return
+                }
+            } else if let fp = filePath {
+                
+                // TODO: Load Image
+                
+                let image = UIImage(contentsOfFile: fp)
+                
+                if isCancelled() {
+                    return
+                }
+                
+                if image != nil {
+                    image!.drawInRect(CGRectMake(0, 0, r * 2, r * 2))
+                } else {
+                    // Clean BG
+                    CGContextSetFillColorWithColor(context, UIColor.whiteColor().CGColor)
+                    
+                    CGContextAddArc(context, r, r, r, CGFloat(M_PI * 0), CGFloat(M_PI * 2), 0)
+                    
+                    CGContextDrawPath(context, .Fill)
+                }
+                
+                if isCancelled() {
+                    return
+                }
+            } else {
+                // Clean BG
+                CGContextSetFillColorWithColor(context, UIColor.whiteColor().CGColor)
+                
+                CGContextAddArc(context, r, r, r, CGFloat(M_PI * 0), CGFloat(M_PI * 2), 0)
+                
+                CGContextDrawPath(context, .Fill)
+                
+                if isCancelled() {
+                    return
+                }
+            }
             
-            UIColor.whiteColor().set()
-            
-            let font = UIFont.systemFontOfSize(r)
-            var rect = CGRectMake(0, 0, r * 2, r * 2)
-            rect.origin.y = round(CGFloat(r * 2 * 47 / 100) - font.pointSize / 2);
-            
-            let style : NSMutableParagraphStyle = NSParagraphStyle.defaultParagraphStyle().mutableCopy() as! NSMutableParagraphStyle
-            style.alignment = NSTextAlignment.Center
-            style.lineBreakMode = NSLineBreakMode.ByWordWrapping;
-            
-            let short = _title.trim().smallValue()
-            
-            short.drawInRect(rect, withAttributes: [NSParagraphStyleAttributeName:style, NSFontAttributeName:font,
-                NSForegroundColorAttributeName:ActorSDK.sharedActor().style.avatarTextColor])
-
             // Border
             
-            CGContextSetStrokeColorWithColor(context, UIColor(red: 0, green: 0, blue: 0, alpha: 0x10/255.0).CGColor);
-            CGContextAddArc(context, r, r, r, CGFloat(M_PI * 0), CGFloat(M_PI * 2), 0);
-            CGContextDrawPath(context, .Stroke);
+            CGContextSetStrokeColorWithColor(context, UIColor(red: 0, green: 0, blue: 0, alpha: 0x10/255.0).CGColor)
+            CGContextAddArc(context, r, r, r, CGFloat(M_PI * 0), CGFloat(M_PI * 2), 0)
+            CGContextDrawPath(context, .Stroke)
         }
         return res
     }
 }
-
-class Placeholders {
-    
-    class func avatarPlaceholder(index: jint, size: Int, title: NSString, rounded: Bool) -> UIImage {
-        let colors = ActorSDK.sharedActor().style.avatarColors
-        let color = colors[Int(abs(index)) % colors.count].CGColor
-        
-        UIGraphicsBeginImageContextWithOptions(CGSize(width: size, height: size), false, UIScreen.mainScreen().scale);
-        let context = UIGraphicsGetCurrentContext();
-        
-        // Background
-        
-        CGContextSetFillColorWithColor(context, color);
-        
-        if rounded {
-            CGContextAddArc(context, CGFloat(size)/2, CGFloat(size)/2, CGFloat(size)/2, CGFloat(M_PI * 0), CGFloat(M_PI * 2), 0);
-        } else {
-            CGContextAddRect(context, CGRect(x: 0, y: 0, width: size, height: size))
-        }
-        
-        CGContextDrawPath(context, .Fill);
-        
-        // Text
-        
-        UIColor.whiteColor().set()
-        
-        let font = UIFont.systemFontOfSize(CGFloat(size / 2));
-        var rect = CGRectMake(0, 0, CGFloat(size), CGFloat(size))
-        rect.origin.y = round(CGFloat(size * 47 / 100) - font.pointSize / 2);
-        
-        let style : NSMutableParagraphStyle = NSParagraphStyle.defaultParagraphStyle().mutableCopy() as! NSMutableParagraphStyle
-        style.alignment = NSTextAlignment.Center
-        style.lineBreakMode = NSLineBreakMode.ByWordWrapping;
-        
-        title.drawInRect(rect, withAttributes: [NSParagraphStyleAttributeName:style, NSFontAttributeName:font,
-            NSForegroundColorAttributeName:ActorSDK.sharedActor().style.avatarTextColor])
-        
-        // Border
-        
-        if rounded {
-            CGContextSetStrokeColorWithColor(context, UIColor(red: 0, green: 0, blue: 0, alpha: 0x10/255.0).CGColor);
-            CGContextAddArc(context,CGFloat(size)/2, CGFloat(size)/2, CGFloat(size)/2, CGFloat(M_PI * 0), CGFloat(M_PI * 2), 0);
-            CGContextDrawPath(context, .Stroke);
-        }
-        
-        let image = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        return image;
-    }
-    
-}
-
-private class AvatarLayer: YYAsyncLayer {
-    
-}
-
-private class AvatarRender {
-    
-}
+//
+//class Placeholders {
+//    
+//    class func avatarPlaceholder(index: jint, size: Int, title: NSString, rounded: Bool) -> UIImage {
+//        let colors = ActorSDK.sharedActor().style.avatarColors
+//        let color = colors[Int(abs(index)) % colors.count].CGColor
+//        
+//        UIGraphicsBeginImageContextWithOptions(CGSize(width: size, height: size), false, UIScreen.mainScreen().scale);
+//        let context = UIGraphicsGetCurrentContext();
+//        
+//        // Background
+//        
+//        CGContextSetFillColorWithColor(context, color);
+//        
+//        if rounded {
+//            CGContextAddArc(context, CGFloat(size)/2, CGFloat(size)/2, CGFloat(size)/2, CGFloat(M_PI * 0), CGFloat(M_PI * 2), 0);
+//        } else {
+//            CGContextAddRect(context, CGRect(x: 0, y: 0, width: size, height: size))
+//        }
+//        
+//        CGContextDrawPath(context, .Fill);
+//        
+//        // Text
+//        
+//        UIColor.whiteColor().set()
+//        
+//        let font = UIFont.systemFontOfSize(CGFloat(size / 2));
+//        var rect = CGRectMake(0, 0, CGFloat(size), CGFloat(size))
+//        rect.origin.y = round(CGFloat(size * 47 / 100) - font.pointSize / 2);
+//        
+//        let style : NSMutableParagraphStyle = NSParagraphStyle.defaultParagraphStyle().mutableCopy() as! NSMutableParagraphStyle
+//        style.alignment = NSTextAlignment.Center
+//        style.lineBreakMode = NSLineBreakMode.ByWordWrapping;
+//        
+//        title.drawInRect(rect, withAttributes: [NSParagraphStyleAttributeName:style, NSFontAttributeName:font,
+//            NSForegroundColorAttributeName:ActorSDK.sharedActor().style.avatarTextColor])
+//        
+//        // Border
+//        
+//        if rounded {
+//            CGContextSetStrokeColorWithColor(context, UIColor(red: 0, green: 0, blue: 0, alpha: 0x10/255.0).CGColor);
+//            CGContextAddArc(context,CGFloat(size)/2, CGFloat(size)/2, CGFloat(size)/2, CGFloat(M_PI * 0), CGFloat(M_PI * 2), 0);
+//            CGContextDrawPath(context, .Stroke);
+//        }
+//        
+//        let image = UIGraphicsGetImageFromCurrentImageContext();
+//        UIGraphicsEndImageContext();
+//        return image;
+//    }
+//    
+//}
+//
+//private class AvatarLayer: YYAsyncLayer {
+//    
+//}
+//
+//private class AvatarRender {
+//    
+//}
