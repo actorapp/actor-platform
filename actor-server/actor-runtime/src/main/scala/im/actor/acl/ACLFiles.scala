784988d638d84400193a85548589fc7daf0d1790
Nikolay Tatarinov <rockjam@actor.im>
2016-03-15 20:40:51 +0300

    fix(server:acl): deprecate md5 digest; use sha-256 instead

--- actor-server/actor-runtime/src/main/scala/im/actor/acl/ACLFiles.scala
+++ actor-server/actor-runtime/src/main/scala/im/actor/acl/ACLFiles.scala
@@ -6,7 +6,7 @@ object ACLFiles extends ACLFiles
 
 trait ACLFiles extends ACLBase {
   def fileAccessHash(fileId: Long, accessSalt: String)(implicit s: ActorSystem): Long =
-    hash(s"$fileId:$accessSalt:${secretKey()}")
+    hashObsolete(s"$fileId:$accessSalt:${secretKey()}")
 
   def fileUrlBuilderSecret(seed: String, expire: Long)(implicit s: ActorSystem): Long =
     hash(s"$seed:$expire:${secretKey()}")
