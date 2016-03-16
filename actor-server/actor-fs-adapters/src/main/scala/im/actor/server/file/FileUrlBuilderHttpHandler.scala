951af96a140b4a0fc2d4d0fa5c3684b3b9db9023
Nikolay Tatarinov <rockjam@actor.im>
2016-03-16 18:17:31 +0300

    fix(server:files): reimplement file url builder

--- actor-server/actor-fs-adapters/src/main/scala/im/actor/server/file/FileUrlBuilderHttpHandler.scala
+++ actor-server/actor-fs-adapters/src/main/scala/im/actor/server/file/FileUrlBuilderHttpHandler.scala
@@ -15,6 +15,8 @@ import im.actor.server.persist.files.FileRepo
 import im.actor.util.log.AnyRefLogSource
 import org.apache.commons.codec.digest.HmacUtils
 import scodec.bits.BitVector
+import scodec._
+import scodec.codecs.{ byte, int32, long }
 
 object FileUrlBuilderRejections {
   case object SecretExpiredRejection extends Rejection
@@ -22,8 +24,14 @@ object FileUrlBuilderRejections {
   case object IncorrectSignatureRejection extends Rejection
 }
 
+final case class Seed(version: Byte, expire: Int, randomPart: Long)
+object SeedDecoder {
+  implicit val seedDecoder = (byte :: int32 :: long(64)).as[Seed]
+}
+
 private[file] final class FileUrlBuilderHttpHandler(fsAdapter: FileStorageAdapter)(implicit system: ActorSystem) extends HttpHandler with AnyRefLogSource {
   import FileUrlBuilderRejections._
+  import SeedDecoder._
 
   private val db = DbExtension(system).db
   private val log = Logging(system, this)
@@ -46,10 +54,11 @@ private[file] final class FileUrlBuilderHttpHandler(fsAdapter: FileStorageAdapte
 
   // format: OFF
   def routes: Route =
-    handleRejections(myRejectionHandler) {
-      defaultVersion {
-        pathPrefix("fileUrlBuilder") {
-          parameter("fileId".as[Long]) { fileId =>
+    extractRequest { request =>
+      log.debug("Got file url builder request: {}", request)
+      handleRejections(myRejectionHandler) {
+        defaultVersion {
+          pathPrefix("files" / SignedLongNumber) { fileId =>
             get {
               validateBuilderRequest(fileId) { case (fileModel, accessHash) =>
                 onSuccess(fsAdapter.getFileDownloadUrl(fileModel, accessHash)) {
@@ -64,24 +73,55 @@ private[file] final class FileUrlBuilderHttpHandler(fsAdapter: FileStorageAdapte
     }
   // format: ON
 
+  // monad transformers could make this code easier
   def validateBuilderRequest(fileId: Long): Directive1[(FileModel, Long)] =
-    parameters(("seed", "expire".as[Long], "signature", "accessHash".as[Long])) tflatMap {
-      case (seed, expire, signature, accessHash) ⇒
-        val secret = BitVector.fromLong(ACLFiles.fileUrlBuilderSecret(seed, expire)).toByteArray
-        if (signature == HmacUtils.hmacSha256Hex(secret, s"$fileId$accessHash".getBytes)) {
-          if (isExpired(expire)) {
-            reject(SecretExpiredRejection)
-          } else {
-            onSuccess(db.run(FileRepo.find(fileId))) flatMap {
-              case Some(file) ⇒ provide((file, accessHash))
-              case None       ⇒ reject(FileNotFoundRejection)
+    parameter("signature") flatMap {
+      case s ⇒
+        s split "_" match {
+          case Array(originalSeed, originalSignature) ⇒
+            (for {
+              bitSeed ← BitVector.fromHex(originalSeed)
+              seed ← Codec.decode[Seed](bitSeed).toOption
+            } yield seed) match {
+              case Some(DecodeResult(seed, BitVector.empty)) ⇒
+                onSuccess(db.run(FileRepo.find(fileId))) flatMap {
+                  case Some(file) ⇒
+                    val expire = seed.expire
+                    val secret = BitVector.fromLong(ACLFiles.fileUrlBuilderSecret(originalSeed, expire)).toHex
+                    val accessHash = ACLFiles.fileAccessHash(file.id, file.accessSalt)
+                    val calculatedSignature = HmacUtils.hmacSha256Hex(secret, s"$fileId$accessHash")
+                    if (originalSignature == calculatedSignature) {
+                      val now = Instant.now
+                      if (isExpired(expire, now)) {
+                        log.debug(
+                          "Signature expired. Signature: {}, expire: {}, now: {}",
+                          originalSignature, expire, Instant.now
+                        )
+                        reject(SecretExpiredRejection)
+                      } else {
+                        provide((file, accessHash))
+                      }
+                    } else {
+                      log.debug(
+                        "Incorrect signature. Signature from request: {}, calculated signature: {}",
+                        originalSignature, calculatedSignature
+                      )
+                      reject(IncorrectSignatureRejection)
+                    }
+                  case None ⇒
+                    log.debug("File not found, id: {}", fileId)
+                    reject(FileNotFoundRejection)
+                }
+              case _ ⇒
+                log.debug("Unable to decode seed: {}", originalSeed)
+                reject(IncorrectSignatureRejection)
             }
-          }
-        } else {
-          reject(IncorrectSignatureRejection)
+          case _ ⇒
+            log.debug("Wrong query signature: {}", s)
+            reject(IncorrectSignatureRejection)
         }
     }
 
-  def isExpired(expire: Long): Boolean = Instant.ofEpochSecond(expire).isAfter(Instant.now)
+  def isExpired(expire: Int, now: Instant): Boolean = Instant.ofEpochSecond(expire.toLong).isAfter(now)
 
 }
