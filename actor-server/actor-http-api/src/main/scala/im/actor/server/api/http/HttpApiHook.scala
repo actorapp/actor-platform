aaaf74925e7ac3a1c6752d667937a24e4f6973e9
rockjam <5min4eq.unity@gmail.com>
2016-03-16 23:10:16 +0300

    feat(server:http): allow custom rejection handlers

--- actor-server/actor-http-api/src/main/scala/im/actor/server/api/http/HttpApiHook.scala
+++ actor-server/actor-http-api/src/main/scala/im/actor/server/api/http/HttpApiHook.scala
@@ -1,17 +1,19 @@
 package im.actor.server.api.http
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.server.Route
-import im.actor.hook.{ SyncHook0, SyncHooksStorage0, HooksControl }
+import akka.http.scaladsl.server.{ RejectionHandler, Route }
+import im.actor.hook.{ HooksControl, SyncHook0, SyncHooksStorage0 }
 
 import scala.concurrent.ExecutionContext
 
 object HttpApiHook {
   abstract class RoutesHook(system: ActorSystem) extends SyncHook0[Route]
+  abstract class RejectionsHook(system: ActorSystem) extends SyncHook0[RejectionHandler]
 }
 
 final class HttpApiHookControl(implicit ec: ExecutionContext) extends HooksControl {
   import HttpApiHook._
 
   val routesHook = new SyncHooksStorage0[RoutesHook, Route]()
+  val rejectionsHook = new SyncHooksStorage0[RejectionsHook, RejectionHandler]()
 }
