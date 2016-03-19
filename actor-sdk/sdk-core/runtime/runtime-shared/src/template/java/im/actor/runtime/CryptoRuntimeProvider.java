1117f1a7b2c66d65da3223583a63fcfdcfa250c5
Steve Kite <steve@actor.im>
2016-03-20 00:10:12 +0300

    fix(runtime): Fixing template class

--- actor-sdk/sdk-core/runtime/runtime-shared/src/template/java/im/actor/runtime/CryptoRuntimeProvider.java
+++ actor-sdk/sdk-core/runtime/runtime-shared/src/template/java/im/actor/runtime/CryptoRuntimeProvider.java
@@ -1,11 +1,17 @@
 package im.actor.runtime;
 
+import im.actor.runtime.crypto.BlockCipher;
 import im.actor.runtime.crypto.Digest;
 
 public class CryptoRuntimeProvider implements CryptoRuntime {
 
     @Override
     public Digest SHA256() {
+        throw new RuntimeException("Dumb");
+    }
+
+    @Override
+    public BlockCipher AES128(byte[] key) {
         throw new RuntimeException("Dumb");
     }
 
