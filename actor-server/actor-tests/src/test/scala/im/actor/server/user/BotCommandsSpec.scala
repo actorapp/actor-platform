4e7fa43a96d9ed158940ee4e4c844acd0ea3d44c
rockjam <5min4eq.unity@gmail.com>
2016-03-24 22:35:29 +0300

    test(server): add bot command spec

--- actor-server/actor-tests/src/test/scala/im/actor/server/user/BotCommandsSpec.scala
+++ actor-server/actor-tests/src/test/scala/im/actor/server/user/BotCommandsSpec.scala
@@ -1,9 +1,18 @@
 package im.actor.server.user
 
+import im.actor.api.rpc.messaging.ApiTextMessage
+import im.actor.api.rpc.users.UpdateUserBotCommandsChanged
+import im.actor.api.rpc.{ AuthData, ClientData }
 import im.actor.server.bots.BotCommand
-import im.actor.server.{ BaseAppSuite, ImplicitAuthService, ImplicitSessionRegion }
+import im.actor.server._
+import im.actor.server.api.rpc.service.messaging.MessagingServiceImpl
+import im.actor.server.sequence.{ SeqState, SeqUpdatesExtension }
 
-class BotCommandsSpec extends BaseAppSuite with ImplicitAuthService with ImplicitSessionRegion {
+class BotCommandsSpec extends BaseAppSuite
+  with ImplicitAuthService
+  with ImplicitSessionRegion
+  with MessagingSpecHelpers
+  with SeqUpdateMatchers {
 
   behavior of "User's bot commands"
 
@@ -11,7 +20,10 @@ class BotCommandsSpec extends BaseAppSuite with ImplicitAuthService with Implici
 
   it should "add and remove" in addAndRemove()
 
+  it should "send updates to related users, when commands change" in changeCommands()
+
   val userExt = UserExtension(system)
+  implicit val msgService = new MessagingServiceImpl()
 
   def dontAddWrong() = {
     val (botUser, _, _, _) = createUser()
@@ -62,8 +74,8 @@ class BotCommandsSpec extends BaseAppSuite with ImplicitAuthService with Implici
     }
 
     // respond with success when trying to remove same command multiple times
-    whenReady(userExt.removeBotCommand(botUser.id, fireCommand.slashCommand))(identity)
-    whenReady(userExt.removeBotCommand(botUser.id, fireCommand.slashCommand))(identity)
+    whenReady(removeCommand(botUser.id, fireCommand.slashCommand))(identity)
+    whenReady(removeCommand(botUser.id, fireCommand.slashCommand))(identity)
 
     whenReady(userExt.getApiStruct(botUser.id, 0, 0L)) { resp ⇒
       resp.botCommands should have size 1
@@ -72,7 +84,52 @@ class BotCommandsSpec extends BaseAppSuite with ImplicitAuthService with Implici
     }
   }
 
+  def changeCommands() = {
+    val (alice, aliceAuthId, aliceAuthSid, _) = createUser()
+    val sessionId = createSessionId()
+    val clientData = ClientData(aliceAuthId, sessionId, Some(AuthData(alice.id, aliceAuthSid)))
+
+    val (botUser, _, _, _) = createUser()
+
+    {
+      // send message to note relation
+      implicit val cd = clientData
+      sendPrivateMessage(botUser.id, ApiTextMessage("text", Vector.empty, None))
+    }
+
+    val runCommand = BotCommand("run", "Run away as fast as you can", Some("run"))
+
+    whenReady(addCommand(botUser.id, runCommand))(identity)
+
+    {
+      implicit val cd = clientData
+      expectUpdate(classOf[UpdateUserBotCommandsChanged]) { upd ⇒
+        upd.userId shouldEqual botUser.id
+        upd.commands should have size 1
+        val run = upd.commands.head
+        run.slashCommand shouldEqual runCommand.slashCommand
+        run.description shouldEqual runCommand.description
+        run.locKey shouldEqual runCommand.locKey
+      }
+    }
+
+    val SeqState(seq, _) = whenReady(SeqUpdatesExtension(system).getSeqState(alice.id))(identity)
+
+    whenReady(removeCommand(botUser.id, runCommand.slashCommand))(identity)
+
+    {
+      implicit val cd = clientData
+      expectUpdate(seq, classOf[UpdateUserBotCommandsChanged]) { upd ⇒
+        upd.userId shouldEqual botUser.id
+        upd.commands shouldBe empty
+      }
+    }
+  }
+
   private def addCommand(userId: Int, commmand: BotCommand) =
     userExt.addBotCommand(userId, commmand)
+
+  private def removeCommand(userId: Int, slashCommand: String) =
+    userExt.removeBotCommand(userId, slashCommand)
 
 }
