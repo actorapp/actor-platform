77764eff59d4fa2a48dcaa35e0521d4b49815819
Nikolay Tatarinov <rockjam@actor.im>
2016-03-25 23:01:59 +0300

    fix(server:bots): compilation error

--- actor-server/actor-runtime/src/main/scala/im/actor/concurrent/ImActorLogging.scala
+++ actor-server/actor-runtime/src/main/scala/im/actor/concurrent/ImActorLogging.scala
@@ -3,8 +3,8 @@ package im.actor.concurrent
 import akka.actor.DiagnosticActorLogging
 
 trait ImActorLogging extends DiagnosticActorLogging {
-  def markFailure[T](f: => T): T = {
-    log.mdc(log.mdc + ("failure" -> true))
+  def markFailure[T](f: ⇒ T): T = {
+    log.mdc(log.mdc + ("failure" → true))
     f
   }
 }
