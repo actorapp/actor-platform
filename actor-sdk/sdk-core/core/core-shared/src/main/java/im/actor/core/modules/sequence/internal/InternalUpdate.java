31a9c1a4b1cebb0697ed583ffb29db9d0d55966c
Steve Kite <steve@actor.im>
2016-03-26 07:05:23 +0300

    feat(core): Reorganized modules packages

---
+++ actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/sequence/internal/InternalUpdate.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2015 Actor LLC. <https://actor.im>
+ */
+
+package im.actor.core.modules.sequence.internal;
+
+public class InternalUpdate {
+}
---
+++ actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/stickers/StickersModule.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2015 Actor LLC. <https://actor.im>
+ */
+
+package im.actor.core.modules.stickers;
+
+import im.actor.core.modules.AbsModule;
+import im.actor.core.modules.ModuleContext;
+import im.actor.core.modules.stickers.StickersActor;
+import im.actor.core.viewmodel.StickersVM;
+import im.actor.runtime.actors.ActorCreator;
+import im.actor.runtime.actors.ActorRef;
+import im.actor.runtime.actors.Props;
+
+import static im.actor.runtime.actors.ActorSystem.system;
+
+public class StickersModule extends AbsModule {
+
+    private ActorRef stickersActor;
+    private StickersVM stickersVM;
+
+    public StickersModule(ModuleContext context) {
+        super(context);
+    }
+
+    public void run() {
+        this.stickersVM = new StickersVM();
+        this.stickersActor = system().actorOf(Props.create(new ActorCreator() {
+            @Override
+            public StickersActor create() {
+                return new StickersActor(context());
+            }
+        }), "actor/stickers");
+    }
+
+    public ActorRef getStickersActor() {
+        return stickersActor;
+    }
+
+    public StickersVM getStickersVM() {
+        return stickersVM;
+    }
+}
