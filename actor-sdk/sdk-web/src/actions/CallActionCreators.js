d007b9c323f6eeebc3e2b793bc836bde2adbeb6c
John Doe <speed@actor.im>
2016-03-29 19:48:09 +0300

    ref(web): change binding interface

--- actor-sdk/sdk-web/src/actions/CallActionCreators.js
+++ actor-sdk/sdk-web/src/actions/CallActionCreators.js
@@ -5,31 +5,31 @@
 import { dispatch, dispatchAsync } from '../dispatcher/ActorAppDispatcher';
 import { ActionTypes, CallTypes } from '../constants/ActorAppConstants';
 import ActorClient from '../utils/ActorClient';
+
 import CallStore from '../stores/CallStore';
 
-const HIDE_MODAL_AFTER = 3000;
+import ActionCreators from './ActionCreators';
 
-export default {
+class CallActionCreators extends ActionCreators {
   hide() {
     dispatch(ActionTypes.CALL_MODAL_HIDE);
-  },
+  }
 
   handleCall(event) {
     const { id, type } = event;
     switch (type) {
       case CallTypes.STARTED:
-        ActorClient.bindCall(id, this.setCall);
+        this.setBindings('call', [
+          ActorClient.bindCall(id, this.setCall)
+        ]);
         dispatch(ActionTypes.CALL_MODAL_OPEN, { id });
         break;
       case CallTypes.ENDED:
-        setTimeout(() => {
-          ActorClient.unbindCall(id, this.setCall);
-          if (CallStore.isOpen()) dispatch(ActionTypes.CALL_MODAL_HIDE);
-        }, HIDE_MODAL_AFTER);
+        this.removeBindings('call');
+        dispatch(ActionTypes.CALL_MODAL_HIDE)
         break;
-      default:
     }
-  },
+  }
 
   makeCall(peerId) {
     dispatchAsync(ActorClient.makeCall(peerId), {
@@ -37,7 +37,7 @@ export default {
       success: ActionTypes.CALL_SUCCESS,
       failure: ActionTypes.CALL_ERROR
     }, { peerId });
-  },
+  }
 
   makeGroupCall(peerId) {
     dispatchAsync(ActorClient.makeGroupCall(peerId), {
@@ -45,28 +45,30 @@ export default {
       success: ActionTypes.CALL_SUCCESS,
       failure: ActionTypes.CALL_ERROR
     }, { peerId });
-  },
+  }
 
   setCall(call) {
     dispatch(ActionTypes.CALL_CHANGED, { call });
-  },
+  }
 
   answerCall(callId) {
     ActorClient.answerCall(callId);
     dispatch(ActionTypes.CALL_ANSWER, { callId })
-  },
+  }
 
   endCall(callId) {
     ActorClient.endCall(callId);
     dispatch(ActionTypes.CALL_END, { callId })
-  },
+  }
 
   toggleCallMute(callId) {
     ActorClient.toggleCallMute(callId);
     dispatch(ActionTypes.CALL_MUTE_TOGGLE, { callId })
-  },
-  
+  }
+
   toggleFloating() {
     dispatch(ActionTypes.CALL_FLOAT_TOGGLE)
   }
 }
+
+export default new CallActionCreators();
