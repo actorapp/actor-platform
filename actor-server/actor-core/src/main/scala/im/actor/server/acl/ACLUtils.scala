9f9dc4c0e501e39ee1e7f38b6e523cd8f142fba3
n1ghtw1sh <116516842@qq.com>
2016-03-30 10:02:11 +0800

    change password to md5 encrypto

--- actor-server/actor-core/src/main/scala/im/actor/server/acl/ACLUtils.scala
+++ actor-server/actor-core/src/main/scala/im/actor/server/acl/ACLUtils.scala
@@ -114,13 +114,19 @@ object ACLUtils extends ACLBase with ACLFiles {
    * @param password
    * @return (hash, salt)
    */
+  lazy private val md5handle = java.security.MessageDigest.getInstance("MD5")
+  def md5(password: String): Hash = {
+    md5handle.digest(password.getBytes)
+
+  }
+
   def hashPassword(password: String): (Hash, Salt) = {
     val seedBytes = 20
 
     val random = new SecureRandom()
     val salt = random.generateSeed(seedBytes)
 
-    (hashPassword(password, salt), salt)
+    (md5(password), salt)
   }
 
   def hashPassword(password: String, salt: Array[Byte]): Hash = {
@@ -129,7 +135,8 @@ object ACLUtils extends ACLBase with ACLFiles {
 
     val spec = new PBEKeySpec(password.toCharArray, salt, iterations, hashBytes * 8)
     val skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
-    skf.generateSecret(spec).getEncoded
+    /*skf.generateSecret(spec).getEncoded*/
+    md5(password)
   }
 
   def checkPassword(userId: Int, password: String)(implicit ec: ExecutionContext): DBIO[Boolean] =
