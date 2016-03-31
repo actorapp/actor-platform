dd91a94b7b3f99171ef644ea2f72d43528e5ee33
Oleg Shilov <me@olegshilov.com>
2016-03-31 17:56:53 +0300

    fix+feat(web): remove shadow from tooltip, add more translated tooltips

--- actor-sdk/sdk-web/src/l18n/ru-RU.js
+++ actor-sdk/sdk-web/src/l18n/ru-RU.js
@@ -299,6 +299,19 @@ export default {
         in_progress: 'Звонок: {time}',
         ended: 'завершен'
       }
+    },
+
+    tooltip: {
+      toolbar: {
+        info: 'Информация о текущей беседе',
+        favorite: 'Добавить/удалить в избранное'
+      },
+      recent: {
+        groupList: 'Список групповых диалогов',
+        privateList: 'Список приватных диалогов',
+        addContact: 'Добавить новый контакт',
+        createGroup: 'Создать группу'
+      }
     }
   }
 };
