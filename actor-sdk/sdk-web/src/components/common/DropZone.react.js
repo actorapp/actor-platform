6e9ff844c116622ad65838769fd15d1f82697e55
Nikita Gusakov <dev@nkt.me>
2016-04-04 17:30:46 +0300

    fix(web): make it possible to drag files from chrome's download bar

--- actor-sdk/sdk-web/src/components/common/DropZone.react.js
+++ actor-sdk/sdk-web/src/components/common/DropZone.react.js
@@ -2,97 +2,125 @@
  * Copyright (C) 2015-2016 Actor LLC. <https://actor.im>
  */
 
-import { union, without } from 'lodash';
 import React, { Component, PropTypes } from 'react';
 import classnames from 'classnames';
 
-let targetCollection = [];
-
-export default class DropZone extends Component {
+class DropZone extends Component {
   static propTypes = {
-    children: PropTypes.node,
-
-    onDropComplete: PropTypes.func.isRequired,
-
-    // Callbacks
-    onDragEnterCallback: PropTypes.func,
-    onDragLeaveCallback: PropTypes.func,
-    onDropCallback: PropTypes.func
+    children: PropTypes.node.isRequired,
+    onDropComplete: PropTypes.func.isRequired
   };
 
   constructor(props) {
     super(props);
 
+    this.dragging = false;
     this.state = {
       isActive: false,
       isHovered: false
     };
 
+    this.onWindowDragEnter = this.onWindowDragEnter.bind(this);
+    this.onWindowDragOver = this.onWindowDragOver.bind(this);
+    this.onWindowDragLeave = this.onWindowDragLeave.bind(this);
+
+    this.onDrop = this.onDrop.bind(this);
+    this.onDragEnter = this.onDragEnter.bind(this);
+    this.onDragLeave = this.onDragLeave.bind(this);
+  }
+
+  componentDidMount() {
     window.addEventListener('dragenter', this.onWindowDragEnter, false);
     window.addEventListener('dragover', this.onWindowDragOver, false);
     window.addEventListener('dragleave', this.onWindowDragLeave, false);
-    window.addEventListener('drop', this.onWindowDragLeave, false);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('dragenter', this.onWindowDragEnter, false);
-    window.removeEventListener('dragover', this.onWindowDragOver, false);
-    window.removeEventListener('dragleave', this.onWindowDragLeave, false);
-    window.removeEventListener('drop', this.onWindowDragLeave, false);
+    window.addEventListener('dragenter', this.onWindowDragEnter, false);
+    window.addEventListener('dragover', this.onWindowDragOver, false);
+    window.addEventListener('dragleave', this.onWindowDragLeave, false);
   }
 
-  onWindowDragEnter = (event) => {
-    const { onDragEnterCallback } = this.props;
-    event.preventDefault();
-
-    if (targetCollection.length === 0) {
-      this.setState({isActive: true});
-      onDragEnterCallback && onDragEnterCallback();
+  onWindowDragEnter() {
+    this.dragging = true;
+    if (this.state.isActive) {
+      return;
     }
 
-    targetCollection = union(targetCollection, [event.target]);
-  };
-  onWindowDragOver = (event) => event.preventDefault();
-  onWindowDragLeave = (event) => {
-    const { onDragLeaveCallback } = this.props;
+    this.setState({isActive: true});
+  }
+
+  onWindowDragOver() {
+    this.dragging = true;
+  }
+
+  onWindowDragLeave() {
+    this.dragging = false;
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      if (!this.dragging) {
+        this.setState({isActive: false});
+      }
+    }, 300);
+  }
+
+  onDrop(event) {
     event.preventDefault();
+    event.stopPropagation();
 
-    targetCollection = without(targetCollection, event.target);
-
-    if (targetCollection.length === 0) {
-      this.setState({isActive: false});
-      onDragLeaveCallback && onDragLeaveCallback();
-    }
-  };
-
-  onDragEnter = () => this.setState({isHovered: true});
-  onDragLeave = () => this.setState({isHovered: false});
-  onDrop = (event) => {
-    const { onDropCallback, onDropComplete } = this.props;
     this.onDragLeave();
-    this.onWindowDragLeave(event);
-    onDropCallback && onDropCallback();
-    onDropComplete(event.dataTransfer.files);
-  };
+    this.props.onDropComplete(event.dataTransfer.files);
+  }
+
+  onDragOver(event) {
+    // Makes it possible to drag files from chrome's download bar
+    // http://stackoverflow.com/questions/19526430/drag-and-drop-file-uploads-from-chrome-downloads-bar
+    try {
+      const effect = event.dataTransfer.effectAllowed;
+      if (effect === 'move' || effect === 'linkMove') {
+        event.dataTransfer.dropEffect = 'move';
+      } else {
+        event.dataTransfer.dropEffect = 'copy';
+      }
+    } catch (e) {
+      // do nothing
+    }
+
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  onDragEnter() {
+    this.setState({isHovered: true});
+  }
+
+  onDragLeave() {
+    this.setState({isHovered: false});
+  }
 
   render() {
     const { isActive, isHovered } = this.state;
 
-    const dropzoneClassName = classnames('dropzone', {
+    if (!isActive) {
+      return null;
+    }
+
+    const className = classnames('dropzone', {
       'dropzone--hover': isHovered
     });
 
-    if (isActive) {
-      return (
-        <div className={dropzoneClassName}
-             onDragEnter={this.onDragEnter}
-             onDragLeave={this.onDragLeave}
-             onDrop={this.onDrop}>
-          {this.props.children || 'Drop here'}
-        </div>
-      );
-    } else {
-      return null;
-    }
+    return (
+      <div
+        className={className}
+        onDrop={this.onDrop}
+        onDragOver={this.onDragOver}
+        onDragEnter={this.onDragEnter}
+        onDragLeave={this.onDragLeave}
+      >
+        {this.props.children}
+      </div>
+    );
   }
 }
+
+export default DropZone;
