2a29c30d6436cc35f6e3af9b35bdbd321f35dc2e
Nikita Gusakov <nkt@actor.im>
2016-04-06 23:14:57 +0300

    wip(web): add message art open/close by mouse

--- actor-sdk/sdk-web/src/components/messageArt/MessageArt.react.js
+++ actor-sdk/sdk-web/src/components/messageArt/MessageArt.react.js
@@ -10,16 +10,39 @@ import Trigger from './Trigger.react';
 import Popup from './Popup.react';
 
 class MessageArt extends Component {
+  static propTypes = {
+    isActive: PropTypes.bool.isRequired,
+    onOpen: PropTypes.func.isRequired,
+    onClose: PropTypes.func.isRequired,
+    onSelect: PropTypes.func.isRequired,
+    onStickerSelect: PropTypes.func.isRequired
+  }
+
+  static defaultProps = {
+    onOpen: MessageArtActionCreators.open,
+    onClose: MessageArtActionCreators.close
+  }
+
   constructor(props) {
     super(props);
 
-    this.openMessageArtPopup = MessageArtActionCreators.open.bind(this);
-    this.closeMessageArtPopup = MessageArtActionCreators.close.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+    this.onMouseEnter = this.onMouseEnter.bind(this);
   }
-  static propTypes = {
-    isActive: PropTypes.bool.isRequired,
-    onSelect: PropTypes.func.isRequired,
-    onStickerSelect: PropTypes.func.isRequired
+
+  onMouseLeave() {
+    this.closeTimeout = setTimeout(this.props.onClose, 300);
+  }
+
+  onMouseEnter() {
+    if (this.closeTimeout) {
+      clearTimeout(this.closeTimeout);
+      this.closeTimeout = false;
+    }
+
+    if (!this.props.isActive) {
+      this.props.onOpen();
+    }
   }
 
   renderPopup() {
@@ -30,7 +53,8 @@ class MessageArt extends Component {
       <Popup
         onSelect={onSelect}
         onStickerSelect={onStickerSelect}
-        onClose={this.closeMessageArtPopup}
+        onMouseEnter={this.onMouseEnter}
+        onMouseLeave={this.onMouseLeave}
       />
     );
   }
@@ -41,9 +65,12 @@ class MessageArt extends Component {
     return (
       <div className="message-art">
 
-        <Trigger onMouseEnter={this.openMessageArtPopup}
-                 isActive={isActive}
-                 isDotVisible>
+        <Trigger
+          onMouseEnter={this.onMouseEnter}
+          onMouseLeave={this.onMouseLeave}
+          isActive={isActive}
+          isDotVisible
+        >
           <i className="material-icons">insert_emoticon</i>
         </Trigger>
 
