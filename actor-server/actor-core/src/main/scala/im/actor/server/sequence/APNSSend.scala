0dadd9380f0da38982cc130dd62a08f37fee8709
rockjam <5min4eq.unity@gmail.com>
2016-04-08 04:04:39 +0300

    fix(server:push): apple push token encoding

--- actor-server/actor-core/src/main/scala/im/actor/server/sequence/APNSSend.scala
+++ actor-server/actor-core/src/main/scala/im/actor/server/sequence/APNSSend.scala
@@ -3,6 +3,7 @@ package im.actor.server.sequence
 import akka.actor.ActorSystem
 import com.relayrides.pushy.apns.util.{ SimpleApnsPushNotification, TokenUtil }
 import im.actor.server.model.push.ApplePushCredentials
+import scodec.bits.BitVector
 
 import scala.collection.concurrent.TrieMap
 
@@ -12,8 +13,10 @@ trait APNSSend {
 
   protected def sendNotification(payload: String, creds: ApplePushCredentials, userId: Int)(implicit client: ApplePushExtension#Client, system: ActorSystem) = {
     // when topic is null, it will be taken from APNs certificate
-    // http://relayrides.github.io/pushy/apidocs/0.6/com/relayrides/pushy/apns/ApnsPushNotification.html#getToken--
-    val notification = new SimpleApnsPushNotification(TokenUtil.sanitizeTokenString(creds.token.toStringUtf8), null, payload)
+    // http://relayrides.github.io/pushy/apidocs/0.6/com/relayrides/pushy/apns/ApnsPushNotification.html#getTopic--
+    val token = BitVector(creds.token.toByteArray).toHex
+    system.log.debug("Sending APNS, token: {}", token)
+    val notification = new SimpleApnsPushNotification(TokenUtil.sanitizeTokenString(token), null, payload)
     val listener = listeners.getOrElseUpdate(creds.apnsKey, new PushFutureListener(userId, creds.token)(system))
     client.sendNotification(notification).addListener(listener)
   }
