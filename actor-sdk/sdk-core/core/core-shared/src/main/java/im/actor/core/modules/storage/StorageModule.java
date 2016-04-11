0758670c2f5a1906dcc6f4d24b9f3f74549bf61e
Steve Kite <steve@actor.im>
2016-04-11 04:33:04 +0300

    fix(core): Fixing storage upgrade

--- actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/storage/StorageModule.java
+++ actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/storage/StorageModule.java
@@ -4,52 +4,65 @@ import im.actor.core.modules.AbsModule;
 import im.actor.core.modules.ModuleContext;
 import im.actor.core.modules.auth.AuthenticationBackupData;
 import im.actor.core.network.AuthKeyStorage;
+import im.actor.runtime.Log;
 import im.actor.runtime.Storage;
 
 public class StorageModule extends AbsModule {
 
-    private static final int STORAGE_SCHEME_VERSION = 1;
+    private static String TAG = "StorageModule";
+
+    private static final int STORAGE_SCHEME_VERSION = 12;
     private static final String STORAGE_SCHEME_VERSION_KEY = "storage_sheme_version";
 
     public StorageModule(ModuleContext context) {
         super(context);
     }
 
-    public void applyStorage() {
+    public void applyStorage(boolean isFirst) {
         int version = preferences().getInt(STORAGE_SCHEME_VERSION_KEY, 0);
         if (version != STORAGE_SCHEME_VERSION) {
-            performUpgrade();
+            Log.w(TAG, "Upgrading scheme from " + version + " to " + STORAGE_SCHEME_VERSION);
+            performUpgrade(isFirst);
             preferences().putInt(STORAGE_SCHEME_VERSION_KEY, STORAGE_SCHEME_VERSION);
         }
     }
 
-    private void performUpgrade() {
+    private void performUpgrade(boolean isFirst) {
 
         //
-        // Backing Up sensitive information
+        // Backing Up sensitive data
         //
-
+        Log.w(TAG, "Backing up sensitive data");
         AuthKeyStorage storage = context().getActorApi().getKeyStorage();
-
         long authKey = storage.getAuthKey();
         byte[] masterKey = storage.getAuthMasterKey();
-
-        AuthenticationBackupData authenticationBackupData = context().getAuthModule().performBackup();
+        AuthenticationBackupData authenticationBackupData = null;
+        if (!isFirst) {
+            authenticationBackupData = context().getAuthModule().performBackup();
+        }
 
         //
         // Clear Storage
         //
-
+        Log.w(TAG, "Resetting storage");
         Storage.resetStorage();
+
+        //
+        // Notify Modules
+        //
+        Log.w(TAG, "Resetting modules");
+        context().afterStorageReset();
 
         //
         // Restore Data
         //
-
+        Log.w(TAG, "Restoring data");
         storage.saveAuthKey(authKey);
         if (masterKey != null) {
             storage.saveMasterKey(masterKey);
         }
-        context().getAuthModule().restoreBackup(authenticationBackupData);
+        if (authenticationBackupData != null) {
+            context().getAuthModule().restoreBackup(authenticationBackupData);
+        }
     }
 }
