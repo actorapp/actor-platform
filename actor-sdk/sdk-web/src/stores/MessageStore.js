a121b37ad13692073be985d02c125e2ea8ecdc26
John Doe <speed@actor.im>
2016-04-12 01:29:28 +0300

    fix(web): calculate message state using new API

--- actor-sdk/sdk-web/src/stores/MessageStore.js
+++ actor-sdk/sdk-web/src/stores/MessageStore.js
@@ -3,99 +3,89 @@
  */
 
 import Immutable from 'immutable';
-import { Store } from 'flux/utils';
+import { ReduceStore } from 'flux/utils';
 import Dispatcher from '../dispatcher/ActorAppDispatcher';
 import { ActionTypes } from '../constants/ActorAppConstants';
 
-const initialRenderMessagesCount = 20;
-const renderMessagesStep = 20;
+const INITIAL_MESSAGES_COUNT = 20;
+const MESSAGE_COUNT_STEP = 20;
 
-/**
- * Class representing a store for messages.
- */
-class MessageStore extends Store {
-  constructor(dispatcher) {
-    super(dispatcher);
-
-    this._renderMessagesCount = initialRenderMessagesCount;
-    this._messages = [];
-    this._overlay = [];
-    this._isLoaded = false;
-    this._selectedMessages = new Immutable.Set();
+class MessageStore extends ReduceStore {
+  getInitialState() {
+    return {
+      messages: [],
+      overlay: [],
+      isLoaded: false,
+      receiveDate: 0,
+      readDate: 0,
+      count: INITIAL_MESSAGES_COUNT,
+      selected: new Immutable.Set()
+    };
   }
 
-  /**
-   * @returns {Array} All messages stored for currently bound conversation
-   */
   getAll() {
-    return this._messages;
+    return this.getState().messages;
   }
 
   getRenderMessagesCount() {
-    return this._renderMessagesCount;
+    return this.getState().count;
   }
 
   getMessages() {
-    return this._messages;
+    return this.getState().messages;
   }
 
-  /**
-   * @returns {Array} Messages overlay
-   */
   getOverlay() {
-    return this._overlay;
+    return this.getState().overlay;
   }
 
-  /**
-   * @returns {Boolean} is all messages loaded for current conversation
-   */
   isLoaded() {
-    return this._isLoaded;
+    return this.getState().isLoaded;
   }
 
   isAllRendered() {
-    return this._messages.length === this._renderMessagesCount;
+    const { messages, count } = this.getState();
+    return messages.length === count;
   }
 
-  /**
-   * @returns {Array} Selected messages
-   */
   getSelected() {
-    return this._selectedMessages;
+    return this.getState().selected;
   }
 
-  __onDispatch(action) {
-    switch(action.type) {
+  reduce (state, action) {
+    switch (action.type) {
       case ActionTypes.BIND_DIALOG_PEER:
-        this._renderMessagesCount = initialRenderMessagesCount;
-        this._messages = [];
-        this._overlay = [];
-        this._selectedMessages = new Immutable.Set();
-        this.__emitChange();
-        break;
+        return {
+          ...state,
+          count: INITIAL_MESSAGES_COUNT,
+          selected: state.selected.clear()
+        };
 
       case ActionTypes.MESSAGES_CHANGED:
-        this._messages = action.messages;
-        this._overlay = action.overlay;
-        this._renderMessagesCount = Math.min(action.messages.length, this._renderMessagesCount);
-        this._isLoaded = action.isLoaded;
-        this.__emitChange();
-        break;
+        return {
+          ...state,
+          messages: action.messages,
+          overlay: action.overlay,
+          isLoaded: action.isLoaded,
+          receiveDate: action.receiveDate,
+          readDate: action.readDate,
+          count: Math.min(action.messages.length, state.count)
+        };
 
       case ActionTypes.MESSAGES_SET_SELECTED:
-        this._selectedMessages = action.selectedMesages;
-        this.__emitChange();
-        break;
+        return {
+          ...state,
+          selected: action.selectedMesages
+        };
 
       case ActionTypes.MESSAGES_LOAD_MORE:
-        this._renderMessagesCount += renderMessagesStep;
-        if (this._renderMessagesCount > this._messages.length) {
-          this._renderMessagesCount = this._messages.length;
-        }
-        this.__emitChange();
-        break;
+        return {
+          ...state,
+          count: Math.min(state.messages.length, state.count + MESSAGE_COUNT_STEP)
+        };
 
       default:
+        return state;
     }
   }
 }
