d5c199bd2c394c1bcf07727e48fd9456d13ff188
rockjam <5min4eq.unity@gmail.com>
2016-04-15 02:37:00 +0300

    fix(server:http): flatten endpoint in about route

--- actor-server/actor-http-api/src/main/scala/im/actor/server/api/http/json/JsonEncoders.scala
+++ actor-server/actor-http-api/src/main/scala/im/actor/server/api/http/json/JsonEncoders.scala
@@ -4,6 +4,5 @@ import io.circe._
 import io.circe.generic.semiauto._
 
 trait JsonEncoders {
-  implicit val endpointsFormat = deriveFor[Endpoints].encoder
   implicit val serverInfoFormat = deriveFor[ServerInfo].encoder
 }
