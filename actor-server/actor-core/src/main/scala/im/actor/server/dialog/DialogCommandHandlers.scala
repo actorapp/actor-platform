c7987d48ef0df7cf4250acd3692286ddf0e3733f
prettynatty <fear@loathing.in>
2016-04-16 04:48:08 +0300

    fix(server:messaging): fixed persistence problems

--- actor-server/actor-core/src/main/scala/im/actor/server/dialog/DialogCommandHandlers.scala
+++ actor-server/actor-core/src/main/scala/im/actor/server/dialog/DialogCommandHandlers.scala
@@ -2,15 +2,13 @@ package im.actor.server.dialog
 
 import java.time.Instant
 
-import akka.actor.{ ActorRef, PoisonPill, Status }
+import akka.actor.Status
 import akka.pattern.pipe
 import im.actor.api.rpc.PeersImplicits
 import im.actor.api.rpc.messaging._
 import im.actor.server.ApiConversions._
 import im.actor.server.dialog.HistoryUtils._
 import im.actor.server.model._
-import im.actor.server.persist.HistoryMessageRepo
-import im.actor.server.persist.dialog.DialogRepo
 import im.actor.server.persist.messaging.ReactionEventRepo
 import im.actor.server.pubsub.{ PeerMessage, PubSubExtension }
 import im.actor.server.sequence.{ SeqState, SeqStateDate }
@@ -24,7 +22,6 @@ import scala.util.Failure
 trait DialogCommandHandlers extends PeersImplicits {
   this: DialogProcessor ⇒
 
-  import DialogEvents._
   import DialogCommands._
   import DialogEvents._
 
@@ -144,13 +141,14 @@ trait DialogCommandHandlers extends PeersImplicits {
     }
   }
 
-  protected def ackMessageRead(mr: MessageRead): Unit =
+  protected def ackMessageRead(mr: MessageRead): Unit = {
     persist(MessagesRead(Instant.ofEpochMilli(mr.date))) { e ⇒
       commit(e)
       (deliveryExt.notifyRead(userId, peer, mr.date, mr.now) map { _ ⇒ MessageReadAck() }) pipeTo sender() andThen {
-        case Failure(e) ⇒ log.error(e, "Failed to ack MessageRead")
+        case Failure(err) ⇒ log.error(err, "Failed to ack MessageRead")
       }
     }
+  }
 
   protected def setReaction(state: DialogState, sr: SetReaction): Unit = {
     (for {
