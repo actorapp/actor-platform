ebfd8bf7f0318e69281e07cbf460952a5dee0c88
kor_ka <badgrib@gmmail.com>
2016-04-18 17:03:41 +0300

    chore(web): add dummy JsPromiseDispatcher implementation

---
+++ actor-sdk/sdk-core/runtime/runtime-js/src/main/java/im/actor/runtime/js/utils/JsPromiseDispatcher.java
@@ -0,0 +1,17 @@
+package im.actor.runtime.js.utils;
+
+import im.actor.runtime.promise.Promise;
+import im.actor.runtime.promise.PromiseDispatcher;
+
+public class JsPromiseDispatcher extends PromiseDispatcher {
+
+    public static final JsPromiseDispatcher INSTANCE = new JsPromiseDispatcher();
+
+    public JsPromiseDispatcher() {
+    }
+
+    @Override
+    public void dispatch(Promise promise, Runnable runnable) {
+
+    }
+}
