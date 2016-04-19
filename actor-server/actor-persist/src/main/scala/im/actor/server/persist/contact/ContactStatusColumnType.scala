c733b9d8b4a180aa204e8a79029244a1d48a5092
rockjam <5min4eq.unity@gmail.com>
2016-04-19 22:46:42 +0300

    fix(server): block user via user relations

--- actor-server/actor-persist/src/main/scala/im/actor/server/persist/contact/ContactStatusColumnType.scala
+++
@@ -1,8 +0,0 @@
-package im.actor.server.persist.contact
-
-import im.actor.server.db.ActorPostgresDriver.api._
-import im.actor.server.model.contact.ContactStatus
-
-object ContactStatusColumnType {
-  implicit val actorColumnType = MappedColumnType.base[ContactStatus, Int](_.intValue, ContactStatus.apply)
-}
