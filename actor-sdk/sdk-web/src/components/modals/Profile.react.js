9ef46c7a90efcefccb164ceab98eb379ed5d86f9
John Doe <speed@actor.im>
2016-04-21 00:11:24 +0300

    wip+refactor(web): add transfer search element styles, remove all modals from sidebar header component to modal wrapper, refactor create group modal and store, add default cursor to disabled buttons

--- actor-sdk/sdk-web/src/components/modals/Profile.react.js
+++ actor-sdk/sdk-web/src/components/modals/Profile.react.js
@@ -2,7 +2,7 @@
  * Copyright (C) 2015-2016 Actor LLC. <https://actor.im>
  */
 
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import { Container } from 'flux/utils';
 import Modal from 'react-modal';
 import { FormattedMessage } from 'react-intl';
