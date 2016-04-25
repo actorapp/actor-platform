c61ec0f965d19a1e987844fc34ed1560ce241790
Nikita Gusakov <nkt@actor.im>
2016-04-25 12:51:35 +0300

    fix(web): fix drag-n-drop files uploading
    Fixes #664

--- actor-sdk/sdk-web/src/components/common/DropZone.react.js
+++ actor-sdk/sdk-web/src/components/common/DropZone.react.js
@@ -3,6 +3,7 @@
  */
 
 import React, { Component, PropTypes } from 'react';
+import EventListener from 'fbjs/lib/EventListener';
 import classnames from 'classnames';
 
 class DropZone extends Component {
@@ -14,35 +15,58 @@ class DropZone extends Component {
   constructor(props) {
     super(props);
 
-    this.dragging = false;
+    this.windowDragging = false;
+    this.zoneDragging = false;
+
     this.state = {
       isActive: false,
       isHovered: false
     };
 
+    this.onWindowDrop = this.onWindowDrop.bind(this);
     this.onWindowDragEnter = this.onWindowDragEnter.bind(this);
     this.onWindowDragOver = this.onWindowDragOver.bind(this);
     this.onWindowDragLeave = this.onWindowDragLeave.bind(this);
 
     this.onDrop = this.onDrop.bind(this);
     this.onDragEnter = this.onDragEnter.bind(this);
+    this.onDragOver = this.onDragOver.bind(this);
     this.onDragLeave = this.onDragLeave.bind(this);
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.isActive !== this.state.isActive ||
+           nextState.isHovered !== this.state.isHovered;
+  }
+
   componentDidMount() {
-    window.addEventListener('dragenter', this.onWindowDragEnter, false);
-    window.addEventListener('dragover', this.onWindowDragOver, false);
-    window.addEventListener('dragleave', this.onWindowDragLeave, false);
+    this.listeners = [
+      EventListener.listen(window, 'drop', this.onWindowDrop),
+      EventListener.listen(window, 'dragenter', this.onWindowDragEnter),
+      EventListener.listen(window, 'dragover', this.onWindowDragOver),
+      EventListener.listen(window, 'dragleave', this.onWindowDragLeave)
+    ];
   }
 
   componentWillUnmount() {
-    window.addEventListener('dragenter', this.onWindowDragEnter, false);
-    window.addEventListener('dragover', this.onWindowDragOver, false);
-    window.addEventListener('dragleave', this.onWindowDragLeave, false);
+    this.listeners.forEach((listener) => {
+      listener.remove();
+    });
+
+    this.listeners = null;
+  }
+
+  onWindowDrop(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.setState({ isActive: false, isHovered: false });
   }
 
   onWindowDragEnter() {
-    this.dragging = true;
+    this.windowDragging = true;
+    clearTimeout(this.windowTimeout);
+
     if (this.state.isActive) {
       return;
     }
@@ -50,18 +74,23 @@ class DropZone extends Component {
     this.setState({ isActive: true });
   }
 
-  onWindowDragOver() {
-    this.dragging = true;
+  onWindowDragOver(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.windowDragging = true;
+    clearTimeout(this.windowTimeout);
   }
 
   onWindowDragLeave() {
-    this.dragging = false;
-    clearTimeout(this.timeout);
-    this.timeout = setTimeout(() => {
-      if (!this.dragging) {
+    this.windowDragging = false;
+    clearTimeout(this.windowTimeout);
+
+    this.windowTimeout = setTimeout(() => {
+      if (!this.windowDragging) {
         this.setState({ isActive: false });
       }
-    }, 300);
+    }, 60);
   }
 
   onDrop(event) {
@@ -69,10 +98,32 @@ class DropZone extends Component {
     event.stopPropagation();
 
     this.onDragLeave();
+    this.onWindowDragLeave();
     this.props.onDropComplete(event.dataTransfer.files);
   }
 
+  onDragEnter() {
+    this.zoneDragging = true;
+    this.windowDragging = true;
+    clearTimeout(this.zoneTimeout);
+    clearTimeout(this.windowTimeout);
+
+    if (this.state.isHovered) {
+      return;
+    }
+
+    this.setState({ isHovered: true });
+  }
+
   onDragOver(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.zoneDragging = true;
+    this.windowDragging = true;
+    clearTimeout(this.zoneTimeout);
+    clearTimeout(this.windowTimeout);
+
     // Makes it possible to drag files from chrome's download bar
     // http://stackoverflow.com/questions/19526430/drag-and-drop-file-uploads-from-chrome-downloads-bar
     try {
@@ -85,17 +136,17 @@ class DropZone extends Component {
     } catch (e) {
       // do nothing
     }
-
-    event.preventDefault();
-    event.stopPropagation();
-  }
-
-  onDragEnter() {
-    this.setState({ isHovered: true });
   }
 
   onDragLeave() {
-    this.setState({ isHovered: false });
+    this.zoneDragging = false;
+    clearTimeout(this.zoneTimeout);
+
+    this.zoneTimeout = setTimeout(() => {
+      if (!this.zoneDragging) {
+        this.setState({ isHovered: false });
+      }
+    }, 60);
   }
 
   render() {
