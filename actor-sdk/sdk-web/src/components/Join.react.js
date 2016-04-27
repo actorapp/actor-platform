ee5de5c075bbb780a98fefd4600b9ec757827cc8
Nikita Gusakov <nkt@actor.im>
2016-04-27 15:35:28 +0300

    fix(web): show status while user joining group
    Related to #236

--- actor-sdk/sdk-web/src/components/Join.react.js
+++ actor-sdk/sdk-web/src/components/Join.react.js
@@ -3,7 +3,10 @@
  */
 
 import React, { Component, PropTypes } from 'react';
+import { Container } from 'flux/utils';
 
+import { AsyncActionStates } from '../constants/ActorAppConstants';
+import JoinGroupStore from '../stores/JoinGroupStore';
 import JoinGroupActions from '../actions/JoinGroupActions';
 
 class Join extends Component {
@@ -13,15 +16,54 @@ class Join extends Component {
     }).isRequired
   };
 
+  static getStores() {
+    return [JoinGroupStore];
+  }
+
+  static calculateState() {
+    return JoinGroupStore.getState();
+  }
+
   constructor(props) {
     super(props);
 
     JoinGroupActions.joinGroupViaLink(props.params.token);
   }
 
+  renderStatus() {
+    const { status, token, error } = this.state;
+    switch (status) {
+      case AsyncActionStates.PROCESSING:
+      case AsyncActionStates.PENDING:
+        return (
+          <div className="join__message">
+            Joining to {token}...
+          </div>
+        );
+
+      case AsyncActionStates.SUCCESS:
+        return (
+          <div className="join__message join__message--success">
+            Successfully joined to group!
+          </div>
+        );
+
+      case AsyncActionStates.FAILURE:
+        return (
+          <div className="join__message join__message--error">
+            {error}
+          </div>
+        );
+    }
+  }
+
   render() {
-    return <div />;
+    return (
+      <div className="join__container">
+        {this.renderStatus()}
+      </div>
+    );
   }
 }
 
-export default Join;
+export default Container.create(Join);
