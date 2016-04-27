212fecf08e52e3965671f932e628199d3b3b9479
Amir <amsjavan@elenoon.ir>
2016-04-27 08:20:15 +0430

    fix(server): change mechanism of getting remoteAddr

--- actor-server/actor-session/src/main/scala/im/actor/server/session/Session.scala
+++ actor-server/actor-session/src/main/scala/im/actor/server/session/Session.scala
@@ -44,15 +44,15 @@ object SessionConfig {
 object Session {
 
   private[this] val extractEntityId: ShardRegion.ExtractEntityId = {
-    case env @ SessionEnvelope(authId, sessionId, clientAddr, payload) ⇒
+    case env @ SessionEnvelope(authId, sessionId, payload) ⇒
       Try(env.getField(SessionEnvelope.descriptor.findFieldByNumber(payload.number))) match {
-        case Success(any) ⇒ s"${authId}_${sessionId}_${clientAddr.getOrElse(" ")}" → any
+        case Success(any) ⇒ s"${authId}_${sessionId}" → any
         case _            ⇒ throw new RuntimeException(s"Empty payload $env")
       }
   }
 
   private[this] val extractShardId: ShardRegion.ExtractShardId = {
-    case SessionEnvelope(authId, sessionId, _, _) ⇒ (authId % 10).toString // TODO: configurable
+    case SessionEnvelope(authId, sessionId, _) ⇒ (authId % 10).toString // TODO: configurable
   }
 
   private val typeName = "Session"
@@ -101,8 +101,8 @@ final private class Session(implicit config: SessionConfig, materializer: Materi
   private[this] var clients = immutable.Set.empty[ActorRef]
   private[this] var updateOptimizations = immutable.Set.empty[Int]
 
-  private val (authId, sessionId, remoteAddr) = self.path.name.split("_").toList match {
-    case a :: s :: r :: Nil ⇒ (a.toLong, s.toLong, r.toString)
+  private val (authId, sessionId) = self.path.name.split("_").toList match {
+    case a :: s :: Nil ⇒ (a.toLong, s.toLong)
     case _ ⇒
       val e = new RuntimeException("Wrong actor name")
       log.error(e, e.getMessage)
@@ -161,7 +161,7 @@ final private class Session(implicit config: SessionConfig, materializer: Materi
   }
 
   def anonymous: Receive = {
-    case HandleMessageBox(messageBoxBytes) ⇒
+    case HandleMessageBox(messageBoxBytes, remoteAddr) ⇒
       idleControl.keepAlive()
 
       withValidMessageBox(messageBoxBytes.toByteArray) { mb ⇒
@@ -193,7 +193,7 @@ final private class Session(implicit config: SessionConfig, materializer: Materi
         }).run()
 
         // sessionMessagePublisher ! SessionStreamMessage.SendProtoMessage(NewSession(sessionId, mb.messageId))
-        sessionMessagePublisher ! Tuple2(mb, ClientData(authId, sessionId, authData, Option(remoteAddr)))
+        sessionMessagePublisher ! Tuple2(mb, ClientData(authId, sessionId, authData, remoteAddr.map(_.getHostAddress)))
 
         unstashAll()
         context.become(resolved(sessionMessagePublisher, reSender))
@@ -203,12 +203,12 @@ final private class Session(implicit config: SessionConfig, materializer: Materi
   }
 
   def resolved(publisher: ActorRef, reSender: ActorRef): Receive = {
-    case HandleMessageBox(messageBoxBytes) ⇒
+    case HandleMessageBox(messageBoxBytes, remoteAddr) ⇒
       idleControl.keepAlive()
       recordClient(sender(), reSender)
 
       withValidMessageBox(messageBoxBytes.toByteArray) { mb ⇒
-        publisher ! Tuple2(mb, ClientData(authId, sessionId, authData, Option(remoteAddr)))
+        publisher ! Tuple2(mb, ClientData(authId, sessionId, authData, remoteAddr.map(_.getHostAddress)))
       }
     case cmd: SubscribeCommand ⇒
       idleControl.keepAlive()
