5bb96bf1f2bd43ec4783bea752612596889ea26b
Steve Kite <steve@actor.im>
2016-04-29 03:05:32 +0300

    ref(core): Refactogin of messages router

--- actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/messaging/router/entity/RouterOutgoingMessage.java
+++ actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/messaging/router/entity/RouterOutgoingMessage.java
@@ -2,8 +2,10 @@ package im.actor.core.modules.messaging.router.entity;
 
 import im.actor.core.entity.Message;
 import im.actor.core.entity.Peer;
+import im.actor.runtime.actors.ask.AskMessage;
+import im.actor.runtime.actors.messages.Void;
 
-public class RouterOutgoingMessage implements RouterMessageOnlyActive {
+public class RouterOutgoingMessage implements AskMessage<Void>, RouterMessageOnlyActive {
 
     private Peer peer;
     private Message message;
