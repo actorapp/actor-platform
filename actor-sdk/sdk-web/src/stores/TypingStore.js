244c78960c5f7d8d420021d68483d422a4e5f6f0
John Doe <speed@actor.im>
2016-05-01 15:51:55 +0300

    fix(web): reduce typing overhead

--- actor-sdk/sdk-web/src/stores/TypingStore.js
+++ actor-sdk/sdk-web/src/stores/TypingStore.js
@@ -2,31 +2,25 @@
  * Copyright (C) 2015 Actor LLC. <https://actor.im>
  */
 
-import { Store } from 'flux/utils';
+import { ReduceStore } from 'flux/utils';
 import Dispatcher from '../dispatcher/ActorAppDispatcher';
 import { ActionTypes } from '../constants/ActorAppConstants';
 
-let _typing = null;
-
-/**
- * Class representing a store for typing info.
- */
-class TypingStore extends Store {
-  /**
-   * @returns {String}
-   */
-  getTyping() {
-    return _typing;
+class TypingStore extends ReduceStore {
+  getInitialState() {
+    return {
+      typing: null
+    };
   }
 
-  __onDispatch(action) {
-    switch(action.type) {
-      case ActionTypes.TYPING_CHANGED:
-        _typing = action.typing;
-        this.__emitChange();
-        break;
-      default:
+  reduce(state, action) {
+    if (action.type === ActionTypes.TYPING_CHANGED) {
+      return {
+        typing: action.typing
+      };
     }
+
+    return state;
   }
 }
 
