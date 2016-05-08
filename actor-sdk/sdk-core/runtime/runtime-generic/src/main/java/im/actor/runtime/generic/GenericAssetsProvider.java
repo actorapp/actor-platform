eca4a38ba560805ceaad1ac6383f74ce4cc84ca9
Steve Kite <steve@actor.im>
2016-05-08 17:26:09 +0300

    feat(android): Move i18n files to assets

--- actor-sdk/sdk-core/runtime/runtime-generic/src/main/java/im/actor/runtime/generic/GenericAssetsProvider.java
+++ actor-sdk/sdk-core/runtime/runtime-generic/src/main/java/im/actor/runtime/generic/GenericAssetsProvider.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import im.actor.runtime.AssetsRuntime;
+import im.actor.runtime.Log;
+import im.actor.runtime.Runtime;
 
 public class GenericAssetsProvider implements AssetsRuntime {
 
