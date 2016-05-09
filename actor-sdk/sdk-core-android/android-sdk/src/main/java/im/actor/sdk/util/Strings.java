528e77bd77b32a7ea1521660c409fd894b9305f1
Steve Kite <steve@actor.im>
2016-05-09 05:16:14 +0300

    perf(core+android): Bunch of micro optimizations

--- actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/util/Strings.java
+++ actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/util/Strings.java
@@ -91,70 +91,73 @@ public class Strings {
 
     private static final Map<Character, String> charMap = new HashMap<Character, String>();
 
-    static {
-        charMap.put('а', "a");
-        charMap.put('б', "b");
-        charMap.put('в', "v");
-        charMap.put('г', "g");
-        charMap.put('д', "d");
-        charMap.put('е', "e");
-        charMap.put('ё', "e");
-        charMap.put('ж', "zh");
-        charMap.put('з', "z");
-        charMap.put('и', "i");
-        charMap.put('й', "i");
-        charMap.put('к', "k");
-        charMap.put('л', "l");
-        charMap.put('м', "m");
-        charMap.put('н', "n");
-        charMap.put('о', "o");
-        charMap.put('п', "p");
-        charMap.put('р', "r");
-        charMap.put('с', "s");
-        charMap.put('т', "t");
-        charMap.put('у', "u");
-        charMap.put('ф', "f");
-        charMap.put('х', "h");
-        charMap.put('ц', "c");
-        charMap.put('ч', "ch");
-        charMap.put('ш', "sh");
-        charMap.put('щ', "sh");
-        charMap.put('ъ', "'");
-        charMap.put('ы', "y");
-        charMap.put('ь', "'");
-        charMap.put('э', "e");
-        charMap.put('ю', "u");
-        charMap.put('я', "ya");
-
-        charMap.put('a', "а");
-        charMap.put('b', "б");
-        charMap.put('c', "ц");
-        charMap.put('d', "д");
-        charMap.put('e', "е");
-        charMap.put('f', "ф");
-        charMap.put('g', "г");
-        charMap.put('h', "х");
-        charMap.put('i', "и");
-        charMap.put('j', "дж");
-        charMap.put('k', "к");
-        charMap.put('l', "л");
-        charMap.put('m', "м");
-        charMap.put('n', "н");
-        charMap.put('o', "о");
-        charMap.put('p', "п");
-        charMap.put('q', "к");
-        charMap.put('r', "р");
-        charMap.put('s', "с");
-        charMap.put('t', "т");
-        charMap.put('u', "ю");
-        charMap.put('v', "в");
-        charMap.put('w', "в");
-        charMap.put('x', "кс");
-        charMap.put('y', "й");
-        charMap.put('z', "з");
-    }
-
     public static String transliterate(String string) {
+        if (charMap.size() == 0) {
+            synchronized (charMap) {
+                if (charMap.size() == 0) {
+                    charMap.put('а', "a");
+                    charMap.put('б', "b");
+                    charMap.put('в', "v");
+                    charMap.put('г', "g");
+                    charMap.put('д', "d");
+                    charMap.put('е', "e");
+                    charMap.put('ё', "e");
+                    charMap.put('ж', "zh");
+                    charMap.put('з', "z");
+                    charMap.put('и', "i");
+                    charMap.put('й', "i");
+                    charMap.put('к', "k");
+                    charMap.put('л', "l");
+                    charMap.put('м', "m");
+                    charMap.put('н', "n");
+                    charMap.put('о', "o");
+                    charMap.put('п', "p");
+                    charMap.put('р', "r");
+                    charMap.put('с', "s");
+                    charMap.put('т', "t");
+                    charMap.put('у', "u");
+                    charMap.put('ф', "f");
+                    charMap.put('х', "h");
+                    charMap.put('ц', "c");
+                    charMap.put('ч', "ch");
+                    charMap.put('ш', "sh");
+                    charMap.put('щ', "sh");
+                    charMap.put('ъ', "'");
+                    charMap.put('ы', "y");
+                    charMap.put('ь', "'");
+                    charMap.put('э', "e");
+                    charMap.put('ю', "u");
+                    charMap.put('я', "ya");
+
+                    charMap.put('a', "а");
+                    charMap.put('b', "б");
+                    charMap.put('c', "ц");
+                    charMap.put('d', "д");
+                    charMap.put('e', "е");
+                    charMap.put('f', "ф");
+                    charMap.put('g', "г");
+                    charMap.put('h', "х");
+                    charMap.put('i', "и");
+                    charMap.put('j', "дж");
+                    charMap.put('k', "к");
+                    charMap.put('l', "л");
+                    charMap.put('m', "м");
+                    charMap.put('n', "н");
+                    charMap.put('o', "о");
+                    charMap.put('p', "п");
+                    charMap.put('q', "к");
+                    charMap.put('r', "р");
+                    charMap.put('s', "с");
+                    charMap.put('t', "т");
+                    charMap.put('u', "ю");
+                    charMap.put('v', "в");
+                    charMap.put('w', "в");
+                    charMap.put('x', "кс");
+                    charMap.put('y', "й");
+                    charMap.put('z', "з");
+                }
+            }
+        }
         StringBuilder transliteratedString = new StringBuilder();
         for (int i = 0; i < string.length(); i++) {
             Character ch = string.charAt(i);
--- actor-sdk/sdk-core/runtime/runtime-shared/src/main/java/im/actor/runtime/util/Hex.java
+++ actor-sdk/sdk-core/runtime/runtime-shared/src/main/java/im/actor/runtime/util/Hex.java
@@ -2,23 +2,7 @@ package im.actor.runtime.util;
 
 public class Hex {
 
-    public static byte[] fromHex(String hex) {
-        byte[] res = new byte[hex.length() / 2];
-        for (int j = 0; j < hex.length() / 2; j++) {
-            String dg = hex.charAt(j * 2) + "" + hex.charAt(j * 2 + 1);
-            res[j] = (byte) Integer.parseInt(dg, 16);
-        }
-        return res;
-    }
-
-    public static byte[] fromHexReverse(String hex) {
-        byte[] res = new byte[hex.length() / 2];
-        for (int j = 0; j < hex.length() / 2; j++) {
-            String dg = hex.charAt(j * 2) + "" + hex.charAt(j * 2 + 1);
-            res[res.length - j - 1] = (byte) Integer.parseInt(dg, 16);
-        }
-        return res;
-    }
+    final protected static char[] HEXES_SMALL = "0123456789abcdef".toCharArray();
 
     private static final String HEXES = "0123456789ABCDEF";
 
@@ -28,5 +12,41 @@ public class Hex {
             hex.append(HEXES.charAt((b & 0xF0) >> 4)).append(HEXES.charAt((b & 0x0F)));
         }
         return hex.toString();
+    }
+
+
+    /**
+     * Calculating lowcase hex string
+     *
+     * @param bytes data for hex
+     * @return hex string
+     */
+    public static String hex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEXES_SMALL[v >>> 4];
+            hexChars[j * 2 + 1] = HEXES_SMALL[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    private static int fromHexShort(char a) {
+        if (a >= '0' && a <= '9') {
+            return a - '0';
+        }
+        if (a >= 'a' && a <= 'f') {
+            return 10 + (a - 'a');
+        }
+
+        throw new RuntimeException();
+    }
+
+    public static byte[] fromHex(String hex) {
+        byte[] res = new byte[hex.length() / 2];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = (byte) ((fromHexShort(hex.charAt(i * 2)) << 4) + fromHexShort(hex.charAt(i * 2 + 1)));
+        }
+        return res;
     }
 }
