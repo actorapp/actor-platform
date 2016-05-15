a7348624f7a0e56d736ba6f8d04acc3854ffecb8
Steve Kite <steve@actor.im>
2016-05-15 21:58:06 +0300

    feat(android): Removed PageView from MainActivity

--- actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/controllers/profile/ProfileActivity.java
+++ actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/controllers/profile/ProfileActivity.java
@@ -3,8 +3,6 @@ package im.actor.sdk.controllers.profile;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
