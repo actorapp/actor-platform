8c446404aa6605070ac9a4170a7d62c4dc57ad95
Steve Kite <steve@actor.im>
2016-05-15 21:58:06 +0300

    feat(android): Removed PageView from MainActivity

--- actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/controllers/root/MainBaseController.java
+++ actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/controllers/root/MainBaseController.java
@@ -5,10 +5,8 @@ import im.actor.sdk.controllers.activity.ActorMainActivity;
 import im.actor.sdk.controllers.activity.base.Controller;
 import im.actor.core.entity.Dialog;
 
-/**
- * Created by ex3ndr on 25.10.14.
- */
 public abstract class MainBaseController extends Controller<ActorMainActivity> {
+
     protected MainBaseController(ActorMainActivity activity) {
         super(activity);
     }
