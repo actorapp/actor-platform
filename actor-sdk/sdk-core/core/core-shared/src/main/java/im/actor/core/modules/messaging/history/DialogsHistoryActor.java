6a351154e2deec0306d947d4b258f11fb15b307d
kor_ka <badgrib@gmmail.com>
2016-05-16 20:08:36 +0300

    fix(android): notify app state dialogs loaded if dialogs  are empty

--- actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/messaging/history/DialogsHistoryActor.java
+++ actor-sdk/sdk-core/core/core-shared/src/main/java/im/actor/core/modules/messaging/history/DialogsHistoryActor.java
@@ -88,6 +88,7 @@ public class DialogsHistoryActor extends ModuleActor {
                 }
             });
         } else {
+            context().getAppStateModule().onDialogsLoaded();
             markAsLoaded();
         }
     }
