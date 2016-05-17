3e15736c63e95be65b97ab22b289a31840d75697
John Doe <speed@actor.im>
2016-05-17 03:31:29 +0300

    fix(web): add isExperimental keys for write and call buttons, update toolbar styles, disable message background change on hover

--- actor-sdk/sdk-web/src/components/Toolbar.react.js
+++ actor-sdk/sdk-web/src/components/Toolbar.react.js
@@ -3,26 +3,49 @@
  */
 
 import React, { Component, PropTypes } from 'react';
+import alert from '../utils/alert';
+
 import UserMenu from './common/UserMenu.react';
 
 class AppHeader extends Component {
+  static contextTypes = {
+    isExperimental: PropTypes.bool
+  }
+
   constructor(props) {
     super(props);
+  }
+
+  handleWriteButtonClick() {
+    alert('writeButtonClick')
+      .then(() => console.debug('Alert closed'));
+  }
+
+  renderWriteButton() {
+    const { isExperimental } = this.context;
+
+    if (!isExperimental) {
+      return null;
+    }
+
+    return (
+      <button className="toolbar__button" onClick={this.handleWriteButtonClick}>
+        <i className="material-icons">edit</i>
+      </button>
+    );
   }
 
   render() {
     return (
       <header className={`toolbar row`}>
-        <div className="toolbar__menu">
+        <div className="toolbar__aside">
           <span>Actor</span>
         </div>
         <div className="toolbar__controls col-xs">
-          <button className="button">
-            <i className="material-icons">edit</i>
-          </button>
+          {this.renderWriteButton()}
         </div>
         <div className="toolbar__profile">
-          <UserMenu/>
+          <UserMenu className="toolbar__button"/>
         </div>
       </header>
     );
