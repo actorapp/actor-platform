7c6924f122b7cb10696bdcb6c58f37f37a3c22dc
Nikolay Tatarinov <rockjam@actor.im>
2016-06-03 01:38:50 +0300

    feat(server:push): enable custom ringtones for APNS

--- actor-server/actor-core/src/main/scala/im/actor/server/userconfig/SettingsKeys.scala
+++ actor-server/actor-core/src/main/scala/im/actor/server/userconfig/SettingsKeys.scala
@@ -9,12 +9,6 @@ object SettingsKeys {
 
   private def wrapEnabled(deviceType: String, postfix: String): String = s"category.$deviceType.notification.$postfix.enabled"
 
-  private def peerStr(peer: Peer) = peer match {
-    case Peer(PeerType.Private, id) ⇒ s"PRIVATE_$id"
-    case Peer(PeerType.Group, id)   ⇒ s"GROUP_$id"
-    case _                          ⇒ throw new RuntimeException(s"Unknown peer $peer")
-  }
-
   def enabled(deviceType: String) = wrapEnabled(deviceType)
 
   def soundEnabled(deviceType: String) = wrapEnabled(deviceType, "sound")
@@ -22,6 +16,4 @@ object SettingsKeys {
   def vibrationEnabled(deviceType: String) = wrapEnabled(deviceType, "vibration")
 
   def textEnabled(deviceType: String) = wrap(deviceType, "show_text")
-
-  def peerEnabled(deviceType: String, peer: Peer) = wrapEnabled(deviceType, s"chat.${peerStr(peer)}")
 }
