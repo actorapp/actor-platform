d41340abb3d71f0582ee9fe364de9ff956b1b3f7
Steve Kite <steve@actor.im>
2016-06-30 17:50:11 +0300

    feat(android): Coloring and emoji handling in dialog view

--- actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/controllers/dialogs/view/DialogView.java
+++ actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/controllers/dialogs/view/DialogView.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.support.v7.widget.RecyclerView;
 import android.text.Layout;
-import android.text.StaticLayout;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
 import android.text.TextPaint;
-import android.text.TextUtils;
+import android.text.style.ForegroundColorSpan;
 import android.util.AttributeSet;
 import android.view.ViewGroup;
 
+import im.actor.core.entity.ContentType;
 import im.actor.core.entity.Dialog;
 import im.actor.core.entity.PeerType;
 import im.actor.sdk.ActorSDK;
@@ -29,6 +32,8 @@ public class DialogView extends ListItemBackgroundView<Dialog, DialogView.Dialog
     private static TextPaint titlePaint;
     private static TextPaint datePaint;
     private static TextPaint textPaint;
+    private static TextPaint textActivePaint;
+    private static int senderTextColor;
 
     public DialogView(Context context) {
         super(context);
@@ -89,15 +94,18 @@ public class DialogView extends ListItemBackgroundView<Dialog, DialogView.Dialog
     @Override
     public DialogLayout buildLayout(Dialog arg, int width, int height) {
         if (!isStylesLoaded) {
+            isStylesLoaded = true;
             ActorStyle style = ActorSDK.sharedActor().style;
             titlePaint = createTextPaint(Fonts.medium(), 16, style.getDialogsTitleColor());
             datePaint = createTextPaint(Fonts.regular(), 14, style.getDialogsTimeColor());
             textPaint = createTextPaint(Fonts.regular(), 16, style.getDialogsTimeColor());
+            textActivePaint = createTextPaint(Fonts.regular(), 16, style.getDialogsActiveTextColor());
+            senderTextColor = style.getDialogsActiveTextColor();
         }
 
         DialogLayout res = new DialogLayout();
 
-        // Date
+        // Top Row
         int maxTitleWidth = (width - Screen.dp(72)) - Screen.dp(8);
         if (arg.getDate() > 0) {
             String dateText = messenger().getFormatter().formatShortDate(arg.getDate());
@@ -106,39 +114,61 @@ public class DialogView extends ListItemBackgroundView<Dialog, DialogView.Dialog
             maxTitleWidth -= dateWidth + Screen.dp(16);
         }
 
-        // Title
         res.setTitleLayout(singleLineText(arg.getDialogTitle(), titlePaint, maxTitleWidth));
 
-        // Text
+        // Second Row
+        int maxWidth = width - Screen.dp(72) - Screen.dp(8);
         if (arg.getSenderId() > 0) {
-
             String contentText = messenger().getFormatter().formatContentText(arg.getSenderId(),
                     arg.getMessageType(), arg.getText().replace("\n", " "), arg.getRelatedUid());
-            CharSequence contentResult;
+
             if (arg.getPeer().getPeerType() == PeerType.GROUP) {
+                // Is Large Service Message
                 if (messenger().getFormatter().isLargeDialogMessage(arg.getMessageType())) {
-                    // Is Large Service Message
-                    contentResult = contentText;
+                    res.setTextLayout(singleLineText(handleEmoji(contentText), textActivePaint, maxWidth));
                 } else {
-                    contentResult = users().get(arg.getSenderId()).getName().get() + ": " + contentText;
+                    String senderName = users().get(arg.getSenderId()).getName().get() + ": ";
+                    if (arg.getMessageType() == ContentType.TEXT) {
+                        SpannableStringBuilder builder = new SpannableStringBuilder();
+                        builder.append(senderName);
+                        builder.setSpan(new ForegroundColorSpan(senderTextColor), 0, senderName.length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+                        builder.append(contentText);
+                        //builder.append(senderName, new ForegroundColorSpan(senderTextColor), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+                        // SpannableString sender = new SpannableString(senderName);
+                        // sender.setSpan(new ForegroundColorSpan(senderTextColor), 0, sender.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+                        // CharSequence contentResult = handleEmoji(sender, contentText);
+                        res.setTextLayout(singleLineText(builder, textPaint, maxWidth));
+                    } else {
+                        CharSequence contentResult = handleEmoji(senderName, contentText);
+                        res.setTextLayout(singleLineText(contentResult, textActivePaint, maxWidth));
+                    }
                 }
             } else {
-                contentResult = contentText;
+                if (arg.getMessageType() == ContentType.TEXT) {
+                    res.setTextLayout(singleLineText(handleEmoji(contentText), textPaint, maxWidth));
+                } else {
+                    res.setTextLayout(singleLineText(handleEmoji(contentText), textActivePaint, maxWidth));
+                }
             }
-            
-            res.setTextLayout(singleLineText(contentResult, textPaint, width - Screen.dp(72) - Screen.dp(8)));
-
-//            CharSequence text = messenger().getFormatter().formatDialogText(arg).replace("\n", " ");
-//            if (SmileProcessor.containsEmoji(text)) {
-//                if (emoji().isLoaded()) {
-//                    text = emoji().processEmojiCompatMutable(text, SmileProcessor.CONFIGURATION_BUBBLES);
-//                }
-//            }
-
-//            res.setTextLayout(singleLineText(text, textPaint, width - Screen.dp(72) - Screen.dp(8)));
         }
 
         return res;
+    }
+
+    private CharSequence handleEmoji(CharSequence... args) {
+        StringBuilder builder = new StringBuilder();
+        for (CharSequence seq : args) {
+            if (SmileProcessor.containsEmoji(seq)) {
+                if (emoji().isLoaded()) {
+                    builder.append(emoji().processEmojiCompatMutable(seq, SmileProcessor.CONFIGURATION_BUBBLES));
+                } else {
+                    builder.append(seq);
+                }
+            } else {
+                builder.append(seq);
+            }
+        }
+        return builder;
     }
 
     private void unbind() {
