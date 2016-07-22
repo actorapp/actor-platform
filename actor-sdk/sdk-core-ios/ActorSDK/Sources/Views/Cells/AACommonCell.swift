f8e1eda590c7f6c1fef9566311e2516469134cc1
Steve Kite <steve@actor.im>
2016-07-22 16:57:54 +0300

    fix(iOS): Fixing not updating common cell on hide/show label

--- actor-sdk/sdk-core-ios/ActorSDK/Sources/Views/Cells/AACommonCell.swift
+++ actor-sdk/sdk-core-ios/ActorSDK/Sources/Views/Cells/AACommonCell.swift
@@ -54,6 +54,7 @@ public class AACommonCell: AATableViewCell {
             hintLabel.text = hint
             hintLabel.hidden = false
         }
+        setNeedsLayout()
     }
     
     // Setting switcher content
