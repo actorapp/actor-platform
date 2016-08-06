c69de4b1f7d1a5c082b7ea9a1935e11f95351fab
Alashov Berkeli <yunus.alashow@gmail.com>
2016-08-06 13:30:19 +0500

    refactor(android): lambdas in SecuritySettingsFragment
    DividerView instead of View

--- actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/controllers/settings/SecuritySettingsFragment.java
+++ actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/controllers/settings/SecuritySettingsFragment.java
@@ -1,7 +1,6 @@
 package im.actor.sdk.controllers.settings;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +11,6 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import im.actor.core.api.ApiAuthHolder;
@@ -39,45 +37,29 @@ public class SecuritySettingsFragment extends BaseFragment {
         loading = (TextView) res.findViewById(R.id.loading);
         loading.setTextColor(ActorSDK.sharedActor().style.getTextPrimaryColor());
         loading.setVisibility(View.GONE);
-        loading.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                performLoad();
-            }
-        });
+        loading.setOnClickListener(v -> performLoad());
         authItems = (LinearLayout) res.findViewById(R.id.authItems);
-        res.findViewById(R.id.divider).setBackgroundColor(ActorSDK.sharedActor().style.getDividerColor());
 
-        res.findViewById(R.id.terminateSessions).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new AlertDialog.Builder(getActivity())
-                        .setMessage(R.string.security_terminate_message)
-                        .setPositiveButton(R.string.dialog_yes, new DialogInterface.OnClickListener() {
+        res.findViewById(R.id.terminateSessions).setOnClickListener(v -> new AlertDialog.Builder(getActivity())
+                .setMessage(R.string.security_terminate_message)
+                .setPositiveButton(R.string.dialog_yes, (dialog, which) -> execute(messenger().terminateAllSessions(), R.string.progress_common,
+                        new CommandCallback<Void>() {
                             @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                execute(messenger().terminateAllSessions(), R.string.progress_common,
-                                        new CommandCallback<Void>() {
-                                            @Override
-                                            public void onResult(Void res) {
-                                                performLoad();
-                                            }
-
-                                            @Override
-                                            public void onError(Exception e) {
-                                                performLoad();
-                                                Toast.makeText(getActivity(),
-                                                        R.string.security_toast_unable_remove_auth, Toast.LENGTH_SHORT)
-                                                        .show();
-                                            }
-                                        });
+                            public void onResult(Void res1) {
+                                performLoad();
                             }
-                        })
-                        .setNegativeButton(R.string.dialog_no, null)
-                        .show()
-                        .setCanceledOnTouchOutside(true);
-            }
-        });
+
+                            @Override
+                            public void onError(Exception e) {
+                                performLoad();
+                                Toast.makeText(getActivity(),
+                                        R.string.security_toast_unable_remove_auth, Toast.LENGTH_SHORT)
+                                        .show();
+                            }
+                        }))
+                .setNegativeButton(R.string.dialog_no, null)
+                .show()
+                .setCanceledOnTouchOutside(true));
         ((TextView) res.findViewById(R.id.settings_terminate_sessions_title)).setTextColor(ActorSDK.sharedActor().style.getTextPrimaryColor());
         ((TextView) res.findViewById(R.id.settings_terminate_sessions_hint)).setTextColor(ActorSDK.sharedActor().style.getTextSecondaryColor());
         performLoad();
@@ -96,12 +78,7 @@ public class SecuritySettingsFragment extends BaseFragment {
                 goneView(loading, false);
                 authItems.removeAllViews();
                 ArrayList<ApiAuthSession> items = new ArrayList<ApiAuthSession>(res);
-                Collections.sort(items, new Comparator<ApiAuthSession>() {
-                    @Override
-                    public int compare(ApiAuthSession lhs, ApiAuthSession rhs) {
-                        return rhs.getAuthTime() - lhs.getAuthTime();
-                    }
-                });
+                Collections.sort(items, (lhs, rhs) -> rhs.getAuthTime() - lhs.getAuthTime());
                 for (final ApiAuthSession item : items) {
                     if (getActivity() == null) return;
                     View view = getActivity().getLayoutInflater().inflate(R.layout.adapter_auth, authItems, false);
@@ -115,34 +92,24 @@ public class SecuritySettingsFragment extends BaseFragment {
                     ((TextView) view.findViewById(R.id.deviceTitle)).setText(deviceTitle);
                     ((TextView) view.findViewById(R.id.deviceTitle)).setTextColor(ActorSDK.sharedActor().style.getTextPrimaryColor());
                     if (!isThisDevice) {
-                        view.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                new AlertDialog.Builder(getActivity())
-                                        .setMessage(getString(R.string.security_terminate_this_message).replace("{device}", item.getDeviceTitle() ))
-                                        .setPositiveButton(R.string.dialog_yes, new DialogInterface.OnClickListener() {
+                        view.setOnClickListener(v -> new AlertDialog.Builder(getActivity())
+                                .setMessage(getString(R.string.security_terminate_this_message).replace("{device}", item.getDeviceTitle() ))
+                                .setPositiveButton(R.string.dialog_yes, (dialog, which) -> execute(messenger().terminateSession(item.getId()), R.string.progress_common,
+                                        new CommandCallback<Void>() {
                                             @Override
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                execute(messenger().terminateSession(item.getId()), R.string.progress_common,
-                                                        new CommandCallback<Void>() {
-                                                            @Override
-                                                            public void onResult(Void res) {
-                                                                performLoad();
-                                                            }
-
-                                                            @Override
-                                                            public void onError(Exception e) {
-                                                                Toast.makeText(getActivity(), R.string.security_toast_unable_remove_auth , Toast.LENGTH_SHORT).show();
-                                                                performLoad();
-                                                            }
-                                                        });
+                                            public void onResult(Void res1) {
+                                                performLoad();
                                             }
-                                        })
-                                        .setNegativeButton(R.string.dialog_no, null)
-                                        .show()
-                                        .setCanceledOnTouchOutside(true);
-                            }
-                        });
+
+                                            @Override
+                                            public void onError(Exception e) {
+                                                Toast.makeText(getActivity(), R.string.security_toast_unable_remove_auth , Toast.LENGTH_SHORT).show();
+                                                performLoad();
+                                            }
+                                        }))
+                                .setNegativeButton(R.string.dialog_no, null)
+                                .show()
+                                .setCanceledOnTouchOutside(true));
                     }
                     authItems.addView(view);
                 }
