de8e568b3437483909e415927010702eff69306e
Diego Silva <diegosiuniube@gmail.com>
2017-02-01 08:11:08 -0200

    servicos de grupos pre

---
+++ actor-server/actor-core/src/main/scala/im/actor/server/grouppre/GroupPreProcessor.scala
@@ -0,0 +1,39 @@
+package im.actor.server.grouppre
+
+import java.time.Instant
+
+import akka.actor.Props
+import im.actor.serialization.ActorSerializer
+import im.actor.server.cqrs.TaggedEvent
+
+
+trait GroupEvent extends TaggedEvent {
+  val ts: Instant
+
+  override def tags: Set[String] = Set("grouppre")
+}
+
+case object StopProcessor
+
+object GroupPreProcessor {
+  def register(): Unit =
+    ActorSerializer.register(
+//      30001 → classOf[GroupCommands.Create],
+//
+//      31001 → classOf[GroupQueries.GetIntegrationToken],
+//
+//      32005 → classOf[GroupEvents.Created]
+
+    )
+
+  def persistenceIdFor(groupId: Int): String = s"Grouppre-${groupId}"
+
+  private[grouppre] def props: Props = Props(classOf[GroupPreProcessor])
+}
+
+/**
+  * Created by 98379720172 on 31/01/17.
+  */
+private[grouppre] final class GroupPreProcessor {
+
+}
--- actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/survey/SurveyServiceImpl.scala
+++ actor-server/actor-rpc-api/src/main/scala/im/actor/server/api/rpc/service/survey/SurveyServiceImpl.scala
@@ -10,28 +10,28 @@ import im.actor.api.rpc.PeerHelpers._
 import im.actor.api.rpc._
 import im.actor.api.rpc.files.ApiFileLocation
 import im.actor.api.rpc.groups._
-import im.actor.api.rpc.misc.{ResponseSeq, ResponseSeqDate, ResponseVoid}
-import im.actor.api.rpc.peers.{ApiGroupOutPeer, ApiUserOutPeer}
+import im.actor.api.rpc.misc.{ ResponseSeq, ResponseSeqDate, ResponseVoid }
+import im.actor.api.rpc.peers.{ ApiGroupOutPeer, ApiUserOutPeer }
 import im.actor.api.rpc.sequence.ApiUpdateOptimization
-import im.actor.api.rpc.surveys.{ApiSurvey, ApiSurveyAnswer, SurveysService}
+import im.actor.api.rpc.surveys.{ ApiSurvey, ApiSurveyAnswer, SurveysService }
 import im.actor.api.rpc.users.ApiUser
 import im.actor.concurrent.FutureExt
 import im.actor.server.acl.ACLUtils
 import im.actor.server.db.DbExtension
 import im.actor.server.dialog.DialogExtension
-import im.actor.server.file.{FileErrors, ImageUtils}
+import im.actor.server.file.{ FileErrors, ImageUtils }
 import im.actor.server.group._
 import im.actor.server.model.GroupInviteToken
 import im.actor.server.names.GlobalNamesStorageKeyValueStorage
-import im.actor.server.persist.{GroupInviteTokenRepo, GroupUserRepo}
+import im.actor.server.persist.{ GroupInviteTokenRepo, GroupUserRepo }
 import im.actor.server.presences.GroupPresenceExtension
 import im.actor.server.user.UserExtension
 import im.actor.util.ThreadLocalSecureRandom
-import im.actor.util.misc.{IdUtils, StringUtils}
+import im.actor.util.misc.{ IdUtils, StringUtils }
 import slick.dbio.DBIO
 import slick.driver.PostgresDriver.api._
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 /**
  * Created by 98379720172 on 16/11/16.
  */
